11e80109e96d4fb97f9a095da283aece790ce867
Darrick J. Wong <darrick.wong@oracle.com>
2018-02-28 13:57:15 -0800

    xfs_scrub: scrub the realtime rmap btree
    Teach scrub to ask the kernel to scrub the rtrmapbt.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- scrub/scrub.c
+++ scrub/scrub.c
@@ -87,6 +87,8 @@ static const struct scrub_descr scrubbers[XFS_SCRUB_TYPE_NR] = {
 		{"project quotas",			ST_FS},
 	[XFS_SCRUB_TYPE_FSCOUNTERS] =
 		{"filesystem summary counters",		ST_SUMMARY},
+	[XFS_SCRUB_TYPE_RTRMAPBT] =
+		{"realtime reverse mapping btree",	ST_FS},
 };
 
 /* Format a scrub description. */
