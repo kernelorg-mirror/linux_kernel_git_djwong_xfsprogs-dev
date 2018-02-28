31407a77ceb6fdecb29844c83ee99ab1914b2854
Darrick J. Wong <darrick.wong@oracle.com>
2018-02-28 13:57:13 -0800

    xfs: widen xfs_refcount_irec fields to handle realtime rmapbt
    Change the startblock and blockcount fields of xfs_refcount_irec to
    be 64 bits wide.  This enables us to use the same high level rmap
    code for either tree.  We'll also collect all the resulting breakage
    fixes here.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_refcount.c
+++ libxfs/xfs_refcount.c
@@ -1559,8 +1559,7 @@ xfs_refcount_alloc_cow_extent(
 		return error;
 
 	/* Add rmap entry */
-	return xfs_rmap_alloc_extent(tp, XFS_FSB_TO_AGNO(mp, fsb),
-			XFS_FSB_TO_AGBNO(mp, fsb), len, XFS_RMAP_OWN_COW);
+	return xfs_rmap_alloc_extent(tp, fsb, len, XFS_RMAP_OWN_COW);
 }
 
 /* Forget a CoW staging event in the refcount btree. */
@@ -1577,8 +1576,7 @@ xfs_refcount_free_cow_extent(
 		return 0;
 
 	/* Remove rmap entry */
-	error = xfs_rmap_free_extent(tp, XFS_FSB_TO_AGNO(mp, fsb),
-			XFS_FSB_TO_AGBNO(mp, fsb), len, XFS_RMAP_OWN_COW);
+	error = xfs_rmap_free_extent(tp, fsb, len, XFS_RMAP_OWN_COW);
 	if (error)
 		return error;
 
