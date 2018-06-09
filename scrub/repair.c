d7f0aacf1cb5b5d672556a28b862a25c64770528
Darrick J. Wong <darrick.wong@oracle.com>
2018-06-08 18:56:15 -0700

    xfs_scrub: add -q to disable operations requiring fs freeze
    Add a new option to disable any operation that requires a filesystem
    freeze.  This option is for people who can afford quick fixes but not
    things that will keep the filesystem offline for a long time.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- scrub/repair.c
+++ scrub/repair.c
@@ -236,6 +236,7 @@ xfs_action_list_process(
 	list_for_each_entry_safe(aitem, n, &alist->list, list) {
 		fix = xfs_repair_metadata(ctx, fd, aitem, repair_flags);
 		switch (fix) {
+		case CHECK_TOOSLOW:
 		case CHECK_DONE:
 			if (!(repair_flags & ALP_NOPROGRESS))
 				progress_add(1);
