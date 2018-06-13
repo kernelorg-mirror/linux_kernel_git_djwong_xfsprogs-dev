13013895198bef10e838e338d3dd7a5b78fa7654
Darrick J. Wong <darrick.wong@oracle.com>
2018-06-12 17:06:02 -0700

    xfs_scrub: check summary counters
    Teach scrub to ask the kernel to check and repair summary counters
    during phase 7.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- scrub/scrub.h
+++ scrub/scrub.h
@@ -28,6 +28,8 @@ bool xfs_scrub_fs_metadata(struct scrub_ctx *ctx,
 		struct xfs_action_list *alist);
 bool xfs_scrub_clean_health(struct scrub_ctx *ctx,
 		struct xfs_action_list *alist);
+bool xfs_scrub_fs_summary(struct scrub_ctx *ctx,
+		struct xfs_action_list *alist);
 
 bool xfs_can_scrub_fs_metadata(struct scrub_ctx *ctx);
 bool xfs_can_scrub_inode(struct scrub_ctx *ctx);
