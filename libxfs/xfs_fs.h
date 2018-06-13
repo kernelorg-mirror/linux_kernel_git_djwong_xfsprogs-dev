8dcf5e21648b49135df2af05b9c6b165c6bc3071
Darrick J. Wong <darrick.wong@oracle.com>
2018-06-13 09:54:13 -0700

    xfs: add online scrub/repair for superblock counters
    Teach online scrub and repair how to check and reset the superblock
    inode and block counters.  The AG rebuilding functions will need these
    to adjust the counts if they need to change as a part of recovering from
    corruption.  We must use the repair freeze mechanism to prevent any
    other changes while we do this.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_fs.h
+++ libxfs/xfs_fs.h
@@ -596,9 +596,10 @@ struct xfs_scrub_metadata {
 #define XFS_SCRUB_TYPE_GQUOTA	22	/* group quotas */
 #define XFS_SCRUB_TYPE_PQUOTA	23	/* project quotas */
 #define XFS_SCRUB_TYPE_HEALTHY	24	/* everything checked out ok */
+#define XFS_SCRUB_TYPE_FSCOUNTERS 25	/* fs summary counters */
 
 /* Number of scrub subcommands. */
-#define XFS_SCRUB_TYPE_NR	25
+#define XFS_SCRUB_TYPE_NR	26
 
 /* i: Repair this metadata. */
 #define XFS_SCRUB_IFLAG_REPAIR		(1 << 0)
