84e212ae026ff2f1b6530345a07853d1b350a16a
Darrick J. Wong <darrick.wong@oracle.com>
2018-11-10 12:00:48 -0800

    xfs: separate inode geometry
    Separate the inode geometry information into a distinct structure.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- repair/phase2.c
+++ repair/phase2.c
@@ -195,7 +195,7 @@ phase2(
 		 * also mark blocks
 		 */
 		set_bmap_ext(0, XFS_INO_TO_AGBNO(mp, mp->m_sb.sb_rootino),
-			     mp->m_ialloc_blks, XR_E_INO);
+			     mp->m_ino_geo.ig_ialloc_blks, XR_E_INO);
 	} else  {
 		do_log(_("        - found root inode chunk\n"));
 
