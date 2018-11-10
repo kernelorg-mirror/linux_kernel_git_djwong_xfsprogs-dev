803d8cf7ef1a0df01f79cc84f6cc73c904d5736e
Darrick J. Wong <darrick.wong@oracle.com>
2018-11-10 11:35:31 -0800

    xfs: remove xfs_rmap_ag_owner and friends
    Owner information for static fs metadata can be defined readonly at
    build time because it never changes across filesystems.  This enables us
    to reduce stack usage (particularly in scrub) because we can use the
    statically defined oinfo structures.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- repair/rmap.c
+++ repair/rmap.c
@@ -454,7 +454,6 @@ rmap_store_ag_btree_rec(
 	struct xfs_ag_rmap	*ag_rmap = &ag_rmaps[agno];
 	struct bitmap		*own_ag_bitmap = NULL;
 	int			error = 0;
-	struct xfs_owner_info	oinfo;
 
 	if (!xfs_sb_version_hasrmapbt(&mp->m_sb))
 		return 0;
@@ -539,6 +538,10 @@ rmap_store_ag_btree_rec(
 	/* Insert rmaps into the btree one at a time */
 	rm_rec = pop_slab_cursor(rm_cur);
 	while (rm_rec) {
+		struct xfs_owner_info	oinfo = {
+			.oi_owner	= rm_rec->rm_owner,
+		};
+
 		error = -libxfs_trans_alloc_rollable(mp, 16, &tp);
 		if (error)
 			goto err_slab;
@@ -548,7 +551,6 @@ rmap_store_ag_btree_rec(
 			goto err_trans;
 
 		ASSERT(XFS_RMAP_NON_INODE_OWNER(rm_rec->rm_owner));
-		libxfs_rmap_ag_owner(&oinfo, rm_rec->rm_owner);
 		error = -libxfs_rmap_alloc(tp, agbp, agno, rm_rec->rm_startblock,
 				rm_rec->rm_blockcount, &oinfo);
 		if (error)
