b8cce1ed12469cabeadb9b6cee6dcb0509c52db2
Darrick J. Wong <darrick.wong@oracle.com>
2018-11-10 12:00:48 -0800

    xfs: separate inode geometry
    Separate the inode geometry information into a distinct structure.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- db/inode.c
+++ db/inode.c
@@ -652,15 +652,15 @@ set_cur_inode(
 	}
 	cur_agno = agno;
 
-	if (mp->m_inode_cluster_size > mp->m_sb.sb_blocksize &&
-	    mp->m_inoalign_mask) {
+	if (mp->m_ino_geo.ig_min_cluster_size > mp->m_sb.sb_blocksize &&
+	    mp->m_ino_geo.ig_inoalign_mask) {
 		xfs_agblock_t	chunk_agbno;
 		xfs_agblock_t	offset_agbno;
 		int		blks_per_cluster;
 
-		blks_per_cluster = mp->m_inode_cluster_size >>
+		blks_per_cluster = mp->m_ino_geo.ig_min_cluster_size >>
 							mp->m_sb.sb_blocklog;
-		offset_agbno = agbno & mp->m_inoalign_mask;
+		offset_agbno = agbno & mp->m_ino_geo.ig_inoalign_mask;
 		chunk_agbno = agbno - offset_agbno;
 		cluster_agbno = chunk_agbno +
 			((offset_agbno / blks_per_cluster) * blks_per_cluster);
