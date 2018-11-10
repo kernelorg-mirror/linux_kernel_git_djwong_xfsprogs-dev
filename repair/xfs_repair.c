320827d9ce8aa817021cc85ae52e253a0b72713c
Darrick J. Wong <darrick.wong@oracle.com>
2018-11-10 12:00:48 -0800

    xfs: separate inode geometry
    Separate the inode geometry information into a distinct structure.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- repair/xfs_repair.c
+++ repair/xfs_repair.c
@@ -399,7 +399,7 @@ calc_mkfs(xfs_mount_t *mp)
 	xfs_agblock_t	fino_bno;
 	int		do_inoalign;
 
-	do_inoalign = mp->m_sinoalign;
+	do_inoalign = mp->m_ino_geo.ig_sinoalign;
 
 	/*
 	 * Pre-calculate the geometry of ag 0. We know what it looks like
@@ -458,9 +458,9 @@ calc_mkfs(xfs_mount_t *mp)
 		first_prealloc_ino = XFS_AGB_TO_AGINO(mp, fino_bno);
 	}
 
-	ASSERT(mp->m_ialloc_blks > 0);
+	ASSERT(mp->m_ino_geo.ig_ialloc_blks > 0);
 
-	if (mp->m_ialloc_blks > 1)
+	if (mp->m_ino_geo.ig_ialloc_blks > 1)
 		last_prealloc_ino = first_prealloc_ino + XFS_INODES_PER_CHUNK;
 	else
 		last_prealloc_ino = XFS_AGB_TO_AGINO(mp, fino_bno + 1);
@@ -760,7 +760,7 @@ main(int argc, char **argv)
 	chunks_pblock = mp->m_sb.sb_inopblock / XFS_INODES_PER_CHUNK;
 	max_symlink_blocks = libxfs_symlink_blocks(mp, XFS_SYMLINK_MAXLEN);
 	inodes_per_cluster = max(mp->m_sb.sb_inopblock,
-			mp->m_inode_cluster_size >> mp->m_sb.sb_inodelog);
+			mp->m_ino_geo.ig_min_cluster_size >> mp->m_sb.sb_inodelog);
 
 	/*
 	 * Automatic striding for high agcount filesystems.
@@ -896,7 +896,7 @@ main(int argc, char **argv)
 		if (max_mem >= (1 << 30))
 			max_mem = 1 << 30;
 		libxfs_bhash_size = max_mem / (HASH_CACHE_RATIO *
-				(mp->m_inode_cluster_size >> 10));
+				(mp->m_ino_geo.ig_min_cluster_size >> 10));
 		if (libxfs_bhash_size < 512)
 			libxfs_bhash_size = 512;
 
