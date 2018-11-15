1625fa021f1fba9a3152d0d3816cc003af5f1fc6
Darrick J. Wong <darrick.wong@oracle.com>
2018-11-15 13:36:57 -0800

    xfs: create imeta abstractions to get and set metadata inodes
    Create some helper routines to get and set metadata inode numbers
    instead of open-coding them throughout xfs.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- include/libxfs.h
+++ include/libxfs.h
@@ -68,6 +68,7 @@ extern uint32_t crc32c_le(uint32_t crc, unsigned char const *p, size_t len);
 #include "xfs_rmap.h"
 #include "xfs_refcount_btree.h"
 #include "xfs_refcount.h"
+#include "xfs_imeta.h"
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
