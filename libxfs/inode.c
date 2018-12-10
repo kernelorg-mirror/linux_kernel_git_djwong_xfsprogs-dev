22b30a7ff8e93f333dd4d43c4367c3a5bb83547d
Darrick J. Wong <darrick.wong@oracle.com>
2018-12-10 12:02:52 -0800

    libxfs: pass IGET flags through to xfs_iread
    Change the lock_flags parameter to iget_flags so that we can supply
    XFS_IGET_ flags in future patches.  All callers of libxfs_iget and
    libxfs_trans_iget pass zero for this parameter and there are no inode
    locks in xfsprogs, so there's no behavior change here.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/inode.c
+++ libxfs/inode.c
@@ -381,7 +381,7 @@ libxfs_iget(
 	struct xfs_mount	*mp,
 	struct xfs_trans	*tp,
 	xfs_ino_t		ino,
-	uint			lock_flags,
+	uint			iget_flags,
 	struct xfs_inode	**ipp,
 	struct xfs_ifork_ops	*ifork_ops)
 {
@@ -394,7 +394,7 @@ libxfs_iget(
 
 	ip->i_ino = ino;
 	ip->i_mount = mp;
-	error = xfs_iread(mp, tp, ip, 0);
+	error = xfs_iread(mp, tp, ip, iget_flags);
 	if (error) {
 		kmem_zone_free(xfs_inode_zone, ip);
 		*ipp = NULL;
