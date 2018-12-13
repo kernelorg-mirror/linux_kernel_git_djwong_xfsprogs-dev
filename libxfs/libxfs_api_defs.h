581b522bea65edf51f2feaa0542d45dc7357ac81
Darrick J. Wong <darrick.wong@oracle.com>
2018-12-12 16:57:49 -0800

    xfs: ensure metadata directory paths exist before creating files
    Since xfs_imeta_create can create new metadata files arbitrarily deep in
    the metadata directory tree, we must supply a function that can ensure
    that all directories in a path exist, and call it before the quota
    functions create the quota inodes.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -178,5 +178,6 @@
 #define xfs_imeta_unlink_space_res	libxfs_imeta_unlink_space_res
 #define xfs_imeta_iget			libxfs_imeta_iget
 #define xfs_imeta_irele			libxfs_imeta_irele
+#define xfs_imeta_ensure_dirpath	libxfs_imeta_ensure_dirpath
 
 #endif /* __LIBXFS_API_DEFS_H__ */
