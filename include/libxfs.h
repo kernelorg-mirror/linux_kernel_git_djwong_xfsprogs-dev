877e090948e4ff71cc2263c08d0c391b07ae1af4
Darrick J. Wong <darrick.wong@oracle.com>
2018-12-19 17:58:54 -0800

    libxfs: resync libxfs_alloc_file_space interface with the kernel
    Make the userspace xfs_alloc_file_space behave (more or less) like the
    kernel version, at least as far as the interface goes.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- include/libxfs.h
+++ include/libxfs.h
@@ -154,8 +154,8 @@ extern int	libxfs_log_header(char *, uuid_t *, int, int, int, xfs_lsn_t,
 
 /* Shared utility routines */
 
-extern int	libxfs_alloc_file_space (struct xfs_inode *, xfs_off_t,
-				xfs_off_t, int, int);
+extern int	libxfs_alloc_file_space(struct xfs_inode *ip, xfs_off_t offset,
+					xfs_off_t len, int alloc_type);
 
 extern void	libxfs_fs_repair_cmn_err(int, struct xfs_mount *, char *, ...);
 extern void	libxfs_fs_cmn_err(int, struct xfs_mount *, char *, ...);
