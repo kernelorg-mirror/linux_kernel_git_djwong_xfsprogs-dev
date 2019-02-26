0437cfbd6d69a51c055687586f42da8298a89830
Darrick J. Wong <darrick.wong@oracle.com>
2019-02-26 11:14:08 -0800

    xfs: store free inode btree block counts in AGI header
    Add a free inode btree block usage counter to the AGI header so that we
    don't have to walk the entire finobt at mount time to create the per-AG
    reservations.  We piggyback on the metadir feature bit to avoid burning
    extra incompat bits.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_ialloc_btree.c
+++ libxfs/xfs_ialloc_btree.c
@@ -68,6 +68,21 @@ xfs_finobt_set_root(
 			   XFS_AGI_FREE_ROOT | XFS_AGI_FREE_LEVEL);
 }
 
+static inline void
+xfs_finobt_change_blocks(
+	struct xfs_btree_cur	*cur,
+	int			howmuch)
+{
+	struct xfs_buf		*agbp = cur->bc_private.a.agbp;
+	struct xfs_agi		*agi = XFS_BUF_TO_AGI(agbp);
+
+	if (!xfs_sb_version_hasfinobtblocks(&cur->bc_mp->m_sb))
+		return;
+
+	be32_add_cpu(&agi->agi_fino_blocks, howmuch);
+	xfs_ialloc_log_agi(cur->bc_tp, agbp, XFS_AGI_FINO_BLOCKS);
+}
+
 STATIC int
 __xfs_inobt_alloc_block(
 	struct xfs_btree_cur	*cur,
@@ -123,10 +138,18 @@ xfs_finobt_alloc_block(
 	union xfs_btree_ptr	*new,
 	int			*stat)
 {
+	int			error;
+
 	if (cur->bc_mp->m_finobt_nores)
-		return xfs_inobt_alloc_block(cur, start, new, stat);
-	return __xfs_inobt_alloc_block(cur, start, new, stat,
-			XFS_AG_RESV_METADATA);
+		error = xfs_inobt_alloc_block(cur, start, new, stat);
+	else
+		error = __xfs_inobt_alloc_block(cur, start, new, stat,
+				XFS_AG_RESV_METADATA);
+	if (error)
+		return error;
+
+	xfs_finobt_change_blocks(cur, 1);
+	return 0;
 }
 
 STATIC int
@@ -153,6 +176,7 @@ xfs_finobt_free_block(
 	struct xfs_btree_cur	*cur,
 	struct xfs_buf		*bp)
 {
+	xfs_finobt_change_blocks(cur, -1);
 	if (cur->bc_mp->m_finobt_nores)
 		return xfs_inobt_free_block(cur, bp);
 	return __xfs_inobt_free_block(cur, bp, XFS_AG_RESV_METADATA);
@@ -602,6 +626,20 @@ xfs_inobt_count_blocks(
 	struct xfs_buf		*agbp;
 	struct xfs_btree_cur	*cur;
 	int			error;
+
+	if (btnum == XFS_BTNUM_FINO &&
+	    xfs_sb_version_hasfinobtblocks(&mp->m_sb)) {
+		struct xfs_agi	*agi;
+
+		error = xfs_ialloc_read_agi(mp, tp, agno, &agbp);
+		if (error)
+			return error;
+
+		agi = XFS_BUF_TO_AGI(agbp);
+		*tree_blocks = be32_to_cpu(agi->agi_fino_blocks);
+		xfs_trans_brelse(tp, agbp);
+		return 0;
+	}
 
 	error = xfs_inobt_cur(mp, tp, agno, &cur, &agbp);
 	if (error)
