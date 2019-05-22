5e7a8c3ee850c9e599a70586c2bd0a1f81607a28
Darrick J. Wong <darrick.wong@oracle.com>
2019-05-22 13:26:02 -0700

    misc: convert from XFS_IOC_FSINUMBERS to XFS_IOC_INUMBERS
    Convert all programs to use the v5 inumbers ioctl.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- io/imap.c
+++ io/imap.c
@@ -15,9 +15,7 @@ static cmdinfo_t imap_cmd;
 static int
 imap_f(int argc, char **argv)
 {
-	struct xfs_inogrp	*t;
-	uint64_t		last = 0;
-	uint32_t		count;
+	struct xfs_inumbers_req	*ireq;
 	uint32_t		nent;
 	int			i;
 	int			error;
@@ -27,17 +25,19 @@ imap_f(int argc, char **argv)
 	else
 		nent = atoi(argv[1]);
 
-	t = malloc(nent * sizeof(*t));
-	if (!t)
+	ireq = xfrog_inumbers_alloc_req(nent, 0);
+	if (!ireq) {
+		perror("alloc req");
 		return 0;
+	}
 
-	while ((error = xfrog_inumbers(file->fd, &last, nent, t, &count)) == 0 &&
-	       count > 0) {
-		for (i = 0; i < count; i++) {
+	while ((error = xfrog_inumbers(file->fd, NULL, ireq)) == 0 &&
+	       ireq->hdr.ocount > 0) {
+		for (i = 0; i < ireq->hdr.ocount; i++) {
 			printf(_("ino %10llu count %2d mask %016llx\n"),
-				(unsigned long long)t[i].xi_startino,
-				t[i].xi_alloccount,
-				(unsigned long long)t[i].xi_allocmask);
+				(unsigned long long)ireq->inumbers[i].xi_startino,
+				ireq->inumbers[i].xi_alloccount,
+				(unsigned long long)ireq->inumbers[i].xi_allocmask);
 		}
 	}
 
@@ -45,7 +45,7 @@ imap_f(int argc, char **argv)
 		perror("xfsctl(XFS_IOC_FSINUMBERS)");
 		exitcode = 1;
 	}
-	free(t);
+	free(ireq);
 	return 0;
 }
 
