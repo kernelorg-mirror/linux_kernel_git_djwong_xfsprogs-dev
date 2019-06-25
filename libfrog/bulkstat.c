627e6f7091046d0a33677987fb1a7b16c9df0d45
Darrick J. Wong <darrick.wong@oracle.com>
2019-06-25 14:58:03 -0700

    xfs_io: add a bulkstat command
    Add a bulkstat command to xfs_io so that we can test our new xfrog code.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libfrog/bulkstat.c
+++ libfrog/bulkstat.c
@@ -353,6 +353,16 @@ xfrog_bulkstat_alloc_req(
 	return breq;
 }
 
+/* Set a bulkstat cursor to iterate only a particular AG. */
+void
+xfrog_bulkstat_set_ag(
+	struct xfs_bulkstat_req	*req,
+	uint32_t		agno)
+{
+	req->hdr.agno = agno;
+	req->hdr.flags |= XFS_BULK_IREQ_AGNO;
+}
+
 /* Convert an inumbers (v5) struct to a inogrp (v1) struct. */
 void
 xfrog_inumbers_to_inogrp(
@@ -469,4 +479,14 @@ xfrog_inumbers_alloc_req(
 	ireq->hdr.ino = startino;
 
 	return ireq;
+}
+
+/* Set an inumbers cursor to iterate only a particular AG. */
+void
+xfrog_inumbers_set_ag(
+	struct xfs_inumbers_req	*req,
+	uint32_t		agno)
+{
+	req->hdr.agno = agno;
+	req->hdr.flags |= XFS_BULK_IREQ_AGNO;
 }
