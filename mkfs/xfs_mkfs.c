aa1ccdcb1af05b63ce59cd93164ebcee396e78ef
Darrick J. Wong <darrick.wong@oracle.com>
2019-06-25 14:58:10 -0700

    xfs: create imeta abstractions to get and set metadata inodes
    Create some helper routines to get and set metadata inode numbers
    instead of open-coding them throughout xfs.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -3228,6 +3228,7 @@ start_superblock_setup(
 	struct xfs_mount	*mp,
 	struct xfs_sb		*sbp)
 {
+	sbp->sb_inprogress = 1;	/* mkfs is in progress */
 	sbp->sb_magicnum = XFS_SB_MAGIC;
 	sbp->sb_sectsize = (uint16_t)cfg->sectorsize;
 	sbp->sb_sectlog = (uint8_t)cfg->sectorlog;
@@ -3309,7 +3310,6 @@ finish_superblock_setup(
 	sbp->sb_logblocks = (xfs_extlen_t)cfg->logblocks;
 	sbp->sb_rextslog = (uint8_t)(cfg->rtextents ?
 			libxfs_highbit32((unsigned int)cfg->rtextents) : 0);
-	sbp->sb_inprogress = 1;	/* mkfs is in progress */
 	sbp->sb_imax_pct = cfg->imaxpct;
 	sbp->sb_icount = 0;
 	sbp->sb_ifree = 0;
