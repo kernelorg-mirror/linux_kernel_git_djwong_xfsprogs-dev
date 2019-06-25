68db34690ceb89225203d09b18e28ea94bf64b2f
Darrick J. Wong <darrick.wong@oracle.com>
2019-06-25 14:58:02 -0700

    misc: convert XFS_IOC_FSBULKSTAT to XFS_IOC_BULKSTAT
    Convert the v1 calls to v5 calls.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- scrub/unicrash.c
+++ scrub/unicrash.c
@@ -433,7 +433,7 @@ out_free:
  */
 static bool
 is_only_root_writable(
-	struct xfs_bstat	*bstat)
+	struct xfs_bulkstat	*bstat)
 {
 	if (bstat->bs_uid != 0 || bstat->bs_gid != 0)
 		return false;
@@ -445,7 +445,7 @@ bool
 unicrash_dir_init(
 	struct unicrash		**ucp,
 	struct scrub_ctx	*ctx,
-	struct xfs_bstat	*bstat)
+	struct xfs_bulkstat	*bstat)
 {
 	/*
 	 * Assume 64 bytes per dentry, clamp buckets between 16 and 64k.
@@ -460,7 +460,7 @@ bool
 unicrash_xattr_init(
 	struct unicrash		**ucp,
 	struct scrub_ctx	*ctx,
-	struct xfs_bstat	*bstat)
+	struct xfs_bulkstat	*bstat)
 {
 	/* Assume 16 attributes per extent for lack of a better idea. */
 	return unicrash_init(ucp, ctx, false, 16 * (1 + bstat->bs_aextents),
