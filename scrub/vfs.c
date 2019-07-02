ef17965e1507081b90c6098d05651b90afa7f21a
Darrick J. Wong <darrick.wong@oracle.com>
2019-07-02 16:26:28 -0700

    libfrog: split workqueue destroy functions
    Split the workqueue destroy function into two parts -- one to signal all
    the threads to exit and wait for them, and a second one that actually
    destroys all the memory associated with the workqueue.  This mean we can
    report latent workqueue errors independent of the freeing function.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- scrub/vfs.c
+++ scrub/vfs.c
@@ -219,6 +219,11 @@ scan_fs_tree(
 	assert(sft.nr_dirs == 0);
 	pthread_mutex_unlock(&sft.lock);
 
+	ret = workqueue_terminate(&wq);
+	if (ret) {
+		sft.moveon = false;
+		str_liberror(ctx, ret, _("finishing directory scan work"));
+	}
 out_wq:
 	workqueue_destroy(&wq);
 	return sft.moveon;
