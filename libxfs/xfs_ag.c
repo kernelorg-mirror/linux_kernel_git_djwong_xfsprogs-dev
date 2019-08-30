d49acdb193d051848404889ed1a5b15ccf06a47f
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:50:13 -0700

    xfs: store inode btree block counts in AGI header
    Add a btree block usage counter for both inode btrees to the AGI header
    so that we don't have to walk the entire finobt at mount time to create
    the per-AG reservations.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_ag.c
+++ libxfs/xfs_ag.c
@@ -330,6 +330,10 @@ xfs_agiblock_init(
 	}
 	for (bucket = 0; bucket < XFS_AGI_UNLINKED_BUCKETS; bucket++)
 		agi->agi_unlinked[bucket] = cpu_to_be32(NULLAGINO);
+	if (xfs_sb_version_hasinobtcounts(&mp->m_sb)) {
+		agi->agi_iblocks = cpu_to_be32(1);
+		agi->agi_fblocks = cpu_to_be32(1);
+	}
 }
 
 typedef void (*aghdr_init_work_f)(struct xfs_mount *mp, struct xfs_buf *bp,
