f67751a8768f8e6529efa6bbdffd76b426c61570
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:49:56 -0700

    xfs_scrub: remove moveon from phase 1 functions
    Replace the moveon returns in the phase 1 code with a direct integer
    error return.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- scrub/xfs_scrub.c
+++ scrub/xfs_scrub.c
@@ -602,6 +602,7 @@ main(
 	int			c;
 	int			fd;
 	int			ret = SCRUB_RET_SUCCESS;
+	int			error;
 
 	fprintf(stdout, "EXPERIMENTAL xfs_scrub program in use! Use at your own risk!\n");
 
@@ -776,8 +777,8 @@ main(
 		str_info(&ctx, ctx.mntpoint, _("Injecting error."));
 
 	/* Clean up scan data. */
-	moveon = xfs_cleanup_fs(&ctx);
-	if (!moveon && ctx.runtime_errors == 0)
+	error = scrub_cleanup(&ctx);
+	if (error && ctx.runtime_errors == 0)
 		ctx.runtime_errors++;
 
 out:
