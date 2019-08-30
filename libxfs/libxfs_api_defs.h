8b8595482f7b178c1547eb56cd915674be8d090a
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:50:03 -0700

    xfs_repair: port the online repair newbt structure
    Port the new btree staging context and related block reservation helper
    code from the kernel to repair.  We'll use this in subsequent patches to
    implement btree bulk loading.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -51,6 +51,8 @@
 #define xfs_attr_remove			libxfs_attr_remove
 #define xfs_attr_leaf_newentsize	libxfs_attr_leaf_newentsize
 
+#define xfs_alloc_vextent		libxfs_alloc_vextent
+#define __xfs_bmap_add_free		__libxfs_bmap_add_free
 #define xfs_agfl_walk			libxfs_agfl_walk
 #define xfs_alloc_fix_freelist		libxfs_alloc_fix_freelist
 #define xfs_alloc_min_freelist		libxfs_alloc_min_freelist
