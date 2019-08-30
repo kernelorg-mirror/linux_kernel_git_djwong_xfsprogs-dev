da24fa099cf98c66bce606ff9afb1b5326cb6686
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:50:11 -0700

    xfs_repair: use library functions to reset root/rbm/rsum inodes
    Use the iroot reset function to reset root inodes instead of open-coding
    the reset routine.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -154,6 +154,7 @@
 #define xfs_rmap_query_all		libxfs_rmap_query_all
 #define xfs_bmapi_remap			libxfs_bmapi_remap
 #define xfs_imap_to_bp			libxfs_imap_to_bp
+#define xfs_fixed_inode_reset		libxfs_fixed_inode_reset
 
 #define LIBXFS_ATTR_ROOT		ATTR_ROOT
 #define LIBXFS_ATTR_SECURE		ATTR_SECURE
@@ -164,6 +165,7 @@
 #define xfs_buf_delwri_submit		libxfs_buf_delwri_submit
 #define xfs_get_projid			libxfs_get_projid
 #define xfs_set_projid			libxfs_set_projid
+#define xfs_inode_init			libxfs_inode_init
 
 #define xfs_btree_bload_compute_geometry libxfs_btree_bload_compute_geometry
 #define xfs_btree_bload			libxfs_btree_bload
