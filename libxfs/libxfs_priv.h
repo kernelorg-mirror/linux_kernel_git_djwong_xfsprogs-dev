82670eb218e354570909d01c4c8d4cc00016aa41
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:50:12 -0700

    xfs: read and write metadata inode directory
    Plumb in the bits we need to look up metadata inode numbers from the
    metadata inode directory and save them back.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/libxfs_priv.h
+++ libxfs/libxfs_priv.h
@@ -157,6 +157,7 @@ enum ce { CE_DEBUG, CE_CONT, CE_NOTE, CE_WARN, CE_ALERT, CE_PANIC };
 #define XFS_ERRLEVEL_LOW		1
 #define XFS_FORCED_SHUTDOWN(mp)		0
 #define XFS_ILOCK_EXCL			0
+#define XFS_IOLOCK_EXCL			0
 #define XFS_STATS_INC(mp, count)	do { (mp) = (mp); } while (0)
 #define XFS_STATS_DEC(mp, count, x)	do { (mp) = (mp); } while (0)
 #define XFS_STATS_ADD(mp, count, x)	do { (mp) = (mp); } while (0)
@@ -442,6 +443,7 @@ xfs_buf_incore(struct xfs_buftarg *target, xfs_daddr_t blkno, size_t numblks,
 	typeof(mode) __mode = mode;				\
 	__mode = __mode; /* no set-but-unused warning */	\
 })
+#define xfs_lock_two_inodes(dp, dl, ip, il)		((void) 0)
 
 #define mutex_lock(l)					((void) 0)
 #define mutex_unlock(l)					((void) 0)
