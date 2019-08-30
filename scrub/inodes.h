5738b9444b0cf5e021704898c99287a10b034fc9
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:49:56 -0700

    xfs_scrub: remove XFS_ITERATE_INODES_ABORT from inode iterator
    Remove the _ABORT code since nobody uses it and we're slowly moving to
    ECANCELED anyway.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- scrub/inodes.h
+++ scrub/inodes.h
@@ -6,10 +6,16 @@
 #ifndef XFS_SCRUB_INODES_H_
 #define XFS_SCRUB_INODES_H_
 
+/*
+ * Return codes for the inode iterator function are 0 to continue iterating,
+ * and non-zero to stop iterating.  Any non-zero value will be passed up to the
+ * iteration caller.  The special value ECANCELED can be used to stop
+ * iteration, because the inode iteration function never generates that error
+ * code on its own.
+ */
 typedef int (*scrub_inode_iter_fn)(struct scrub_ctx *ctx,
 		struct xfs_handle *handle, struct xfs_bulkstat *bs, void *arg);
 
-#define XFS_ITERATE_INODES_ABORT	(-1)
 int scrub_scan_all_inodes(struct scrub_ctx *ctx, scrub_inode_iter_fn fn,
 		void *arg);
 
