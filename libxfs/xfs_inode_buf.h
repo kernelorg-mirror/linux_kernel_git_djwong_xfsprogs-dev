733d1ec9e0fc2da933d77734b2d768a0c1fd17d4
Darrick J. Wong <darrick.wong@oracle.com>
2019-10-22 13:22:11 -0700

    xfs: remove xfs_timestamp_t
    Kill this old typedef.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_inode_buf.h
+++ libxfs/xfs_inode_buf.h
@@ -37,7 +37,7 @@ struct xfs_icdinode {
 	uint64_t	di_flags2;	/* more random flags */
 	uint32_t	di_cowextsize;	/* basic cow extent size for file */
 
-	xfs_ictimestamp_t di_crtime;	/* time created */
+	struct xfs_ictimestamp di_crtime;	/* time created */
 };
 
 /*
