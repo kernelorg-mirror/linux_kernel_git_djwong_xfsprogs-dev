6531a4bd2e49d5d09ba0f37c24ed860c8fbdac2d
Darrick J. Wong <darrick.wong@oracle.com>
2019-11-22 13:31:15 -0800

    Subject: [PATCH] xfs_db: add bigtime upgrade path
    Enable users to upgrade their filesystems to bigtime support.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- db/xfs_admin.sh
+++ db/xfs_admin.sh
@@ -7,9 +7,9 @@
 status=0
 DB_OPTS=""
 REPAIR_OPTS=""
-USAGE="Usage: xfs_admin [-efjlpuV] [-c 0|1] [-L label] [-U uuid] device"
+USAGE="Usage: xfs_admin [-efjlpuV] [-O feature] [-c 0|1] [-L label] [-U uuid] device"
 
-while getopts "efjlpuc:L:U:V" c
+while getopts "efjlpuc:L:O:U:V" c
 do
 	case $c in
 	c)	REPAIR_OPTS=$REPAIR_OPTS" -c lazycount="$OPTARG;;
@@ -19,6 +19,8 @@ do
 	l)	DB_OPTS=$DB_OPTS" -r -c label";;
 	L)	DB_OPTS=$DB_OPTS" -c 'label "$OPTARG"'";;
 	p)	DB_OPTS=$DB_OPTS" -c 'version projid32bit'";;
+	O)	DB_OPTS=$DB_OPTS" -c 'version "$OPTARG"'";
+		REPAIR_OPTS="$REPAIR_OPTS ";;
 	u)	DB_OPTS=$DB_OPTS" -r -c uuid";;
 	U)	DB_OPTS=$DB_OPTS" -c 'uuid "$OPTARG"'";;
 	V)	xfs_db -p xfs_admin -V
@@ -38,7 +40,7 @@ case $# in
 			eval xfs_db -x -p xfs_admin $DB_OPTS $1
 			status=$?
 		fi
-		if [ -n "$REPAIR_OPTS" ]
+		if [ $status -eq 0 ] && [ -n "$REPAIR_OPTS" ]
 		then
 			# Hide normal repair output which is sent to stderr
 			# assuming the filesystem is fine when a user is
