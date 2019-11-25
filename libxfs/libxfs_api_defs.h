268143152e4832b52a5b9b56dd7932728eca4063
Darrick J. Wong <darrick.wong@oracle.com>
2019-11-25 09:15:43 -0800

    mkfs: check root inode location
    Make sure the root inode gets created where repair thinks it should be
    created.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -156,5 +156,6 @@
 
 #define xfs_ag_init_headers		libxfs_ag_init_headers
 #define xfs_buf_delwri_submit		libxfs_buf_delwri_submit
+#define xfs_ialloc_calc_rootino		libxfs_ialloc_calc_rootino
 
 #endif /* __LIBXFS_API_DEFS_H__ */
