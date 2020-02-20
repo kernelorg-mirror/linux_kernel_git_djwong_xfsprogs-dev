bf07792a2479cb4e8aeb59d8cbe352f431320493
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-19 16:36:22 -0800

    libxfs: rearrange libxfs_trans_ichgtime call when creating inodes
    Rearrange the libxfs_trans_ichgtime call in libxfs_ialloc so that we
    call it once with the flags we want.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/inode.c
+++ libxfs/inode.c
@@ -66,6 +66,7 @@ libxfs_ialloc(
 	struct xfs_inode		*pip = args->pip;
 	xfs_ino_t			ino;
 	uint				flags;
+	int				times;
 	int				error;
 
 	/*
@@ -91,7 +92,7 @@ libxfs_ialloc(
 	set_nlink(VFS_I(ip), args->nlink);
 	VFS_I(ip)->i_uid = args->uid;
 	ip->i_d.di_projid = args->prid;
-	xfs_trans_ichgtime(tp, ip, XFS_ICHGTIME_CHG | XFS_ICHGTIME_MOD);
+	times = XFS_ICHGTIME_CHG | XFS_ICHGTIME_MOD;
 
 	if (pip && (VFS_I(pip)->i_mode & S_ISGID)) {
 		VFS_I(ip)->i_gid = VFS_I(pip)->i_gid;
@@ -115,9 +116,11 @@ libxfs_ialloc(
 		ip->i_d.di_flags2 = 0;
 		if (xfs_sb_version_hasbigtime(&ip->i_mount->m_sb))
 			ip->i_d.di_flags2 |= XFS_DIFLAG2_BIGTIME;
-		ip->i_d.di_crtime = VFS_I(ip)->i_mtime;
+		times |= XFS_ICHGTIME_CREATE;
 		ip->i_d.di_cowextsize = 0;
 	}
+
+	xfs_trans_ichgtime(tp, ip, times);
 
 	flags = XFS_ILOG_CORE;
 	switch (args->mode & S_IFMT) {
