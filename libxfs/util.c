37533a228e9c26ba7c36bcf4f1db6e13573f5b6f
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-19 16:36:33 -0800

    xfs_repair: refactor file writes into a common helper
    Create a helper function to write data to a file, then kill the open
    coded versions of file writes for rt metadata regeneration.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/util.c
+++ libxfs/util.c
@@ -514,3 +514,65 @@ libxfs_imeta_ensure_dirpath(
 
 	return error == -EEXIST ? 0 : error;
 }
+
+/*
+ * Write a buffer to a file on the data device.  We assume there are no holes
+ * and no unwritten extents.
+ */
+int
+libxfs_file_write(
+	struct xfs_trans	*tp,
+	struct xfs_inode	*ip,
+	void			*buf,
+	size_t			len,
+	bool			logit)
+{
+	struct xfs_bmbt_irec	map;
+	struct xfs_mount	*mp = tp->t_mountp;
+	struct xfs_buf		*bp;
+	xfs_daddr_t		d;
+	xfs_fileoff_t		bno = 0;
+	xfs_fileoff_t		end_bno = XFS_B_TO_FSB(mp, len);
+	size_t			count;
+	int			nmap;
+	int			error = 0;
+
+	/* Write one block at a time. */
+	while (bno < end_bno) {
+		nmap = 1;
+		error = xfs_bmapi_read(ip, bno, end_bno, &map, &nmap, 0);
+		if (error)
+			return error;
+		if (nmap != 1)
+			return -ENOSPC;
+
+		if (map.br_startblock == HOLESTARTBLOCK ||
+		    map.br_state == XFS_EXT_UNWRITTEN)
+			return -EINVAL;
+
+		d = XFS_FSB_TO_DADDR(mp, map.br_startblock);
+		error = xfs_trans_get_buf(logit ? tp : NULL, mp->m_dev, d,
+				map.br_blockcount << mp->m_blkbb_log, 0, &bp);
+		if (error)
+			break;
+		count = min(len, XFS_FSB_TO_B(mp, map.br_blockcount));
+		memmove(bp->b_addr, buf, count);
+		if (count < bp->b_bcount)
+			memset((char *)bp->b_addr + count, 0,
+					bp->b_bcount - count);
+		if (logit)
+			xfs_trans_log_buf(tp, bp, 0, bp->b_bcount - 1);
+		else {
+			libxfs_buf_mark_dirty(bp);
+			xfs_buf_relse(bp);
+		}
+		if (error)
+			break;
+
+		buf += count;
+		len -= count;
+		bno += map.br_blockcount;
+	}
+
+	return error;
+}
