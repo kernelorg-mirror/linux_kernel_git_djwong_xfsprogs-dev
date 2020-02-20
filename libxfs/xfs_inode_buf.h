c9088f6b6de7082ae1f60391449317f475e2d6f6
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-19 16:36:19 -0800

    xfs: move xfs_log_dinode_to_disk to the log code
    Move this function to xfs_inode_item.c to match the encoding function
    that's already there.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_inode_buf.h
+++ libxfs/xfs_inode_buf.h
@@ -55,8 +55,6 @@ void	xfs_dinode_calc_crc(struct xfs_mount *, struct xfs_dinode *);
 void	xfs_inode_to_disk(struct xfs_inode *ip, struct xfs_dinode *to,
 			  xfs_lsn_t lsn);
 void	xfs_inode_from_disk(struct xfs_inode *ip, struct xfs_dinode *from);
-void	xfs_log_dinode_to_disk(struct xfs_log_dinode *from,
-			       struct xfs_dinode *to);
 
 #if defined(DEBUG)
 void	xfs_inobp_check(struct xfs_mount *, struct xfs_buf *);
