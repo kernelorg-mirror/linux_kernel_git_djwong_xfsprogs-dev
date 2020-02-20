bd7848fa8ef4883f8600ab1f0ec509d2f098893f
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-19 16:36:21 -0800

    xfs: hoist extent size helpers to libxfs
    Move the extent size helpers to xfs_bmap.c in libxfs since they're used
    there already.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- include/xfs_inode.h
+++ include/xfs_inode.h
@@ -140,6 +140,11 @@ static inline bool xfs_is_reflink_inode(struct xfs_inode *ip)
 	return ip->i_d.di_flags2 & XFS_DIFLAG2_REFLINK;
 }
 
+static inline bool xfs_is_always_cow_inode(struct xfs_inode *ip)
+{
+	return false;
+}
+
 typedef struct cred {
 	uid_t	cr_uid;
 	gid_t	cr_gid;
@@ -162,5 +167,7 @@ extern int	libxfs_iget(struct xfs_mount *mp, struct xfs_trans *tp,
 				xfs_ino_t ino, uint lock_flags,
 				struct xfs_inode **ipp);
 extern void	libxfs_irele(struct xfs_inode *ip);
+
+#define XFS_DEFAULT_COWEXTSZ_HINT 32
 
 #endif /* __XFS_INODE_H__ */
