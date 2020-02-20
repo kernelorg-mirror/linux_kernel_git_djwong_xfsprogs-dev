0d6d990ae00b9356d7a1c017c545c3cf6ab50f35
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-19 16:36:06 -0800

    libxfs: replace libxfs_readbuf with libxfs_buf_read
    Change all the libxfs_readbuf calls to libxfs_buf_read to match the
    kernel interface.  This enables us to hide libxfs_readbuf and simplify
    the userspace buffer interface further.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -3573,7 +3573,7 @@ rewrite_secondary_superblocks(
 	struct xfs_buf		*buf;
 
 	/* rewrite the last superblock */
-	buf = libxfs_readbuf(mp->m_dev,
+	buf = libxfs_buf_read(mp->m_dev,
 			XFS_AGB_TO_DADDR(mp, mp->m_sb.sb_agcount - 1,
 				XFS_SB_DADDR),
 			XFS_FSS_TO_BB(mp, 1),
@@ -3585,7 +3585,7 @@ rewrite_secondary_superblocks(
 	if (mp->m_sb.sb_agcount <= 2)
 		return;
 
-	buf = libxfs_readbuf(mp->m_dev,
+	buf = libxfs_buf_read(mp->m_dev,
 			XFS_AGB_TO_DADDR(mp, (mp->m_sb.sb_agcount - 1) / 2,
 				XFS_SB_DADDR),
 			XFS_FSS_TO_BB(mp, 1),
