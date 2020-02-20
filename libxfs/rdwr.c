2a0df455a61db7f89356a3fd59ec0874630111b7
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-19 16:36:12 -0800

    xfs: support bulk loading of staged btrees
    Add a new btree function that enables us to bulk load a btree cursor.
    This will be used by the upcoming online repair patches to generate new
    btrees.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/rdwr.c
+++ libxfs/rdwr.c
@@ -1621,3 +1621,22 @@ libxfs_log_header(
 	return BBTOB(len);
 }
 
+/*
+ * Cancel a delayed write list.
+ *
+ * Remove each buffer from the list, clear the delwri queue flag and drop the
+ * associated buffer reference.
+ */
+void
+xfs_buf_delwri_cancel(
+	struct list_head	*list)
+{
+	struct xfs_buf		*bp;
+
+	while (!list_empty(list)) {
+		bp = list_first_entry(list, struct xfs_buf, b_list);
+		list_del_init(&bp->b_list);
+		bp->b_flags &= ~LIBXFS_B_DIRTY;
+		libxfs_buf_relse(bp);
+	}
+}
