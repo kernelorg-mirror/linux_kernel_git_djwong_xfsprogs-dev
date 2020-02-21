10d3615b91e2cfe78e0f17475ef19d22285226c8
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-21 13:25:39 -0800

    libxfs: remove unused flags parameter to libxfs_buf_mark_dirty
    Nobody uses the flags parameter, so get rid of it.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/rdwr.c
+++ libxfs/rdwr.c
@@ -194,11 +194,10 @@ libxfs_trace_dirtybuf(
 	const char		*func,
 	const char		*file,
 	int			line,
-	struct xfs_buf		*bp,
-	int			flags)
+	struct xfs_buf		*bp)
 {
 	__add_trace(bp, func, file, line);
-	libxfs_buf_mark_dirty(bp, flags);
+	libxfs_buf_mark_dirty(bp);
 }
 
 struct xfs_buf *
@@ -999,8 +998,7 @@ libxfs_writebuf_int(xfs_buf_t *bp, int flags)
  */
 void
 libxfs_buf_mark_dirty(
-	struct xfs_buf	*bp,
-	int		flags)
+	struct xfs_buf	*bp)
 {
 #ifdef IO_DEBUG
 	printf("%lx: %s: dirty blkno=%llu(%llu)\n",
@@ -1014,7 +1012,7 @@ libxfs_buf_mark_dirty(
 	 */
 	bp->b_error = 0;
 	bp->b_flags &= ~LIBXFS_B_STALE;
-	bp->b_flags |= (LIBXFS_B_DIRTY | flags);
+	bp->b_flags |= LIBXFS_B_DIRTY;
 }
 
 void
@@ -1416,7 +1414,7 @@ libxfs_log_clear(
 	libxfs_log_header(ptr, fs_uuid, version, sunit, fmt, lsn, tail_lsn,
 			  next, bp);
 	if (bp) {
-		libxfs_buf_mark_dirty(bp, 0);
+		libxfs_buf_mark_dirty(bp);
 		libxfs_buf_relse(bp);
 	}
 
@@ -1468,7 +1466,7 @@ libxfs_log_clear(
 		libxfs_log_header(ptr, fs_uuid, version, BBTOB(len), fmt, lsn,
 				  tail_lsn, next, bp);
 		if (bp) {
-			libxfs_buf_mark_dirty(bp, 0);
+			libxfs_buf_mark_dirty(bp);
 			libxfs_buf_relse(bp);
 		}
 
