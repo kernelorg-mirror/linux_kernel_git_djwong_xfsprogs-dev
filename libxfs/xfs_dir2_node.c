eeea077fefc13fa107805dd36ecdc4010e2dac50
Darrick J. Wong <darrick.wong@oracle.com>
2020-03-03 13:01:46 -0800

    xfs: fix buffer corruption reporting when xfs_dir3_free_header_check fails
    xfs_verifier_error is supposed to be called on a corrupt metadata buffer
    from within a buffer verifier function, whereas xfs_buf_corruption_error
    is the function to be called when a piece of code has read a buffer and
    catches something that a read verifier cannot.  The first function sets
    b_error anticipating that the low level buffer handling code will see
    the nonzero b_error and clear XBF_DONE on the buffer, whereas the second
    function does not.
    
    Since xfs_dir3_free_header_check examines fields in the dir free block
    header that require more context than can be provided to read verifiers,
    we must call xfs_buf_corruption_error when it finds a problem.
    
    Switching the calls has a secondary effect that we no longer corrupt the
    buffer state by setting b_error and leaving XBF_DONE set.  When /that/
    happens, we'll trip over various state assertions (most commonly the
    b_error check in xfs_buf_reverify) on a subsequent attempt to read the
    buffer.
    
    Fixes: bc1a09b8e334bf5f ("xfs: refactor verifier callers to print address of failing check")
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_dir2_node.c
+++ libxfs/xfs_dir2_node.c
@@ -223,7 +223,7 @@ __xfs_dir3_free_read(
 	/* Check things that we can't do in the verifier. */
 	fa = xfs_dir3_free_header_check(dp, fbno, *bpp);
 	if (fa) {
-		xfs_verifier_error(*bpp, -EFSCORRUPTED, fa);
+		xfs_buf_corruption_error(*bpp, fa);
 		xfs_trans_brelse(tp, *bpp);
 		return -EFSCORRUPTED;
 	}
