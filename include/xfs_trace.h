25bfa370c76a41256a686bd3c1ba771ff19fcd33
Darrick J. Wong <darrick.wong@oracle.com>
2020-03-31 09:46:09 -0700

    xfs: create deferred log items for extent swapping
    Now that we've created the skeleton of a log intent item to track and
    restart extent swap operations, add the upper level logic to commit
    intent items and turn them into concrete work recorded in the log.  We
    use the deferred item "multihop" feature that was introduced a few
    patches ago to constrain the number of active swap operations to one per
    thread.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- include/xfs_trace.h
+++ include/xfs_trace.h
@@ -332,4 +332,10 @@
 #define trace_xfs_imeta_dir_unlinked(...)	((void) 0)
 #define trace_xfs_imeta_dir_zap(...)		((void) 0)
 
+#define trace_xfs_swapext_defer(...)		((void) 0)
+#define trace_xfs_swapext_estimate(...)		((void) 0)
+#define trace_xfs_swapext_extent1(...)		((void) 0)
+#define trace_xfs_swapext_extent2(...)		((void) 0)
+#define trace_xfs_swapext_update_inode_size(...) ((void) 0)
+
 #endif /* __TRACE_H__ */
