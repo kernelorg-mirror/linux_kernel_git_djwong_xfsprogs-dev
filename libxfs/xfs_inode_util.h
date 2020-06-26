54c7b1a2d945dc316acae55430d47ebf49b03964
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:28:11 -0700

    xfs: hoist inode flag conversion functions
    Hoist the inode flag conversion functions into libxfs so that we can
    keep them in sync.  Do this by creating a new xfs_inode_utils.c file in
    libxfs.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

---
+++ libxfs/xfs_inode_util.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2000-2003,2005 Silicon Graphics, Inc.
+ * All Rights Reserved.
+ */
+#ifndef	__XFS_INODE_UTIL_H__
+#define	__XFS_INODE_UTIL_H__
+
+uint16_t	xfs_flags2diflags(struct xfs_inode *ip, unsigned int xflags);
+uint64_t	xfs_flags2diflags2(struct xfs_inode *ip, unsigned int xflags);
+uint32_t	xfs_dic2xflags(uint16_t di_flags, uint64_t di_flags2,
+			       bool has_attr);
+
+#endif /* __XFS_INODE_UTIL_H__ */
