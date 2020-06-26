c5093348196993db9f5a87369d3ece472f40ed64
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:28:08 -0700

    xfs: widen ondisk timestamps to deal with y2038 problem
    Redesign the ondisk timestamps to be a simple unsigned 64-bit counter of
    nanoseconds since 14 Dec 1901 (i.e. the minimum time in the 32-bit unix
    time epoch).  This enables us to handle dates up to 2486, which solves
    the y2038 problem.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- repair/dinode.c
+++ repair/dinode.c
@@ -2159,10 +2159,12 @@ static void
 check_nsec(
 	const char		*name,
 	xfs_ino_t		lino,
+	struct xfs_dinode	*dip,
 	union xfs_timestamp	*t,
 	int			*dirty)
 {
-	if (be32_to_cpu(t->t_nsec) < NSEC_PER_SEC)
+	if ((dip->di_flags2 & be64_to_cpu(XFS_DIFLAG2_BIGTIME)) ||
+	    be32_to_cpu(t->t_nsec) < NSEC_PER_SEC)
 		return;
 
 	do_warn(
@@ -2716,11 +2718,11 @@ _("Bad CoW extent size %u on inode %" PRIu64 ", "),
 	}
 
 	/* nsec fields cannot be larger than 1 billion */
-	check_nsec("atime", lino, &dino->di_atime, dirty);
-	check_nsec("mtime", lino, &dino->di_mtime, dirty);
-	check_nsec("ctime", lino, &dino->di_ctime, dirty);
+	check_nsec("atime", lino, dino, &dino->di_atime, dirty);
+	check_nsec("mtime", lino, dino, &dino->di_mtime, dirty);
+	check_nsec("ctime", lino, dino, &dino->di_ctime, dirty);
 	if (dino->di_version >= 3)
-		check_nsec("crtime", lino, &dino->di_crtime, dirty);
+		check_nsec("crtime", lino, dino, &dino->di_crtime, dirty);
 
 	/*
 	 * general size/consistency checks:
