d462721fb8eb0d6735aece55a0ef2acd045df6bb
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:28:17 -0700

    xfs_db: make fsmap query the realtime reverse mapping tree
    Extend the 'fsmap' debugger command to support querying the realtime
    rmap btree via a new -r argument.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -185,6 +185,7 @@
 #define xfs_rmap_query_range		libxfs_rmap_query_range
 
 #define xfs_rtrmapbt_droot_maxrecs	libxfs_rtrmapbt_droot_maxrecs
+#define xfs_rtrmapbt_init_cursor	libxfs_rtrmapbt_init_cursor
 #define xfs_rtrmapbt_maxrecs		libxfs_rtrmapbt_maxrecs
 
 #define xfs_rtfree_extent		libxfs_rtfree_extent
