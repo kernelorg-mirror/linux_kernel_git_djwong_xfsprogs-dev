b6144f845588d0ff4a2938b9faa0b1a787822976
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:28:10 -0700

    xfs: add error injection to test swapext recovery
    Add an errortag so that we can test recovery of swapext log items.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- io/inject.c
+++ io/inject.c
@@ -55,6 +55,7 @@ error_tag(char *name)
 		{ XFS_ERRTAG_FORCE_SUMMARY_RECALC,	"bad_summary" },
 		{ XFS_ERRTAG_IUNLINK_FALLBACK,		"iunlink_fallback" },
 		{ XFS_ERRTAG_BUF_IOERROR,		"buf_ioerror" },
+		{ XFS_ERRTAG_SWAPEXT_FINISH_ONE,	"swapext_finish_one" },
 		{ XFS_ERRTAG_MAX,			NULL }
 	};
 	int	count;
