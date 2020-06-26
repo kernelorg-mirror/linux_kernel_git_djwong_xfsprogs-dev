d11e5e1e137a0c4c87058e7e9e17beed4eff3b33
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:28:10 -0700

    libfrog: convert xfs_io swapext command to use new libfrog wrapper
    Create an abstraction layer for the two swapext ioctls and port xfs_io
    to use it.  Now we're insulated from the differences between the XFS v0
    ioctl and the new vfs ioctl.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- include/linux.h
+++ include/linux.h
@@ -345,4 +345,83 @@ fsmap_advance(
 #include <asm-generic/mman-common.h>
 #endif /* HAVE_MAP_SYNC */
 
+#ifdef HAVE_FISWAPRANGE
+# include <linux/swaprange.h>
+#else
+/*
+ * Swap part of file1 with part of the file that this ioctl that is being
+ * called against (which we'll call file2).  Filesystems must be able to
+ * complete the operation even if the system goes down.
+ */
+struct file_swap_range {
+	__s64		file1_fd;
+	__s64		file1_offset;	/* file1 offset, bytes */
+	__s64		file2_offset;	/* file2 offset, bytes */
+	__s64		length;		/* bytes to swap */
+
+	__u64		flags;		/* see FILE_SWAP_RANGE_* below */
+
+	/* file2 metadata for optional freshness checks */
+	__s64		file2_ino;	/* inode number */
+	__s64		file2_mtime;	/* modification time */
+	__s64		file2_ctime;	/* change time */
+	__s32		file2_mtime_nsec; /* mod time, nsec */
+	__s32		file2_ctime_nsec; /* change time, nsec */
+
+	__u64		pad[6];		/* must be zeroes */
+};
+
+/*
+ * Atomic swap operations are not required.  This relaxes the requirement that
+ * the filesystem must be able to complete the operation after a crash.
+ */
+#define FILE_SWAP_RANGE_NONATOMIC	(1 << 0)
+
+/*
+ * Check that file2's inode number, mtime, and ctime against the values
+ * provided, and return -EBUSY if there isn't an exact match.
+ */
+#define FILE_SWAP_RANGE_FILE2_FRESH	(1 << 1)
+
+/*
+ * Check that the file1's length is equal to file1_offset + length, and that
+ * file2's length is equal to file2_offset + length.  Returns -EDOM if there
+ * isn't an exact match.
+ */
+#define FILE_SWAP_RANGE_FULL_FILES	(1 << 2)
+
+/*
+ * Swap file data all the way to the ends of both files, and then swap the file
+ * sizes.  This flag can be used to replace a file's contents with a different
+ * amount of data.  length will be ignored.
+ */
+#define FILE_SWAP_RANGE_TO_EOF		(1 << 3)
+
+/*
+ * Flush all changes in file data and file metadata to disk before returning.
+ */
+#define FILE_SWAP_RANGE_FSYNC		(1 << 4)
+
+/*
+ * Commit the contents of file1 into file2 if file2 has the same inode number,
+ * mtime, and ctime as the arguments provided to the call.  The old contents of
+ * file2 will be moved to file1.
+ *
+ * With this flag, all committed information can be retrieved even if the
+ * system crashes or is rebooted.  This includes writing through or flushing a
+ * disk cache if present.  The call blocks until the device reports that the
+ * commit is complete.
+ */
+#define FILE_SWAP_RANGE_COMMIT		(FILE_SWAP_RANGE_FILE2_FRESH | \
+					 FILE_SWAP_RANGE_FSYNC)
+
+#define FILE_SWAP_RANGE_ALL_FLAGS	(FILE_SWAP_RANGE_NONATOMIC | \
+					 FILE_SWAP_RANGE_FILE2_FRESH | \
+					 FILE_SWAP_RANGE_FULL_FILES | \
+					 FILE_SWAP_RANGE_TO_EOF | \
+					 FILE_SWAP_RANGE_FSYNC)
+
+#define FISWAPRANGE	_IOWR('X', 129, struct file_swap_range)
+#endif /* HAVE_FISWAPRANGE */
+
 #endif	/* __XFS_LINUX_H__ */
