2c624ab154e87927503b0cfe6494b4e9bfdc16e6
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:28:16 -0700

    xfs: wire up rmap map and unmap to the realtime rmapbt
    Connect the map and unmap reverse-mapping operations to the realtime
    rmapbt via the deferred operation callbacks.  This enables us to
    perform rmap operations against the correct btree.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/defer_item.c
+++ libxfs/defer_item.c
@@ -150,6 +150,7 @@ xfs_rmap_update_finish_item(
 			rmap->ri_bmap.br_startblock,
 			rmap->ri_bmap.br_blockcount,
 			rmap->ri_bmap.br_state,
+			rmap->ri_realtime,
 			state);
 	kmem_free(rmap);
 	return error;
