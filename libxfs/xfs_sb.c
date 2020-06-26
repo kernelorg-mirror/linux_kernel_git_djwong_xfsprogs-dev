af30a1fda658de0070aa2c7fcd7cf5ec383dd790
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:28:09 -0700

    xfs: create a log incompat flag for atomic extent swapping
    Create a log incompat flag so that we only attempt to process swap
    extent log items if the filesystem supports it.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_sb.c
+++ libxfs/xfs_sb.c
@@ -1145,6 +1145,8 @@ xfs_fs_geometry(
 		geo->flags |= XFS_FSOP_GEOM_FLAGS_REFLINK;
 	if (xfs_sb_version_hasbigtime(sbp))
 		geo->flags |= XFS_FSOP_GEOM_FLAGS_BIGTIME;
+	if (xfs_sb_version_hasatomicswap(sbp))
+		geo->flags |= XFS_FSOP_GEOM_FLAGS_ATOMIC_SWAP;
 	if (xfs_sb_version_hassector(sbp))
 		geo->logsectsize = sbp->sb_logsectsize;
 	else
