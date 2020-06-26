3de0b456124bd1023a86137bdbe7ff3f833770a6
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:28:12 -0700

    xfs: hoist xfs_iunlink to libxfs
    Move xfs_iunlink and xfs_iunlink_remove to libxfs.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/libxfs_priv.h
+++ libxfs/libxfs_priv.h
@@ -463,6 +463,9 @@ void __xfs_buf_mark_corrupt(struct xfs_buf *bp, xfs_failaddr_t fa);
 	__mode = __mode; /* no set-but-unused warning */	\
 })
 
+#define mutex_lock(l)					((void) 0)
+#define mutex_unlock(l)					((void) 0)
+
 /* space allocation */
 #define XFS_EXTENT_BUSY_DISCARDED	0x01	/* undergoing a discard op. */
 #define XFS_EXTENT_BUSY_SKIP_DISCARD	0x02	/* do not discard */
@@ -485,6 +488,8 @@ void __xfs_buf_mark_corrupt(struct xfs_buf *bp, xfs_failaddr_t fa);
 #define xfs_inode_is_filestream(ip)		(0)
 #define xfs_filestream_lookup_ag(ip)		(0)
 #define xfs_filestream_new_ag(ip,ag)		(0)
+
+#define xfs_trans_inode_buf(tp, bp)		((void) 0)
 
 /* quota bits */
 #define xfs_trans_mod_dquot_byino(t,i,f,d)		((void) 0)
