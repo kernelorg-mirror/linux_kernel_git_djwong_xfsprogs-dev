ee616909957422405f5d2a704abb319d9a9e021b
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:28:18 -0700

    mkfs: create the realtime rmap inode
    Create a realtime rmapbt inode if we format the fs with realtime
    and rmap.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -2135,12 +2135,14 @@ _("reflink not supported with realtime devices\n"));
 		}
 		cli->sb_feat.reflink = false;
 
-		if (cli->sb_feat.rmapbt && cli_opt_set(&mopts, M_RMAPBT)) {
-			fprintf(stderr,
-_("rmapbt not supported with realtime devices\n"));
-			usage();
+		if (!cli->sb_feat.metadir && cli->sb_feat.rmapbt) {
+			if (cli_opt_set(&mopts, M_RMAPBT)) {
+				fprintf(stderr,
+_("rmapbt not supported on realtime device without metadir feature\n"));
+				usage();
+			}
+			cli->sb_feat.rmapbt = false;
 		}
-		cli->sb_feat.rmapbt = false;
 	}
 
 	/*
