986324c627dfe17d0dc710e979db75978851ce80
Darrick J. Wong <darrick.wong@oracle.com>
2020-08-18 14:28:51 -0700

    xfs: create a noalloc mode for allocation groups
    Create a new noalloc state for the per-AG structure that will disable
    block allocation in this AG.  We accomplish this by subtracting from
    fdblocks all the free blocks in this AG, hiding those blocks from the
    allocator, and preventing freed blocks from updating fdblocks until
    we're ready to lift noalloc mode.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_ag_resv.c
+++ libxfs/xfs_ag_resv.c
@@ -74,6 +74,13 @@ xfs_ag_resv_critical(
 	xfs_filblks_t			avail;
 	xfs_filblks_t			orig;
 
+	/*
+	 * Pretend we're critically low on reservations in this AG to scare
+	 * everyone else away.
+	 */
+	if (pag->pagf_noalloc)
+		return true;
+
 	switch (type) {
 	case XFS_AG_RESV_RTMETADATA:
 		avail = percpu_counter_sum(&pag->pag_mount->m_fdblocks);
@@ -120,7 +127,12 @@ xfs_ag_resv_needed(
 		len -= xfs_perag_resv(pag, type)->ar_reserved;
 		break;
 	case XFS_AG_RESV_NONE:
-		/* empty */
+		/*
+		 * In noalloc mode, we pretend that all the free blocks in this
+		 * AG have been allocated.  Make this AG look full.
+		 */
+		if (pag->pagf_noalloc)
+			len += xfs_ag_fdblocks(pag);
 		break;
 	default:
 		ASSERT(0);
@@ -415,6 +427,8 @@ xfs_ag_resv_alloc_extent(
 	xfs_filblks_t			len;
 	uint				field;
 
+	ASSERT(type != XFS_AG_RESV_NONE || !pag->pagf_noalloc);
+
 	trace_xfs_ag_resv_alloc_extent(pag, type, args->len);
 
 	switch (type) {
@@ -472,7 +486,13 @@ xfs_ag_resv_free_extent(
 		ASSERT(0);
 		/* fall through */
 	case XFS_AG_RESV_NONE:
-		xfs_trans_mod_sb(tp, XFS_TRANS_SB_FDBLOCKS, (int64_t)len);
+		/*
+		 * If the AG is in noalloc mode, we don't give back to fdblocks
+		 * until we lift noalloc mode.
+		 */
+		if (!pag->pagf_noalloc)
+			xfs_trans_mod_sb(tp, XFS_TRANS_SB_FDBLOCKS,
+					(int64_t)len);
 		/* fall through */
 	case XFS_AG_RESV_IGNORE:
 		return;
@@ -485,6 +505,6 @@ xfs_ag_resv_free_extent(
 	/* Freeing into the reserved pool only requires on-disk update... */
 	xfs_trans_mod_sb(tp, XFS_TRANS_SB_RES_FDBLOCKS, len);
 	/* ...but freeing beyond that requires in-core and on-disk update. */
-	if (len > leftover)
+	if (len > leftover && !pag->pagf_noalloc)
 		xfs_trans_mod_sb(tp, XFS_TRANS_SB_FDBLOCKS, len - leftover);
 }
