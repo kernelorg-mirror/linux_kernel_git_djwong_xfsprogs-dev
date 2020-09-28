d619d84e22af9acd23b9055ccb8333dd53df3ee2
Darrick J. Wong <darrick.wong@oracle.com>
2020-09-28 15:32:20 -0700

    xfs: create routine to allocate and initialize a realtime refcount btree inode
    Create a library routine to allocate and initialize an empty realtime
    refcountbt inode.  We'll use this for growfs, mkfs, and repair.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_rtrefcount_btree.h
+++ libxfs/xfs_rtrefcount_btree.h
@@ -176,4 +176,9 @@ void xfs_rtrefcountbt_to_disk(struct xfs_mount *mp,
 		struct xfs_rtrefcount_root *dblock, int dblocklen);
 void xfs_iflush_rtrefcount(struct xfs_inode *ip, struct xfs_dinode *dip);
 
+struct xfs_imeta_end;
+
+int xfs_rtrefcountbt_create(struct xfs_trans **tpp, struct xfs_imeta_end *ic,
+		struct xfs_inode **ipp);
+
 #endif	/* __XFS_RTREFCOUNT_BTREE_H__ */
