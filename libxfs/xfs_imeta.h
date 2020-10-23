c3bb531a0189eda2179a28c49bcbd834cb232bf2
Darrick J. Wong <darrick.wong@oracle.com>
2020-10-22 17:03:33 -0700

    xfs: add realtime reverse map inode to metadata directory
    Add a metadir path to select the realtime refcount btree inode and load
    it at mount time.  The rtrefcountbt inode will have a unique extent format
    code, which means that we also have to update the inode validation and
    flush routines to look for it.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_imeta.h
+++ libxfs/xfs_imeta.h
@@ -38,6 +38,7 @@ struct xfs_imeta_end {
 extern const struct xfs_imeta_path XFS_IMETA_RTBITMAP;
 extern const struct xfs_imeta_path XFS_IMETA_RTSUMMARY;
 extern const struct xfs_imeta_path XFS_IMETA_RTRMAPBT;
+extern const struct xfs_imeta_path XFS_IMETA_RTREFCOUNTBT;
 extern const struct xfs_imeta_path XFS_IMETA_USRQUOTA;
 extern const struct xfs_imeta_path XFS_IMETA_GRPQUOTA;
 extern const struct xfs_imeta_path XFS_IMETA_PRJQUOTA;
