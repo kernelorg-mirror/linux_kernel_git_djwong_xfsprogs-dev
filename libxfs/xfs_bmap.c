3a8c8f97e3f0be70b7d6ea504d0f6d4b091727ef
Darrick J. Wong <darrick.wong@oracle.com>
2020-10-22 17:03:33 -0700

    xfs: add a realtime flag to the refcount update log redo items
    Extend the refcount update (CUI) log items with a new realtime flag that
    indicates that the updates apply against the realtime refcountbt.  We'll
    wire up the actual refcount code later.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_bmap.c
+++ libxfs/xfs_bmap.c
@@ -4506,7 +4506,8 @@ xfs_bmapi_write(
 			 */
 			if (whichfork == XFS_COW_FORK)
 				xfs_refcount_alloc_cow_extent(tp, bma.blkno,
-						bma.length);
+						bma.length,
+						XFS_IS_REALTIME_INODE(ip));
 		}
 
 		/* Deal with the allocated space we found.  */
@@ -4661,7 +4662,8 @@ xfs_bmapi_convert_delalloc(
 	*seq = READ_ONCE(ifp->if_seq);
 
 	if (whichfork == XFS_COW_FORK)
-		xfs_refcount_alloc_cow_extent(tp, bma.blkno, bma.length);
+		xfs_refcount_alloc_cow_extent(tp, bma.blkno, bma.length,
+				XFS_IS_REALTIME_INODE(ip));
 
 	error = xfs_bmap_btree_to_extents(tp, ip, bma.cur, &bma.logflags,
 			whichfork);
@@ -5299,7 +5301,7 @@ xfs_bmap_del_extent_real(
 	 */
 	if (want_free && !(bflags & XFS_BMAPI_REMAP)) {
 		if (xfs_is_reflink_inode(ip) && whichfork == XFS_DATA_FORK) {
-			xfs_refcount_decrease_extent(tp, del);
+			xfs_refcount_decrease_extent(tp, del, isrt);
 		} else {
 			__xfs_bmap_add_free(tp, del->br_startblock,
 					del->br_blockcount, NULL, isrt,
