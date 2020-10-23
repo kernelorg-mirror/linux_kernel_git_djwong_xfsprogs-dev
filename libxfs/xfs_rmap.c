f40dc0ff46b0b5d422376eba1f9ea33c93ff962e
Darrick J. Wong <darrick.wong@oracle.com>
2020-10-22 17:03:29 -0700

    xfs: add a realtime flag to the rmap update log redo items
    Extend the rmap update (RUI) log items with a new realtime flag that
    indicates that the updates apply against the realtime rmapbt.  We'll
    wire up the actual rmap code later.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_rmap.c
+++ libxfs/xfs_rmap.c
@@ -2594,18 +2594,25 @@ __xfs_rmap_add(
 	enum xfs_rmap_intent_type	type,
 	uint64_t			owner,
 	int				whichfork,
-	struct xfs_bmbt_irec		*bmap)
+	struct xfs_bmbt_irec		*bmap,
+	bool				realtime)
 {
 	struct xfs_rmap_intent		*ri;
 
-	trace_xfs_rmap_defer(tp->t_mountp,
-			XFS_FSB_TO_AGNO(tp->t_mountp, bmap->br_startblock),
-			type,
-			XFS_FSB_TO_AGBNO(tp->t_mountp, bmap->br_startblock),
-			owner, whichfork,
-			bmap->br_startoff,
-			bmap->br_blockcount,
+	if (realtime)
+		trace_xfs_rmap_defer(tp->t_mountp, NULLAGNUMBER, type,
+				bmap->br_startblock, owner, whichfork,
+				bmap->br_startoff, bmap->br_blockcount,
 			bmap->br_state);
+	else
+		trace_xfs_rmap_defer(tp->t_mountp,
+				XFS_FSB_TO_AGNO(tp->t_mountp,
+						bmap->br_startblock),
+				type,
+				XFS_FSB_TO_AGBNO(tp->t_mountp,
+						bmap->br_startblock),
+				owner, whichfork, bmap->br_startoff,
+				bmap->br_blockcount, bmap->br_state);
 
 	ri = kmem_alloc(sizeof(struct xfs_rmap_intent), KM_NOFS);
 	INIT_LIST_HEAD(&ri->ri_list);
@@ -2613,6 +2620,7 @@ __xfs_rmap_add(
 	ri->ri_owner = owner;
 	ri->ri_whichfork = whichfork;
 	ri->ri_bmap = *bmap;
+	ri->ri_realtime = realtime;
 
 	xfs_defer_add(tp, XFS_DEFER_OPS_TYPE_RMAP, &ri->ri_list);
 }
@@ -2626,14 +2634,18 @@ xfs_rmap_map_extent(
 	struct xfs_bmbt_irec	*PREV)
 {
 	enum xfs_rmap_intent_type type = XFS_RMAP_MAP;
+	bool			isrt = false;
 
 	if (!xfs_rmap_update_is_needed(tp->t_mountp, whichfork))
 		return;
 
-	if (whichfork != XFS_ATTR_FORK && xfs_is_reflink_inode(ip))
-		type = XFS_RMAP_MAP_SHARED;
+	if (whichfork != XFS_ATTR_FORK) {
+		if (xfs_is_reflink_inode(ip))
+			type = XFS_RMAP_MAP_SHARED;
+		isrt = XFS_IS_REALTIME_INODE(ip);
+	}
 
-	__xfs_rmap_add(tp, type, ip->i_ino, whichfork, PREV);
+	__xfs_rmap_add(tp, type, ip->i_ino, whichfork, PREV, isrt);
 }
 
 /* Unmap an extent out of a file. */
@@ -2645,14 +2657,18 @@ xfs_rmap_unmap_extent(
 	struct xfs_bmbt_irec	*PREV)
 {
 	enum xfs_rmap_intent_type type = XFS_RMAP_UNMAP;
+	bool			isrt = false;
 
 	if (!xfs_rmap_update_is_needed(tp->t_mountp, whichfork))
 		return;
 
-	if (whichfork != XFS_ATTR_FORK && xfs_is_reflink_inode(ip))
-		type = XFS_RMAP_UNMAP_SHARED;
+	if (whichfork != XFS_ATTR_FORK) {
+		if (xfs_is_reflink_inode(ip))
+			type = XFS_RMAP_UNMAP_SHARED;
+		isrt = XFS_IS_REALTIME_INODE(ip);
+	}
 
-	__xfs_rmap_add(tp, type, ip->i_ino, whichfork, PREV);
+	__xfs_rmap_add(tp, type, ip->i_ino, whichfork, PREV, isrt);
 }
 
 /*
@@ -2670,14 +2686,18 @@ xfs_rmap_convert_extent(
 	struct xfs_bmbt_irec	*PREV)
 {
 	enum xfs_rmap_intent_type type = XFS_RMAP_CONVERT;
+	bool			isrt = false;
 
 	if (!xfs_rmap_update_is_needed(mp, whichfork))
 		return;
 
-	if (whichfork != XFS_ATTR_FORK && xfs_is_reflink_inode(ip))
-		type = XFS_RMAP_CONVERT_SHARED;
+	if (whichfork != XFS_ATTR_FORK) {
+		if (xfs_is_reflink_inode(ip))
+			type = XFS_RMAP_CONVERT_SHARED;
+		isrt = XFS_IS_REALTIME_INODE(ip);
+	}
 
-	__xfs_rmap_add(tp, type, ip->i_ino, whichfork, PREV);
+	__xfs_rmap_add(tp, type, ip->i_ino, whichfork, PREV, isrt);
 }
 
 /* Schedule the creation of an rmap for non-file data. */
@@ -2686,7 +2706,8 @@ xfs_rmap_alloc_extent(
 	struct xfs_trans	*tp,
 	xfs_fsblock_t		fsbno,
 	xfs_filblks_t		len,
-	uint64_t		owner)
+	uint64_t		owner,
+	bool			isrt)
 {
 	struct xfs_bmbt_irec	bmap;
 
@@ -2698,7 +2719,7 @@ xfs_rmap_alloc_extent(
 	bmap.br_startoff = 0;
 	bmap.br_state = XFS_EXT_NORM;
 
-	__xfs_rmap_add(tp, XFS_RMAP_ALLOC, owner, XFS_DATA_FORK, &bmap);
+	__xfs_rmap_add(tp, XFS_RMAP_ALLOC, owner, XFS_DATA_FORK, &bmap, isrt);
 }
 
 /* Schedule the deletion of an rmap for non-file data. */
@@ -2707,7 +2728,8 @@ xfs_rmap_free_extent(
 	struct xfs_trans	*tp,
 	xfs_fsblock_t		fsbno,
 	xfs_filblks_t		len,
-	uint64_t		owner)
+	uint64_t		owner,
+	bool			isrt)
 {
 	struct xfs_bmbt_irec	bmap;
 
@@ -2719,7 +2741,7 @@ xfs_rmap_free_extent(
 	bmap.br_startoff = 0;
 	bmap.br_state = XFS_EXT_NORM;
 
-	__xfs_rmap_add(tp, XFS_RMAP_FREE, owner, XFS_DATA_FORK, &bmap);
+	__xfs_rmap_add(tp, XFS_RMAP_FREE, owner, XFS_DATA_FORK, &bmap, isrt);
 }
 
 /* Compare rmap records.  Returns -1 if a < b, 1 if a > b, and 0 if equal. */
