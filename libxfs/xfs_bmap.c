8fec055760e464057bb4124eb702d69d56b77108
Darrick J. Wong <darrick.wong@oracle.com>
2020-10-22 17:03:28 -0700

    xfs: support logging EFIs for realtime extents
    Teach the EFI mechanism how to free realtime extents.  We do this very
    sneakily, by using the upper bit of the length field in the log format
    (and a boolean flag incore) to convey the realtime status.  We're going
    to need this to enforce proper ordering of operations when we enable
    realtime rmap.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_bmap.c
+++ libxfs/xfs_bmap.c
@@ -531,6 +531,7 @@ __xfs_bmap_add_free(
 	xfs_fsblock_t			bno,
 	xfs_filblks_t			len,
 	const struct xfs_owner_info	*oinfo,
+	bool				realtime,
 	bool				skip_discard)
 {
 	struct xfs_extent_free_item	*new;		/* new element */
@@ -543,27 +544,41 @@ __xfs_bmap_add_free(
 	ASSERT(len > 0);
 	ASSERT(len <= MAXEXTLEN);
 	ASSERT(!isnullstartblock(bno));
-	agno = XFS_FSB_TO_AGNO(mp, bno);
-	agbno = XFS_FSB_TO_AGBNO(mp, bno);
-	ASSERT(agno < mp->m_sb.sb_agcount);
-	ASSERT(agbno < mp->m_sb.sb_agblocks);
-	ASSERT(len < mp->m_sb.sb_agblocks);
-	ASSERT(agbno + len <= mp->m_sb.sb_agblocks);
+	if (realtime) {
+		agno = 0;
+		agbno = bno;
+		ASSERT(bno < mp->m_sb.sb_rblocks);
+		ASSERT(len <= mp->m_sb.sb_rblocks);
+		ASSERT(bno + len <= mp->m_sb.sb_rblocks);
+	} else {
+		agno = XFS_FSB_TO_AGNO(mp, bno);
+		agbno = XFS_FSB_TO_AGBNO(mp, bno);
+		ASSERT(agno < mp->m_sb.sb_agcount);
+		ASSERT(agbno < mp->m_sb.sb_agblocks);
+		ASSERT(len < mp->m_sb.sb_agblocks);
+		ASSERT(agbno + len <= mp->m_sb.sb_agblocks);
+	}
 #endif
 	ASSERT(xfs_bmap_free_item_zone != NULL);
+
+	if (realtime)
+		trace_xfs_bmap_free_defer(tp->t_mountp, NULLAGNUMBER, 0,
+				bno, len);
+	else
+		trace_xfs_bmap_free_defer(tp->t_mountp,
+				XFS_FSB_TO_AGNO(tp->t_mountp, bno), 0,
+				XFS_FSB_TO_AGBNO(tp->t_mountp, bno), len);
 
 	new = kmem_cache_alloc(xfs_bmap_free_item_zone,
 			       GFP_KERNEL | __GFP_NOFAIL);
 	new->xefi_startblock = bno;
 	new->xefi_blockcount = (xfs_extlen_t)len;
+	new->xefi_realtime = realtime;
 	if (oinfo)
 		new->xefi_oinfo = *oinfo;
 	else
 		new->xefi_oinfo = XFS_RMAP_OINFO_SKIP_UPDATE;
 	new->xefi_skip_discard = skip_discard;
-	trace_xfs_bmap_free_defer(tp->t_mountp,
-			XFS_FSB_TO_AGNO(tp->t_mountp, bno), 0,
-			XFS_FSB_TO_AGBNO(tp->t_mountp, bno), len);
 	xfs_defer_add(tp, XFS_DEFER_OPS_TYPE_FREE, &new->xefi_list);
 }
 
@@ -626,7 +641,7 @@ xfs_bmap_btree_to_extents(
 	if ((error = xfs_btree_check_block(cur, cblock, 0, cbp)))
 		return error;
 	xfs_rmap_ino_bmbt_owner(&oinfo, ip->i_ino, whichfork);
-	xfs_bmap_add_free(cur->bc_tp, cbno, 1, &oinfo);
+	xfs_bmap_add_free(cur->bc_tp, cbno, 1, &oinfo, false);
 	ip->i_d.di_nblocks--;
 	xfs_trans_mod_dquot_byino(tp, ip, XFS_TRANS_DQ_BCOUNT, -1L);
 	xfs_trans_binval(tp, cbp);
@@ -5279,7 +5294,7 @@ xfs_bmap_del_extent_real(
 			xfs_refcount_decrease_extent(tp, del);
 		} else {
 			__xfs_bmap_add_free(tp, del->br_startblock,
-					del->br_blockcount, NULL,
+					del->br_blockcount, NULL, false,
 					(bflags & XFS_BMAPI_NODISCARD) ||
 					del->br_state == XFS_EXT_UNWRITTEN);
 		}
