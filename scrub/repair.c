15b0c7d3c1d858f9a780d93454007066c5670a8d
Darrick J. Wong <darrick.wong@oracle.com>
2020-11-24 11:34:17 -0800

    xfs_scrub: remove unused action_list fields
    Remove some fields since we don't need them anymore.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- scrub/repair.c
+++ scrub/repair.c
@@ -220,7 +220,6 @@ action_list_discard(
 	struct action_item		*n;
 
 	list_for_each_entry_safe(aitem, n, &alist->list, list) {
-		alist->nr--;
 		list_del(&aitem->list);
 		free(aitem);
 	}
@@ -241,8 +240,6 @@ action_list_init(
 	struct action_list		*alist)
 {
 	INIT_LIST_HEAD(&alist->list);
-	alist->nr = 0;
-	alist->sorted = false;
 }
 
 /* Number of pending repairs in this list. */
@@ -271,8 +268,6 @@ action_list_add(
 	struct action_item		*aitem)
 {
 	list_add_tail(&aitem->list, &alist->list);
-	alist->nr++;
-	alist->sorted = false;
 }
 
 /* Repair everything on this list. */
