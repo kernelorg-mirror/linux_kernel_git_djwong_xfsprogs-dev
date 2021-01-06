257e1e172d76240f40c79e5b05a68f3c2b333ed8
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:59:52 -0800

    libxfs: pack inode allocation parameters into a separate structure
    Instead of open-coding new inode parameters through xfs_dir_ialloc and
    xfs_ialloc, move all the code that sets inode fields from a struct
    fsxattr into a separate helper function and call it after the inode has
    been allocated and initialized.  This will make it easier to share code
    with xfsprogs while maintaining the ability for xfsprogs to supply extra
    new inode parameters.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_inode_util.h
+++ libxfs/xfs_inode_util.h
@@ -13,4 +13,30 @@ uint32_t	xfs_dic2xflags(uint16_t di_flags, uint64_t di_flags2,
 
 prid_t		xfs_get_initial_prid(struct xfs_inode *dp);
 
+/*
+ * Initial ids, link count, device number, and mode of a new inode.
+ *
+ * Due to our only partial reliance on the VFS to propagate uid and gid values
+ * according to accepted Unix behaviors, callers must initialize uid to
+ * current_fsuid() and gid to current_fsgid() to get the standard inheritance
+ * behaviors when XFS_MOUNT_GRPID is set.  To override the default ids, use the
+ * FORCE flags defined below.
+ */
+struct xfs_ialloc_args {
+	struct xfs_inode	*pip;	/* parent inode or null */
+
+	kuid_t			uid;
+	kgid_t			gid;
+	prid_t			prid;
+
+	xfs_nlink_t		nlink;
+	dev_t			rdev;
+
+	umode_t			mode;
+
+#define XFS_IALLOC_ARGS_FORCE_UID	(1 << 0)
+#define XFS_IALLOC_ARGS_FORCE_GID	(1 << 1)
+	uint16_t		flags;
+};
+
 #endif /* __XFS_INODE_UTIL_H__ */
