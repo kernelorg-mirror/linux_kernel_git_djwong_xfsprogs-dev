0a3fc579847683f7bb519a354faf99f96636b926
Darrick J. Wong <djwong@kernel.org>
2021-01-05 18:00:12 -0800

    mkfs: enable inobtcount and bigtime by default
    Enable the inode btree counters and large timestamp features by default.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -3941,11 +3941,11 @@ main(
 			.spinodes = true,
 			.rmapbt = true,
 			.reflink = true,
-			.inobtcnt = false,
+			.inobtcnt = true,
 			.parent_pointers = false,
 			.nodalign = false,
 			.nortalign = false,
-			.bigtime = false,
+			.bigtime = true,
 		},
 	};
 
