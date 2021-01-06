7052cfd772c84ba840431b9637f5ad03f77cac7d
Darrick J. Wong <djwong@kernel.org>
2021-01-05 18:00:02 -0800

    xfs: support leaves in the incore btree root block in xfs_iroot_realloc
    Add some logic to xfs_iroot_realloc so that we can handle leaf records
    in the btree root block correctly.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_bmap_btree.c
+++ libxfs/xfs_bmap_btree.c
@@ -518,6 +518,7 @@ xfs_bmbt_broot_move(
 	size_t			dst_bytes,
 	struct xfs_btree_block	*src_broot,
 	size_t			src_bytes,
+	unsigned int		level,
 	unsigned int		numrecs)
 {
 	struct xfs_mount	*mp = ip->i_mount;
@@ -525,6 +526,7 @@ xfs_bmbt_broot_move(
 	void			*sptr;
 
 	ASSERT(xfs_bmap_bmdr_space(src_broot) <= XFS_IFORK_SIZE(ip, whichfork));
+	ASSERT(level > 0);
 
 	/*
 	 * We always have to move the pointers because they are not butted
@@ -794,7 +796,7 @@ xfs_bmbt_iroot_alloc(
 	struct xfs_ifork	*ifp = XFS_IFORK_PTR(ip, whichfork);
 
 	xfs_iroot_alloc(ip, whichfork,
-			xfs_bmap_broot_space_calc(ip->i_mount, 1));
+			xfs_bmap_broot_space_calc(ip->i_mount, 1, 1));
 
 	/* Fill in the root. */
 	xfs_btree_init_block_int(ip->i_mount, ifp->if_broot,
