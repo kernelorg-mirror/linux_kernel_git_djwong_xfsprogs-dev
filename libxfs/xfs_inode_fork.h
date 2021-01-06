76f91f1fdfe9b43755157373f26711b8bf3a76ae
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:59:48 -0800

    xfs: create a helper to decide if a file mapping targets the rt volume
    Create a helper so that we can stop open-coding this decision
    everywhere.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_inode_fork.h
+++ libxfs/xfs_inode_fork.h
@@ -238,5 +238,6 @@ int xfs_ifork_verify_local_data(struct xfs_inode *ip);
 int xfs_ifork_verify_local_attr(struct xfs_inode *ip);
 int xfs_iext_count_may_overflow(struct xfs_inode *ip, int whichfork,
 		int nr_to_add);
+bool xfs_ifork_is_realtime(struct xfs_inode *ip, int whichfork);
 
 #endif	/* __XFS_INODE_FORK_H__ */
