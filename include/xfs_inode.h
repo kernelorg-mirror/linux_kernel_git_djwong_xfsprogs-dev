cc4af8fac249147f521caf34fc63f2e8b22a03e4
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:59:57 -0800

    xfs: read and write metadata inode directory
    Plumb in the bits we need to look up metadata inode numbers from the
    metadata inode directory and save them back.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/xfs_inode.h
+++ include/xfs_inode.h
@@ -42,6 +42,11 @@ static inline kgid_t current_fsgid(void)
 	return make_kgid(0);
 }
 
+#define KUIDT_INIT(value) (kuid_t){ value }
+#define KGIDT_INIT(value) (kgid_t){ value }
+#define GLOBAL_ROOT_UID KUIDT_INIT(0)
+#define GLOBAL_ROOT_GID KGIDT_INIT(0)
+
 /* These match kernel side includes */
 #include "xfs_inode_buf.h"
 #include "xfs_inode_fork.h"
