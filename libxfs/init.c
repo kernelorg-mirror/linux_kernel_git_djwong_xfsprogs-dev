ce3c30b2a02be681a890b8f272cdf7ffcd2630d5
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:59:42 -0800

    xfs_repair: clear the needsrepair flag
    Clear the needsrepair flag, since it's used to prevent mounting of an
    inconsistent filesystem.  We only do this if we make it to the end of
    repair with a non-zero error code, and all the rebuilt indices and
    corrected metadata are persisted correctly.
    
    Note that we cannot combine clearing needsrepair with clearing the quota
    checked flags because we need to clear the quota flags even if
    reformatting the log fails, whereas we can't clear needsrepair if the
    log reformat fails.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/init.c
+++ libxfs/init.c
@@ -867,24 +867,16 @@ _("%s: Flushing the %s failed, err=%d!\n"),
 }
 
 /*
- * Flush all dirty buffers to stable storage and report on writes that didn't
- * make it to stable storage.
+ * Persist all disk write caches and report on writes that didn't make it to
+ * stable storage.  Callers should flush (or purge) the libxfs buffer caches
+ * before calling this function.
  */
-static int
+int
 libxfs_flush_mount(
 	struct xfs_mount	*mp)
 {
 	int			error = 0;
 	int			err2;
-
-	/*
-	 * Purge the buffer cache to write all dirty buffers to disk and free
-	 * all incore buffers.  Buffers that fail write verification will cause
-	 * the CORRUPT_WRITE flag to be set in the buftarg.  Buffers that
-	 * cannot be written will cause the LOST_WRITE flag to be set in the
-	 * buftarg.
-	 */
-	libxfs_bcache_purge();
 
 	/* Flush all kernel and disk write caches, and report failures. */
 	if (mp->m_ddev_targp) {
@@ -923,6 +915,15 @@ libxfs_umount(
 
 	libxfs_rtmount_destroy(mp);
 
+	/*
+	 * Purge the buffer cache to write all dirty buffers to disk and free
+	 * all incore buffers.  Buffers that fail write verification will cause
+	 * the CORRUPT_WRITE flag to be set in the buftarg.  Buffers that
+	 * cannot be written will cause the LOST_WRITE flag to be set in the
+	 * buftarg.  Once that's done, instruct the disks to persist their
+	 * write caches.
+	 */
+	libxfs_bcache_purge();
 	error = libxfs_flush_mount(mp);
 
 	for (agno = 0; agno < mp->m_maxagi; agno++) {
