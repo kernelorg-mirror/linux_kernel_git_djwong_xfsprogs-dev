223a9113f83f8650c85a50d45a6a3d711e96f494
Darrick J. Wong <djwong@kernel.org>
2021-01-05 18:00:05 -0800

    xfs: support logging EFIs for realtime extents
    Teach the EFI mechanism how to free realtime extents.  We do this very
    sneakily, by using the upper bit of the length field in the log format
    (and a boolean flag incore) to convey the realtime status.  We're going
    to need this to enforce proper ordering of operations when we enable
    realtime rmap.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_refcount.c
+++ libxfs/xfs_refcount.c
@@ -1000,7 +1000,8 @@ xfs_refcount_adjust_extents(
 						cur->bc_ag.agno,
 						tmp.rc_startblock);
 				xfs_bmap_add_free(cur->bc_tp, fsbno,
-						  tmp.rc_blockcount, oinfo);
+						  tmp.rc_blockcount, oinfo,
+						  false);
 			}
 
 			(*agbno) += tmp.rc_blockcount;
@@ -1045,7 +1046,7 @@ xfs_refcount_adjust_extents(
 					cur->bc_ag.agno,
 					ext.rc_startblock);
 			xfs_bmap_add_free(cur->bc_tp, fsbno, ext.rc_blockcount,
-					  oinfo);
+					  oinfo, false);
 		}
 
 skip:
@@ -1775,7 +1776,8 @@ xfs_refcount_recover_cow_leftovers(
 				rr->rr_rrec.rc_blockcount);
 
 		/* Free the block. */
-		xfs_bmap_add_free(tp, fsb, rr->rr_rrec.rc_blockcount, NULL);
+		xfs_bmap_add_free(tp, fsb, rr->rr_rrec.rc_blockcount, NULL,
+				false);
 
 		error = xfs_trans_commit(tp);
 		if (error)
