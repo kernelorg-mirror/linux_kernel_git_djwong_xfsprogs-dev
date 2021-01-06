be61500b857144689b767ef921078e9a421940f8
Darrick J. Wong <djwong@kernel.org>
2021-01-05 18:02:31 -0800

    xfs: add realtime reverse map inode to metadata directory
    Add a metadir path to select the realtime refcount btree inode and load
    it at mount time.  The rtrefcountbt inode will have a unique extent format
    code, which means that we also have to update the inode validation and
    flush routines to look for it.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_format.h
+++ libxfs/xfs_format.h
@@ -656,6 +656,12 @@ static inline bool xfs_sb_version_hasrtrmapbt(struct xfs_sb *sbp)
 	       xfs_sb_version_hasrmapbt(sbp);
 }
 
+static inline bool xfs_sb_version_hasrtreflink(struct xfs_sb *sbp)
+{
+	return xfs_sb_version_hasmetadir(sbp) && sbp->sb_rblocks > 0 &&
+	       xfs_sb_version_hasreflink(sbp);
+}
+
 /*
  * end of superblock version macros
  */
@@ -1108,6 +1114,7 @@ enum xfs_dinode_fmt {
 	XFS_DINODE_FMT_BTREE,		/* struct xfs_bmdr_block */
 	XFS_DINODE_FMT_UUID,		/* added long ago, but never used */
 	XFS_DINODE_FMT_RMAP,		/* reverse mapping btree */
+	XFS_DINODE_FMT_REFCOUNT,	/* reference count btree */
 };
 
 #define XFS_INODE_FORMAT_STR \
