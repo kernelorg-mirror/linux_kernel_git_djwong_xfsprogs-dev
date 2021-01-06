36992787316fda8f6445604901a564c39a24de4b
Darrick J. Wong <djwong@kernel.org>
2021-01-05 18:00:12 -0800

    xfs: apply noalloc mode to inode allocations too
    Don't allow inode allocations from this group if it's marked noalloc.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_ialloc.c
+++ libxfs/xfs_ialloc.c
@@ -991,7 +991,7 @@ xfs_ialloc_ag_select(
 	flags = XFS_ALLOC_FLAG_TRYLOCK;
 	for (;;) {
 		pag = xfs_perag_get(mp, agno);
-		if (!pag->pagi_inodeok) {
+		if (!pag->pagi_inodeok || pag->pagf_noalloc) {
 			xfs_ialloc_next_ag(mp);
 			goto nextag;
 		}
@@ -1168,6 +1168,7 @@ xfs_dialloc_ag_inobt(
 
 	ASSERT(pag->pagi_init);
 	ASSERT(pag->pagi_inodeok);
+	ASSERT(!pag->pagf_noalloc);
 	ASSERT(pag->pagi_freecount > 0);
 
  restart_pagno:
@@ -1823,7 +1824,7 @@ xfs_dialloc_select_ag(
 	agno = start_agno;
 	for (;;) {
 		pag = xfs_perag_get(mp, agno);
-		if (!pag->pagi_inodeok) {
+		if (!pag->pagi_inodeok || pag->pagf_noalloc) {
 			xfs_ialloc_next_ag(mp);
 			goto nextag;
 		}
