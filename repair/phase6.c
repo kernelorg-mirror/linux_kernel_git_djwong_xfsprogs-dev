28194cd0f5663fe6f67a79c62f8782e9d4eb741e
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:59:53 -0800

    xfs: hoist inode allocation function
    Move the inode allocation function into libxfs.  Now userspace and
    kernel have the exact same inode initialization behaviors.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/phase6.c
+++ repair/phase6.c
@@ -819,7 +819,8 @@ mk_root_dir(xfs_mount_t *mp)
 	}
 
 	/*
-	 * take care of the core -- initialization from xfs_ialloc()
+	 * take care of the core since we didn't call the libxfs ialloc function
+	 * (comment changed to avoid tangling xfs/437)
 	 */
 	memset(&ip->i_d, 0, sizeof(ip->i_d));
 
@@ -874,6 +875,8 @@ mk_orphanage(
 	struct xfs_ialloc_args	args = {
 		.mode		= S_IFDIR | 0755,
 		.nlink		= 2,
+		.flags		= XFS_IALLOC_ARGS_FORCE_UID |
+				  XFS_IALLOC_ARGS_FORCE_GID,
 	};
 	struct xfs_trans	*tp;
 	struct xfs_inode	*ip;
