3d761e23ad9a46c9b6504a5c5e3d72f254b5b770
Darrick J. Wong <djwong@kernel.org>
2021-01-28 13:40:15 -0800

    xfs: reserve data and rt quota at the same time
    Source kernel commit: 3fc46cbbdf2cd97be9569f92a85b5ce4fbf6e515
    
    Modify xfs_trans_reserve_quota_nblks so that we can reserve data and
    realtime blocks from the dquot at the same time.  This change has the
    theoretical side effect that for allocations to realtime files we will
    reserve from the dquot both the number of rtblocks being allocated and
    the number of bmbt blocks that might be needed to add the mapping.
    However, since the mount code disables quota if it finds a realtime
    device, this should not result in any behavior changes.
    
    Now that we've moved the inode creation callers away from using the
    _nblks function, we can repurpose the (now unused) ninos argument for
    realtime blocks, so make that change.  This also replaces the flags
    argument with a boolean parameter to force the reservation since we
    don't need to distinguish between data and rt quota reservations any
    more, and the only flag being passed in was FORCE_RES.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_priv.h
+++ libxfs/libxfs_priv.h
@@ -495,8 +495,7 @@ void __xfs_buf_mark_corrupt(struct xfs_buf *bp, xfs_failaddr_t fa);
 /* quota bits */
 static inline int retzero(void) { return 0; }
 #define xfs_trans_mod_dquot_byino(t,i,f,d)		((void) 0)
-#define xfs_trans_reserve_quota_nblks(t,i,b,n,f)	(0)
-#define xfs_trans_unreserve_quota_nblks(t,i,b,n,f)	retzero()
+#define xfs_trans_reserve_quota_nblks(t,i,d,r,f)	(0)
 #define xfs_qm_dqattach(i)				(0)
 
 #define xfs_quota_reserve_blkres(ip, alen)		(0)
