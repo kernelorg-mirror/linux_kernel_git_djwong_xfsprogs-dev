b44682a7f5216becb78eb647bb690b2a301790cb
Darrick J. Wong <djwong@kernel.org>
2021-02-16 10:53:50 -0800

    libfrog: support vectored scrub
    Enhance libfrog to support performing vectored metadata scrub.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libfrog/scrub.h
+++ libfrog/scrub.h
@@ -27,5 +27,6 @@ extern const struct xfrog_scrub_descr xfrog_scrubbers[XFS_SCRUB_TYPE_NR];
 extern const unsigned int xfrog_scrubber_deps[XFS_SCRUB_TYPE_NR];
 
 int xfrog_scrub_metadata(struct xfs_fd *xfd, struct xfs_scrub_metadata *meta);
+int xfrog_scrubv_metadata(struct xfs_fd *xfd, struct xfs_scrub_vec_head *vhead);
 
 #endif	/* __LIBFROG_SCRUB_H__ */
