aa942ff0f1b574a064abe6a0307f1db55e3dfbfa
Darrick J. Wong <djwong@kernel.org>
2021-02-17 16:09:04 -0800

    xfs_scrub: vectorize scrub calls
    Use the new vectorized kernel scrub calls to reduce the overhead of
    checking metadata.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/scrub.h
+++ scrub/scrub.h
@@ -130,6 +130,8 @@ bool can_scrub_symlink(struct scrub_ctx *ctx);
 bool can_scrub_parent(struct scrub_ctx *ctx);
 bool can_repair(struct scrub_ctx *ctx);
 
+void check_scrubv(struct scrub_ctx *ctx);
+
 int scrub_file(struct scrub_ctx *ctx, int fd, const struct xfs_bulkstat *bstat,
 		unsigned int type, struct scrub_item *sri);
 
