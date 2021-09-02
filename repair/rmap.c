5ba22f7b16350843295f9cc7e62ce1b2874be87f
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:49:17 -0700

    xfs_db: allow administrators to add older v5 features
    Add to xfs_db the ability to add certain existing features (finobt,
    reflink, and rmapbt) to an existing filesystem if it's eligible.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/rmap.c
+++ repair/rmap.c
@@ -63,8 +63,8 @@ bool
 rmap_needs_work(
 	struct xfs_mount	*mp)
 {
-	return xfs_has_reflink(mp) ||
-	       xfs_has_rmapbt(mp);
+	return xfs_has_reflink(mp) || add_reflink ||
+	       xfs_has_rmapbt(mp) || add_rmapbt;
 }
 
 /* Is this a realtime rmap inode? */
