da3e805d3a9ab43cc231a2b6ca482b5ad6dcd127
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:46:45 -0700

    xfs: apply rt extent alignment constraints to CoW extsize hint
    The copy-on-write extent size hint is subject to the same alignment
    constraints as the regular extent size hint.  Since we're in the process
    of adding reflink (and therefore CoW) to the realtime device, we must
    apply the same scattered rextsize alignment validation strategies to
    both hints to deal with the possibility of rextsize changing.
    
    Therefore, fix the inode validator to perform rextsize alignment checks
    on regular realtime files, and to remove misaligned directory hints.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_trans_inode.c
+++ libxfs/xfs_trans_inode.c
@@ -157,6 +157,20 @@ xfs_trans_log_inode(
 	}
 
 	/*
+	 * Inode verifiers do not check that the CoW extent size hint is an
+	 * integer multiple of the rt extent size on a directory with both
+	 * rtinherit and cowextsize flags set.  If we're logging a directory
+	 * that is misconfigured in this way, clear the hint.
+	 */
+	if ((ip->i_diflags & XFS_DIFLAG_RTINHERIT) &&
+	    (ip->i_diflags2 & XFS_DIFLAG2_COWEXTSIZE) &&
+	    (ip->i_cowextsize % ip->i_mount->m_sb.sb_rextsize) > 0) {
+		ip->i_diflags2 &= ~XFS_DIFLAG2_COWEXTSIZE;
+		ip->i_cowextsize = 0;
+		flags |= XFS_ILOG_CORE;
+	}
+
+	/*
 	 * Record the specific change for fdatasync optimisation. This allows
 	 * fdatasync to skip log forces for inodes that are only timestamp
 	 * dirty.
