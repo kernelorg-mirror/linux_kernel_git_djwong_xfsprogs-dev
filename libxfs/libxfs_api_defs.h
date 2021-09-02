1f0584b0051bc715d272116acf4f99aeb7c86f27
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:42:00 -0700

    xfs_repair: use library functions for orphanage creation
    Use new library functions to create lost+found.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -100,6 +100,7 @@
 #define xfs_dir2_shrink_inode		libxfs_dir2_shrink_inode
 
 #define xfs_dir_createname		libxfs_dir_createname
+#define xfs_dir_create_new_child	libxfs_dir_create_new_child
 #define xfs_dir_init			libxfs_dir_init
 #define xfs_dir_ino_validate		libxfs_dir_ino_validate
 #define xfs_dir_lookup			libxfs_dir_lookup
