8d01aa2157da889fb7664fc997d0d911994d2183
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:49:14 -0700

    xfs_scrub: remove unused fd argument when repairing metadata
    Remove the unused fd parameter from xfs_repair_metadata, and then remove
    it from all the callers.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/phase5.c
+++ scrub/phase5.c
@@ -420,7 +420,7 @@ iscan_worker(
 		goto out;
 	}
 
-	ret = action_list_process(ctx, ctx->mnt.fd, &item->alist,
+	ret = action_list_process(ctx, &item->alist,
 			ALP_COMPLAIN_IF_UNFIXED | ALP_NOPROGRESS);
 	if (ret) {
 		str_liberror(ctx, ret, _("repairing iscan metadata"));
