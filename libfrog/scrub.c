7c22845f253c034eabee86eaf5285ada01bd4300
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:46:46 -0700

    libfrog: enable scrubbng of the realtime refcount data
    Add a new entry so that we can scrub the rtrefcountbt.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libfrog/scrub.c
+++ libfrog/scrub.c
@@ -149,6 +149,11 @@ const struct xfrog_scrub_descr xfrog_scrubbers[XFS_SCRUB_TYPE_NR] = {
 		.descr	= "realtime reverse mapping btree",
 		.group	= XFROG_SCRUB_GROUP_FS,
 	},
+	[XFS_SCRUB_TYPE_RTREFCBT] = {
+		.name	= "rtrefcountbt",
+		.descr	= "realtime reference count btree",
+		.group	= XFROG_SCRUB_GROUP_FS,
+	},
 };
 
 /* Invoke the scrub ioctl.  Returns zero or negative error code. */
