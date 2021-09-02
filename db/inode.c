cca561c6840e359d46f81124af274c401b139ee6
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:44:12 -0700

    xfs_db: report metadir support for version command
    Report metadir support if we have it enabled.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- db/inode.c
+++ db/inode.c
@@ -178,6 +178,9 @@ const field_t	inode_v3_flds[] = {
 	{ "bigtime", FLDT_UINT1,
 	  OI(COFF(flags2) + bitsz(uint64_t) - XFS_DIFLAG2_BIGTIME_BIT - 1), C1,
 	  0, TYP_NONE },
+	{ "metadata", FLDT_UINT1,
+	  OI(COFF(flags2) + bitsz(uint64_t) - XFS_DIFLAG2_METADATA_BIT-1), C1,
+	  0, TYP_NONE },
 	{ NULL }
 };
 
