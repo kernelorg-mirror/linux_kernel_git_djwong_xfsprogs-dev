0d46b39b30e3274071df350e2526f516c4720d91
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:49:19 -0700

    xfs: allow queued AG intents to drain before scrubbing
    Currently, online scrub isn't sufficiently careful about quiescing
    allocation groups before checking them.  While scrub does take the AG
    header locks, it doesn't serialize against chains of AG update intents
    that are being processed concurrently.  If there's a collision,
    cross-referencing between data structures (e.g. rmapbt and refcountbt)
    can yield false corruption events; if repair is running, this results in
    incorrect repairs.
    
    Fix this by adding to the perag structure the count of active intents
    and make scrub wait until there aren't any to continue.  This is a
    little stupid since transactions can queue intents without taking buffer
    locks, but we'll also wait for those transactions.
    
    XXX: should have instead a per-ag rwsem that gets taken as soon as the
    AG[IF] are locked and stays held until the transaction commits or moves
    on to the next AG?  would we rather have a six lock so that intents can
    take an ix lock, and not have to upgrade to x until we actually want to
    make changes to that ag?  is that how those even work??
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/defer_item.c
+++ libxfs/defer_item.c
@@ -24,6 +24,14 @@
 
 /* Dummy defer item ops, since we don't do logging. */
 
+/* Dummy add item function. */
+static void
+xfs_dummy_add_item(
+	struct xfs_mount		*mp,
+	const struct list_head		*item)
+{
+}
+
 /* Extent Freeing */
 
 /* Sort bmap items by AG. */
@@ -102,6 +110,7 @@ xfs_extent_free_abort_intent(
 /* Cancel a free extent. */
 STATIC void
 xfs_extent_free_cancel_item(
+	struct xfs_mount		*mp,
 	struct list_head		*item)
 {
 	struct xfs_extent_free_item	*free;
@@ -116,6 +125,7 @@ const struct xfs_defer_op_type xfs_extent_free_defer_type = {
 	.create_done	= xfs_extent_free_create_done,
 	.finish_item	= xfs_extent_free_finish_item,
 	.cancel_item	= xfs_extent_free_cancel_item,
+	.add_item	= xfs_dummy_add_item,
 };
 
 /*
@@ -157,6 +167,7 @@ const struct xfs_defer_op_type xfs_agfl_free_defer_type = {
 	.create_done	= xfs_extent_free_create_done,
 	.finish_item	= xfs_agfl_free_finish_item,
 	.cancel_item	= xfs_extent_free_cancel_item,
+	.add_item	= xfs_dummy_add_item,
 };
 
 /* Reverse Mapping */
@@ -230,6 +241,7 @@ xfs_rmap_update_abort_intent(
 /* Cancel a deferred rmap update. */
 STATIC void
 xfs_rmap_update_cancel_item(
+	struct xfs_mount		*mp,
 	struct list_head		*item)
 {
 	struct xfs_rmap_intent		*ri;
@@ -245,6 +257,7 @@ const struct xfs_defer_op_type xfs_rmap_update_defer_type = {
 	.finish_item	= xfs_rmap_update_finish_item,
 	.finish_cleanup = xfs_rmap_finish_one_cleanup,
 	.cancel_item	= xfs_rmap_update_cancel_item,
+	.add_item	= xfs_dummy_add_item,
 };
 
 /* Reference Counting */
@@ -325,6 +338,7 @@ xfs_refcount_update_abort_intent(
 /* Cancel a deferred refcount update. */
 STATIC void
 xfs_refcount_update_cancel_item(
+	struct xfs_mount		*mp,
 	struct list_head		*item)
 {
 	struct xfs_refcount_intent	*ri;
@@ -340,6 +354,7 @@ const struct xfs_defer_op_type xfs_refcount_update_defer_type = {
 	.finish_item	= xfs_refcount_update_finish_item,
 	.finish_cleanup = xfs_refcount_finish_one_cleanup,
 	.cancel_item	= xfs_refcount_update_cancel_item,
+	.add_item	= xfs_dummy_add_item,
 };
 
 /* Inode Block Mapping */
@@ -415,6 +430,7 @@ xfs_bmap_update_abort_intent(
 /* Cancel a deferred rmap update. */
 STATIC void
 xfs_bmap_update_cancel_item(
+	struct xfs_mount		*mp,
 	struct list_head		*item)
 {
 	struct xfs_bmap_intent		*bi;
@@ -429,6 +445,7 @@ const struct xfs_defer_op_type xfs_bmap_update_defer_type = {
 	.create_done	= xfs_bmap_update_create_done,
 	.finish_item	= xfs_bmap_update_finish_item,
 	.cancel_item	= xfs_bmap_update_cancel_item,
+	.add_item	= xfs_dummy_add_item,
 };
 
 /* Atomic Swapping of File Ranges */
@@ -493,6 +510,7 @@ xfs_swapext_abort_intent(
 /* Cancel a deferred swapext update. */
 STATIC void
 xfs_swapext_cancel_item(
+	struct xfs_mount		*mp,
 	struct list_head		*item)
 {
 	struct xfs_swapext_intent	*sxi;
@@ -507,4 +525,5 @@ const struct xfs_defer_op_type xfs_swapext_defer_type = {
 	.create_done	= xfs_swapext_create_done,
 	.finish_item	= xfs_swapext_finish_item,
 	.cancel_item	= xfs_swapext_cancel_item,
+	.add_item	= xfs_dummy_add_item,
 };
