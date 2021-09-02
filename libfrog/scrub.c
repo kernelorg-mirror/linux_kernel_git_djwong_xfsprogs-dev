4c69ae5e2a29bfeed2fc509a00cb1d533954fbc2
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:45:50 -0700

    libfrog: enable scrubbng of the realtime rmap
    Add a new entry so that we can scrub the rtrmapbt.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libfrog/scrub.c
+++ libfrog/scrub.c
@@ -144,6 +144,11 @@ const struct xfrog_scrub_descr xfrog_scrubbers[XFS_SCRUB_TYPE_NR] = {
 		.descr	= "retained health records",
 		.group	= XFROG_SCRUB_GROUP_NONE,
 	},
+	[XFS_SCRUB_TYPE_RTRMAPBT] = {
+		.name	= "rtrmapbt",
+		.descr	= "realtime reverse mapping btree",
+		.group	= XFROG_SCRUB_GROUP_FS,
+	},
 };
 
 /* Invoke the scrub ioctl.  Returns zero or negative error code. */
