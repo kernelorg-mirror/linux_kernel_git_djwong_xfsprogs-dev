433bc43ad1af921ebd29cdc08160d041fa491f17
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:46:26 -0700

    xfs: define the on-disk realtime refcount btree format
    Start filling out the rtrefcount btree implementation. Start with the
    on-disk btree format; add everything needed to read, write and
    manipulate refcount btree blocks. This prepares the way for connecting
    the btree operations implementation.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/init.c
+++ libxfs/init.c
@@ -724,7 +724,10 @@ static inline void
 xfs_rtbtree_compute_maxlevels(
 	struct xfs_mount	*mp)
 {
-	mp->m_rtbtree_maxlevels = mp->m_rtrmap_maxlevels;
+	unsigned int		levels;
+
+	levels = max(mp->m_rtrmap_maxlevels, mp->m_rtrefc_maxlevels);
+	mp->m_rtbtree_maxlevels = levels;
 }
 
 STATIC void
@@ -809,6 +812,7 @@ libxfs_mount(
 	xfs_rmapbt_compute_maxlevels(mp);
 	xfs_rtrmapbt_compute_maxlevels(mp);
 	xfs_refcountbt_compute_maxlevels(mp);
+	xfs_rtrefcountbt_compute_maxlevels(mp);
 
 	xfs_agbtree_compute_maxlevels(mp);
 	xfs_rtbtree_compute_maxlevels(mp);
