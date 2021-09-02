0148adf0eab0d87c9c41191f2b66b0ab5d6acf91
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:49:15 -0700

    xfs_scrub: remove action lists from phaseX code
    Now that we track repair schedules by filesystem object (and not
    individual repairs) we can get rid of all the onstack list heads and
    whatnot in the phaseX code.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/phase4.c
+++ scrub/phase4.c
@@ -112,7 +112,6 @@ phase4_func(
 	struct scrub_ctx	*ctx)
 {
 	struct xfs_fsop_geom	fsgeom;
-	struct action_list	alist;
 	struct scrub_item	sri;
 	int			ret;
 
@@ -124,8 +123,7 @@ phase4_func(
 	 * metadata.  If repairs fails, we'll come back during phase 7.
 	 */
 	scrub_item_init_fs(&sri);
-	action_list_init(&alist);
-	ret = scrub_meta_type(ctx, XFS_SCRUB_TYPE_FSCOUNTERS, 0, &alist, &sri);
+	ret = scrub_meta_type(ctx, XFS_SCRUB_TYPE_FSCOUNTERS, &sri);
 	if (ret)
 		return ret;
 
@@ -140,8 +138,7 @@ phase4_func(
 		return ret;
 
 	if (fsgeom.sick & XFS_FSOP_GEOM_SICK_QUOTACHECK) {
-		ret = scrub_meta_type(ctx, XFS_SCRUB_TYPE_QUOTACHECK, 0,
-				&alist, &sri);
+		ret = scrub_meta_type(ctx, XFS_SCRUB_TYPE_QUOTACHECK, &sri);
 		if (ret)
 			return ret;
 	}
@@ -150,7 +147,6 @@ phase4_func(
 	ret = repair_item_corruption(ctx, &sri);
 	if (ret)
 		return ret;
-	action_list_discard(&alist);
 
 	return repair_everything(ctx);
 }
