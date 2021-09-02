ec232619aaeb20ebb582899d8b2b50706f3a3f6f
Darrick J. Wong <djwong@kernel.org>
2021-09-02 14:46:44 -0700

    xfs: wire up realtime refcount btree cursors
    Wire up realtime refcount btree cursors wherever they're needed
    throughout the code base.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_refcount.c
+++ libxfs/xfs_refcount.c
@@ -23,6 +23,7 @@
 #include "xfs_rmap.h"
 #include "xfs_ag.h"
 #include "xfs_health.h"
+#include "xfs_rtrefcount_btree.h"
 
 /* Allowable refcount adjustment amounts. */
 enum xfs_refc_adjust_op {
@@ -1271,9 +1272,9 @@ xfs_refcount_finish_one(
 	}
 	if (rcur == NULL) {
 		if (ri->ri_realtime) {
-			/* coming in a later patch */
-			ASSERT(0);
-			return -EFSCORRUPTED;
+			xfs_rtlock(tp, mp, XFS_RTLOCK_REFCOUNT);
+			rcur = xfs_rtrefcountbt_init_cursor(mp, tp,
+					mp->m_rrefcountip);
 		} else {
 			error = xfs_alloc_read_agf(mp, tp, pag->pag_agno,
 					XFS_ALLOC_FLAG_FREEING, &agbp);
