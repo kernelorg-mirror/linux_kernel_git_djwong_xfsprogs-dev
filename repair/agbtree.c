0204164e87b247cdf9ca93d7acf9548de317433f
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:13:31 -0800

    xfs_repair: convert regular rmap repair to use in-memory btrees
    Convert the rmap btree repair code to use in-memory rmap btrees to store
    the observed reverse mapping records.  This will eliminate the need for
    a separate record sorting step, as well as eliminate the need for all
    the code that turns multiple consecutive bmap records into a single rmap
    record.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/agbtree.c
+++ repair/agbtree.c
@@ -99,7 +99,8 @@ reserve_agblocks(
 			do_error(_("could not set up btree reservation: %s\n"),
 				strerror(-error));
 
-		error = rmap_add_ag_rec(mp, agno, ext_ptr->ex_startblock, len,
+		error = rmap_add_agbtree_mapping(mp, agno,
+				ext_ptr->ex_startblock, len,
 				btr->newbt.oinfo.oi_owner);
 		if (error)
 			do_error(_("could not set up btree rmaps: %s\n"),
@@ -561,15 +562,21 @@ _("Error %d while creating finobt btree for AG %u.\n"), error, agno);
 /* Grab one rmap record. */
 static int
 get_rmapbt_record(
-	struct xfs_btree_cur		*cur,
-	void				*priv)
+	struct xfs_btree_cur	*cur,
+	void			*priv)
 {
-	struct xfs_rmap_irec		*rec;
-	struct bt_rebuild		*btr = priv;
+	struct bt_rebuild	*btr = priv;
+	int			ret;
 
-	rec = pop_slab_cursor(btr->slab_cursor);
-	memcpy(&cur->bc_rec.r, rec, sizeof(struct xfs_rmap_irec));
+	ret = rmap_get_mem_rec(&btr->rmapbt_cursor, &cur->bc_rec.r);
+	if (ret < 0)
+		return ret;
+	if (ret == 0)
+		do_error(
+ _("ran out of records while rebuilding AG %u rmap btree\n"),
+				cur->bc_ag.pag->pag_agno);
 	return 0;
+
 }
 
 /* Set up the rmap rebuild parameters. */
@@ -611,7 +618,7 @@ build_rmap_tree(
 {
 	int			error;
 
-	error = rmap_init_cursor(agno, &btr->slab_cursor);
+	error = rmap_init_mem_cursor(sc->mp, NULL, agno, &btr->rmapbt_cursor);
 	if (error)
 		do_error(
 _("Insufficient memory to construct rmap cursor.\n"));
@@ -624,7 +631,7 @@ _("Error %d while creating rmap btree for AG %u.\n"), error, agno);
 
 	/* Since we're not writing the AGF yet, no need to commit the cursor */
 	libxfs_btree_del_cursor(btr->cur, 0);
-	free_slab_cursor(&btr->slab_cursor);
+	rmap_free_mem_cursor(NULL, &btr->rmapbt_cursor, 0);
 }
 
 /* rebuild the refcount tree */
