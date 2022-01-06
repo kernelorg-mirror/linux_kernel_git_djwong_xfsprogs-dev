854a1aecae341cbdb87c6c0e84d6c83f677541a1
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:13:51 -0800

    xfs_repair: rebuild the realtime rmap btree
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/xfs_repair.c
+++ repair/xfs_repair.c
@@ -1306,13 +1306,17 @@ main(int argc, char **argv)
 	phase_end(mp, 5);
 
 	/*
-	 * Done with the block usage maps, toss them...
+	 * Done with the block usage maps, toss them.  Realtime metadata aren't
+	 * rebuilt until phase 6, so we have to keep them around.
 	 */
-	rmaps_free(mp);
+	if (mp->m_sb.sb_rblocks == 0)
+		rmaps_free(mp);
 	free_bmaps(mp);
 
 	if (!bad_ino_btree)  {
 		phase6(mp);
+		if (mp->m_sb.sb_rblocks != 0)
+			rmaps_free(mp);
 		phase_end(mp, 6);
 
 		phase7(mp, phase2_threads);
