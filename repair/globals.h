4b0f0d0894835965deaed9803d514138f719c2f9
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:14:03 -0800

    xfs_repair: allow sysadmins to add reverse mapping indexes
    Allow the sysadmin to use xfs_repair to upgrade an existing filesystem
    to support the reverse mapping btree index.  This is needed for online
    fsck.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/globals.h
+++ repair/globals.h
@@ -94,6 +94,7 @@ extern bool	add_inobtcount;		/* add inode btree counts to AGI */
 extern bool	add_bigtime;		/* add support for timestamps up to 2486 */
 extern bool	add_finobt;		/* add free inode btrees */
 extern bool	add_reflink;		/* add reference count btrees */
+extern bool	add_rmapbt;		/* add reverse mapping btrees */
 
 /* misc status variables */
 
