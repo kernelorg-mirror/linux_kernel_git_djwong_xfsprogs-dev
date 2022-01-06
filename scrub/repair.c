306d7f2035890f5a8bb5e1900b0be44994418122
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:14:01 -0800

    xfs_scrub: remove unused action_list fields
    Remove some fields since we don't need them anymore.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/repair.c
+++ scrub/repair.c
@@ -375,7 +375,6 @@ action_list_discard(
 	struct action_item		*n;
 
 	list_for_each_entry_safe(aitem, n, &alist->list, list) {
-		alist->nr--;
 		list_del(&aitem->list);
 		free(aitem);
 	}
@@ -396,8 +395,6 @@ action_list_init(
 	struct action_list		*alist)
 {
 	INIT_LIST_HEAD(&alist->list);
-	alist->nr = 0;
-	alist->sorted = false;
 }
 
 /* Number of pending repairs in this list. */
@@ -420,8 +417,6 @@ action_list_add(
 	struct action_item		*aitem)
 {
 	list_add_tail(&aitem->list, &alist->list);
-	alist->nr++;
-	alist->sorted = false;
 }
 
 /* Repair everything on this list. */
