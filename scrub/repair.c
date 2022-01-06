90791554a49ddf1aa4a7a300751f3b0c2d6bac2b
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:14:02 -0800

    xfs_scrub: hoist repair retry loop to repair_item_class
    For metadata repair calls, move the ioctl retry and freeze permission
    tracking into scrub_item.  This enables us to move the repair retry loop
    out of xfs_repair_metadata and into its caller to remove a long
    backwards jump, and gets us closer to vectorizing scrub calls.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/repair.c
+++ scrub/repair.c
@@ -55,7 +55,6 @@ xfs_repair_metadata(
 	struct xfs_scrub_metadata	oldm;
 	DEFINE_DESCR(dsc, ctx, format_scrub_descr);
 	bool				repair_only;
-	unsigned int			tries = 0;
 	int				error;
 
 	/*
@@ -95,7 +94,6 @@ xfs_repair_metadata(
 		str_info(ctx, descr_render(&dsc),
 				_("Attempting optimization."));
 
-retry:
 	error = -xfrog_scrub_metadata(&ctx->mnt, &meta);
 	switch (error) {
 	case 0:
@@ -180,10 +178,8 @@ _("Read-only filesystem; cannot make changes."));
 	 * the repair again, just in case the fs was busy.  Only retry so many
 	 * times.
 	 */
-	if (want_retry(&meta) && tries < 10) {
-		tries++;
-		goto retry;
-	}
+	if (want_retry(&meta) && scrub_item_schedule_retry(sri, scrub_type))
+		return 0;
 
 	if (repair_flags & XRM_COMPLAIN_IF_UNFIXED)
 		scrub_warn_incomplete_scrub(ctx, &dsc, &meta);
@@ -480,6 +476,7 @@ repair_item_class(
 	uint8_t				repair_mask,
 	unsigned int			flags)
 {
+	struct scrub_item		old_sri;
 	unsigned int			scrub_type;
 	int				error = 0;
 
@@ -502,9 +499,15 @@ repair_item_class(
 		    !repair_item_dependencies_ok(sri, scrub_type))
 			continue;
 
-		error = xfs_repair_metadata(ctx, scrub_type, sri, flags);
-		if (error)
-			break;
+		sri->sri_tries[scrub_type] = SCRUB_ITEM_MAX_RETRIES;
+		do {
+			memcpy(&old_sri, sri, sizeof(old_sri));
+			error = xfs_repair_metadata(ctx, scrub_type, sri,
+					flags);
+			if (error)
+				return error;
+		} while (scrub_item_call_kernel_again(sri, scrub_type,
+					repair_mask, &old_sri));
 
 		/* Maybe update progress if we fixed the problem. */
 		if (!(flags & XRM_NOPROGRESS) &&
