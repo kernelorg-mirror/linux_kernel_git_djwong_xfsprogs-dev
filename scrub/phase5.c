35f65883098158a6443c1a937dce996e70f06800
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:14:01 -0800

    xfs_scrub: remove action lists from phaseX code
    Now that we track repair schedules by filesystem object (and not
    individual repairs) we can get rid of all the onstack list heads and
    whatnot in the phaseX code.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/phase5.c
+++ scrub/phase5.c
@@ -385,7 +385,6 @@ out:
 
 struct iscan_item {
 	struct scrub_item	sri;
-	struct action_list	alist;
 	bool			*abortedp;
 	unsigned int		scrub_type;
 };
@@ -412,16 +411,14 @@ iscan_worker(
 		nanosleep(&tv, NULL);
 	}
 
-	ret = scrub_meta_type(ctx, item->scrub_type, 0, &item->alist,
-			&item->sri);
+	ret = scrub_meta_type(ctx, item->scrub_type, &item->sri);
 	if (ret) {
 		str_liberror(ctx, ret, _("checking iscan metadata"));
 		*item->abortedp = true;
 		goto out;
 	}
 
-	ret = action_list_process(ctx, &item->alist,
-			XRM_FINAL_WARNING | XRM_NOPROGRESS);
+	ret = repair_item_completely(ctx, &item->sri);
 	if (ret) {
 		str_liberror(ctx, ret, _("repairing iscan metadata"));
 		*item->abortedp = true;
@@ -452,7 +449,6 @@ queue_iscan(
 		return ret;
 	}
 	scrub_item_init_fs(&item->sri);
-	action_list_init(&item->alist);
 	item->scrub_type = scrub_type;
 	item->abortedp = abortedp;
 
