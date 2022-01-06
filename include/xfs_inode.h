1687e880a329762d5622bea705c3f39a936db935
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:13:37 -0800

    libxfs: remove libxfs_dir_ialloc
    This function no longer exists in the kernel, and it's not really needed
    in userspace either.  There are two users of it: repair and mkfs.
    Repair passes in zeroed cred and fsxattr structures so it can call
    libxfs_dialloc and libxfs_icreate directly.  For mkfs we'll move the
    guts of libxfs_dir_ialloc into proto.c as a creatproto function that
    takes care of all that, and move struct cred to mkfs since it's now the
    only user.
    
    This gets us ready to hoist the rest of the inode initialization code to
    libxfs for metadata directories.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/xfs_inode.h
+++ include/xfs_inode.h
@@ -210,23 +210,16 @@ static inline bool xfs_is_always_cow_inode(struct xfs_inode *ip)
 	return false;
 }
 
-/* Always set the child's GID to this value, even if the parent is setgid. */
-#define CRED_FORCE_GID	(1U << 0)
-struct cred {
-	uid_t		cr_uid;
-	gid_t		cr_gid;
-	unsigned int	cr_flags;
-};
-
-extern int	libxfs_dir_ialloc (struct xfs_trans **, struct xfs_inode *,
-				mode_t, nlink_t, xfs_dev_t, struct cred *,
-				struct fsxattr *, struct xfs_inode **);
 extern void	libxfs_trans_inode_alloc_buf (struct xfs_trans *,
 				struct xfs_buf *);
 
 extern void	libxfs_trans_ichgtime(struct xfs_trans *,
 				struct xfs_inode *, int);
 extern int	libxfs_iflush_int (struct xfs_inode *, struct xfs_buf *);
+
+int libxfs_icreate(struct xfs_trans *tp, xfs_ino_t ino,
+		const struct xfs_icreate_args *args, struct xfs_inode **ipp);
+void libxfs_icreate_args_rootfile(struct xfs_icreate_args *args, umode_t mode);
 
 extern struct timespec64 current_time(struct inode *inode);
 
