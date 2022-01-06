371b97ddf5b957f9d7270340d3f65606f606dec5
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:14:00 -0800

    xfs_scrub: require primary superblock repairs to complete before proceeding
    Phase 2 of the xfs_scrub program calls the kernel to check the primary
    superblock before scanning the rest of the filesystem.  Though doing so
    is a no-op now (since the primary super must pass all checks as a
    prerequisite for mounting), the goal of this code is to enable future
    kernel code to intercept an xfs_scrub run before it actually does
    anything.  If this some day involves fixing the primary superblock, it
    seems reasonable to require that /all/ repairs complete successfully
    before moving on to the rest of the filesystem.
    
    Unfortunately, that's not what xfs_scrub does now -- primary super
    repairs that fail are theoretically deferred to phase 4!  So make this
    mandatory.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/phase2.c
+++ scrub/phase2.c
@@ -144,7 +144,8 @@ phase2_func(
 	ret = scrub_primary_super(ctx, &alist);
 	if (ret)
 		goto out;
-	ret = action_list_process_or_defer(ctx, 0, &alist);
+	ret = action_list_process(ctx, &alist,
+			XRM_COMPLAIN_IF_UNFIXED | XRM_NOPROGRESS);
 	if (ret)
 		goto out;
 
