5932a5ea861c625d2c4487aca15a43f375489a73
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:14:00 -0800

    xfs_scrub: track repair items by principal, not by individual repairs
    Create a new structure to track scrub and repair state by principal
    filesystem object (e.g. ag number or inode number/generation) so that we
    can more easily examine and ensure that we satisfy repair order
    dependencies.  This transposition will eventually enable bulk scrub
    operations and will also save a lot of memory if a given object needs a
    lot of work.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/phase2.c
+++ scrub/phase2.c
@@ -43,6 +43,7 @@ scan_ag_metadata(
 	xfs_agnumber_t			agno,
 	void				*arg)
 {
+	struct scrub_item		sri;
 	struct scrub_ctx		*ctx = (struct scrub_ctx *)wq->wq_ctx;
 	bool				*aborted = arg;
 	struct action_list		alist;
@@ -54,6 +55,7 @@ scan_ag_metadata(
 	if (*aborted)
 		return;
 
+	scrub_item_init_ag(&sri, agno);
 	action_list_init(&alist);
 	action_list_init(&immediate_alist);
 	snprintf(descr, DESCR_BUFSZ, _("AG %u"), agno);
@@ -62,7 +64,7 @@ scan_ag_metadata(
 	 * First we scrub and fix the AG headers, because we need
 	 * them to work well enough to check the AG btrees.
 	 */
-	ret = scrub_ag_headers(ctx, agno, &alist);
+	ret = scrub_ag_headers(ctx, agno, &alist, &sri);
 	if (ret)
 		goto err;
 
@@ -72,7 +74,7 @@ scan_ag_metadata(
 		goto err;
 
 	/* Now scrub the AG btrees. */
-	ret = scrub_ag_metadata(ctx, agno, &alist);
+	ret = scrub_ag_metadata(ctx, agno, &alist, &sri);
 	if (ret)
 		goto err;
 
@@ -104,10 +106,12 @@ static void
 scan_fs_metadata(
 	struct scrub_ctx	*ctx,
 	int			(*fn)(struct scrub_ctx *ctx,
-				      struct action_list *alist),
+				      struct action_list *alist,
+				      struct scrub_item *sri),
 	const char		*descr,
 	bool			*aborted)
 {
+	struct scrub_item	sri;
 	struct action_list	alist;
 	unsigned int		difficulty;
 	int			ret;
@@ -115,8 +119,9 @@ scan_fs_metadata(
 	if (*aborted)
 		return;
 
+	scrub_item_init_fs(&sri);
 	action_list_init(&alist);
-	ret = fn(ctx, &alist);
+	ret = fn(ctx, &alist, &sri);
 	if (ret) {
 		*aborted = true;
 		return;
@@ -157,6 +162,7 @@ phase2_func(
 	struct scrub_ctx	*ctx)
 {
 	struct action_list	alist;
+	struct scrub_item	sri;
 	struct workqueue	wq;
 	xfs_agnumber_t		agno;
 	bool			aborted = false;
@@ -175,8 +181,9 @@ phase2_func(
 	 * upgrades) off of the sb 0 scrubber (which currently does nothing).
 	 * If errors occur, this function will log them and return nonzero.
 	 */
+	scrub_item_init_ag(&sri, 0);
 	action_list_init(&alist);
-	ret = scrub_meta_type(ctx, XFS_SCRUB_TYPE_SB, 0, &alist);
+	ret = scrub_meta_type(ctx, XFS_SCRUB_TYPE_SB, 0, &alist, &sri);
 	if (ret)
 		goto out;
 	ret = action_list_process(ctx, -1, &alist,
