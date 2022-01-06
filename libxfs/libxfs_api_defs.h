2ae971958c19aead16408eb0aa3a50dd5ddb1016
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:14:03 -0800

    xfs_repair: allow sysadmins to add realtime reflink
    Allow the sysadmin to use xfs_repair to upgrade an existing filesystem
    to support the realtime reference count btree, and therefore reflink on
    realtime volumes.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -221,6 +221,7 @@
 #define xfs_rtfree_extent		libxfs_rtfree_extent
 
 #define xfs_rtrefcountbt_absolute_maxlevels	libxfs_rtrefcountbt_absolute_maxlevels
+#define xfs_rtrefcountbt_calc_reserves	libxfs_rtrefcountbt_calc_reserves
 #define xfs_rtrefcountbt_commit_staged_btree	libxfs_rtrefcountbt_commit_staged_btree
 #define xfs_rtrefcountbt_create		libxfs_rtrefcountbt_create
 #define xfs_rtrefcountbt_droot_maxrecs	libxfs_rtrefcountbt_droot_maxrecs
