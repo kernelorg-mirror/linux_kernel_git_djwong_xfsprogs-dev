ca2526a482cf98a65422c6090b4f4d67e700ba72
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:13:34 -0800

    xfs: add error injection to test swapext recovery
    Add an errortag so that we can test recovery of swapext log items.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_swapext.c
+++ libxfs/xfs_swapext.c
@@ -19,6 +19,7 @@
 #include "xfs_bmap_btree.h"
 #include "xfs_trans_space.h"
 #include "xfs_quota_defs.h"
+#include "xfs_errortag.h"
 
 struct kmem_cache	*xfs_swapext_intent_cache;
 
@@ -416,6 +417,9 @@ xfs_swapext_finish_one(
 		if (error)
 			return error;
 	}
+
+	if (XFS_TEST_ERROR(false, tp->t_mountp, XFS_ERRTAG_SWAPEXT_FINISH_ONE))
+		return -EIO;
 
 	/* If we still have work to do, ask for a new transaction. */
 	if (sxi_has_more_swap_work(sxi) || sxi_has_postop_work(sxi)) {
