80b8b3ed506718d202066c3da946e5c35e7d2379
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:14:00 -0800

    xfs_scrub: move repair functions to repair.c
    Move all the repair functions to repair.c.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/scrub.h
+++ scrub/scrub.h
@@ -37,7 +37,7 @@ bool can_scrub_dir(struct scrub_ctx *ctx);
 bool can_scrub_attr(struct scrub_ctx *ctx);
 bool can_scrub_symlink(struct scrub_ctx *ctx);
 bool can_scrub_parent(struct scrub_ctx *ctx);
-bool xfs_can_repair(struct scrub_ctx *ctx);
+bool can_repair(struct scrub_ctx *ctx);
 bool can_force_rebuild(struct scrub_ctx *ctx);
 
 int scrub_file(struct scrub_ctx *ctx, int fd, const struct xfs_bulkstat *bstat,
@@ -52,9 +52,5 @@ struct action_item {
 	__u32			gen;
 	__u32			agno;
 };
-
-enum check_outcome xfs_repair_metadata(struct scrub_ctx *ctx,
-		struct xfs_fd *xfdp, struct action_item *aitem,
-		unsigned int repair_flags);
 
 #endif /* XFS_SCRUB_SCRUB_H_ */
