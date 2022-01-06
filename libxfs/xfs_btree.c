3eb66c543838ab8a58bf1beaea7efc5e65d396ca
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:13:30 -0800

    xfs: consolidate btree block freeing tracepoints
    Don't waste tracepoint segment memory on per-btree block freeing
    tracepoints when we can do it from the generic btree code.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_btree.c
+++ libxfs/xfs_btree.c
@@ -395,6 +395,8 @@ xfs_btree_free_block(
 {
 	int			error;
 
+	trace_xfs_btree_free_block(cur, bp);
+
 	error = cur->bc_ops->free_block(cur, bp);
 	if (!error) {
 		xfs_trans_binval(cur->bc_tp, bp);
