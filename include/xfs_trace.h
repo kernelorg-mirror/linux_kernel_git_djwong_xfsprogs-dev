2944faf5df5c0e9a0b94dd628717d47b1ab4bdd4
Darrick J. Wong <djwong@kernel.org>
2022-04-27 15:16:24 -0700

    xfs: report "max_resp" used for min log size computation
    Source kernel commit: f41663ef67e53f3e8a27b2a25cbfcb61c2ad037d
    
    Move the tracepoint that computes the size of the transaction used to
    compute the minimum log size into xfs_log_get_max_trans_res so that we
    only have to compute this stuff once.
    
    Leave xfs_log_get_max_trans_res as a non-static function so that xfs_db
    can call it to report the results of the userspace computation of the
    same value to diagnose mkfs/kernel misinteractions.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>

--- include/xfs_trace.h
+++ include/xfs_trace.h
@@ -194,6 +194,7 @@
 #define trace_xfs_trans_read_buf_recur(a)	((void) 0)
 #define trace_xfs_trans_read_buf(a)		((void) 0)
 #define trace_xfs_trans_resv_calc_minlogsize(...) ((void) 0)
+#define trace_xfs_log_get_max_trans_res(...)	((void) 0)
 #define trace_xfs_trans_commit(a,b)		((void) 0)
 
 #define trace_xfs_defer_cancel(a,b)		((void) 0)
