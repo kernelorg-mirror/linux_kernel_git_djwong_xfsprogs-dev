3c3fb476da414f78497248a32ffaf5b45baee923
Darrick J. Wong <djwong@kernel.org>
2022-05-24 12:04:40 -0700

    xfs: allow ->create_intent to return negative errnos
    Source kernel commit: 3df0341704f984147a4950cb68e45125de32449f
    
    Currently, the deferred operation manager assumes that all the
    ->create_intent implementations return only NULL or a pointer to a log
    item.  If xattr log intent item code cannot allocate memory for shared
    state, it can only shut down the log, which is suboptimal.  Rework the
    deferred op manager to handle ERR_PTR returns from ->create_intent, and
    adjust the xattr log items to return one.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_priv.h
+++ libxfs/libxfs_priv.h
@@ -716,4 +716,33 @@ int xfs_bmap_last_extent(struct xfs_trans *tp, struct xfs_inode *ip,
 /* xfs_inode.h */
 #define xfs_iflags_set(ip, flags)	do { } while (0)
 
+#define __must_check                    __attribute__((__warn_unused_result__))
+
+/*
+ * Kernel pointers have redundant information, so we can use a
+ * scheme where we can return either an error code or a normal
+ * pointer with the same return value.
+ *
+ * This should be a per-architecture thing, to allow different
+ * error and pointer decisions.
+ */
+#define MAX_ERRNO	4095
+
+#define IS_ERR_VALUE(x) unlikely((unsigned long)(void *)(x) >= (unsigned long)-MAX_ERRNO)
+
+static inline void * __must_check ERR_PTR(long error)
+{
+	return (void *) error;
+}
+
+static inline long __must_check PTR_ERR(__force const void *ptr)
+{
+	return (long) ptr;
+}
+
+static inline bool __must_check IS_ERR(__force const void *ptr)
+{
+	return IS_ERR_VALUE((unsigned long)ptr);
+}
+
 #endif	/* __LIBXFS_INTERNAL_XFS_H__ */
