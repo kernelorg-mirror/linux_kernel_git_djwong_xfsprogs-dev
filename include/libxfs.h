408a6ac9af36a1a6bc659c213d61b165c41a2395
Darrick J. Wong <djwong@kernel.org>
2022-05-31 11:51:19 +1000

    xfs_repair: check filesystem geometry before allowing upgrades
    Currently, the two V5 feature upgrades permitted by xfs_repair do not
    affect filesystem space usage, so we haven't needed to verify the
    geometry.
    
    However, this will change once we start to allow the sysadmin to add new
    metadata indexes to existing filesystems.  Add all the infrastructure we
    need to ensure that the log will still be large enough, that there's
    enough space for metadata space reservations, and the root inode will
    still be where we expect it to be after the upgrade.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>
    [david: Recompute transaction reservation values; Exit with error if upgrade fails]
    Signed-off-by: Dave Chinner <david@fromorbit.com>
    [djwong: Refuse to upgrade if any part of the fs has < 10% free]

--- include/libxfs.h
+++ include/libxfs.h
@@ -77,6 +77,7 @@ struct iomap;
 #include "xfs_refcount_btree.h"
 #include "xfs_refcount.h"
 #include "xfs_btree_staging.h"
+#include "xfs_ag_resv.h"
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
