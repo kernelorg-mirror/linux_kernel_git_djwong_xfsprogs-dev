08b735628cad19733b7f29d2304ba108b39bb3c7
Darrick J. Wong <djwong@kernel.org>
2022-06-03 14:28:55 -0700

    mkfs: enable reflink on the realtime device
    Allow the creation of filesystems with both reflink and realtime volumes
    enabled.  For now we don't support a realtime extent size > 1.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -2329,12 +2329,18 @@ _("inode btree counters not supported without finobt support\n"));
 	}
 
 	if (cli->xi->rtname) {
-		if (cli->sb_feat.reflink && cli_opt_set(&mopts, M_REFLINK)) {
-			fprintf(stderr,
-_("reflink not supported with realtime devices\n"));
-			usage();
+		if (!cli->sb_feat.metadir && cli->sb_feat.reflink) {
+			if (cli_opt_set(&mopts, M_REFLINK) &&
+			    cli_opt_set(&mopts, M_METADIR)) {
+				fprintf(stderr,
+_("reflink not supported on realtime devices without metadir feature\n"));
+				usage();
+			} else if (cli_opt_set(&mopts, M_REFLINK)) {
+				cli->sb_feat.metadir = true;
+			} else {
+				cli->sb_feat.reflink = false;
+			}
 		}
-		cli->sb_feat.reflink = false;
 
 		if (!cli->sb_feat.metadir && cli->sb_feat.rmapbt) {
 			if (cli_opt_set(&mopts, M_RMAPBT) &&
@@ -2524,6 +2530,12 @@ validate_rtextsize(
 		}
 	}
 	ASSERT(cfg->rtextblocks);
+
+	if (cli->sb_feat.reflink && cfg->rtblocks > 0 && cfg->rtextblocks > 1) {
+		fprintf(stderr,
+_("reflink not supported on realtime with extent sizes > 1\n"));
+		usage();
+	}
 }
 
 /* Validate the incoming extsize hint. */
@@ -4311,7 +4323,14 @@ check_rt_meta_prealloc(
 	if (error)
 		prealloc_fail(mp, error, ask, _("realtime rmap btree"));
 
+	/* Realtime reference count btree inode */
+	ask = libxfs_rtrefcountbt_calc_reserves(mp);
+	error = -libxfs_imeta_resv_init_inode(mp->m_rrefcountip, ask);
+	if (error)
+		prealloc_fail(mp, error, ask, _("realtime refcount btree"));
+
 	/* Unreserve the realtime metadata reservations. */
+	libxfs_imeta_resv_free_inode(mp->m_rrefcountip);
 	libxfs_imeta_resv_free_inode(mp->m_rrmapip);
 
 	/* Unreserve the per-AG reservations. */
