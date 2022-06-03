0477f98d897e9e34830764d4734a194791220067
Darrick J. Wong <djwong@kernel.org>
2022-06-03 14:28:30 -0700

    xfs: add error injection to test swapext recovery
    Add an errortag so that we can test recovery of swapext log items.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- io/inject.c
+++ io/inject.c
@@ -61,6 +61,7 @@ error_tag(char *name)
 		{ XFS_ERRTAG_LARP,			"larp" },
 		{ XFS_ERRTAG_DA_LEAF_SPLIT,		"da_leaf_split" },
 		{ XFS_ERRTAG_ATTR_LEAF_TO_NODE,		"attr_leaf_to_node" },
+		{ XFS_ERRTAG_SWAPEXT_FINISH_ONE,	"swapext_finish_one" },
 		{ XFS_ERRTAG_MAX,			NULL }
 	};
 	int	count;
