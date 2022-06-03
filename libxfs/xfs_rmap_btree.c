5fa7076fa31c3a4be57ab0f9ad35ac28b4da1845
Darrick J. Wong <djwong@kernel.org>
2022-06-03 14:28:43 -0700

    xfs: standardize the btree maxrecs function parameters
    Standardize the parameters in xfs_{alloc,bm,ino,rmap,refcount}bt_maxrecs
    so that we have consistent calling conventions.  This doesn't affect the
    kernel that much, but enables us to clean up userspace a bit.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rmap_btree.c
+++ libxfs/xfs_rmap_btree.c
@@ -599,7 +599,7 @@ xfs_rmapbt_mem_verify(
 	}
 
 	return xfbtree_sblock_verify(bp,
-			xfs_rmapbt_maxrecs(xfo_to_b(1), level == 0));
+			xfs_rmapbt_maxrecs(mp, xfo_to_b(1), level == 0));
 }
 
 static void
@@ -748,10 +748,11 @@ xfs_rmapbt_block_maxrecs(
 /*
  * Calculate number of records in an rmap btree block.
  */
-int
+unsigned int
 xfs_rmapbt_maxrecs(
-	int			blocklen,
-	int			leaf)
+	struct xfs_mount	*mp,
+	unsigned int		blocklen,
+	bool			leaf)
 {
 	blocklen -= XFS_RMAP_BLOCK_LEN;
 	return xfs_rmapbt_block_maxrecs(blocklen, leaf);
