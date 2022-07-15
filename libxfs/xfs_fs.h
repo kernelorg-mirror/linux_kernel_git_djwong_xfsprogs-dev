7efc982fa5578cbef38ce012a25dde9a8437a467
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:24:23 -0700

    xfs: scrub the realtime rmapbt
    Check the realtime reverse mapping btree against the rtbitmap, and
    modify the rtbitmap scrub to check against the rtrmapbt.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_fs.h
+++ libxfs/xfs_fs.h
@@ -725,9 +725,10 @@ struct xfs_scrub_metadata {
 #define XFS_SCRUB_TYPE_QUOTACHECK 25	/* quota counters */
 #define XFS_SCRUB_TYPE_NLINKS	26	/* inode link counts */
 #define XFS_SCRUB_TYPE_HEALTHY	27	/* everything checked out ok */
+#define XFS_SCRUB_TYPE_RTRMAPBT	28	/* realtime reverse mapping btree */
 
 /* Number of scrub subcommands. */
-#define XFS_SCRUB_TYPE_NR	28
+#define XFS_SCRUB_TYPE_NR	29
 
 /* i: Repair this metadata. */
 #define XFS_SCRUB_IFLAG_REPAIR		(1u << 0)
