dbf51b9a95ca84a1ddce7378dfbd18ae263b795f
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:23:07 -0700

    xfs: pass rmap space mapping directly through the log intent code
    Pass the incore rmap space mapping through the RUI logging code instead
    of repeatedly boxing and unboxing parameters.  We'll clean up the
    tracepoints shortly.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/defer_item.c
+++ libxfs/defer_item.c
@@ -220,19 +220,12 @@ xfs_rmap_update_finish_item(
 	struct list_head		*item,
 	struct xfs_btree_cur		**state)
 {
-	struct xfs_rmap_intent		*rmap;
+	struct xfs_rmap_intent		*ri;
 	int				error;
 
-	rmap = container_of(item, struct xfs_rmap_intent, ri_list);
-	error = xfs_rmap_finish_one(tp,
-			rmap->ri_type,
-			rmap->ri_owner, rmap->ri_whichfork,
-			rmap->ri_bmap.br_startoff,
-			rmap->ri_bmap.br_startblock,
-			rmap->ri_bmap.br_blockcount,
-			rmap->ri_bmap.br_state,
-			state);
-	kmem_cache_free(xfs_rmap_intent_cache, rmap);
+	ri = container_of(item, struct xfs_rmap_intent, ri_list);
+	error = xfs_rmap_finish_one(tp, ri, state);
+	kmem_cache_free(xfs_rmap_intent_cache, ri);
 	return error;
 }
 
@@ -249,10 +242,10 @@ xfs_rmap_update_cancel_item(
 	struct xfs_mount		*mp,
 	struct list_head		*item)
 {
-	struct xfs_rmap_intent		*rmap;
+	struct xfs_rmap_intent		*ri;
 
-	rmap = container_of(item, struct xfs_rmap_intent, ri_list);
-	kmem_cache_free(xfs_rmap_intent_cache, rmap);
+	ri = container_of(item, struct xfs_rmap_intent, ri_list);
+	kmem_cache_free(xfs_rmap_intent_cache, ri);
 }
 
 const struct xfs_defer_op_type xfs_rmap_update_defer_type = {
