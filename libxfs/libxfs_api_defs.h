de59fc2b1d4e13164de3e1b76f0492ec95b21375
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:33:04 -0700

    xfs_repair: check existing realtime refcountbt entries against observed refcounts
    Once we've finished collecting reverse mapping observations from the
    metadata scan, check those observations against the realtime refcount
    btree (particularly if we're in -n mode) to detect rtrefcountbt
    problems.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -225,6 +225,7 @@
 
 #define xfs_rtrefcountbt_absolute_maxlevels	libxfs_rtrefcountbt_absolute_maxlevels
 #define xfs_rtrefcountbt_droot_maxrecs	libxfs_rtrefcountbt_droot_maxrecs
+#define xfs_rtrefcountbt_init_cursor	libxfs_rtrefcountbt_init_cursor
 #define xfs_rtrefcountbt_maxlevels_ondisk	libxfs_rtrefcountbt_maxlevels_ondisk
 #define xfs_rtrefcountbt_maxrecs	libxfs_rtrefcountbt_maxrecs
 
