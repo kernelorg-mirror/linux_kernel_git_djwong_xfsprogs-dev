9e7e55553df805eb378152ebd7af032dbbe5c319
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:07:00 -0700

    xfs: connect in-memory btrees to xfiles
    Add to our stubbed-out in-memory btrees the ability to connect them with
    an actual in-memory backing file (aka xfiles) and the necessary pieces
    to track free space in the xfile and flush dirty xfbtree buffers on
    demand, which we'll need for online repair.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfile.h
+++ libxfs/xfile.h
@@ -47,6 +47,9 @@ xfile_obj_store(struct xfile *xf, void *buf, size_t count, loff_t pos)
 	return 0;
 }
 
+void xfile_discard(struct xfile *xf, loff_t pos, uint64_t count);
+int xfile_prealloc(struct xfile *xf, loff_t pos, uint64_t count);
+
 struct xfile_stat {
 	loff_t			size;
 	unsigned long long	bytes;
