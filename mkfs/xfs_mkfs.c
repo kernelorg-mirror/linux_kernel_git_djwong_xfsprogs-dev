ff1f031ff298607a88e0253b1f79e90777781858
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:01:38 -0700

    libxfs: teach buftargs to maintain their own buffer hashtable
    Currently, cached buffers are indexed with a single global bcache
    structure.  This works ok for the limited use case where we only support
    reading from the data device, but will fail badly when we want to
    support buffers from in-memory btrees.  Move the bcache structure into
    the buftarg.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -4518,7 +4518,7 @@ main(
 	/*
 	 * we need the libxfs buffer cache from here on in.
 	 */
-	libxfs_buftarg_init(mp, xi.ddev, xi.logdev, xi.rtdev);
+	libxfs_buftarg_init(mp, xi.ddev, xi.logdev, xi.rtdev, 0);
 
 	/*
 	 * Before we mount the filesystem we need to make sure the devices have
@@ -4587,7 +4587,7 @@ main(
 	 * Need to drop references to inodes we still hold, first.
 	 */
 	libxfs_rtmount_destroy(mp);
-	libxfs_bcache_purge();
+	libxfs_bcache_purge(mp);
 
 	/*
 	 * Mark the filesystem ok.
