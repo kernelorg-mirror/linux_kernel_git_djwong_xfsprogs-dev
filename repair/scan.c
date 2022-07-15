401982442395b0fd3e355cdfd9c47a50a69383ea
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:27:25 -0700

    xfs_repair: find and mark the rtrmapbt inodes
    Make sure that we find the realtime rmapbt inodes and mark them
    appropriately, just in case we find a rogue inode claiming to be an
    rtrmap, or garbage in the metadata directory tree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/scan.c
+++ repair/scan.c
@@ -1294,7 +1294,7 @@ advance:
 
 out:
 	if (suspect)
-		rmap_avoid_check();
+		rmap_avoid_check(mp);
 }
 
 int
@@ -1674,7 +1674,7 @@ _("bad %s btree ptr 0x%llx in ino %" PRIu64 "\n"),
 
 out:
 	if (hdr_errors || suspect) {
-		rmap_avoid_check();
+		rmap_avoid_check(mp);
 		return 1;
 	}
 	return 0;
@@ -2741,7 +2741,7 @@ validate_agf(
 		if (levels == 0 || levels > mp->m_rmap_maxlevels) {
 			do_warn(_("bad levels %u for rmapbt root, agno %d\n"),
 				levels, agno);
-			rmap_avoid_check();
+			rmap_avoid_check(mp);
 		}
 
 		bno = be32_to_cpu(agf->agf_roots[XFS_BTNUM_RMAP]);
@@ -2756,7 +2756,7 @@ validate_agf(
 		} else {
 			do_warn(_("bad agbno %u for rmapbt root, agno %d\n"),
 				bno, agno);
-			rmap_avoid_check();
+			rmap_avoid_check(mp);
 		}
 	}
 
