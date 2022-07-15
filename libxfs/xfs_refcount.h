641276a0b38ddeda9b6a0ab6c0001d7ad4a919f1
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:29:59 -0700

    xfs: prepare refcount functions to deal with rtrefcountbt
    Prepare the high-level refcount functions to deal with the new realtime
    refcountbt and its slightly different conventions.  Provide the ability
    to talk to either refcountbt or rtrefcountbt formats from the same high
    level code.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_refcount.h
+++ libxfs/xfs_refcount.h
@@ -90,7 +90,8 @@ extern int xfs_refcount_scan_keyfill(struct xfs_btree_cur *cur,
 		enum xfs_rcext_domain domain, xfs_agblock_t bno,
 		xfs_extlen_t len, enum xfs_btree_keyfill *outcome);
 union xfs_btree_rec;
-extern void xfs_refcount_btrec_to_irec(const union xfs_btree_rec *rec,
+void xfs_refcount_btrec_to_irec(struct xfs_btree_cur *cur,
+		const union xfs_btree_rec *rec,
 		struct xfs_refcount_irec *irec);
 extern int xfs_refcount_insert(struct xfs_btree_cur *cur,
 		struct xfs_refcount_irec *irec, int *stat);
