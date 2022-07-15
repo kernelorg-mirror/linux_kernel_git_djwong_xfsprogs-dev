c9784590f50f1f5cad5b9f600f1c3f86c015aa7e
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:22:59 -0700

    xfs: allow inode-based btrees to reserve space in the data device
    Create a new space reservation scheme so that btree metadata for the
    realtime volume can reserve space in the data device to avoid space
    underruns.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_errortag.h
+++ libxfs/xfs_errortag.h
@@ -63,7 +63,8 @@
 #define XFS_ERRTAG_DA_LEAF_SPLIT			40
 #define XFS_ERRTAG_ATTR_LEAF_TO_NODE			41
 #define XFS_ERRTAG_SWAPEXT_FINISH_ONE			42
-#define XFS_ERRTAG_MAX					43
+#define XFS_ERRTAG_IMETA_RESV_CRITICAL			43
+#define XFS_ERRTAG_MAX					44
 
 /*
  * Random factors for above tags, 1 means always, 2 means 1/2 time, etc.
@@ -111,5 +112,6 @@
 #define XFS_RANDOM_DA_LEAF_SPLIT			1
 #define XFS_RANDOM_ATTR_LEAF_TO_NODE			1
 #define XFS_RANDOM_SWAPEXT_FINISH_ONE			1
+#define XFS_RANDOM_IMETA_RESV_CRITICAL			4
 
 #endif /* __XFS_ERRORTAG_H_ */
