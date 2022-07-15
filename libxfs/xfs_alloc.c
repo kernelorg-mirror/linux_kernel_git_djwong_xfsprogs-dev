be35efc283c1c184270055c8b854eb9dbfdb70fd
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:23:05 -0700

    xfs: clean up extent free log intent item tracepoint callsites
    Pass the incore EFI structure to the tracepoints instead of open-coding
    the argument passing.  This cleans up the call sites a bit.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_alloc.c
+++ libxfs/xfs_alloc.c
@@ -2583,7 +2583,7 @@ xfs_defer_agfl_block(
 	xefi->xefi_blockcount = 1;
 	xefi->xefi_owner = oinfo->oi_owner;
 
-	trace_xfs_agfl_free_defer(mp, agno, 0, agbno, 1);
+	trace_xfs_agfl_free_defer(mp, xefi);
 
 	xfs_extent_free_get_group(mp, xefi);
 	xfs_defer_add(tp, XFS_DEFER_OPS_TYPE_AGFL_FREE, &xefi->xefi_list);
@@ -2637,9 +2637,8 @@ __xfs_free_extent_later(
 	} else {
 		xefi->xefi_owner = XFS_RMAP_OWN_NULL;
 	}
-	trace_xfs_bmap_free_defer(mp,
-			XFS_FSB_TO_AGNO(tp->t_mountp, bno), 0,
-			XFS_FSB_TO_AGBNO(tp->t_mountp, bno), len);
+
+	trace_xfs_extent_free_defer(mp, xefi);
 
 	xfs_extent_free_get_group(mp, xefi);
 	xfs_defer_add(tp, XFS_DEFER_OPS_TYPE_FREE, &xefi->xefi_list);
