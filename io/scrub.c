569cc9e8f4ca7a6c900d6ac982c81c154a083b48
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:34:53 -0700

    xfs: upgrade filesystem features
    Add the ability to upgrade *some* filesystem features.  Note that you'll
    have to run online fsck immediately afterwards to build metadata!
    
    XXX DO NOT MERGE
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- io/scrub.c
+++ io/scrub.c
@@ -19,6 +19,7 @@
 static struct cmdinfo scrub_cmd;
 static struct cmdinfo repair_cmd;
 static const struct cmdinfo scrubv_cmd;
+static const struct cmdinfo addfeature_cmd;
 
 static void
 scrub_help(void)
@@ -234,6 +235,7 @@ scrub_init(void)
 
 	add_command(&scrub_cmd);
 	add_command(&scrubv_cmd);
+	add_command(&addfeature_cmd);
 }
 
 static void
@@ -649,4 +651,64 @@ static const struct cmdinfo scrubv_cmd = {
 	.flags		= CMD_NOMAP_OK,
 	.oneline	= N_("vectored metadata scrub"),
 	.help		= scrubv_help,
+};
+
+static void
+addfeature_help(void)
+{
+	printf(_(
+"\n"
+" Upgrade the feature set of the mounted filesystem.\n"
+"\n"
+"Supported features: bigtime and inobtcount.\n"));
+}
+
+static int
+addfeature_f(
+	int		argc,
+	char		**argv)
+{
+	__u64		flags = 0;
+	int		c;
+	int		error;
+
+	while ((c = getopt(argc, argv, "")) != EOF) {
+		switch (c) {
+		default:
+			addfeature_help();
+			return 0;
+		}
+	}
+
+	for (c = optind; c < argc; c++) {
+		if (!strcmp(argv[c], "bigtime"))
+			flags |= XFS_FSOP_GEOM_FLAGS_BIGTIME;
+		else if (!strcmp(argv[c], "inobtcount"))
+			flags |= XFS_FSOP_GEOM_FLAGS_INOBTCNT;
+		else {
+			addfeature_help();
+			return 1;
+		}
+	}
+
+	if (!expert) {
+		printf(_("addfeature requires expert mode.\n"));
+		return 1;
+	}
+
+	error = ioctl(file->fd, XFS_IOC_ADDFEATURE, &flags);
+	if (error)
+		perror("addfeature");
+
+	return 0;
+}
+
+static const struct cmdinfo addfeature_cmd = {
+	.name		= "addfeature",
+	.cfunc		= addfeature_f,
+	.argmin		= -1,
+	.argmax		= -1,
+	.flags		= CMD_NOMAP_OK,
+	.oneline	= N_("add bigtime"),
+	.help		= addfeature_help,
 };
