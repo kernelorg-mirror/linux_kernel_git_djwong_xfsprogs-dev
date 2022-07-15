059a13266f903430c286b8a6092d5c7b859954b1
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:14:02 -0700

    libxfs: backport inode init code from the kernel
    Reorganize the userspace inode initialization code to more closely resemble
    its kernel counterpart.  This is preparation to hoist the initialization
    routines to libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_priv.h
+++ libxfs/libxfs_priv.h
@@ -219,6 +219,12 @@ static inline bool WARN_ON(bool expr) {
 	(inode)->i_version = (version);	\
 } while (0)
 
+struct inode;
+struct user_namespace;
+
+void inode_init_owner(struct user_namespace *mnt_userns, struct inode *inode,
+		      const struct inode *dir, umode_t mode);
+
 #define __must_check                    __attribute__((__warn_unused_result__))
 
 /*
