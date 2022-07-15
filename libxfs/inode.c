64eaba7df4015f8bfe39a4af3322ee878b8daaea
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:14:40 -0700

    xfs: hoist xfs_iunlink to libxfs
    Move xfs_iunlink and xfs_iunlink_remove to libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/inode.c
+++ libxfs/inode.c
@@ -169,6 +169,8 @@ libxfs_iget(
 	ip->i_mount = mp;
 	ip->i_diflags2 = mp->m_ino_geo.new_diflags2;
 	ip->i_af.if_format = XFS_DINODE_FMT_EXTENTS;
+	ip->i_next_unlinked = NULLAGINO;
+	ip->i_prev_unlinked = NULLAGINO;
 	spin_lock_init(&VFS_I(ip)->i_lock);
 
 	error = xfs_imap(mp, tp, ip->i_ino, &ip->i_imap, 0);
