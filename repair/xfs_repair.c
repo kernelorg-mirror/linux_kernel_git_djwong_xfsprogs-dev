4ad8125b6d01e25a54c8f05027ba32a33bc71e91
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:22:06 -0700

    xfs_repair: allow sysadmins to add metadata directories
    Allow the sysadmin to use xfs_repair to upgrade an existing filesystem
    to support metadata directories.  This will be needed to upgrade
    filesystems to support realtime rmap and reflink.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/xfs_repair.c
+++ repair/xfs_repair.c
@@ -71,6 +71,7 @@ enum c_opt_nums {
 	CONVERT_FINOBT,
 	CONVERT_REFLINK,
 	CONVERT_RMAPBT,
+	CONVERT_METADIR,
 	C_MAX_OPTS,
 };
 
@@ -82,6 +83,7 @@ static char *c_opts[] = {
 	[CONVERT_FINOBT]	= "finobt",
 	[CONVERT_REFLINK]	= "reflink",
 	[CONVERT_RMAPBT]	= "rmapbt",
+	[CONVERT_METADIR]	= "metadir",
 	[C_MAX_OPTS]		= NULL,
 };
 
@@ -367,6 +369,15 @@ process_args(int argc, char **argv)
 						do_abort(
 		_("-c rmapbt only supports upgrades\n"));
 					add_rmapbt = true;
+					break;
+				case CONVERT_METADIR:
+					if (!val)
+						do_abort(
+		_("-c metadir requires a parameter\n"));
+					if (strtol(val, NULL, 0) != 1)
+						do_abort(
+		_("-c metadir only supports upgrades\n"));
+					add_metadir = true;
 					break;
 				default:
 					unknown('c', val);
