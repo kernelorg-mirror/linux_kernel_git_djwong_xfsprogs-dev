ed1e491c9c83392b266dc29aabdd886842866d59
Darrick J. Wong <djwong@kernel.org>
2022-07-15 13:55:50 -0700

    xfs: teach xfs_btree_has_record to return false if there are gaps
    The current implementation of xfs_btree_has_record returns true if it
    finds /any/ record within the given range.  Unfortunately, that's not
    what the predicate is supposed to do -- it's supposed to test if the
    /entire/ range is covered by records.
    
    Therefore, enhance the routine to check that the first record it
    encounters starts earlier or at the same point as the low key, the last
    record ends at or after the same point as the high key, and that there
    aren't any gaps in the records.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_refcount.h
+++ libxfs/xfs_refcount.h
@@ -78,9 +78,9 @@ extern int xfs_refcount_recover_cow_leftovers(struct xfs_mount *mp,
  */
 #define XFS_REFCOUNT_ITEM_OVERHEAD	32
 
-extern int xfs_refcount_has_record(struct xfs_btree_cur *cur,
+extern int xfs_refcount_scan_keyfill(struct xfs_btree_cur *cur,
 		enum xfs_rcext_domain domain, xfs_agblock_t bno,
-		xfs_extlen_t len, bool *exists);
+		xfs_extlen_t len, enum xfs_btree_keyfill *outcome);
 union xfs_btree_rec;
 extern void xfs_refcount_btrec_to_irec(const union xfs_btree_rec *rec,
 		struct xfs_refcount_irec *irec);
