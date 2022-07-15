0f55af6e9dff1ff094f6515ccdbc769e5faf144e
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:24:40 -0700

    xfs_repair: check existing realtime rmapbt entries against observed rmaps
    Once we've finished collecting reverse mapping observations from the
    metadata scan, check those observations against the realtime rmap btree
    (particularly if we're in -n mode) to detect rtrmapbt problems.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -230,6 +230,7 @@
 #define xfs_rtsummary_wordcount		libxfs_rtsummary_wordcount
 
 #define xfs_rtfree_extent		libxfs_rtfree_extent
+#define xfs_rtgroup_get			libxfs_rtgroup_get
 #define xfs_rtgroup_put			libxfs_rtgroup_put
 #define xfs_rtgroup_update_secondary_sbs	libxfs_rtgroup_update_secondary_sbs
 #define xfs_rtgroup_update_super	libxfs_rtgroup_update_super
