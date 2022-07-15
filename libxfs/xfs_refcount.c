7e614b7c9cafe757fefc4854a373e895c824b455
Darrick J. Wong <djwong@kernel.org>
2022-07-15 13:55:50 -0700

    xfs: mask key comparisons for keyspace fill scans
    For keyspace fullness scans, we want to be able to mask off the parts of
    the key that we don't care about.  For most btree types we /do/ want the
    full keyspace, but for checking that a given space usage also has a full
    complement of rmapbt records (even if different/multiple owners) we need
    this masking so that we only track sparseness of rm_startblock, not the
    whole keyspace (which is extremely sparse).
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_refcount.c
+++ libxfs/xfs_refcount.c
@@ -1784,7 +1784,7 @@ xfs_refcount_scan_keyfill(
 	memset(&high, 0xFF, sizeof(high));
 	high.rc.rc_startblock = bno + len - 1;
 
-	return xfs_btree_scan_keyfill(cur, &low, &high, outcome);
+	return xfs_btree_scan_keyfill(cur, &low, &high, NULL, outcome);
 }
 
 int __init
