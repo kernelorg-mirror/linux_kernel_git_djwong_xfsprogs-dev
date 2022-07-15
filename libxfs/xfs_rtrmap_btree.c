e31e76e16b91cb0130b20bff683082f66e8d3593
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:24:23 -0700

    xfs: create routine to allocate and initialize a realtime rmap btree inode
    Create a library routine to allocate and initialize an empty realtime
    rmapbt inode.  We'll use this for growfs, mkfs, and repair.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtrmap_btree.c
+++ libxfs/xfs_rtrmap_btree.c
@@ -23,6 +23,7 @@
 #include "xfs_trace.h"
 #include "xfs_cksum.h"
 #include "xfs_bmap.h"
+#include "xfs_imeta.h"
 
 static struct kmem_cache	*xfs_rtrmapbt_cur_cache;
 
@@ -847,4 +848,46 @@ xfs_iflush_rtrmap(
 			xfs_inode_fork_size(ip, XFS_DATA_FORK));
 	xfs_rtrmapbt_to_disk(ip->i_mount, ifp->if_broot, ifp->if_broot_bytes,
 			dfp, XFS_DFORK_SIZE(dip, ip->i_mount, XFS_DATA_FORK));
+}
+
+/*
+ * Create a realtime rmap btree inode.
+ *
+ * Regardless of the return value, the caller must clean up @ic.  If a new
+ * inode is returned through *ipp, the caller must finish setting up the incore
+ * inode and release it.
+ */
+int
+xfs_rtrmapbt_create(
+	struct xfs_trans	**tpp,
+	struct xfs_imeta_update	*upd,
+	struct xfs_inode	**ipp)
+{
+	struct xfs_mount	*mp = (*tpp)->t_mountp;
+	struct xfs_ifork	*ifp;
+	struct xfs_inode	*ip;
+	int			error;
+
+	*ipp = NULL;
+
+	error = xfs_imeta_create(tpp, &XFS_IMETA_RTRMAPBT, S_IFREG, 0, ipp,
+			upd);
+	if (error)
+		return error;
+
+	ip = *ipp;
+	ifp = &ip->i_df;
+	ifp->if_format = XFS_DINODE_FMT_RMAP;
+	ASSERT(ifp->if_broot_bytes == 0);
+	ASSERT(ifp->if_bytes == 0);
+
+	/* Initialize the empty incore btree root. */
+	xfs_iroot_alloc(ip, XFS_DATA_FORK,
+			xfs_rtrmap_broot_space_calc(mp, 0, 0));
+	xfs_btree_init_block_int(ip->i_mount, ifp->if_broot,
+			XFS_BUF_DADDR_NULL, XFS_BTNUM_RTRMAP, 0, 0, ip->i_ino,
+			XFS_BTREE_LONG_PTRS | XFS_BTREE_CRC_BLOCKS);
+	xfs_trans_log_inode(*tpp, ip, XFS_ILOG_CORE | XFS_ILOG_DBROOT);
+
+	return 0;
 }
