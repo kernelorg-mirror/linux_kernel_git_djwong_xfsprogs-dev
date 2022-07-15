77dc62fcf80fe929f4f29e7ddef83d364b87e697
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:22:03 -0700

    xfs: advertise metadata directory feature
    Advertise the existence of the metadata directory feature; this will be
    used by scrub to decide if it needs to scan the metadir too.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_fs.h
+++ libxfs/xfs_fs.h
@@ -239,6 +239,7 @@ typedef struct xfs_fsop_resblks {
 #define XFS_FSOP_GEOM_FLAGS_BIGTIME	(1 << 21) /* 64-bit nsec timestamps */
 #define XFS_FSOP_GEOM_FLAGS_INOBTCNT	(1 << 22) /* inobt btree counter */
 #define XFS_FSOP_GEOM_FLAGS_NREXT64	(1 << 23) /* large extent counters */
+#define XFS_FSOP_GEOM_FLAGS_METADIR	(1 << 30) /* metadata directories */
 #define XFS_FSOP_GEOM_FLAGS_ATOMIC_SWAP	(1 << 31) /* atomic file extent swap */
 
 /*
