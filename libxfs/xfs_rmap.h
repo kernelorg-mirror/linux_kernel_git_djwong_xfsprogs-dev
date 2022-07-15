8c94eb68f7d3d1c1ec4ea04263665375cca0d9f5
Darrick J. Wong <djwong@kernel.org>
2022-07-15 13:55:50 -0700

    xfs: teach xfs_btree_has_record to return false if there are gaps
    The current implementation of xfs_btree_has_record returns true if it
    finds /any/ record within the given range.  Unfortunately, that's not
    what the predicate is supposed to do -- it's supposed to test if the
    /entire/ range is covered by records.
    
    Therefore, enhance the routine to check that the first record it
    encounters starts earlier or at the same point as the low key, the last
    record ends at or after the same point as the high key, and that there
    aren't any gaps in the records.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rmap.h
+++ libxfs/xfs_rmap.h
@@ -194,8 +194,8 @@ int xfs_rmap_compare(const struct xfs_rmap_irec *a,
 union xfs_btree_rec;
 int xfs_rmap_btrec_to_irec(const union xfs_btree_rec *rec,
 		struct xfs_rmap_irec *irec);
-int xfs_rmap_has_record(struct xfs_btree_cur *cur, xfs_agblock_t bno,
-		xfs_extlen_t len, bool *exists);
+int xfs_rmap_scan_keyfill(struct xfs_btree_cur *cur, xfs_agblock_t bno,
+		xfs_extlen_t len, enum xfs_btree_keyfill *outcome);
 int xfs_rmap_record_exists(struct xfs_btree_cur *cur, xfs_agblock_t bno,
 		xfs_extlen_t len, const struct xfs_owner_info *oinfo,
 		bool *has_rmap);
