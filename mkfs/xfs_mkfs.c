4693f3c56cc3eb391215bf166d8e0e125c1cc87e
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:08:55 -0700

    mkfs: enable reverse mapping by default
    Now that the scrub part of online fsck is feature complete (scrub and
    health reporting are done) there's actually a compelling story for
    having the reverse mappings enabled.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -4366,7 +4366,7 @@ main(
 			.dirftype = true,
 			.finobt = true,
 			.spinodes = true,
-			.rmapbt = false,
+			.rmapbt = true,
 			.reflink = true,
 			.inobtcnt = true,
 			.parent_pointers = false,
