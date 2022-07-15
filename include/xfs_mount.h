51b112aa767f8147ce69357696d290a131a4c499
Darrick J. Wong <djwong@kernel.org>
2022-07-15 13:55:49 -0700

    xfs: allow queued AG intents to drain before scrubbing
    When a writer thread executes a chain of log intent items, the AG header
    buffer locks will cycle during a transaction roll to get from one intent
    item to the next in a chain.  Although scrub takes all AG header buffer
    locks, this isn't sufficient to guard against scrub checking an AG while
    that writer thread is in the middle of finishing a chain because there's
    no higher level locking primitive guarding allocation groups.
    
    When there's a collision, cross-referencing between data structures
    (e.g. rmapbt and refcountbt) yields false corruption events; if repair
    is running, this results in incorrect repairs, which is catastrophic.
    
    Fix this by adding to the perag structure the count of active intents
    and make scrub wait until it has both AG header buffer locks and the
    intent counter reaches zero.  This is a little stupid since transactions
    can queue intents without taking buffer locks, but it's not the end of
    the world for scrub to wait (in KILLABLE state) for those transactions.
    
    In the next patch we'll improve on this facility, but this patch
    provides the basic functionality.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/xfs_mount.h
+++ include/xfs_mount.h
@@ -271,4 +271,13 @@ struct xfs_dquot {
 	int		q_type;
 };
 
+/* Dummy xfs_drain so that libxfs compiles. */
+struct xfs_drain { /* empty */ };
+
+static inline void xfs_perag_bump_intents(struct xfs_perag *pag) { }
+static inline void xfs_perag_drop_intents(struct xfs_perag *pag) { }
+
+#define xfs_drain_free(dr)		((void)0)
+#define xfs_drain_init(dr)		((void)0)
+
 #endif	/* __XFS_MOUNT_H__ */
