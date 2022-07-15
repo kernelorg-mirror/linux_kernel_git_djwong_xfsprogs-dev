213621e3eab38c8c9d1b13516c66de388ba1892a
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:14:40 -0700

    xfs: hoist xfs_iunlink to libxfs
    Move xfs_iunlink and xfs_iunlink_remove to libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_inode_util.h
+++ libxfs/xfs_inode_util.h
@@ -55,6 +55,10 @@ void xfs_trans_ichgtime(struct xfs_trans *tp, struct xfs_inode *ip, int flags);
 void xfs_inode_init(struct xfs_trans *tp, const struct xfs_icreate_args *args,
 		struct xfs_inode *ip);
 
+int xfs_iunlink(struct xfs_trans *tp, struct xfs_inode *ip);
+int xfs_iunlink_remove(struct xfs_trans *tp, struct xfs_perag *pag,
+		struct xfs_inode *ip);
+
 /* The libxfs client must provide this group of helper functions. */
 
 /* Handle legacy Irix sgid inheritance quirks. */
