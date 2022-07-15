90e2d3d36f31cd4931f964efb10ca6433942e9b7
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:27:25 -0700

    xfs_repair: reserve per-AG space while rebuilding rt metadata
    Realtime metadata btrees can consume quite a bit of space on a full
    filesystem.  Since the metadata are just regular files, we need to
    make the per-AG reservations to avoid overfilling any of the AGs while
    rebuilding metadata.  This avoids the situation where a filesystem comes
    straight from repair and immediately trips over not having enough space
    in an AG.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/phase6.c
+++ repair/phase6.c
@@ -3736,10 +3736,42 @@ traverse_ags(
 	do_inode_prefetch(mp, ag_stride, traverse_function, false, true);
 }
 
+int
+reserve_ag_blocks(
+	struct xfs_mount	*mp)
+{
+	struct xfs_perag	*pag;
+	xfs_agnumber_t		agno;
+	int			error = 0;
+	int			err2;
+
+	mp->m_finobt_nores = false;
+
+	for_each_perag(mp, agno, pag) {
+		err2 = -libxfs_ag_resv_init(pag, NULL);
+		if (err2 && !error)
+			error = err2;
+	}
+
+	return error;
+}
+
+void
+unreserve_ag_blocks(
+	struct xfs_mount	*mp)
+{
+	struct xfs_perag	*pag;
+	xfs_agnumber_t		agno;
+
+	for_each_perag(mp, agno, pag)
+		libxfs_ag_resv_free(pag);
+}
+
 void
 phase6(xfs_mount_t *mp)
 {
 	ino_tree_node_t		*irec;
+	bool			reserve_perag;
 	int			error;
 	int			i;
 
@@ -3776,6 +3808,17 @@ phase6(xfs_mount_t *mp)
 		need_metadir_dotdot = 0;
 	} else if (need_metadir_inode) {
 		do_warn(_("would reinitialize metadata root directory\n"));
+	}
+
+	reserve_perag = xfs_has_realtime(mp) && !no_modify;
+	if (reserve_perag) {
+		error = reserve_ag_blocks(mp);
+		if (error) {
+			if (error != ENOSPC)
+				do_warn(
+	_("could not reserve per-AG space to rebuild realtime metadata"));
+			reserve_perag = false;
+		}
 	}
 
 	if (need_rbmino)  {
@@ -3821,6 +3864,9 @@ _("        - resetting contents of realtime bitmap and summary inodes\n"));
 	_("Realtime reverse mapping btree could not be rebuilt, error=%d\n"),
 					error);
 	}
+
+	if (reserve_perag)
+		unreserve_ag_blocks(mp);
 
 	reattach_metadir_quota_inodes(mp);
 
