d2ca08c9ea30c4ffccb118bca55f752f01abe38b
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:09:44 -0700

    xfs_scrub: track repair items by principal, not by individual repairs
    Create a new structure to track scrub and repair state by principal
    filesystem object (e.g. ag number or inode number/generation) so that we
    can more easily examine and ensure that we satisfy repair order
    dependencies.  This transposition will eventually enable bulk scrub
    operations and will also save a lot of memory if a given object needs a
    lot of work.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/phase5.c
+++ scrub/phase5.c
@@ -384,6 +384,7 @@ out:
 }
 
 struct iscan_item {
+	struct scrub_item	sri;
 	struct action_list	alist;
 	bool			*abortedp;
 	unsigned int		scrub_type;
@@ -411,7 +412,8 @@ iscan_worker(
 		nanosleep(&tv, NULL);
 	}
 
-	ret = scrub_meta_type(ctx, item->scrub_type, 0, &item->alist);
+	ret = scrub_meta_type(ctx, item->scrub_type, 0, &item->alist,
+			&item->sri);
 	if (ret) {
 		str_liberror(ctx, ret, _("checking iscan metadata"));
 		*item->abortedp = true;
@@ -449,6 +451,7 @@ queue_iscan(
 		str_liberror(ctx, ret, _("setting up iscan"));
 		return ret;
 	}
+	scrub_item_init_fs(&item->sri);
 	action_list_init(&item->alist);
 	item->scrub_type = scrub_type;
 	item->abortedp = abortedp;
