a1b9d1d02a0f5ef1098c1cf7ae55e6ffb6ce5514
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:32:55 -0700

    xfs_db: support the realtime refcountbt
    Wire up various parts of xfs_db for realtime refcount support.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -223,7 +223,9 @@
 
 #define xfs_rtfree_extent		libxfs_rtfree_extent
 
+#define xfs_rtrefcountbt_absolute_maxlevels	libxfs_rtrefcountbt_absolute_maxlevels
 #define xfs_rtrefcountbt_droot_maxrecs	libxfs_rtrefcountbt_droot_maxrecs
+#define xfs_rtrefcountbt_maxlevels_ondisk	libxfs_rtrefcountbt_maxlevels_ondisk
 #define xfs_rtrefcountbt_maxrecs	libxfs_rtrefcountbt_maxrecs
 
 #define xfs_rtrmapbt_calc_reserves	libxfs_rtrmapbt_calc_reserves
