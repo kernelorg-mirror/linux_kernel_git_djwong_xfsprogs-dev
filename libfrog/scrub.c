a716d1e85ce4da74f28ffb147458998302054658
Darrick J. Wong <djwong@kernel.org>
2022-08-18 13:34:07 -0700

    xfs: scrub the realtime group superblock
    Enable scrubbing of realtime group superblocks.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libfrog/scrub.c
+++ libfrog/scrub.c
@@ -149,6 +149,11 @@ const struct xfrog_scrub_descr xfrog_scrubbers[XFS_SCRUB_TYPE_NR] = {
 		.descr	= "retained health records",
 		.group	= XFROG_SCRUB_GROUP_NONE,
 	},
+	[XFS_SCRUB_TYPE_RGSUPER] = {
+		.name	= "rgsuper",
+		.descr	= "realtime group superblock",
+		.group	= XFROG_SCRUB_GROUP_RTGROUP,
+	},
 };
 #undef DEP
 
