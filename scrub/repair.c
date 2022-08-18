85f8d65cefda473c282709968bdd787d637bcd15
Darrick J. Wong <djwong@kernel.org>
2022-08-18 13:34:07 -0700

    xfs: scrub the realtime group superblock
    Enable scrubbing of realtime group superblocks.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/repair.c
+++ scrub/repair.c
@@ -108,6 +108,7 @@ xfs_repair_metadata(
 	switch (xfrog_scrubbers[scrub_type].group) {
 	case XFROG_SCRUB_GROUP_AGHEADER:
 	case XFROG_SCRUB_GROUP_PERAG:
+	case XFROG_SCRUB_GROUP_RTGROUP:
 		meta.sm_agno = sri->sri_agno;
 		break;
 	case XFROG_SCRUB_GROUP_INODE:
@@ -412,6 +413,7 @@ repair_item_difficulty(
 		case XFS_SCRUB_TYPE_REFCNTBT:
 		case XFS_SCRUB_TYPE_RTBITMAP:
 		case XFS_SCRUB_TYPE_RTSUM:
+		case XFS_SCRUB_TYPE_RGSUPER:
 			ret |= REPAIR_DIFFICULTY_PRIMARY;
 			break;
 		}
