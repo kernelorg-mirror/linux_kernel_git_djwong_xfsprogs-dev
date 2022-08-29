8063cc520055ac9f30b4d706dbf15bbfd827190b
Darrick J. Wong <djwong@kernel.org>
2022-08-29 09:31:31 -0700

    xfs: namespace the maximum length/refcount symbols
    Actually namespace these variables properly, so that readers can tell
    that this is an XFS symbol, and that it's for the refcount
    functionality.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_refcount.c
+++ libxfs/xfs_refcount.c
@@ -125,7 +125,7 @@ xfs_refcount_check_perag_irec(
 	struct xfs_perag		*pag,
 	const struct xfs_refcount_irec	*irec)
 {
-	if (irec->rc_blockcount == 0 || irec->rc_blockcount > MAXREFCEXTLEN)
+	if (irec->rc_blockcount == 0 || irec->rc_blockcount > XFS_REFC_LEN_MAX)
 		return __this_address;
 
 	if (!xfs_refcount_check_domain(irec))
@@ -135,7 +135,7 @@ xfs_refcount_check_perag_irec(
 	if (!xfs_verify_agbext(pag, irec->rc_startblock, irec->rc_blockcount))
 		return __this_address;
 
-	if (irec->rc_refcount == 0 || irec->rc_refcount > MAXREFCOUNT)
+	if (irec->rc_refcount == 0 || irec->rc_refcount > XFS_REFC_REFCOUNT_MAX)
 		return __this_address;
 
 	return NULL;
@@ -901,7 +901,7 @@ xfs_refcount_merge_extents(
 	    xfs_refc_valid(&cleft) && xfs_refc_valid(&cright) && cequal &&
 	    left.rc_refcount == cleft.rc_refcount + adjust &&
 	    right.rc_refcount == cleft.rc_refcount + adjust &&
-	    ulen < MAXREFCEXTLEN) {
+	    ulen < XFS_REFC_LEN_MAX) {
 		*shape_changed = true;
 		return xfs_refcount_merge_center_extents(cur, &left, &cleft,
 				&right, ulen, aglen);
@@ -911,7 +911,7 @@ xfs_refcount_merge_extents(
 	ulen = (unsigned long long)left.rc_blockcount + cleft.rc_blockcount;
 	if (xfs_refc_valid(&left) && xfs_refc_valid(&cleft) &&
 	    left.rc_refcount == cleft.rc_refcount + adjust &&
-	    ulen < MAXREFCEXTLEN) {
+	    ulen < XFS_REFC_LEN_MAX) {
 		*shape_changed = true;
 		error = xfs_refcount_merge_left_extent(cur, &left, &cleft,
 				agbno, aglen);
@@ -930,7 +930,7 @@ xfs_refcount_merge_extents(
 	ulen = (unsigned long long)right.rc_blockcount + cright.rc_blockcount;
 	if (xfs_refc_valid(&right) && xfs_refc_valid(&cright) &&
 	    right.rc_refcount == cright.rc_refcount + adjust &&
-	    ulen < MAXREFCEXTLEN) {
+	    ulen < XFS_REFC_LEN_MAX) {
 		*shape_changed = true;
 		return xfs_refcount_merge_right_extent(cur, &right, &cright,
 				aglen);
@@ -1086,7 +1086,7 @@ xfs_refcount_adjust_extents(
 		 * Adjust the reference count and either update the tree
 		 * (incr) or free the blocks (decr).
 		 */
-		if (ext.rc_refcount == MAXREFCOUNT)
+		if (ext.rc_refcount == XFS_REFC_REFCOUNT_MAX)
 			goto skip;
 		ext.rc_refcount += adj;
 		trace_xfs_refcount_modify_extent(cur, &ext);
