a9d24294ac2ff48ea961f833155fc09097477d26
Darrick J. Wong <djwong@kernel.org>
2022-09-20 13:26:37 -0700

    xfs_db: allow selecting logdev blocks
    Make it so that xfs_db can examine blocks on an external log device.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- db/io.h
+++ db/io.h
@@ -49,6 +49,8 @@ extern void	push_cur_and_set_type(void);
 extern void	write_cur(void);
 extern void	set_cur(const struct typ *type, xfs_daddr_t blknum,
 			int len, int ring_add, bbmap_t *bbmap);
+extern int	set_log_cur(const struct typ *type, xfs_daddr_t blknum,
+			int len, int ring_add, bbmap_t *bbmap);
 extern int	set_rt_cur(const struct typ *type, xfs_daddr_t blknum,
 			int len, int ring_add, bbmap_t *bbmap);
 extern void     ring_add(void);
