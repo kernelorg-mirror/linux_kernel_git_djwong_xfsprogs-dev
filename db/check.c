86844730bbef86cf249734c0377b99c6319022e3
Darrick J. Wong <djwong@kernel.org>
2022-09-21 11:38:25 -0700

    misc: use m_blockwsize instead of sb_blocksize for rt blocks
    In preparation to add block headers to rt bitmap and summary blocks,
    convert all the relevant calculations in the userspace tools to use the
    per-block word count instead of the raw blocksize.  This is key to
    adding this support outside of libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- db/check.c
+++ db/check.c
@@ -3628,7 +3628,7 @@ process_rtbitmap(
 	int		t;
 	xfs_rtword_t	*words;
 
-	bitsperblock = mp->m_sb.sb_blocksize * NBBY;
+	bitsperblock = mp->m_blockwsize << XFS_NBWORDLOG;
 	words = malloc(mp->m_blockwsize << XFS_WORDLOG);
 	if (!words) {
 		dbprintf(_("could not allocate rtwords buffer\n"));
@@ -3742,7 +3742,7 @@ process_rtsummary(
 		}
 
 		ondisk = xfs_rsumblock_infoptr(iocur_top->bp, 0);
-		memcpy(sfile, ondisk, mp->m_sb.sb_blocksize);
+		memcpy(sfile, ondisk, mp->m_blockwsize << XFS_WORDLOG);
 		pop_cur();
 		sfile += mp->m_blockwsize;
 	}
