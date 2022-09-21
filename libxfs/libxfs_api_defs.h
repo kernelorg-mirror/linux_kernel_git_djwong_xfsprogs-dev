7c7805218b505dcce9bf5452ddb6a399b78cc29a
Darrick J. Wong <djwong@kernel.org>
2022-09-21 10:11:04 -0700

    xfs: create helpers for rtbitmap block/wordcount computations
    Create helper functions that compute the number of blocks or words
    necessary to store the rt bitmap.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -227,6 +227,8 @@
 #define xfs_rmap_query_all		libxfs_rmap_query_all
 #define xfs_rmap_query_range		libxfs_rmap_query_range
 
+#define xfs_rtbitmap_wordcount		libxfs_rtbitmap_wordcount
+
 #define xfs_rtfree_extent		libxfs_rtfree_extent
 #define xfs_sb_from_disk		libxfs_sb_from_disk
 #define xfs_sb_quota_from_disk		libxfs_sb_quota_from_disk
