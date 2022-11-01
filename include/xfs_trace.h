b5f64918ae135c143d90337d1069e69acb6ab7ff
Darrick J. Wong <djwong@kernel.org>
2022-11-01 11:25:44 -0700

    xfs: create a function to duplicate an active perag reference
    There a few object constructor functions throughout XFS where a caller
    provides an active perag reference and the constructor wants to give the
    new object its own active reference.  Replace the open-coded logic with
    a common function to do this instead of open-coding atomic_inc logic.
    
    This new function adds a few safeguards -- it checks that there's at
    least one active reference to the perag structure passed in, and it
    records the refcount bump in the ftrace information.  This makes it much
    easier to debug refcounting problems.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/xfs_trace.h
+++ include/xfs_trace.h
@@ -180,6 +180,7 @@
 
 /* set c = c to avoid unused var warnings */
 #define trace_xfs_write_extent(a,b,c,d)	((c) = (c))
+#define trace_xfs_perag_bump(...)		((void) 0)
 #define trace_xfs_perag_get(a,b,c,d)	((c) = (c))
 #define trace_xfs_perag_get_tag(a,b,c,d) ((c) = (c))
 #define trace_xfs_perag_put(a,b,c,d)	((c) = (c))
