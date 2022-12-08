8acead5429bf5b6c7f76a1dac2932bbf0501591c
Darrick J. Wong <djwong@kernel.org>
2022-12-08 12:33:41 -0800

    xfs: define an in-memory btree for storing refcount bag info
    Create a new in-memory btree type so that we can store refcount bag info
    in a much more memory-efficient format.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_shared.h
+++ libxfs/xfs_shared.h
@@ -128,6 +128,7 @@ void	xfs_log_get_max_trans_res(struct xfs_mount *mp,
 #define	XFS_ATTR_BTREE_REF	1
 #define	XFS_DQUOT_REF		1
 #define	XFS_REFC_BTREE_REF	1
+#define	XFS_RCBAG_BTREE_REF	1
 #define	XFS_SSB_REF		0
 
 /*
