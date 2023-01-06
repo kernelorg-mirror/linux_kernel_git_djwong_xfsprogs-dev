5be1eb370bcab212afb1c940c7b9664f3b857e1b
Darrick J. Wong <djwong@kernel.org>
2023-01-06 13:57:05 -0800

    xfs_scrub: collect free space histograms during phase 7
    Collect a histogram of free space observed during phase 7.  We'll put
    this information to use in the next patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/xfs_scrub.c
+++ scrub/xfs_scrub.c
@@ -18,6 +18,7 @@
 #include "descr.h"
 #include "unicrash.h"
 #include "progress.h"
+#include "libfrog/histogram.h"
 
 /*
  * XFS Online Metadata Scrub (and Repair)
@@ -629,6 +630,8 @@ main(
 	int			ret = SCRUB_RET_SUCCESS;
 	int			error;
 
+	hist_init(&ctx.datadev_hist);
+
 	fprintf(stdout, "EXPERIMENTAL xfs_scrub program in use! Use at your own risk!\n");
 	fflush(stdout);
 
@@ -838,6 +841,8 @@ out:
 	if (progress_fp && fileno(progress_fp) != 1)
 		fclose(progress_fp);
 	unicrash_unload();
+
+	hist_free(&ctx.datadev_hist);
 
 	/*
 	 * If we're being run as a service, the return code must fit the LSB
