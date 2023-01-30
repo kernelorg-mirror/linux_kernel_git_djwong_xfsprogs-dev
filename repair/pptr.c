62cc488e12d3922e47e6c43d4dbc5ea87926bb5b
Darrick J. Wong <djwong@kernel.org>
2023-01-30 15:15:42 -0800

    xfs_repair: dump garbage parent pointer attributes
    Delete xattrs that have ATTR_PARENT set but are so garbage that they
    clearly aren't parent pointers.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/pptr.c
+++ repair/pptr.c
@@ -157,6 +157,12 @@ struct ag_pptrs {
 
 	/* Does this file have garbage xattrs with ATTR_PARENT set? */
 	bool			have_garbage;
+
+	/* xattrs that we have to remove from this file */
+	struct xfs_slab		*garbage_xattr_recs;
+
+	/* attr names associated with garbage_xattr_recs */
+	struct xfblob		*garbage_xattr_names;
 };
 
 struct file_pptr {
@@ -172,6 +178,17 @@ struct file_pptr {
 
 	/* cookie for the file dirent name */
 	xfblob_cookie		name_cookie;
+};
+
+struct garbage_xattr {
+	/* xfs_da_args.attr_filter for the attribute being removed */
+	unsigned int		attr_filter;
+
+	/* attribute name length */
+	unsigned int		attrnamelen;
+
+	/* cookie for the attribute name */
+	xfblob_cookie		attrname_cookie;
 };
 
 /* Global names storage file. */
@@ -327,7 +344,63 @@ add_parent_ptr(
 			(unsigned long long)pptr.name_cookie);
 }
 
-/* Schedule this extended attribute for deletion. */
+/* Remove garbage extended attributes that have ATTR_PARENT set. */
+static void
+remove_garbage_xattrs(
+	struct xfs_inode	*ip,
+	struct ag_pptrs		*ag_pptrs)
+{
+	struct xfs_slab_cursor	*cur;
+	struct garbage_xattr	*ga;
+	int			error;
+
+	error = -init_slab_cursor(ag_pptrs->garbage_xattr_recs, NULL, &cur);
+	if (error)
+		do_error(_("init garbage xattr cursor failed: %s\n"),
+				strerror(error));
+
+	while ((ga = pop_slab_cursor(cur)) != NULL) {
+		struct xfs_da_args	args = {
+			.dp		= ip,
+			.attr_filter	= ga->attr_filter,
+			.namelen	= ga->attrnamelen,
+		};
+		void			*buf;
+
+		buf = malloc(ga->attrnamelen);
+		if (!buf)
+			do_error(
+ _("allocating %u bytes to remove ino %llu garbage xattr failed: %s\n"),
+					ga->attrnamelen,
+					(unsigned long long)ip->i_ino,
+					strerror(errno));
+
+		error = -xfblob_load(ag_pptrs->garbage_xattr_names,
+				ga->attrname_cookie, buf, ga->attrnamelen);
+		if (error)
+			do_error(
+ _("loading garbage xattr name failed: %s\n"),
+					strerror(error));
+
+		args.name = buf;
+		error = -libxfs_attr_set(&args);
+		if (error)
+			do_error(
+ _("removing ino %llu garbage xattr failed: %s\n"),
+					(unsigned long long)ip->i_ino,
+					strerror(error));
+
+		free(buf);
+	}
+
+	free_slab_cursor(&cur);
+
+	free_slab(&ag_pptrs->garbage_xattr_recs);
+	xfblob_destroy(ag_pptrs->garbage_xattr_names);
+	ag_pptrs->garbage_xattr_names = NULL;
+}
+
+/* Schedule this ATTR_PARENT extended attribute for deletion. */
 static void
 record_garbage_xattr(
 	struct xfs_inode	*ip,
@@ -336,21 +409,53 @@ record_garbage_xattr(
 	const void		*name,
 	unsigned int		namelen)
 {
+	struct garbage_xattr	garbage_xattr = {
+		.attr_filter	= attr_filter,
+		.attrnamelen	= namelen,
+	};
+	struct xfs_mount	*mp = ip->i_mount;
+	int			error;
+
 	if (ag_pptrs->have_garbage)
-		return;
+		goto stuffit;
 	ag_pptrs->have_garbage = true;
 
 	if (no_modify) {
 		do_warn(
- _("would delete garbage parent pointer in ino %llu\n"),
+ _("would delete garbage xattr in ino %llu\n"),
 				(unsigned long long)ip->i_ino);
 		return;
 	}
 
 	do_warn(
- _("deleting garbage parent pointer in ino %llu\n"),
+ _("deleting garbage xattr in ino %llu\n"),
 			(unsigned long long)ip->i_ino);
-	/* XXX do the work */
+
+	error = -init_slab(&ag_pptrs->garbage_xattr_recs,
+			sizeof(struct garbage_xattr));
+	if (error)
+		do_error(_("init garbage xattr recs failed: %s\n"),
+				strerror(error));
+
+	error = -xfblob_create(mp, "garbage xattr names",
+			&ag_pptrs->garbage_xattr_names);
+	if (error)
+		do_error("init garbage xattr names failed: %s\n",
+				strerror(error));
+
+stuffit:
+	error = -xfblob_store(ag_pptrs->garbage_xattr_names,
+			&garbage_xattr.attrname_cookie, name, namelen);
+	if (error)
+		do_error(_("storing ino %llu garbage xattr failed: %s\n"),
+				(unsigned long long)ip->i_ino,
+				strerror(error));
+
+	error = -slab_add(ag_pptrs->garbage_xattr_recs, &garbage_xattr);
+	if (error)
+		do_error(_("storing ino %llu garbage xattr rec failed: %s\n"),
+				(unsigned long long)ip->i_ino,
+				strerror(error));
 }
 
 /* Decide if this is a directory parent pointer and stash it if so. */
@@ -761,6 +866,9 @@ check_file_parent_ptrs(
 		do_error(_("ino %llu listxattr failed: %s\n"),
 				(unsigned long long)ip->i_ino,
 				strerror(error));
+
+	if (!no_modify && ag_pptrs->have_garbage)
+		remove_garbage_xattrs(ip, ag_pptrs);
 
 	crosscheck_file_parent_ptrs(ip, ag_pptrs);
 
