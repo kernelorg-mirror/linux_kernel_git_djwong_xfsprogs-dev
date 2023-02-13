0f2cccc8081168afc13e69ef9129b4676a34ecbd
Darrick J. Wong <djwong@kernel.org>
2023-02-13 15:52:46 -0800

    xfs: repair parent pointers by scanning directories
    Walk the filesystem to rebuild parent pointer information.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/xfs_inode.h
+++ include/xfs_inode.h
@@ -175,6 +175,12 @@ static inline struct inode *VFS_I(struct xfs_inode *ip)
 	return &ip->i_vnode;
 }
 
+/* convert from xfs inode to vfs inode */
+static inline const struct inode *VFS_IC(const struct xfs_inode *ip)
+{
+	return &ip->i_vnode;
+}
+
 /* We only have i_size in the xfs inode in userspace */
 static inline loff_t i_size_read(struct inode *inode)
 {
