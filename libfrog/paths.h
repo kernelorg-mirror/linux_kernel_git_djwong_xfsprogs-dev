cdf53949a9e933c30c31cf342c7bae48e24d80bf
Darrick J. Wong <djwong@kernel.org>
2023-02-13 15:52:44 -0800

    xfs_io: move parent pointer filtering and formatting flags out of libhandle
    All this filtering and presentation stuff originates in xfs_io and
    should stay there.  The added arguments seriously complicate the basic
    iterator interface and there are no other users.
    
    While we're at it, fix a bug in path_print where it doesn't actually
    print the path.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libfrog/paths.h
+++ libfrog/paths.h
@@ -63,10 +63,10 @@ extern fs_path_t *fs_cursor_next_entry(fs_cursor_t *__cp);
 struct path_list;
 struct path_component;
 
-struct path_component *path_component_init(const char *name);
+struct path_component *path_component_init(const char *name, uint64_t ino);
 void path_component_free(struct path_component *pc);
 int path_component_change(struct path_component *pc, void *name,
-		size_t namelen);
+		size_t namelen, uint64_t ino);
 
 struct path_list *path_list_init(void);
 void path_list_free(struct path_list *path);
@@ -76,5 +76,9 @@ void path_list_add_component(struct path_list *path, struct path_component *pc);
 void path_list_del_component(struct path_list *path, struct path_component *pc);
 
 ssize_t path_list_to_string(struct path_list *path, char *buf, size_t buflen);
+
+typedef int (*path_walk_fn_t)(const char *name, uint64_t ino, void *arg);
+
+int path_walk_components(struct path_list *path, path_walk_fn_t fn, void *arg);
 
 #endif	/* __PATH_H__ */
