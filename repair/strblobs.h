ca8f1ea7380234d76ff38514af2417a96abc5811
Darrick J. Wong <djwong@kernel.org>
2023-02-15 09:25:33 -0800

    xfs_repair: move the global dirent name store to a separate object
    Abstract the main parent pointer dirent names xfblob object into a
    separate data structure to hide implementation details.  This is the
    first step towards deduplicating the names.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ repair/strblobs.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2023 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+#ifndef __REPAIR_STRBLOBS_H__
+#define __REPAIR_STRBLOBS_H__
+
+struct strblobs;
+
+int strblobs_init(struct xfs_mount *mp, const char *descr,
+		struct strblobs **sblobs);
+void strblobs_destroy(struct strblobs **sblobs);
+
+int strblobs_store(struct strblobs *sblobs, xfblob_cookie *str_cookie,
+		const unsigned char *str, unsigned int str_len);
+int strblobs_load(struct strblobs *sblobs, xfblob_cookie str_cookie,
+		unsigned char *str, unsigned int str_len);
+
+#endif /* __REPAIR_STRBLOBS_H__ */
