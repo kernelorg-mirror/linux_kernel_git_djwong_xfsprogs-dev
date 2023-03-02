9c686daab0fac691fdddf5dec815ce2993de8cbf
Darrick J. Wong <djwong@kernel.org>
2023-03-02 12:41:31 -0800

    xfs: log VLOOKUP xattr removal operations
    If high level code wants to do a deferred xattr remove operation with
    the VLOOKUP flag set, we need to push this through the log.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_log_format.h
+++ libxfs/xfs_log_format.h
@@ -959,6 +959,7 @@ struct xfs_icreate_log {
 #define XFS_ATTRI_OP_FLAGS_REMOVE	2	/* Remove the attribute */
 #define XFS_ATTRI_OP_FLAGS_REPLACE	3	/* Replace the attribute */
 #define XFS_ATTRI_OP_FLAGS_NVREPLACE	4	/* Replace attr name and val */
+#define XFS_ATTRI_OP_FLAGS_NVREMOVE	5	/* Remove attr w/ vlookup */
 #define XFS_ATTRI_OP_FLAGS_TYPE_MASK	0xFF	/* Flags type mask */
 
 /*
