df8daffd653e913018300e914554406fc1c5ce26
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:04 -0800

    xfs: move the xfs_rtbitmap.c declarations to xfs_rtbitmap.h
    Move all the declarations for functionality in xfs_rtbitmap.c into a
    separate xfs_rtbitmap.h header file.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/libxfs.h
+++ include/libxfs.h
@@ -8,6 +8,7 @@
 #define __LIBXFS_H__
 
 #define CONFIG_XFS_BTREE_IN_XFILE
+#define CONFIG_XFS_RT
 
 #include "libxfs_api_defs.h"
 #include "platform_defs.h"
@@ -85,6 +86,7 @@ struct iomap;
 #include "xfs_symlink_remote.h"
 #include "xfs_ag_resv.h"
 #include "xfs_imeta.h"
+#include "xfs_rtbitmap.h"
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
