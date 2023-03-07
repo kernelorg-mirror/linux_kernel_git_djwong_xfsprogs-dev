019cfc63d5284ecb50249fb4528db0196654b36f
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:50 -0800

    xfs: add a realtime flag to the rmap update log redo items
    Extend the rmap update (RUI) log items with a new realtime flag that
    indicates that the updates apply against the realtime rmapbt.  We'll
    wire up the actual rmap code later.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_defer.c
+++ libxfs/xfs_defer.c
@@ -241,6 +241,7 @@ static const struct xfs_defer_op_type *defer_op_types[XFS_DEFER_OPS_TYPE_MAX] = 
 	[XFS_DEFER_OPS_TYPE_SWAPEXT]	= &xfs_swapext_defer_type,
 #ifdef CONFIG_XFS_RT
 	[XFS_DEFER_OPS_TYPE_FREE_RT]	= &xfs_rtextent_free_defer_type,
+	[XFS_DEFER_OPS_TYPE_RMAP_RT]	= &xfs_rtrmap_update_defer_type,
 #endif
 };
 
