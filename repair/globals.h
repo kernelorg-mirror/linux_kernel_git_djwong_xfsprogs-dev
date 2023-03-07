a9a680d77dbe08d39972317cf5d03c431f11dead
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:15 -0800

    xfs: use accessor functions for bitmap words
    Create get and set functions for rtbitmap words so that we can redefine
    the ondisk format with a specific endianness.  Note that this requires
    the definition of a distinct type for ondisk rtbitmap words so that the
    compiler can perform proper typechecking as we go back and forth.
    
    In the upcoming rtgroups feature, we're going to fix the problem that
    rtwords are written in host endian order, which means we'll need the
    distinct rtword/rtword_raw types.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/globals.h
+++ repair/globals.h
@@ -135,7 +135,7 @@ extern int64_t		fs_max_file_offset;
 
 /* realtime info */
 
-extern xfs_rtword_t	*btmcompute;
+extern union xfs_rtword_ondisk		*btmcompute;
 extern xfs_suminfo_t	*sumcompute;
 
 /* inode tree records have full or partial backptr fields ? */
