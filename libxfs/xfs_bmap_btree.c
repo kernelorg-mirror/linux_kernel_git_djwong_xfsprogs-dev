a61312716a863d10f0b2499b4f7f98f216ba19c1
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:51 -0800

    xfs: remove the unnecessary daddr paramter to _init_block
    Now that all of the callers pass XFS_BUF_DADDR_NULL as the daddr
    parameter, we can elide that too.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_bmap_btree.c
+++ libxfs/xfs_bmap_btree.c
@@ -42,8 +42,7 @@ xfs_bmdr_to_bmbt(
 	xfs_bmbt_key_t		*tkp;
 	__be64			*tpp;
 
-	xfs_btree_init_block(mp, rblock, &xfs_bmbt_ops, XFS_BUF_DADDR_NULL,
-			0, 0, ip->i_ino);
+	xfs_btree_init_block(mp, rblock, &xfs_bmbt_ops, 0, 0, ip->i_ino);
 	rblock->bb_level = dblock->bb_level;
 	ASSERT(be16_to_cpu(rblock->bb_level) > 0);
 	rblock->bb_numrecs = dblock->bb_numrecs;
