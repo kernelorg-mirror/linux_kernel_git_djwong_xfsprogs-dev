2fd1ed01ad616397308a4b4f921ad0176579e1b9
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:14 -0800

    xfs: convert rt summary macros to helpers
    Convert the realtime summary file macros to helper functions so that we
    can improve type checking.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- db/check.c
+++ db/check.c
@@ -3663,7 +3663,7 @@ process_rtbitmap(
 				len = ((int)bmbno - start_bmbno) *
 					bitsperblock + (bit - start_bit);
 				log = XFS_RTBLOCKLOG(len);
-				offs = XFS_SUMOFFS(mp, log, start_bmbno);
+				offs = xfs_rtsumoffs(mp, log, start_bmbno);
 				sumcompute[offs]++;
 				prevbit = 0;
 			}
@@ -3676,7 +3676,7 @@ process_rtbitmap(
 		len = ((int)bmbno - start_bmbno) * bitsperblock +
 			(bit - start_bit);
 		log = XFS_RTBLOCKLOG(len);
-		offs = XFS_SUMOFFS(mp, log, start_bmbno);
+		offs = xfs_rtsumoffs(mp, log, start_bmbno);
 		sumcompute[offs]++;
 	}
 }
