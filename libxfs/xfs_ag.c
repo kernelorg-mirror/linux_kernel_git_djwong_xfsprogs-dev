400a3a4a6cbde01d0e9e6654f2e5877233fe6263
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:50 -0800

    xfs: rename btree block/buffer init functions
    Rename xfs_btree_init_block_int to xfs_btree_init_block, and
    xfs_btree_init_block to xfs_btree_init_buf so that the name suggests the
    type that caller are supposed to pass in.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_ag.c
+++ libxfs/xfs_ag.c
@@ -471,7 +471,7 @@ xfs_btroot_init(
 	struct xfs_buf		*bp,
 	struct aghdr_init_data	*id)
 {
-	xfs_btree_init_block(mp, bp, id->bc_ops, 0, 0, id->agno);
+	xfs_btree_init_buf(mp, bp, id->bc_ops, 0, 0, id->agno);
 }
 
 /* Finish initializing a free space btree. */
@@ -537,7 +537,7 @@ xfs_bnoroot_init(
 	struct xfs_buf		*bp,
 	struct aghdr_init_data	*id)
 {
-	xfs_btree_init_block(mp, bp, id->bc_ops, 0, 0, id->agno);
+	xfs_btree_init_buf(mp, bp, id->bc_ops, 0, 0, id->agno);
 	xfs_freesp_init_recs(mp, bp, id);
 }
 
@@ -553,7 +553,7 @@ xfs_rmaproot_init(
 	struct xfs_btree_block	*block = XFS_BUF_TO_BLOCK(bp);
 	struct xfs_rmap_rec	*rrec;
 
-	xfs_btree_init_block(mp, bp, id->bc_ops, 0, 4, id->agno);
+	xfs_btree_init_buf(mp, bp, id->bc_ops, 0, 4, id->agno);
 
 	/*
 	 * mark the AG header regions as static metadata The BNO
