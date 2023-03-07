71120383239b2b86fb103bb0e1ce7dcaead76134
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:52 -0800

    xfs: move lru refs to the btree ops structure
    Move the btree buffer LRU refcount to the btree ops structure so that we
    can eliminate the last bc_btnum switch in the generic btree code.  We're
    about to create repair-specific btree types, and we don't want that
    stuff cluttering up libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_refcount_btree.c
+++ libxfs/xfs_refcount_btree.c
@@ -319,6 +319,7 @@ xfs_refcountbt_keys_contiguous(
 const struct xfs_btree_ops xfs_refcountbt_ops = {
 	.rec_len		= sizeof(struct xfs_refcount_rec),
 	.key_len		= sizeof(struct xfs_refcount_key),
+	.lru_refs		= XFS_REFC_BTREE_REF,
 
 	.dup_cursor		= xfs_refcountbt_dup_cursor,
 	.set_root		= xfs_refcountbt_set_root,
