7d5556220d93ce143075983e3d106c5c44231e06
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:29 -0800

    xfs: scrub each rtgroup's portion of the rtbitmap separately
    Create a new scrub type code so that userspace can scrub each rtgroup's
    portion of the rtbitmap file separately.  This reduces the long tail
    latency that results from scanning the entire bitmap all at once, and
    prepares us for future patchsets, wherein we'll need to be able to lock
    a specific rtgroup so that we can rebuild that rtgroup's part of the
    rtbitmap contents from the rtgroup's rmap btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_fs.h
+++ libxfs/xfs_fs.h
@@ -727,9 +727,10 @@ struct xfs_scrub_metadata {
 #define XFS_SCRUB_TYPE_NLINKS	26	/* inode link counts */
 #define XFS_SCRUB_TYPE_HEALTHY	27	/* everything checked out ok */
 #define XFS_SCRUB_TYPE_RGSUPER	28	/* realtime superblock */
+#define XFS_SCRUB_TYPE_RGBITMAP	29	/* realtime group bitmap */
 
 /* Number of scrub subcommands. */
-#define XFS_SCRUB_TYPE_NR	29
+#define XFS_SCRUB_TYPE_NR	30
 
 /* i: Repair this metadata. */
 #define XFS_SCRUB_IFLAG_REPAIR		(1u << 0)
