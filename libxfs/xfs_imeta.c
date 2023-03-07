a52f54eb8848272bcf7d7cd34f976b6bef53270e
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:54:58 -0800

    xfs: enable creation of dynamically allocated metadir path structures
    Add a few helper functions so that it's possible to allocate
    xfs_imeta_path objects dynamically, along with dynamically allocated
    path components.  Eventually we're going to want to support paths of the
    form "/realtime/$rtgroup.rmap", and this is necessary for that.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_imeta.c
+++ libxfs/xfs_imeta.c
@@ -1011,3 +1011,47 @@ xfs_imeta_droplink(
 	    xfs_is_metadata_inode(ip))
 		ip->i_diflags2 &= ~XFS_DIFLAG2_METADATA;
 }
+
+/* Create a path to a file within the metadata directory tree. */
+int
+xfs_imeta_create_file_path(
+	struct xfs_mount	*mp,
+	unsigned int		nr_components,
+	struct xfs_imeta_path	**pathp)
+{
+	struct xfs_imeta_path	*p;
+	unsigned char		**components;
+
+	p = kmalloc(sizeof(struct xfs_imeta_path), GFP_KERNEL);
+	if (!p)
+		return -ENOMEM;
+
+	components = kvcalloc(nr_components, sizeof(unsigned char *),
+			GFP_KERNEL);
+	if (!components) {
+		kfree(p);
+		return -ENOMEM;
+	}
+
+	p->im_depth = nr_components;
+	p->im_path = (const unsigned char **)components;
+	p->im_ftype = XFS_DIR3_FT_REG_FILE;
+	p->im_dynamicmask = 0;
+	*pathp = p;
+	return 0;
+}
+
+/* Free a metadata directory tree path. */
+void
+xfs_imeta_free_path(
+	struct xfs_imeta_path	*path)
+{
+	unsigned int		i;
+
+	for (i = 0; i < path->im_depth; i++) {
+		if ((path->im_dynamicmask & (1ULL << i)) && path->im_path[i])
+			kfree(path->im_path[i]);
+	}
+	kfree(path->im_path);
+	kfree(path);
+}
