ce71d3d45b47b2ae3d6c8057ba12d1246b622efb
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:27 -0800

    xfs: encode the rtsummary in big endian format
    Currently, the ondisk realtime summary file counters are accessed in
    units of 32-bit words.  There's no endian translation of the contents of
    this file, which means that the Bad Things Happen(tm) if you go from
    (say) x86 to powerpc.  Since we have a new feature flag, let's take the
    opportunity to enforce an endianness on the file.  Encode the summary
    information in big endian format, like most of the rest of the
    filesystem.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtbitmap.c
+++ libxfs/xfs_rtbitmap.c
@@ -558,6 +558,9 @@ xfs_suminfo_get(
 	struct xfs_mount	*mp,
 	union xfs_suminfo_ondisk *infoptr)
 {
+	if (xfs_has_rtgroups(mp))
+		return be32_to_cpu(infoptr->rtg);
+
 	return infoptr->raw;
 }
 
@@ -567,7 +570,10 @@ xfs_suminfo_add(
 	union xfs_suminfo_ondisk *infoptr,
 	int			delta)
 {
-	infoptr->raw += delta;
+	if (xfs_has_rtgroups(mp))
+		be32_add_cpu(&infoptr->rtg, delta);
+	else
+		infoptr->raw += delta;
 }
 
 /*
