99d12e74327a7119e1426c5405c1c042f5ac794d
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:28 -0800

    xfs: scrub the realtime group superblock
    Enable scrubbing of realtime group superblocks.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libfrog/scrub.h
+++ libfrog/scrub.h
@@ -15,6 +15,7 @@ enum xfrog_scrub_group {
 	XFROG_SCRUB_GROUP_INODE,	/* per-inode metadata */
 	XFROG_SCRUB_GROUP_ISCAN,	/* metadata requiring full inode scan */
 	XFROG_SCRUB_GROUP_SUMMARY,	/* summary metadata */
+	XFROG_SCRUB_GROUP_RTGROUP,	/* per-rtgroup metadata */
 };
 
 /* Catalog of scrub types and names, indexed by XFS_SCRUB_TYPE_* */
