2624765ae542bc0182a79f4f0064502c999d4e4c
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:54:43 -0800

    xfs: hoist xfs_iunlink to libxfs
    Move xfs_iunlink and xfs_iunlink_remove to libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/xfs_trace.h
+++ include/xfs_trace.h
@@ -358,4 +358,11 @@
 
 #define trace_xfs_fs_mark_healthy(a,b)		((void) 0)
 
+#define trace_xfs_iunlink_update_bucket(...)	((void) 0)
+#define trace_xfs_iunlink_update_dinode(...)	((void) 0)
+#define trace_xfs_iunlink(...)			((void) 0)
+#define trace_xfs_iunlink_reload_next(...)	((void) 0)
+#define trace_xfs_iunlink_remove(...)		((void) 0)
+#define trace_xfs_iunlink_map_prev_fallback(...)	((void) 0)
+
 #endif /* __TRACE_H__ */
