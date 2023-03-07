a9decc9f713dfab1b3683c6142c4a156930cb33b
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:44 -0800

    xfs: connect in-memory btrees to xfiles
    Add to our stubbed-out in-memory btrees the ability to connect them with
    an actual in-memory backing file (aka xfiles) and the necessary pieces
    to track free space in the xfile and flush dirty xfbtree buffers on
    demand, which we'll need for online repair.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/xfs_mount.h
+++ include/xfs_mount.h
@@ -302,4 +302,14 @@ static inline void xfs_perag_intent_rele(struct xfs_perag *pag) { }
 #define xfs_defer_drain_free(dr)		((void)0)
 #define xfs_defer_drain_init(dr)		((void)0)
 
+static inline void libxfs_buftarg_drain(struct xfs_buftarg *btp)
+{
+	cache_purge(btp->bcache);
+}
+void libxfs_buftarg_free(struct xfs_buftarg *btp);
+
+int xfile_alloc_buftarg(struct xfs_mount *mp, const char *descr,
+		struct xfs_buftarg **btpp);
+void xfile_free_buftarg(struct xfs_buftarg *btp);
+
 #endif	/* __XFS_MOUNT_H__ */
