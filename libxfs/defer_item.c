ec2a26acf235210b9bad22e757aa3e9befdf5478
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:28 -0800

    xfs: store rtgroup information with a bmap intent
    Make the bmap intent items take an active reference to the rtgroup
    containing the space that is being mapped or unmapped.  We will need
    this functionality once we start enabling rmap and reflink on the rt
    volume.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/defer_item.c
+++ libxfs/defer_item.c
@@ -473,8 +473,18 @@ xfs_bmap_update_get_group(
 {
 	xfs_agnumber_t		agno;
 
-	if (xfs_ifork_is_realtime(bi->bi_owner, bi->bi_whichfork))
+	if (xfs_ifork_is_realtime(bi->bi_owner, bi->bi_whichfork)) {
+		if (xfs_has_rtgroups(mp)) {
+			xfs_rgnumber_t	rgno;
+
+			rgno = xfs_rtb_to_rgno(mp, bi->bi_bmap.br_startblock);
+			bi->bi_rtg = xfs_rtgroup_get(mp, rgno);
+		} else {
+			bi->bi_rtg = NULL;
+		}
+
 		return;
+	}
 
 	agno = XFS_FSB_TO_AGNO(mp, bi->bi_bmap.br_startblock);
 
@@ -493,8 +503,11 @@ static inline void
 xfs_bmap_update_put_group(
 	struct xfs_bmap_intent	*bi)
 {
-	if (xfs_ifork_is_realtime(bi->bi_owner, bi->bi_whichfork))
+	if (xfs_ifork_is_realtime(bi->bi_owner, bi->bi_whichfork)) {
+		if (xfs_has_rtgroups(bi->bi_owner->i_mount))
+			xfs_rtgroup_put(bi->bi_rtg);
 		return;
+	}
 
 	xfs_perag_intent_put(bi->bi_pag);
 }
