617a62898f1e4fcbb1d363ddb63501812e13a11f
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:26 -0800

    xfs: encode the rtbitmap in little endian format
    Currently, the ondisk realtime bitmap file is accessed in units of
    32-bit words.  There's no endian translation of the contents of this
    file, which means that the Bad Things Happen(tm) if you go from (say)
    x86 to powerpc.  Since we have a new feature flag, let's take the
    opportunity to enforce an endianness on the file.
    
    The natural format of a bitmap is (IMHO) little endian, because the byte
    offsets of the bitmap data should always increase in step with the
    information being indexed.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtbitmap.c
+++ libxfs/xfs_rtbitmap.c
@@ -173,6 +173,9 @@ xfs_rtbitmap_getword(
 	struct xfs_mount	*mp,
 	union xfs_rtword_ondisk	*wordptr)
 {
+	if (xfs_has_rtgroups(mp))
+		return le32_to_cpu(wordptr->rtg);
+
 	return wordptr->raw;
 }
 
@@ -183,7 +186,10 @@ xfs_rtbitmap_setword(
 	union xfs_rtword_ondisk	*wordptr,
 	xfs_rtword_t		incore)
 {
-	wordptr->raw = incore;
+	if (xfs_has_rtgroups(mp))
+		wordptr->rtg = cpu_to_le32(incore);
+	else
+		wordptr->raw = incore;
 }
 
 /*
