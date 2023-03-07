07e5d45f325f71491936cbc03f19e202c05dffd2
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:54:45 -0800

    xfs: create libxfs helper to link an existing inode into a directory
    Create a new libxfs function to link an existing inode into a directory.
    The upcoming metadata directory feature will need this to create a
    metadata directory tree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_dir2.h
+++ libxfs/xfs_dir2.h
@@ -61,7 +61,7 @@ extern int xfs_dir_replace(struct xfs_trans *tp, struct xfs_inode *dp,
 				const struct xfs_name *name, xfs_ino_t inum,
 				xfs_extlen_t tot);
 extern int xfs_dir_canenter(struct xfs_trans *tp, struct xfs_inode *dp,
-				struct xfs_name *name);
+				const struct xfs_name *name);
 
 /*
  * Direct call from the bmap code, bypassing the generic directory layer.
@@ -303,6 +303,8 @@ struct xfs_dir_update {
 };
 
 int xfs_dir_create_child(struct xfs_trans *tp, unsigned int resblks,
+		struct xfs_dir_update *du);
+int xfs_dir_add_child(struct xfs_trans *tp, unsigned int resblks,
 		struct xfs_dir_update *du);
 
 #endif	/* __XFS_DIR2_H__ */
