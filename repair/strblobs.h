2e83f247aed634f258a57155a49b128a05fc4f76
Darrick J. Wong <djwong@kernel.org>
2023-03-14 10:55:31 -0700

    xfs_repair: deduplicate strings stored in string blob
    Reduce the memory requirements of the string blob structure by
    deduplicating the strings stored within.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/strblobs.h
+++ repair/strblobs.h
@@ -9,12 +9,14 @@
 struct strblobs;
 
 int strblobs_init(struct xfs_mount *mp, const char *descr,
-		struct strblobs **sblobs);
+		unsigned int hash_buckets, struct strblobs **sblobs);
 void strblobs_destroy(struct strblobs **sblobs);
 
 int strblobs_store(struct strblobs *sblobs, xfblob_cookie *str_cookie,
 		const unsigned char *str, unsigned int str_len);
 int strblobs_load(struct strblobs *sblobs, xfblob_cookie str_cookie,
 		unsigned char *str, unsigned int str_len);
+int strblobs_lookup(struct strblobs *sblobs, xfblob_cookie *str_cookie,
+		const unsigned char *str, unsigned int str_len);
 
 #endif /* __REPAIR_STRBLOBS_H__ */
