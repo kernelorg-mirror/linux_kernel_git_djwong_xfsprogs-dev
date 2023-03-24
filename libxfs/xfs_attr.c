01ef36a7d61e24dec45946c714c6f9f4ef5853e3
Darrick J. Wong <djwong@kernel.org>
2023-03-24 13:34:32 -0700

    xfs: make xfs_attr_set require XFS_DA_OP_REMOVE
    Quite a few patches from now, we're going to change the parent pointer
    xattr format to encode as much of the dirent name in the xattr name as
    fits, and spill the rest of it to the xattr value.  To make this work
    correctly, we'll be adding the ability to look up xattrs based on name
    /and/ value.
    
    Internally, the xattr data structure supports attributes with a zero
    length value, which is how we're going to store parent pointers for
    short dirent names.  The parent pointer repair code uses xfs_attr_set to
    add missing and remove dangling parent pointers, so that interface must
    be capable of setting an xattr with args->value == NULL.
    
    The userspace API doesn't support this, so xfs_attr_set currently treats
    a NULL args->value as a request to remove an attr.  However, that's a
    quirk of the existing callers and the interface.  Make the callers of
    xfs_attr_set to declare explicitly that they want to remove an xattr.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_attr.c
+++ libxfs/xfs_attr.c
@@ -965,6 +965,7 @@ xfs_attr_set(
 	struct xfs_mount	*mp = dp->i_mount;
 	struct xfs_trans_res	tres;
 	bool			rsvd = (args->attr_filter & XFS_ATTR_ROOT);
+	bool			is_remove = args->op_flags & XFS_DA_OP_REMOVE;
 	int			error, local;
 	int			rmt_blks = 0;
 	unsigned int		total;
@@ -989,7 +990,7 @@ xfs_attr_set(
 	args->op_flags = XFS_DA_OP_OKNOENT |
 					(args->op_flags & XFS_DA_OP_LOGGED);
 
-	if (args->value) {
+	if (!is_remove) {
 		XFS_STATS_INC(mp, xs_attr_set);
 		args->total = xfs_attr_calc_size(args, &local);
 
@@ -1023,7 +1024,7 @@ xfs_attr_set(
 	if (error)
 		return error;
 
-	if (args->value || xfs_inode_hasattr(dp)) {
+	if (!is_remove || xfs_inode_hasattr(dp)) {
 		error = xfs_iext_count_may_overflow(dp, XFS_ATTR_FORK,
 				XFS_IEXT_ATTR_MANIP_CNT(rmt_blks));
 		if (error == -EFBIG)
@@ -1037,7 +1038,7 @@ xfs_attr_set(
 	switch (error) {
 	case -EEXIST:
 		/* if no value, we are performing a remove operation */
-		if (!args->value) {
+		if (is_remove) {
 			error = xfs_attr_defer_remove(args);
 			break;
 		}
@@ -1049,7 +1050,7 @@ xfs_attr_set(
 		break;
 	case -ENOATTR:
 		/* Can't remove what isn't there. */
-		if (!args->value)
+		if (is_remove)
 			goto out_trans_cancel;
 
 		/* Pure replace fails if no existing attr to replace. */
