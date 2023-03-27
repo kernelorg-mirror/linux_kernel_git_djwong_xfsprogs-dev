fdf47a7f5eae6dc54a707701dda071805f931d4c
Darrick J. Wong <djwong@kernel.org>
2023-03-27 09:12:47 -0700

    xfs_logprint: decode parent pointers in ATTRI items fully
    This patch modifies the ATTRI print routines to look for the parent
    pointer flag, and decode logged parent pointers fully when dumping log
    contents.  Between the existing ATTRI: printouts and the new ones
    introduced here, we can figure out what was stored in each log iovec,
    as well as the higher level parent pointer that was logged.
    
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    [djwong: adjust to new ondisk format]
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -139,6 +139,10 @@
 #define xfs_log_get_max_trans_res	libxfs_log_get_max_trans_res
 #define xfs_log_sb			libxfs_log_sb
 #define xfs_mode_to_ftype		libxfs_mode_to_ftype
+#define xfs_parent_add			libxfs_parent_add
+#define xfs_parent_finish		libxfs_parent_finish
+#define xfs_parent_irec_from_disk	libxfs_parent_irec_from_disk
+#define xfs_parent_start		libxfs_parent_start
 #define xfs_perag_get			libxfs_perag_get
 #define xfs_perag_put			libxfs_perag_put
 #define xfs_prealloc_blocks		libxfs_prealloc_blocks
