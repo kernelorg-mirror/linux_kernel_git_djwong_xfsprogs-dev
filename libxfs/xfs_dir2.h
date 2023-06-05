kernelorg-mirror/linux_kernel_git_djwong_xfs-linux

xfs: create libxfs helper to rename two directory entries
Create a new libxfs function to rename two directory entries.  The
upcoming metadata directory feature will need this to replace a metadata
inode directory entry.

Signed-off-by: Darrick J. Wong <djwong@kernel.org>

fs/xfs/libxfs/xfs_dir2.h
@@ -297,5 +297,11 @@ int xfs_dir_exchange_children(struct xfs_trans *tp, struct xfs_inode *dp1,
 		const struct xfs_name *name1, struct xfs_inode *ip1,
 		struct xfs_inode *dp2, const struct xfs_name *name2,
 		struct xfs_inode *ip2, unsigned int spaceres);
+int xfs_dir_rename_children(struct xfs_trans *tp, struct xfs_inode *src_dp,
+		const struct xfs_name *src_name, struct xfs_inode *src_ip,
+		struct xfs_inode *target_dp,
+		const struct xfs_name *target_name,
+		struct xfs_inode *target_ip, unsigned int spaceres,
+		struct xfs_inode *wip);
 
 #endif	/* __XFS_DIR2_H__ */

kernelorg-mirror/linux_kernel_git_djwong_xfsprogs-dev

libxfs/xfs_dir2.h
@@ -311,5 +311,8 @@ int xfs_dir_remove_child(struct xfs_trans *tp, unsigned int resblks,
 
 int xfs_dir_exchange_children(struct xfs_trans *tp, struct xfs_dir_update *i1,
 		struct xfs_dir_update *i2, unsigned int spaceres);
+int xfs_dir_rename_children(struct xfs_trans *tp, struct xfs_dir_update *src,
+		struct xfs_dir_update *tgt, unsigned int spaceres,
+		struct xfs_inode *wip, struct xfs_parent_defer *wip_pptr);
 
 #endif	/* __XFS_DIR2_H__ */