1404cac1fc840ee951fa9092c9fc183f3e906c84
Darrick J. Wong <djwong@kernel.org>
2023-08-14 12:54:21 -0700

    xfs: allow newer INCOMPAT/RO_COMPAT feature bits to protect ATTRI log items
    Log recovery (which can include replaying ATTRI intent items) occurs on
    rw and ro mounts.  Dirty logs containing these log items must be
    protected from being replayed by older kernels.  The log incompat
    feature XFS_SB_FEAT_INCOMPAT_LOG_XATTRS provides this protection.
    
    However, adding this flag to the filesystem introduces performance
    problems of its own -- each time we do, we must force the log and write
    the primary superblock before writing any ATTRI log items.  This was ok
    when the only users were developers using the debug knob, but this sucks
    for regular users.  We'd like to avoid that.
    
    If a filesystem has ro-compat or incompat feature bits set that weren't
    defined at the time that ATTRI log items were defined, then any kernel
    that doesn't know about ATTRI items will reject that filesystem.  This
    provides the same protection as the log-incompat feature, but at a much
    lower cost because most ro-compat and incompat features are set on a
    permanent basis.
    
    Avoid the performance hit by detecting these feature bits and skipping
    the xfs_add_incompat_log_feature calls.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_attr.c
+++ libxfs/xfs_attr.c
@@ -884,8 +884,12 @@ xfs_attr_intent_init(
 	unsigned int		op_flags,	/* op flag (set or remove) */
 	struct xfs_attr_intent	**attr)		/* new xfs_attr_intent */
 {
-
 	struct xfs_attr_intent	*new;
+
+	/* ATTRI log items must be protected from older kernels */
+	if (args->op_flags & XFS_DA_OP_LOGGED)
+		ASSERT(xfs_attri_can_use_without_log_assistance(args->dp->i_mount) ||
+		       xfs_sb_version_haslogxattrs(&args->dp->i_mount->m_sb));
 
 	new = kmem_cache_zalloc(xfs_attr_intent_cache, GFP_NOFS | __GFP_NOFAIL);
 	new->xattri_op_flags = op_flags;
