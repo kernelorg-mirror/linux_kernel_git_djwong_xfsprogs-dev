2d0dd7abb6f4f787ff6636924842b7766d5054b0
Darrick J. Wong <djwong@kernel.org>
2023-08-27 09:47:30 -0700

    xfs: force small EFIs for reaping btree extents
    Introduce the concept of a defer ops barrier to separate consecutively
    queued pending work items of the same type.  With a barrier in place,
    the two work items will be tracked separately, and receive separate log
    intent items.  The goal here is to prevent reaping of old metadata
    blocks from creating unnecessarily huge EFIs that could then run the
    risk of overflowing the scrub transaction.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_defer.h
+++ libxfs/xfs_defer.h
@@ -20,6 +20,7 @@ enum xfs_defer_ops_type {
 	XFS_DEFER_OPS_TYPE_FREE,
 	XFS_DEFER_OPS_TYPE_AGFL_FREE,
 	XFS_DEFER_OPS_TYPE_ATTR,
+	XFS_DEFER_OPS_TYPE_BARRIER,
 	XFS_DEFER_OPS_TYPE_MAX,
 };
 
@@ -140,5 +141,7 @@ void xfs_defer_resources_rele(struct xfs_defer_resources *dres);
 
 int __init xfs_defer_init_item_caches(void);
 void xfs_defer_destroy_item_caches(void);
+
+void xfs_defer_add_barrier(struct xfs_trans *tp);
 
 #endif /* __XFS_DEFER_H__ */
