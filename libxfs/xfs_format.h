kernelorg-mirror/linux_kernel_git_djwong_xfs-linux

xfs: use accessor functions for summary info words
Create get and set functions for rtsummary words so that we can redefine
the ondisk format with a specific endianness.  Note that this requires
the definition of a distinct type for ondisk summary info words so that
the compiler can perform proper typechecking.

Signed-off-by: Darrick J. Wong <djwong@kernel.org>

fs/xfs/libxfs/xfs_format.h
@@ -726,6 +726,14 @@ union xfs_rtword_ondisk {
 };
 
 /*
+ * Realtime summary counts are accessed by the word, which is currently
+ * stored in host-endian format.
+ */
+union xfs_suminfo_ondisk {
+	__u32		raw;
+};
+
+/*
  * XFS Timestamps
  * ==============
  *

kernelorg-mirror/linux_kernel_git_djwong_xfsprogs-dev

Reviewed-by: Christoph Hellwig <hch@lst.de>

libxfs/xfs_format.h
@@ -737,6 +737,14 @@ union xfs_rtword_raw {
 };
 
 /*
+ * Realtime summary counts are accessed by the word, which is currently
+ * stored in host-endian format.
+ */
+union xfs_suminfo_raw {
+	__u32		old;
+};
+
+/*
  * XFS Timestamps
  * ==============
  *