831a3d8c90f44d08cd1b692f621d8add97ba2c3b
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:39:48 -0800

    xfs_repair: rebuild the metadata directory
    Check the dirents in metadata directories for problems and repair them
    if necessary.  Also make sure that the sb-rooted inodes (root, metadir
    root, rt bitmap, rt summary) are always allocated in that order.
    
    Note that xfs_repair will always rebuild the metadata directory tree
    itself, so we only need to report problems, not fix them.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/libxfs.h
+++ include/libxfs.h
@@ -94,6 +94,7 @@ struct iomap;
 #include "xfs_parent.h"
 #include "xfs_ag_resv.h"
 #include "xfs_imeta.h"
+#include "imeta_utils.h"
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
