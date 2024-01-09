52c4e3e20be3deb5566b8b61c02588e78b2fd721
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:42:18 -0800

    xfs: hoist xfs_{bump,drop}link to libxfs
    Move xfs_bumplink and xfs_droplink to libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -103,6 +103,7 @@
 #define xfs_buf_relse			libxfs_buf_relse
 #define xfs_buf_unlock			libxfs_buf_unlock
 #define xfs_buftarg_drain		libxfs_buftarg_drain
+#define xfs_bumplink			libxfs_bumplink
 #define xfs_bunmapi			libxfs_bunmapi
 #define xfs_bwrite			libxfs_bwrite
 #define xfs_calc_dquots_per_chunk	libxfs_calc_dquots_per_chunk
