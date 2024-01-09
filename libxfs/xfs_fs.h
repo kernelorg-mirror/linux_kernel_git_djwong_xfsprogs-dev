f43f5359919c718cf0c28211581c0d008d09504e
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:45:10 -0800

    xfs: add an ioctl to map free space into a file
    Add a new ioctl to map free physical space into a file, at the same file
    offset as if the file were a sparse image of the physical device backing
    the filesystem.  The intent here is to use this to prototype a free
    space defragmentation tool.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_fs.h
+++ libxfs/xfs_fs.h
@@ -1088,6 +1088,19 @@ xfs_getfsrefs_advance(
 /* fcr_flags values - returned for each non-header segment */
 #define FCR_OF_LAST		(1U << 0) /* last record in the dataset */
 
+/* map free space to file */
+
+/*
+ * XFS_IOC_MAP_FREESP maps all the free physical space in the filesystem into
+ * the file at the same offsets.  This ioctl requires CAP_SYS_ADMIN.
+ */
+struct xfs_map_freesp {
+	__s64	offset;		/* disk address to map, in bytes */
+	__s64	len;		/* length in bytes */
+	__u64	flags;		/* must be zero */
+	__u64	pad;		/* must be zero */
+};
+
 /*
  * ioctl commands that are used by Linux filesystems
  */
@@ -1128,6 +1141,7 @@ xfs_getfsrefs_advance(
 #define XFS_IOC_GETPARENTS_BY_HANDLE _IOWR('X', 63, struct xfs_getparents_by_handle)
 #define XFS_IOC_RTGROUP_GEOMETRY _IOWR('X', 64, struct xfs_rtgroup_geometry)
 #define XFS_IOC_GETFSREFCOUNTS	_IOWR('X', 65, struct xfs_getfsrefs_head)
+#define XFS_IOC_MAP_FREESP	_IOWR('X', 66, struct xfs_map_freesp)
 
 /*
  * ioctl commands that replace IRIX syssgi()'s
