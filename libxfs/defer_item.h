91e3bdb76d054c9c04f11a5195e909d61186b4a6
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:09 -0800

    xfs: move xfs_extent_free_defer_add to xfs_extfree_item.c
    Move the code that adds the incore xfs_extent_free_item deferred work
    data to a transaction live with the EFI log item code.  This means that
    the allocator code no longer has to know about the inner workings of the
    EFI log items.
    
    As a consequence, we can get rid of the _{get,put}_group helpers.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/defer_item.h
+++ libxfs/defer_item.h
@@ -15,4 +15,10 @@ struct xfs_exchmaps_intent;
 void xfs_exchmaps_defer_add(struct xfs_trans *tp,
 		struct xfs_exchmaps_intent *xmi);
 
+struct xfs_extent_free_item;
+
+void xfs_extent_free_defer_add(struct xfs_trans *tp,
+		struct xfs_extent_free_item *xefi,
+		struct xfs_defer_pending **dfpp);
+
 #endif /* __LIBXFS_DEFER_ITEM_H_ */
