d542f488a3eac793bc7ff4e4bb371c9b5f0bf682
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:45:11 -0800

    xfs: create a new inode flag to require extsize alignment of file data space
    Add a new inode flag to require that all file data extent mappings must
    be aligned (both the file offset range and the allocated space itself)
    to the extent size hint.  Having a separate COW extent size hint is no
    longer allowed.
    
    The goal here is to enable sysadmins and users to mandate that all space
    mappings in a file must have a startoff/blockcount that are aligned to
    (say) a 2MB alignment and that the startblock/blockcount will follow the
    same alignment.
    
    Co-developed-by: John Garry <john.g.garry@oracle.com>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_inode_util.c
+++ libxfs/xfs_inode_util.c
@@ -78,6 +78,8 @@ xfs_flags2diflags2(
 		di_flags2 |= XFS_DIFLAG2_DAX;
 	if (xflags & FS_XFLAG_COWEXTSIZE)
 		di_flags2 |= XFS_DIFLAG2_COWEXTSIZE;
+	if (xflags & FS_XFLAG_FORCEALIGN)
+		di_flags2 |= XFS_DIFLAG2_FORCEALIGN;
 
 	return di_flags2;
 }
@@ -126,6 +128,8 @@ xfs_ip2xflags(
 			flags |= FS_XFLAG_COWEXTSIZE;
 		if (ip->i_diflags2 & XFS_DIFLAG2_VERITY)
 			flags |= FS_XFLAG_VERITY;
+		if (ip->i_diflags2 & XFS_DIFLAG2_FORCEALIGN)
+			flags |= FS_XFLAG_FORCEALIGN;
 	}
 
 	if (xfs_inode_has_attr_fork(ip))
@@ -227,6 +231,8 @@ xfs_inode_inherit_flags2(
 		ip->i_diflags2 |= XFS_DIFLAG2_DAX;
 	if (pip->i_diflags2 & XFS_DIFLAG2_METADIR)
 		ip->i_diflags2 |= XFS_DIFLAG2_METADIR;
+	if (pip->i_diflags2 & XFS_DIFLAG2_FORCEALIGN)
+		ip->i_diflags2 |= XFS_DIFLAG2_FORCEALIGN;
 
 	/* Don't let invalid cowextsize hints propagate. */
 	failaddr = xfs_inode_validate_cowextsize(ip->i_mount, ip->i_cowextsize,
@@ -234,6 +240,14 @@ xfs_inode_inherit_flags2(
 	if (failaddr) {
 		ip->i_diflags2 &= ~XFS_DIFLAG2_COWEXTSIZE;
 		ip->i_cowextsize = 0;
+	}
+
+	if (ip->i_diflags2 & XFS_DIFLAG2_FORCEALIGN) {
+		failaddr = xfs_inode_validate_forcealign(ip->i_mount,
+				VFS_I(ip)->i_mode, ip->i_diflags, ip->i_extsize,
+				ip->i_cowextsize);
+		if (failaddr)
+			ip->i_diflags2 &= ~XFS_DIFLAG2_FORCEALIGN;
 	}
 }
 
