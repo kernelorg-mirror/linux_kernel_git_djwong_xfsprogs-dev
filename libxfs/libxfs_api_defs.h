57ebc15a7d8876a445d46ec7c27ccac77307ce49
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:39:51 -0800

    mkfs.xfs: enable metadata directories
    Enable formatting filesystems with metadata directories.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -188,7 +188,10 @@
 #define xfs_ifork_zap_attr		libxfs_ifork_zap_attr
 #define xfs_imap_to_bp			libxfs_imap_to_bp
 
+#define xfs_imeta_cancel		libxfs_imeta_cancel
+#define xfs_imeta_commit		libxfs_imeta_commit
 #define xfs_imeta_create_file_path	libxfs_imeta_create_file_path
+#define xfs_imeta_create_root		libxfs_imeta_create_root
 #define xfs_imeta_ensure_dirpath	libxfs_imeta_ensure_dirpath
 #define xfs_imeta_free_path		libxfs_imeta_free_path
 #define xfs_imeta_iget			libxfs_imeta_iget
