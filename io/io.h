3568594660b399b32c42d043b3b9006bbd79c973
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:02 -0800

    xfs_io: add a command to display allocation group information
    Add a new 'aginfo' command to xfs_io so that we can display allocation
    group geometry.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- io/io.h
+++ io/io.h
@@ -154,3 +154,4 @@ extern void		repair_init(void);
 extern void		crc32cselftest_init(void);
 extern void		bulkstat_init(void);
 extern void		exchangerange_init(void);
+extern void		aginfo_init(void);
