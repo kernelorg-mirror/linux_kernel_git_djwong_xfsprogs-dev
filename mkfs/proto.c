7d4dca6805030ae326a7ca636bec6a19b0fcd89b
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:42:35 -0800

    xfs: ensure metadata directory paths exist before creating files
    Since xfs_imeta_create can create new metadata files arbitrarily deep in
    the metadata directory tree, we must supply a function that can ensure
    that all directories in a path exist, and call it before the quota
    functions create the quota inodes.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- mkfs/proto.c
+++ mkfs/proto.c
@@ -757,6 +757,10 @@ rtbitmap_create(
 	struct xfs_inode	*rbmip;
 	int			error;
 
+	error = -libxfs_imeta_ensure_dirpath(mp, &XFS_IMETA_RTBITMAP);
+	if (error)
+		fail(_("Realtime bitmap directory allocation failed"), error);
+
 	error = -libxfs_imeta_start_create(mp, &XFS_IMETA_RTBITMAP, &upd);
 	if (error)
 		res_failed(error);
@@ -785,6 +789,10 @@ rtsummary_create(
 	struct xfs_imeta_update	upd;
 	struct xfs_inode	*rsumip;
 	int			error;
+
+	error = -libxfs_imeta_ensure_dirpath(mp, &XFS_IMETA_RTSUMMARY);
+	if (error)
+		fail(_("Realtime summary directory allocation failed"), error);
 
 	error = -libxfs_imeta_start_create(mp, &XFS_IMETA_RTSUMMARY, &upd);
 	if (error)
