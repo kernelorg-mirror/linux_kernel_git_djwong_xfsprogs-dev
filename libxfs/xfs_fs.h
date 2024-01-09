e41f623dd418190a3aa2caaf84d782d23a1252df
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:22 -0800

    xfs: introduce new file range exchange ioctls
    Introduce a pair of new ioctls to handle exchanging ranges of bytes
    between files.  The goal here is to perform the exchange atomically with
    respect to applications -- either they see the file contents before the
    exchange or they see that A-B is now B-A, even if the kernel crashes.
    
    The simpler of the two ioctls is XFS_IOC_EXCHANGE_RANGE, which performs
    the exchange unconditionally.  XFS_IOC_COMMIT_RANGE, on the other hand,
    requires the caller to sample the file attributes of one of the files
    participating in the exchange, and aborts the exchange if that file has
    changed in the meantime (presumably by another thread).
    
    For userspace programs the goal is to enable defragmentation of files
    (it's a replacement for the old swapext ioctl), the atomic commit of
    sparse updates to an indexed data file, or emulation of atomic writes on
    software defined storage servers.
    
    My original goal with all this code was to make it so that online repair
    can build a replacement directory or xattr structure in a temporary file
    and commit the repair by atomically exchanging all the data blocks
    between the two files.  However, I needed a way to test this mechanism
    thoroughly, so I've been evolving an ioctl interface since then.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_fs.h
+++ libxfs/xfs_fs.h
@@ -772,6 +772,76 @@ struct xfs_scrub_metadata {
 #  define XFS_XATTR_LIST_MAX 65536
 #endif
 
+/*
+ * Exchange part of file1 with part of the file that this ioctl that is being
+ * called against (which we'll call file2).  Filesystems must be able to
+ * restart and complete the operation even after the system goes down.
+ */
+struct xfs_exch_range {
+	__s64		file1_fd;
+	__s64		file1_offset;	/* file1 offset, bytes */
+	__s64		file2_offset;	/* file2 offset, bytes */
+	__u64		length;		/* bytes to exchange */
+
+	__u64		flags;		/* see XFS_EXCHRANGE_* below */
+
+	__u64		pad;		/* must be zeroes */
+};
+
+/*
+ * Using the same definition of file2 as struct xfs_exch_range, commit the
+ * contents of file1 into file2 if file2 has the same inode number, mtime, and
+ * ctime as the arguments provided to the call.  The old contents of file2 will
+ * be moved to file1.
+ *
+ * Returns -EBUSY if there isn't an exact match for the file2 fields.
+ *
+ * Filesystems must be able to restart and complete the operation even after
+ * the system goes down.
+ */
+struct xfs_commit_range {
+	__s64		file1_fd;
+	__s64		file1_offset;	/* file1 offset, bytes */
+	__s64		file2_offset;	/* file2 offset, bytes */
+	__s64		length;		/* bytes to exchange */
+
+	__u64		flags;		/* see XFS_EXCHRANGE_* below */
+
+	/* file2 metadata for freshness checks */
+	__u64		file2_ino;	/* inode number */
+	__s64		file2_mtime;	/* modification time */
+	__s64		file2_ctime;	/* change time */
+	__s32		file2_mtime_nsec; /* mod time, nsec */
+	__s32		file2_ctime_nsec; /* change time, nsec */
+
+	__u64		pad;		/* must be zeroes */
+};
+
+/*
+ * Exchange file data all the way to the ends of both files, and then exchange
+ * the file sizes.  This flag can be used to replace a file's contents with a
+ * different amount of data.  length will be ignored.
+ */
+#define XFS_EXCHRANGE_TO_EOF		(1ULL << 0)
+
+/* Flush all changes in file data and file metadata to disk before returning. */
+#define XFS_EXCHRANGE_DSYNC		(1ULL << 1)
+
+/* Dry run; do all the parameter verification but do not change anything. */
+#define XFS_EXCHRANGE_DRY_RUN		(1ULL << 2)
+
+/*
+ * Exchange only the parts of the two files where the file allocation units
+ * mapped to file1's range have been written to.  This can accelerate
+ * scatter-gather atomic writes with a temp file if all writes are aligned to
+ * the file allocation unit.
+ */
+#define XFS_EXCHRANGE_FILE1_WRITTEN	(1ULL << 3)
+
+#define XFS_EXCHRANGE_ALL_FLAGS		(XFS_EXCHRANGE_TO_EOF | \
+					 XFS_EXCHRANGE_DSYNC | \
+					 XFS_EXCHRANGE_DRY_RUN | \
+					 XFS_EXCHRANGE_FILE1_WRITTEN)
 
 /*
  * ioctl commands that are used by Linux filesystems
@@ -843,6 +913,8 @@ struct xfs_scrub_metadata {
 #define XFS_IOC_FSGEOMETRY	     _IOR ('X', 126, struct xfs_fsop_geom)
 #define XFS_IOC_BULKSTAT	     _IOR ('X', 127, struct xfs_bulkstat_req)
 #define XFS_IOC_INUMBERS	     _IOR ('X', 128, struct xfs_inumbers_req)
+#define XFS_IOC_EXCHANGE_RANGE	     _IOWR('X', 129, struct xfs_exch_range)
+#define XFS_IOC_COMMIT_RANGE	     _IOWR('X', 129, struct xfs_commit_range)
 /*	XFS_IOC_GETFSUUID ---------- deprecated 140	 */
 
 
