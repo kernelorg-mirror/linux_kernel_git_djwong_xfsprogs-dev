ada02c11e60a703901d9a4106497846daa550f54
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:22 -0800

    mkfs: create the realtime rmap inode
    Create a realtime rmapbt inode if we format the fs with realtime
    and rmap.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- mkfs/proto.c
+++ mkfs/proto.c
@@ -887,6 +887,61 @@ rtsummary_create(
 	mp->m_rsumip = rsumip;
 }
 
+/* Create the realtime rmap btree inode. */
+static void
+rtrmapbt_create(
+	struct xfs_rtgroup	*rtg)
+{
+	struct xfs_imeta_update	upd = { };
+	struct xfs_mount	*mp = rtg->rtg_mount;
+	struct xfs_imeta_path	*path;
+	struct xfs_btree_cur	*cur;
+	int			error;
+
+	path = xfs_rtrmapbt_create_path(mp, rtg->rtg_rgno);
+	if (!path)
+		fail( _("rtrmap inode path creation failed"), ENOMEM);
+
+	error = -libxfs_imeta_ensure_dirpath(mp, path);
+	if (error)
+		fail(_("rtgroup directory allocation failed"), error);
+
+	error = -libxfs_imeta_start_create(mp, path, &upd);
+	if (error)
+		res_failed(error);
+
+	error = -libxfs_rtrmapbt_create(&upd);
+	if (error)
+		fail(_("rtrmap inode creation failed"), error);
+
+	if (xfs_has_rtsuper(mp) && rtg->rtg_rgno == 0) {
+		struct xfs_rmap_irec	rmap = {
+			.rm_startblock	= 0,
+			.rm_blockcount	= mp->m_sb.sb_rextsize,
+			.rm_owner	= XFS_RMAP_OWN_FS,
+			.rm_offset	= 0,
+			.rm_flags	= 0,
+		};
+
+		/*
+		 * Add an rmap the rtgroup superblock; this had better fit in
+		 * the data fork.
+		 */
+		cur = libxfs_rtrmapbt_init_cursor(mp, upd.tp, rtg, upd.ip);
+		error = -libxfs_rmap_map_raw(cur, &rmap);
+		libxfs_btree_del_cursor(cur, error);
+		if (error)
+			fail(_("rtrmapbt initialization failed"), error);
+	}
+
+	error = -libxfs_imeta_commit(&upd);
+	if (error)
+		fail(_("rtrmapbt commit failed"), error);
+
+	libxfs_imeta_free_path(path);
+	rtg->rtg_rmapip = upd.ip;
+}
+
 /* Initialize block headers of rt free space files. */
 static int
 init_rtblock_headers(
@@ -1110,8 +1165,16 @@ static void
 rtinit(
 	struct xfs_mount	*mp)
 {
+	struct xfs_rtgroup	*rtg;
+	xfs_rgnumber_t		rgno;
+
 	rtbitmap_create(mp);
 	rtsummary_create(mp);
+
+	for_each_rtgroup(mp, rgno, rtg) {
+		if (xfs_has_rtrmapbt(mp))
+			rtrmapbt_create(rtg);
+	}
 
 	rtbitmap_init(mp);
 	rtsummary_init(mp);
