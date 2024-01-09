9852b1b217146521a3d7ce7e1d39360b7df35314
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:03 -0800

    xfs_scrub: use histograms to speed up phase 8 on the realtime volume
    Use the same statistical methods that we use on the data volume to
    compute the minimum threshold size for fstrims on the realtime volume.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/xfs_scrub.c
+++ scrub/xfs_scrub.c
@@ -714,6 +714,7 @@ main(
 	int			error;
 
 	hist_init(&ctx.datadev_hist);
+	hist_init(&ctx.rtdev_hist);
 
 	fprintf(stdout, "EXPERIMENTAL xfs_scrub program in use! Use at your own risk!\n");
 	fflush(stdout);
@@ -946,6 +947,7 @@ out_unicrash:
 	unicrash_unload();
 
 	hist_free(&ctx.datadev_hist);
+	hist_free(&ctx.rtdev_hist);
 
 	/*
 	 * If we're being run as a service, the return code must fit the LSB
