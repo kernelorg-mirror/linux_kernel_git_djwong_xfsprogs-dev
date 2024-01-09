0ceddda96298ec443a09a5cdd51dc28e9a85c5bf
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:45:11 -0800

    xfs: create a new inode flag to require extsize alignment of file data space
    Add a new inode flag to require that all file data extent mappings must
    be aligned (both the file offset range and the allocated space itself)
    to the extent size hint.  Having a separate COW extent size hint is no
    longer allowed.
    
    The goal here is to enable sysadmins and users to mandate that all space
    mappings in a file must have a startoff/blockcount that are aligned to
    (say) a 2MB alignment and that the startblock/blockcount will follow the
    same alignment.
    
    Co-developed-by: John Garry <john.g.garry@oracle.com>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_sb.c
+++ libxfs/xfs_sb.c
@@ -166,6 +166,9 @@ xfs_sb_version_to_features(
 		features |= XFS_FEAT_INOBTCNT;
 	if (sbp->sb_features_ro_compat & XFS_SB_FEAT_RO_COMPAT_VERITY)
 		features |= XFS_FEAT_VERITY;
+	if (sbp->sb_features_ro_compat & XFS_SB_FEAT_RO_COMPAT_FORCEALIGN)
+		features |= XFS_FEAT_FORCEALIGN;
+
 	if (sbp->sb_features_incompat & XFS_SB_FEAT_INCOMPAT_FTYPE)
 		features |= XFS_FEAT_FTYPE;
 	if (sbp->sb_features_incompat & XFS_SB_FEAT_INCOMPAT_SPINODES)
@@ -371,6 +374,27 @@ xfs_validate_sb_rtgroups(
 	return 0;
 }
 
+static int
+xfs_validate_sb_forcealign(
+	struct xfs_mount	*mp,
+	struct xfs_sb		*sbp)
+{
+	if (sbp->sb_rextsize == 0) {
+		xfs_warn(mp,
+ "Cannot have forced allocation alignment of zero.");
+		return -EINVAL;
+	}
+
+	if (sbp->sb_agblocks % sbp->sb_rextsize != 0) {
+		xfs_warn(mp,
+ "Allocation group size %u not aligned to forcealign %u.",
+				sbp->sb_agblocks, sbp->sb_rextsize);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 /* Check the validity of the SB. */
 STATIC int
 xfs_validate_sb_common(
@@ -435,6 +459,12 @@ xfs_validate_sb_common(
 
 		if (sbp->sb_features_incompat & XFS_SB_FEAT_INCOMPAT_RTGROUPS) {
 			error = xfs_validate_sb_rtgroups(mp, sbp);
+			if (error)
+				return error;
+		}
+
+		if (sbp->sb_features_ro_compat & XFS_SB_FEAT_RO_COMPAT_FORCEALIGN) {
+			error = xfs_validate_sb_forcealign(mp, sbp);
 			if (error)
 				return error;
 		}
