69218923ab64f10fcfebae3e5de1916e27fc7cb7
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:42:37 -0800

    xfs: advertise metadata directory feature
    Advertise the existence of the metadata directory feature; this will be
    used by scrub to decide if it needs to scan the metadir too.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_sb.c
+++ libxfs/xfs_sb.c
@@ -1296,6 +1296,8 @@ xfs_fs_geometry(
 		geo->flags |= XFS_FSOP_GEOM_FLAGS_NREXT64;
 	if (xfs_exchrange_possible(mp))
 		geo->flags |= XFS_FSOP_GEOM_FLAGS_EXCHANGE_RANGE;
+	if (xfs_has_metadir(mp))
+		geo->flags |= XFS_FSOP_GEOM_FLAGS_METADIR;
 	geo->rtsectsize = sbp->sb_blocksize;
 	geo->dirblocksize = xfs_dir2_dirblock_bytes(sbp);
 
