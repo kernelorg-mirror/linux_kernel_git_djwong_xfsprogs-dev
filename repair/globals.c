72f55876170867ef7f09b5a30e01e286d5bb69ff
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:40 -0800

    xfs_repair: skip free space checks when upgrading
    Add a debug knob to disable the free space checks when upgrading a
    system.  This is extremely risky and will cause severe tire damage!!!
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/globals.c
+++ repair/globals.c
@@ -48,6 +48,7 @@ char	*rt_name;		/* Name of realtime device */
 int	rt_spec;		/* Realtime dev specified as option */
 int	convert_lazy_count;	/* Convert lazy-count mode on/off */
 int	lazy_count;		/* What to set if to if converting */
+bool	skip_freesp_check_on_upgrade; /* do not enable */
 bool	features_changed;	/* did we change superblock feature bits? */
 bool	add_inobtcount;		/* add inode btree counts to AGI */
 bool	add_bigtime;		/* add support for timestamps up to 2486 */
