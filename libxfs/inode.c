cf002587b0671977a068a0bf5013d3c675a7e723
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:39:35 -0800

    libxfs: set access time when creating files
    Set the access time on files that we're creating, to match the behavior
    of the kernel.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/inode.c
+++ libxfs/inode.c
@@ -93,7 +93,8 @@ libxfs_icreate(
 	struct inode		*inode;
 	struct xfs_inode	*ip;
 	unsigned int		flags;
-	int			times = XFS_ICHGTIME_MOD | XFS_ICHGTIME_CHG;
+	int			times = XFS_ICHGTIME_MOD | XFS_ICHGTIME_CHG |
+					XFS_ICHGTIME_ACCESS;
 	int			error;
 
 	error = libxfs_iget(tp->t_mountp, tp, ino, XFS_IGET_CREATE, &ip);
