6562f450cfaf686c697e3a7ce5b46de0f19c2f03
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:04 -0800

    mkfs: add headers to realtime bitmap blocks
    When the rtgroups feature is enabled, format rtbitmap blocks with the
    appropriate block headers.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -918,6 +918,7 @@ struct sb_feat_args {
 	bool	nortalign;
 	bool	nrext64;
 	bool	exchrange;		/* XFS_SB_FEAT_INCOMPAT_EXCHRANGE */
+	bool	rtgroups;		/* XFS_SB_FEAT_INCOMPAT_RTGROUPS */
 };
 
 struct cli_params {
@@ -3140,6 +3141,7 @@ validate_rtdev(
 	struct cli_params	*cli)
 {
 	struct libxfs_init	*xi = cli->xi;
+	unsigned int		rbmblocksize = cfg->blocksize;
 
 	if (!xi->rt.dev) {
 		if (cli->rtsize) {
@@ -3183,8 +3185,10 @@ reported by the device (%u).\n"),
 _("cannot have an rt subvolume with zero extents\n"));
 		usage();
 	}
+	if (cfg->sb_feat.rtgroups)
+		rbmblocksize -= sizeof(struct xfs_rtbuf_blkinfo);
 	cfg->rtbmblocks = (xfs_extlen_t)howmany(cfg->rtextents,
-						NBBY * cfg->blocksize);
+						NBBY * rbmblocksize);
 }
 
 static bool
