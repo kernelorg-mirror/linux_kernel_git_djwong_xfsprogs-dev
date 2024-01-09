4744cdda7b5cf5934ad3b8841bf1263cd178efc0
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:45:12 -0800

    xfs: make file data allocations observe the 'forcealign' flag
    The existing extsize hint code already did the work of expanding file
    range mapping requests so that the range is aligned to the hint value.
    Now add the code we need to guarantee that the space allocations are
    also always aligned.
    
    Co-developed-by: John Garry <john.g.garry@oracle.com>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_bmap.c
+++ libxfs/xfs_bmap.c
@@ -3377,6 +3377,17 @@ xfs_bmap_alloc_account(
 	xfs_trans_mod_dquot_byino(ap->tp, ap->ip, fld, ap->length);
 }
 
+static inline bool
+xfs_bmap_use_forcealign(
+	const struct xfs_bmalloca	*ap)
+{
+	if (!xfs_inode_force_align(ap->ip))
+		return false;
+
+	return  (ap->flags & XFS_BMAPI_COWFORK) ||
+		(ap->datatype & XFS_ALLOC_USERDATA);
+}
+
 static int
 xfs_bmap_compute_alignments(
 	struct xfs_bmalloca	*ap,
@@ -3391,6 +3402,17 @@ xfs_bmap_compute_alignments(
 		stripe_align = mp->m_swidth;
 	else if (mp->m_dalign)
 		stripe_align = mp->m_dalign;
+
+	/*
+	 * File data mappings with forced alignment can use the stripe
+	 * alignment if it's a multiple of the forcealign value.  Otherwise,
+	 * use the regular forcealign value.
+	 */
+	if (xfs_bmap_use_forcealign(ap)) {
+		if (!stripe_align || stripe_align % mp->m_sb.sb_rextsize)
+			stripe_align = mp->m_sb.sb_rextsize;
+		args->alignment = stripe_align;
+	}
 
 	if (ap->flags & XFS_BMAPI_COWFORK)
 		align = xfs_get_cowextsz_hint(ap->ip);
@@ -3463,6 +3485,9 @@ xfs_bmap_exact_minlen_extent_alloc(
 	int			error;
 
 	ASSERT(ap->length);
+
+	if (xfs_inode_force_align(ap->ip))
+		return -ENOSPC;
 
 	if (ap->minlen != 1) {
 		ap->blkno = NULLFSBLOCK;
@@ -3537,6 +3562,7 @@ xfs_bmap_btalloc_at_eof(
 {
 	struct xfs_mount	*mp = args->mp;
 	struct xfs_perag	*caller_pag = args->pag;
+	int			orig_alignment = args->alignment;
 	int			error;
 
 	/*
@@ -3611,10 +3637,10 @@ xfs_bmap_btalloc_at_eof(
 
 	/*
 	 * Allocation failed, so turn return the allocation args to their
-	 * original non-aligned state so the caller can proceed on allocation
-	 * failure as if this function was never called.
+	 * original state so the caller can proceed on allocation failure as
+	 * if this function was never called.
 	 */
-	args->alignment = 1;
+	args->alignment = orig_alignment;
 	return 0;
 }
 
@@ -3636,6 +3662,10 @@ xfs_bmap_btalloc_low_space(
 	struct xfs_alloc_arg	*args)
 {
 	int			error;
+
+	/* Don't try unaligned last-chance allocations with forcealign */
+	if (xfs_inode_force_align(ap->ip))
+		return -ENOSPC;
 
 	if (args->minlen > ap->minlen) {
 		args->minlen = ap->minlen;
@@ -4141,7 +4171,9 @@ xfs_bmap_alloc_userdata(
 		if (bma->offset == 0)
 			bma->datatype |= XFS_ALLOC_INITIAL_USER_DATA;
 
-		if (mp->m_dalign && bma->length >= mp->m_dalign) {
+		/* forcealign mode reuses the stripe unit alignment code */
+		if (xfs_inode_force_align(bma->ip) ||
+		    (mp->m_dalign && bma->length >= mp->m_dalign)) {
 			error = xfs_bmap_isaeof(bma, whichfork);
 			if (error)
 				return error;
@@ -6397,6 +6429,10 @@ xfs_extlen_t
 xfs_get_extsz_hint(
 	struct xfs_inode	*ip)
 {
+	/* forcealign means we align to rextsize */
+	if (xfs_inode_force_align(ip))
+		return ip->i_mount->m_sb.sb_rextsize;
+
 	/*
 	 * No point in aligning allocations if we need to COW to actually
 	 * write to them.
@@ -6420,6 +6456,10 @@ xfs_get_cowextsz_hint(
 	struct xfs_inode	*ip)
 {
 	xfs_extlen_t		a, b;
+
+	/* forcealign means we align to rextsize */
+	if (xfs_inode_force_align(ip))
+		return ip->i_mount->m_sb.sb_rextsize;
 
 	a = 0;
 	if (ip->i_diflags2 & XFS_DIFLAG2_COWEXTSIZE)
