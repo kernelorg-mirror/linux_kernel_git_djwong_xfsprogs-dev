3e4306656c9c92fda7e9779adeb5d4a9142283c9
Christoph Hellwig <hch@lst.de>
2024-01-09 05:30:06 +0100

    xfs: split the per-btree union in struct xfs_btree_cur
    Split up the union that encodes btree-specific fields in struct
    xfs_btree_cur.  Most fields in there are specific to the btree type
    encoded in xfs_btree_ops.type, and we can use the obviously named union
    for that.  But one field is specific to the bmapbt and two are shared by
    the refcount and rtrefcountbt.  Move those to a separate union to make
    the usage clear and not need a separate struct for the refcount-related
    fields.
    
    This will also make unnecessary some very awkward btree cursor
    refc/rtrefc switching logic in the rtrefcount patchset.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_btree_staging.c
+++ libxfs/xfs_btree_staging.c
@@ -172,6 +172,7 @@ xfs_btree_commit_afakeroot(
 	trace_xfs_btree_commit_afakeroot(cur);
 
 	kmem_free((void *)cur->bc_ops);
+	cur->bc_ag.afake = NULL;
 	cur->bc_ag.agbp = agbp;
 	cur->bc_ops = ops;
 	cur->bc_flags &= ~XFS_BTREE_STAGING;
