b3bfe13321e855615eaa121649e98a1c521ba119
Christoph Hellwig <hch@lst.de>
2024-01-23 09:08:46 -0800

    xfs: remove xfs_rmapbt_stage_cursor
    Source kernel commit: b5025425551bf372ab52eb70b91ed32b2743740b
    
    xfs_rmapbt_stage_cursor is currently unused, but future callers can
    trivially open code the two calls.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/agbtree.c
+++ repair/agbtree.c
@@ -637,7 +637,8 @@ init_rmapbt_cursor(
 		return;
 
 	init_rebuild(sc, &XFS_RMAP_OINFO_AG, est_agfreeblocks, btr);
-	btr->cur = libxfs_rmapbt_stage_cursor(sc->mp, &btr->newbt.afake, pag);
+	btr->cur = libxfs_rmapbt_init_cursor(sc->mp, NULL, NULL, pag);
+	libxfs_btree_stage_afakeroot(btr->cur, &btr->newbt.afake);
 
 	btr->bload.get_records = get_rmapbt_records;
 	btr->bload.claim_block = rebuild_claim_block;
