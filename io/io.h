39d7769930c235a443f4c051909511b0a387b7c6
Darrick J. Wong <djwong@kernel.org>
2024-02-06 13:36:47 -0800

    xfs_io: monitor filesystem health events
    Create a subcommand to monitor for health events generated by the kernel.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- io/io.h
+++ io/io.h
@@ -192,3 +192,4 @@ extern void		bulkstat_init(void);
 extern void		atomicupdate_init(void);
 extern void		aginfo_init(void);
 extern void		fsrefcounts_init(void);
+extern void		healthmon_init(void);
