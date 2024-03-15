fd434ad47fe0e1842e007dabec458a51763a8b62
Andrey Albershteyn <aalbersh@redhat.com>
2024-03-15 15:59:02 -0700

    xfs: add attribute type for fs-verity
    The Merkle tree blocks and descriptor are stored in the extended
    attributes of the inode. Add new attribute type for fs-verity
    metadata. Add XFS_ATTR_INTERNAL_MASK to skip parent pointer and
    fs-verity attributes as those are only for internal use. While we're
    at it add a few comments in relevant places that internally visible
    attributes are not suppose to be handled via interface defined in
    xfs_xattr.c.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_da_format.h
+++ libxfs/xfs_da_format.h
@@ -715,14 +715,22 @@ struct xfs_attr3_leafblock {
 #define	XFS_ATTR_ROOT_BIT	1	/* limit access to trusted attrs */
 #define	XFS_ATTR_SECURE_BIT	2	/* limit access to secure attrs */
 #define	XFS_ATTR_PARENT_BIT	3	/* parent pointer attrs */
+#define	XFS_ATTR_VERITY_BIT	4	/* verity merkle tree and descriptor */
 #define	XFS_ATTR_INCOMPLETE_BIT	7	/* attr in middle of create/delete */
 #define XFS_ATTR_LOCAL		(1u << XFS_ATTR_LOCAL_BIT)
 #define XFS_ATTR_ROOT		(1u << XFS_ATTR_ROOT_BIT)
 #define XFS_ATTR_SECURE		(1u << XFS_ATTR_SECURE_BIT)
 #define XFS_ATTR_PARENT		(1u << XFS_ATTR_PARENT_BIT)
+#define XFS_ATTR_VERITY		(1u << XFS_ATTR_VERITY_BIT)
 #define XFS_ATTR_INCOMPLETE	(1u << XFS_ATTR_INCOMPLETE_BIT)
 #define XFS_ATTR_NSP_ONDISK_MASK \
-			(XFS_ATTR_ROOT | XFS_ATTR_SECURE | XFS_ATTR_PARENT)
+			(XFS_ATTR_ROOT | XFS_ATTR_SECURE | XFS_ATTR_PARENT | \
+			 XFS_ATTR_VERITY)
+
+/*
+ * Internal attributes not exposed to the user
+ */
+#define XFS_ATTR_INTERNAL_MASK (XFS_ATTR_PARENT | XFS_ATTR_VERITY)
 
 /*
  * Alignment for namelist and valuelist entries (since they are mixed
