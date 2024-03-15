b388873dbaec4638c457233f005f526a8f9c31cc
Darrick J. Wong <djwong@kernel.org>
2024-03-15 16:00:33 -0700

    xfs: advertise fs-verity being available on filesystem
    Advertise that this filesystem supports fsverity.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_sb.c
+++ libxfs/xfs_sb.c
@@ -1258,6 +1258,8 @@ xfs_fs_geometry(
 	}
 	if (xfs_has_large_extent_counts(mp))
 		geo->flags |= XFS_FSOP_GEOM_FLAGS_NREXT64;
+	if (xfs_has_verity(mp))
+		geo->flags |= XFS_FSOP_GEOM_FLAGS_VERITY;
 	geo->rtsectsize = sbp->sb_blocksize;
 	geo->dirblocksize = xfs_dir2_dirblock_bytes(sbp);
 
