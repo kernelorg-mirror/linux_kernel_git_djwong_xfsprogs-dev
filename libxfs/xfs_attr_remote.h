f801dbe7c4bd4ae3f97103346a01df8f3dcfa7b3
Darrick J. Wong <djwong@kernel.org>
2024-03-21 09:09:32 -0700

    xfs: do not use xfs_attr3_rmt_hdr for remote verity value blocks
    I enabled fsverity for a ~250MB file and noticed the following xattr
    keys that got created for the merkle tree.  These two merkle tree blocks
    are written out in ascending order:
    
    nvlist[52].merkle_off = 0x111000
    nvlist[53].valueblk = 0x222
    nvlist[53].valuelen = 4096
    
    nvlist[53].merkle_off = 0x112000
    nvlist[54].valueblk = 0x224
    nvlist[54].valuelen = 4096
    
    Notice that while the valuelen is 4k, the block offset increases by two.
    Curious, I then loaded up ablock 0x223:
    
    hdr.magic = 0x5841524d
    hdr.offset = 4040
    hdr.bytes = 56
    hdr.crc = 0xad1b8bd8 (correct)
    hdr.uuid = 07d3f25c-e550-4118-8ff5-a45c017ba5ef
    hdr.owner = 133
    hdr.bno = 442144
    hdr.lsn = 0xffffffffffffffff
    data = <56 bytes of charns data>
    
    Ugh!  Each 4k merkle tree block takes up two fsblocks due to the remote
    value header that XFS puts at the start of each remote value block.
    That header is 56 bytes long, which is exactly the length of the
    spillover here.  This isn't good.
    
    The first thing that I tried was enabling fsverity on a bunch of files,
    extracting the merkle tree blocks one by one, and testing their
    compressability with gzip, zstd, and xz.  Merkle tree blocks are nearly
    indistinguishable from random data, with the result that 99% of the
    blocks I sampled got larger under compression.  So that's out.
    
    Next I decided to try eliminating the xfs_attr3_rmt_hdr header, which
    would make verity remote values align perfectly with filesystem blocks.
    Because remote value blocks are written out with xfs_bwrite, the lsn
    field isn't useful.  The merkle tree is itself a bunch of hashes of data
    blocks or other merkle tree blocks, which means that a bitflip will
    result in a verity failure somewhere in the file.  Hence we don't need
    to store an explicit crc, and we could just XOR the ondisk merkle tree
    contents with selected attributes.
    
    In the end I decided to create a smaller header structure containing
    only a magic, the fsuuid, the inode owner, and the ondisk block number.
    These values get XORd into the beginning of the merkle tree block to
    detect lost writes when we're writing remote XFS_ATTR_VERITY values to
    disk, and XORd out when reading them back in.
    
    With this format change applied, the fsverity overhead halves.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_attr_remote.h
+++ libxfs/xfs_attr_remote.h
@@ -6,12 +6,13 @@
 #ifndef __XFS_ATTR_REMOTE_H__
 #define	__XFS_ATTR_REMOTE_H__
 
-unsigned int xfs_attr3_rmt_blocks(struct xfs_mount *mp, unsigned int attrlen);
+unsigned int xfs_attr3_rmt_blocks(struct xfs_mount *mp, unsigned int attrns,
+		unsigned int attrlen);
 
 /* Number of rmt blocks needed to store the maximally sized attr value */
 static inline unsigned int xfs_attr3_max_rmt_blocks(struct xfs_mount *mp)
 {
-	return xfs_attr3_rmt_blocks(mp, XFS_XATTR_SIZE_MAX);
+	return xfs_attr3_rmt_blocks(mp, 0, XFS_XATTR_SIZE_MAX);
 }
 
 int xfs_attr_rmtval_get(struct xfs_da_args *args);
@@ -23,4 +24,7 @@ int xfs_attr_rmt_find_hole(struct xfs_da_args *args);
 int xfs_attr_rmtval_set_value(struct xfs_da_args *args);
 int xfs_attr_rmtval_set_blk(struct xfs_attr_intent *attr);
 int xfs_attr_rmtval_find_space(struct xfs_attr_intent *attr);
+
+const struct xfs_buf_ops *xfs_attr3_remote_buf_ops(unsigned int attrns);
+
 #endif /* __XFS_ATTR_REMOTE_H__ */
