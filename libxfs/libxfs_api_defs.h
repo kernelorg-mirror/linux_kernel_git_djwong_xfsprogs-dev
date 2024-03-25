428211ad555607aae9b5feba396df02c202c677f
Darrick J. Wong <djwong@kernel.org>
2024-03-25 09:44:48 -0700

    xfs: support in-memory btrees
    Source kernel commit: a095686a2383526d7315197e2419d84ee8470217
    
    Adapt the generic btree cursor code to be able to create a btree whose
    buffers come from a (presumably in-memory) buftarg with a header block
    that's specific to in-memory btrees.  We'll connect this to other parts
    of online scrub in the next patches.
    
    Note that in-memory btrees always have a block size matching the system
    memory page size for efficiency reasons.  There are also a few things we
    need to do to finalize a btree update; that's covered in the next patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -76,6 +76,7 @@
 #define xfs_buf_read_uncached		libxfs_buf_read_uncached
 #define xfs_buf_relse			libxfs_buf_relse
 #define xfs_buf_unlock			libxfs_buf_unlock
+#define xfs_buftarg_drain		libxfs_buftarg_drain
 #define xfs_bunmapi			libxfs_bunmapi
 #define xfs_bwrite			libxfs_bwrite
 #define xfs_calc_dquots_per_chunk	libxfs_calc_dquots_per_chunk
