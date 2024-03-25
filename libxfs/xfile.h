552b5f66371cef0e691efa2afa4030443fbe39c5
Darrick J. Wong <djwong@kernel.org>
2024-03-25 09:44:47 -0700

    libxfs: partition memfd files to avoid using too many fds
    Make it so that we can partition a memfd file to avoid running out of
    file descriptors.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfile.h
+++ libxfs/xfile.h
@@ -6,11 +6,24 @@
 #ifndef __LIBXFS_XFILE_H__
 #define __LIBXFS_XFILE_H__
 
-struct xfile {
+struct xfile_fcb {
+	struct list_head	fcb_list;
 	int			fd;
+	unsigned int		refcount;
 };
 
-int xfile_create(const char *description, struct xfile **xfilep);
+struct xfile {
+	struct xfile_fcb	*fcb;
+
+	/* File position within fcb->fd where this partition starts */
+	loff_t			partition_pos;
+
+	/* Maximum number of bytes that can be written to the partition. */
+	uint64_t		maxbytes;
+};
+
+int xfile_create(const char *description, unsigned long long maxbytes,
+		struct xfile **xfilep);
 void xfile_destroy(struct xfile *xf);
 
 ssize_t xfile_load(struct xfile *xf, void *buf, size_t count, loff_t pos);
