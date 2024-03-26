3adf5b382629ce1d4113d563c96f83f2ba5a8f20
Darrick J. Wong <djwong@kernel.org>
2024-03-26 14:49:11 -0700

    xfs: create a separate hashname function for extended attributes
    Create a separate function to compute name hashvalues for extended
    attributes.  When we get to parent pointers we'll be altering the rules
    so that metadump obfuscation doesn't turn heinous.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_attr_leaf.c
+++ libxfs/xfs_attr_leaf.c
@@ -970,9 +970,8 @@ xfs_attr_shortform_to_leaf(
 		nargs.namelen = sfe->namelen;
 		nargs.value = &sfe->nameval[nargs.namelen];
 		nargs.valuelen = sfe->valuelen;
-		nargs.hashval = xfs_da_hashname(sfe->nameval,
-						sfe->namelen);
 		nargs.attr_filter = sfe->flags & XFS_ATTR_NSP_ONDISK_MASK;
+		xfs_attr_sethash(&nargs);
 		error = xfs_attr3_leaf_lookup_int(bp, &nargs); /* set a->index */
 		ASSERT(error == -ENOATTR);
 		error = xfs_attr3_leaf_add(bp, &nargs);
