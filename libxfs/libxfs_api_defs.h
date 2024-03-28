de1d446f4d4170483f28c4d5bf99823967640571
Darrick J. Wong <djwong@kernel.org>
2024-03-27 18:41:06 -0700

    xfs_db: compute hashes of parent pointers
    Enhance the hash command to compute the hashes of parent pointers.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -204,6 +204,7 @@
 #define xfs_mkdir_space_res		libxfs_mkdir_space_res
 #define xfs_parent_add			libxfs_parent_add
 #define xfs_parent_finish		libxfs_parent_finish
+#define xfs_parent_hashval		libxfs_parent_hashval
 #define xfs_parent_irec_from_disk	libxfs_parent_irec_from_disk
 #define xfs_parent_remove		libxfs_parent_remove
 #define xfs_parent_set			libxfs_parent_set
