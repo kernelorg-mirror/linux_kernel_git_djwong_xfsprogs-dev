7ebd8e6922cf424696fa40d87c164d9f9c79b461
Darrick J. Wong <djwong@kernel.org>
2024-04-15 16:08:02 -0700

    xfs: store the btree pointer length in struct xfs_btree_ops
    Source kernel commit: 1a9d26291c68fbb8f8d24f9f694b32223a072745
    
    Make the pointer length an explicit field in the btree operations
    structure so that the next patch (which introduces an explicit btree
    type enum) doesn't have to play a bunch of awkward games with inferring
    the pointer length from the enumeration.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- repair/bulkload.c
+++ repair/bulkload.c
@@ -314,7 +314,7 @@ bulkload_claim_block(
 	if (resv->used == resv->len)
 		list_move_tail(&resv->list, &bkl->resv_list);
 
-	if (cur->bc_ops->geom_flags & XFS_BTGEO_LONG_PTRS)
+	if (cur->bc_ops->ptr_len == XFS_BTREE_LONG_PTR_LEN)
 		ptr->l = cpu_to_be64(XFS_AGB_TO_FSB(mp, resv->pag->pag_agno,
 								agbno));
 	else
