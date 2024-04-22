97b24748a00272abecf52ff079c6f3be9b200d77
Darrick J. Wong <djwong@kernel.org>
2024-04-22 11:53:59 -0700

    xfs_scrub: vectorize repair calls
    Use the new vectorized scrub kernel calls to reduce the overhead of
    performing repairs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- scrub/scrub_private.h
+++ scrub/scrub_private.h
@@ -11,9 +11,10 @@
 void xfrog_scrubv_from_item(struct xfrog_scrubv *scrubv,
 		const struct scrub_item *sri);
 void xfrog_scrubv_add_item(struct xfrog_scrubv *scrubv,
-		const struct scrub_item *sri, unsigned int scrub_type);
+		const struct scrub_item *sri, unsigned int scrub_type,
+		bool want_repair);
 
-int format_scrub_descr(struct scrub_ctx *ctx, char *buf, size_t buflen,
+int format_scrubv_descr(struct scrub_ctx *ctx, char *buf, size_t buflen,
 		void *where);
 
 /* Predicates for scrub flag state. */
@@ -106,8 +107,7 @@ scrub_item_schedule_retry(struct scrub_item *sri, unsigned int scrub_type)
 	return true;
 }
 
-bool scrub_item_call_kernel_again(struct scrub_item *sri,
-		unsigned int scrub_type, uint8_t work_mask,
+bool scrub_item_call_kernel_again(struct scrub_item *sri, uint8_t work_mask,
 		const struct scrub_item *old);
 bool scrub_item_schedule_work(struct scrub_item *sri, uint8_t state_flags);
 
