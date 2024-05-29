f47f2a3241b0881fde3745d3cba8c50d8c50441c
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:10:54 -0700

    xfs: iget for metadata inodes
    Create a xfs_iget_meta function for metadata inodes to ensure that we
    always check that the inobt thinks a metadata inode is in use.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ libxfs/xfs_imeta.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2018-2024 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+#ifndef __XFS_IMETA_H__
+#define __XFS_IMETA_H__
+
+/* Code specific to kernel/userspace; must be provided externally. */
+
+int xfs_imeta_iget(struct xfs_trans *tp, xfs_ino_t ino, umode_t mode,
+		struct xfs_inode **ipp);
+void xfs_imeta_irele(struct xfs_inode *ip);
+
+#endif /* __XFS_IMETA_H__ */
