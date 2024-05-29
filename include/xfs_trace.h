22fab3fec1949625da8eb079a8224b03192e2895
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:11:34 -0700

    xfs: allow inode-based btrees to reserve space in the data device
    Create a new space reservation scheme so that btree metadata for the
    realtime volume can reserve space in the data device to avoid space
    underruns.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/xfs_trace.h
+++ include/xfs_trace.h
@@ -384,4 +384,11 @@
 #define trace_xfs_iunlink_remove(...)		((void) 0)
 #define trace_xfs_iunlink_map_prev_fallback(...)	((void) 0)
 
+#define trace_xfs_metafile_resv_alloc_space(...)	((void) 0)
+#define trace_xfs_metafile_resv_critical(...)	((void) 0)
+#define trace_xfs_metafile_resv_free(...)		((void) 0)
+#define trace_xfs_metafile_resv_free_space(...)	((void) 0)
+#define trace_xfs_metafile_resv_init(...)		((void) 0)
+#define trace_xfs_metafile_resv_init_error(...)	((void) 0)
+
 #endif /* __TRACE_H__ */
