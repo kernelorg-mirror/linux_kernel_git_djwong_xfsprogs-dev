70c2d753019a64f020b72b5313d1c4d58438671d
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:12:51 -0700

    xfs: allow queued realtime intents to drain before scrubbing
    When a writer thread executes a chain of log intent items for the
    realtime volume, the ILOCKs taken during each step are for each rt
    metadata file, not the entire rt volume itself.  Although scrub takes
    all rt metadata ILOCKs, this isn't sufficient to guard against scrub
    checking the rt volume while that writer thread is in the middle of
    finishing a chain because there's no higher level locking primitive
    guarding the realtime volume.
    
    When there's a collision, cross-referencing between data structures
    (e.g. rtrmapbt and rtrefcountbt) yields false corruption events; if
    repair is running, this results in incorrect repairs, which is
    catastrophic.
    
    Fix this by adding to the mount structure the same drain that we use to
    protect scrub against concurrent AG updates, but this time for the
    realtime volume.
    
    [Contains a few cleanups from hch]
    
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtgroup.c
+++ libxfs/xfs_rtgroup.c
@@ -164,6 +164,7 @@ xfs_initialize_rtgroups(
 		/* Place kernel structure only init below this point. */
 		spin_lock_init(&rtg->rtg_state_lock);
 		init_waitqueue_head(&rtg->rtg_active_wq);
+		xfs_defer_drain_init(&rtg->rtg_intents_drain);
 #endif /* __KERNEL__ */
 
 		/* Active ref owned by mount indicates rtgroup is online. */
@@ -203,6 +204,9 @@ xfs_free_unused_rtgroup_range(
 		spin_unlock(&mp->m_rtgroup_lock);
 		if (!rtg)
 			break;
+#ifdef __KERNEL__
+		xfs_defer_drain_free(&rtg->rtg_intents_drain);
+#endif
 		kfree(rtg);
 	}
 }
@@ -236,6 +240,9 @@ xfs_free_rtgroups(
 		spin_unlock(&mp->m_rtgroup_lock);
 		ASSERT(rtg);
 		XFS_IS_CORRUPT(mp, atomic_read(&rtg->rtg_ref) != 0);
+#ifdef __KERNEL__
+		xfs_defer_drain_free(&rtg->rtg_intents_drain);
+#endif
 
 		/* drop the mount's active reference */
 		xfs_rtgroup_rele(rtg);
