b6e192f83ca4ed7308df42cd1cc58aad7faaa57e
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:11:13 -0700

    xfs: update primary realtime super every time we update the primary fs super
    Every time we update parts of the primary filesystem superblock that are
    echoed in the primary rt super, we should update that primary realtime
    super.  Avoid an ondisk log format change by using ordered buffers to
    write the primary rt super.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtgroup.h
+++ libxfs/xfs_rtgroup.h
@@ -213,8 +213,15 @@ xfs_daddr_to_rgbno(
 #ifdef CONFIG_XFS_RT
 xfs_rgblock_t xfs_rtgroup_block_count(struct xfs_mount *mp,
 		xfs_rgnumber_t rgno);
+
+void xfs_rtgroup_update_super(struct xfs_buf *rtsb_bp,
+		const struct xfs_buf *sb_bp);
+struct xfs_buf *xfs_rtgroup_log_super(struct xfs_trans *tp,
+		const struct xfs_buf *sb_bp);
 #else
 # define xfs_rtgroup_block_count(mp, rgno)	(0)
+# define xfs_rtgroup_update_super(bp, sb_bp)	((void)0)
+# define xfs_rtgroup_log_super(tp, sb_bp)	(NULL)
 #endif /* CONFIG_XFS_RT */
 
 #endif /* __LIBXFS_RTGROUP_H */
