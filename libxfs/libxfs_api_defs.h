3c3c34eb1857c6006604acf8a23513f6998dfa6c
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:10:54 -0700

    xfs: iget for metadata inodes
    Create a xfs_imeta_iget function for metadata inodes to ensure that when
    we try to iget a metadata file, the inobt thinks a metadata inode is in
    use and that the file type matches what we are expecting.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -185,6 +185,9 @@
 #define xfs_iext_next			libxfs_iext_next
 #define xfs_ifork_zap_attr		libxfs_ifork_zap_attr
 #define xfs_imap_to_bp			libxfs_imap_to_bp
+
+#define xfs_imeta_iget			libxfs_imeta_iget
+
 #define xfs_initialize_perag		libxfs_initialize_perag
 #define xfs_initialize_perag_data	libxfs_initialize_perag_data
 #define xfs_init_local_fork		libxfs_init_local_fork
