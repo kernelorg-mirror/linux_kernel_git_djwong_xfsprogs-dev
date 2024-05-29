35033654e1e3181de43ac3e4fb16d1a71c9fb680
Andrey Albershteyn <aalbersh@redhat.com>
2024-05-28 21:13:59 -0700

    xfs: add fs-verity ro-compat flag
    To mark inodes with fs-verity enabled the new XFS_DIFLAG2_VERITY flag
    will be added in further patch. This requires ro-compat flag to let
    older kernels know that fs with fs-verity can not be modified.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_format.h
+++ libxfs/xfs_format.h
@@ -366,6 +366,7 @@ xfs_sb_has_compat_feature(
 #define XFS_SB_FEAT_RO_COMPAT_RMAPBT   (1 << 1)		/* reverse map btree */
 #define XFS_SB_FEAT_RO_COMPAT_REFLINK  (1 << 2)		/* reflinked files */
 #define XFS_SB_FEAT_RO_COMPAT_INOBTCNT (1 << 3)		/* inobt block counts */
+#define XFS_SB_FEAT_RO_COMPAT_VERITY   (1U << 29)	/* fs-verity */
 #define XFS_SB_FEAT_RO_COMPAT_RTSUPER  (1U << 30)	/* realtime superblock */
 #define XFS_SB_FEAT_RO_COMPAT_ALL \
 		(XFS_SB_FEAT_RO_COMPAT_FINOBT | \
