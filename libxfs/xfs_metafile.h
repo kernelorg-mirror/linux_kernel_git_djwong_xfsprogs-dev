fd142979d4ab9598e14fcc9cc7ee03e1e49cabec
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:10:58 -0700

    xfs: read and write metadata inode directory tree
    Plumb in the bits we need to load metadata inodes from a named entry in
    a metadir directory, create (or hardlink) inodes into a metadir
    directory, create metadir directories, and flag inodes as being metadata
    files.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_metafile.h
+++ libxfs/xfs_metafile.h
@@ -6,6 +6,9 @@
 #ifndef __XFS_METAFILE_H__
 #define __XFS_METAFILE_H__
 
+void xfs_metafile_set_iflag(struct xfs_trans *tp, struct xfs_inode *ip);
+void xfs_metafile_clear_iflag(struct xfs_trans *tp, struct xfs_inode *ip);
+
 /* Code specific to kernel/userspace; must be provided externally. */
 
 int xfs_metafile_iget(struct xfs_trans *tp, xfs_ino_t ino, umode_t mode,
