fca9a4d0cd827d391f043851dcd1bc1a8b6bf51c
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:22:52 -0700

    xfs: upgrade filesystem features
    Add the ability to upgrade *some* filesystem features.  Note that you'll
    have to run online fsck immediately afterwards to build metadata!
    
    XXX DO NOT MERGE
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_fs.h
+++ libxfs/xfs_fs.h
@@ -1149,6 +1149,7 @@ struct xfs_health_monitor {
 #define XFS_IOC_GETFSREFCOUNTS	_IOWR('X', 66, struct xfs_getfsrefs_head)
 #define XFS_IOC_MAP_FREESP	_IOW ('X', 67, struct xfs_map_freesp)
 #define XFS_IOC_HEALTH_MONITOR	_IOW ('X', 68, struct xfs_health_monitor)
+#define XFS_IOC_ADDFEATURE	_IOW ('X', 69, __u64)
 
 /*
  * ioctl commands that replace IRIX syssgi()'s
