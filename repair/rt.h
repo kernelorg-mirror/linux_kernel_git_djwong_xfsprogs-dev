37ed6d7974f19ac1db5014e84e138b46354d93ea
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:22:34 -0700

    xfs_repair: use realtime refcount btree data to check block types
    Use the realtime refcount btree to pre-populate the block type information
    so that when repair iterates the primary metadata, we can confirm the
    block type.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/rt.h
+++ repair/rt.h
@@ -28,6 +28,12 @@ rtgroup_for_rtrmap_inode(struct xfs_mount *mp, xfs_ino_t ino)
 	return rtgroup_for_rtginode(mp, ino, XFS_RTG_RMAP);
 }
 
+static inline xfs_rgnumber_t
+rtgroup_for_rtrefcount_inode(struct xfs_mount *mp, xfs_ino_t ino)
+{
+	return rtgroup_for_rtginode(mp, ino, XFS_RTG_REFCOUNT);
+}
+
 bool is_rtgroup_inode(xfs_ino_t ino, enum xfs_rtg_inodes type);
 
 static inline bool is_rtrmap_inode(xfs_ino_t ino)
