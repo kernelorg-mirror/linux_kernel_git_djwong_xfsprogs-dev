d5c9b914b53dbe92d101cd11afa603d9b228e21d
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:22:34 -0700

    xfs_repair: find and mark the rtrefcountbt inode
    Make sure that we find the realtime refcountbt inode and mark it
    appropriately, just in case we find a rogue inode claiming to
    be an rtrefcount, or just plain garbage in the superblock field.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/dino_chunks.c
+++ repair/dino_chunks.c
@@ -1017,6 +1017,17 @@ next_readbuf:
 	_("would clear realtime rmap inode %" PRIu64 "\n"),
 						ino);
 				}
+			} else if (is_rtrefcount_inode(ino)) {
+				refcount_avoid_check(mp);
+				if (!no_modify)  {
+					do_warn(
+	_("cleared realtime refcount inode %" PRIu64 "\n"),
+						ino);
+				} else  {
+					do_warn(
+	_("would clear realtime refcount inode %" PRIu64 "\n"),
+						ino);
+				}
 			} else if (!no_modify)  {
 				do_warn(_("cleared inode %" PRIu64 "\n"),
 					ino);
