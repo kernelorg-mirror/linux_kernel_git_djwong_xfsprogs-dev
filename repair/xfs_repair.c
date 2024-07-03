e6e4aac143e75128ccc19ae94ba941bb046a655e
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:21:45 -0700

    xfs_repair: allow sysadmins to add free inode btree indexes
    Allow the sysadmin to use xfs_repair to upgrade an existing filesystem
    to support the free inode btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/xfs_repair.c
+++ repair/xfs_repair.c
@@ -70,6 +70,7 @@ enum c_opt_nums {
 	CONVERT_BIGTIME,
 	CONVERT_NREXT64,
 	CONVERT_EXCHRANGE,
+	CONVERT_FINOBT,
 	C_MAX_OPTS,
 };
 
@@ -79,6 +80,7 @@ static char *c_opts[] = {
 	[CONVERT_BIGTIME]	= "bigtime",
 	[CONVERT_NREXT64]	= "nrext64",
 	[CONVERT_EXCHRANGE]	= "exchange",
+	[CONVERT_FINOBT]	= "finobt",
 	[C_MAX_OPTS]		= NULL,
 };
 
@@ -370,6 +372,15 @@ process_args(int argc, char **argv)
 						do_abort(
 		_("-c exchange only supports upgrades\n"));
 					add_exchrange = true;
+					break;
+				case CONVERT_FINOBT:
+					if (!val)
+						do_abort(
+		_("-c finobt requires a parameter\n"));
+					if (strtol(val, NULL, 0) != 1)
+						do_abort(
+		_("-c finobt only supports upgrades\n"));
+					add_finobt = true;
 					break;
 				default:
 					unknown('c', val);
