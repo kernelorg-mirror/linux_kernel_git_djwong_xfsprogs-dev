cd8aa70fe8490d2cfed0bcbad14940e720b37cfc
Dave Chinner <dchinner@redhat.com>
2024-07-03 14:22:40 -0700

    spaceman: find owners of space in an AG
    Before we can move inodes for a shrink operation, we have to find
    all the inodes that own space in the AG(s) we want to empty.
    
    This implementation uses FS_IOC_GETFSMAP on the assumption that
    filesystems to be shrunk have reverse mapping enabled as it is the
    only way to identify inode related metadata that userspace is unable
    to see or influence (e.g. BMBT blocks) that may be located in the
    specific AG. We can use GETFSMAP to identify both inodes to be moved
    (via XFS_FMR_OWN_INODES records) and inodes with just data and/or
    metadata to be moved.
    
    Once we have identified all the inodes to be moved, we have to
    map them to paths so that we can use renameat2() to exchange the
    directory entries pointing at the moved inode atomically. We also
    need to record inodes with hard links and all of the paths to the
    inode so that hard links can be recreated appropriately.
    
    This requires a directory tree walk to discover the paths (until
    parent pointers are a thing). Hence for filesystems that aren't
    reverse mapping enabled, we can eventually use this pass to discover
    inodes with visible data and metadata that need to be moved.
    
    As we resolve the paths to the inodes to be moved, output the
    information to stdout so that it can be acted upon by other
    utilities. This results in a command that acts similar to find but
    with a physical location filter rather than an inode metadata
    filter.
    
    Again, this is not meant to be an optimal implementation. It
    shouldn't suck, but there is plenty of scope for performance
    optimisation, especially with a multithreaded and/or async directory
    traversal/parent pointer path resolution process to hide access
    latencies.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- spaceman/init.c
+++ spaceman/init.c
@@ -10,6 +10,7 @@
 #include "input.h"
 #include "init.h"
 #include "libfrog/paths.h"
+#include "libfrog/radix-tree.h"
 #include "space.h"
 
 char	*progname;
@@ -38,6 +39,8 @@ init_commands(void)
 	fsprops_init();
 	clearfree_init();
 	move_inode_init();
+	find_owner_init();
+	resolve_owner_init();
 }
 
 static int
@@ -72,6 +75,7 @@ init(
 	setlocale(LC_ALL, "");
 	bindtextdomain(PACKAGE, LOCALEDIR);
 	textdomain(PACKAGE);
+	radix_tree_init();
 
 	fs_table_initialise(0, NULL, 0, NULL);
 	while ((c = getopt(argc, argv, "c:p:V")) != EOF) {
