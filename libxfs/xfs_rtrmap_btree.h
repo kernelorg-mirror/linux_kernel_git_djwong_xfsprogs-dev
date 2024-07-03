e76848fb3c67e4aa4cb65872212a879d9e4898a9
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:22:19 -0700

    xfs: create a shadow rmap btree during realtime rmap repair
    Create an in-memory btree of rmap records instead of an array.  This
    enables us to do live record collection instead of freezing the fs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtrmap_btree.h
+++ libxfs/xfs_rtrmap_btree.h
@@ -12,6 +12,7 @@ struct xfs_mount;
 struct xbtree_ifakeroot;
 struct xfs_rtgroup;
 struct xfs_imeta_path;
+struct xfbtree;
 
 /* rmaps only exist on crc enabled filesystems */
 #define XFS_RTRMAP_BLOCK_LEN	XFS_BTREE_LBLOCK_CRC_LEN
@@ -204,5 +205,10 @@ int xfs_rtrmapbt_create(struct xfs_imeta_update *upd);
 
 unsigned long long xfs_rtrmapbt_calc_size(struct xfs_mount *mp,
 		unsigned long long len);
+
+struct xfs_btree_cur *xfs_rtrmapbt_mem_cursor(struct xfs_rtgroup *rtg,
+		struct xfs_trans *tp, struct xfbtree *xfbtree);
+int xfs_rtrmapbt_mem_init(struct xfs_mount *mp, struct xfbtree *xfbtree,
+		struct xfs_buftarg *btp, xfs_rgnumber_t rgno);
 
 #endif	/* __XFS_RTRMAP_BTREE_H__ */
