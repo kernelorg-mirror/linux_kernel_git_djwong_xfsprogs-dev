28f2d611c46d7dd87080fd1825767840cfcc354a
Andrey Albershteyn <aalbersh@redhat.com>
2024-07-03 14:22:43 -0700

    xfs: add fs-verity support
    Add integration with fs-verity. The XFS store fs-verity metadata in
    the extended file attributes. The metadata consist of verity
    descriptor and Merkle tree blocks.
    
    The descriptor is stored under "vdesc" extended attribute. The
    Merkle tree blocks are stored under binary indexes which are offsets
    into the Merkle tree.
    
    When fs-verity is enabled on an inode, the XFS_IVERITY_CONSTRUCTION
    flag is set meaning that the Merkle tree is being build. The
    initialization ends with storing of verity descriptor and setting
    inode on-disk flag (XFS_DIFLAG2_VERITY).
    
    The verification on read is done in read path of iomap.
    
    Merkle tree blocks are indexed by a per-AG rhashtable to reduce the time
    it takes to load a block from disk in a manner that doesn't bloat struct
    xfs_inode.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    [djwong: replace caching implementation with an xarray, other cleanups]
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ libxfs/xfs_verity.c
@@ -0,0 +1,58 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2023 Red Hat, Inc.
+ */
+#include "libxfs_priv.h"
+#include "xfs_shared.h"
+#include "xfs_format.h"
+#include "xfs_da_format.h"
+#include "xfs_da_btree.h"
+#include "xfs_trans_resv.h"
+#include "xfs_mount.h"
+#include "xfs_inode.h"
+#include "xfs_log_format.h"
+#include "xfs_attr.h"
+#include "xfs_verity.h"
+
+/* Set a merkle tree pos in preparation for setting merkle tree attrs. */
+void
+xfs_merkle_key_to_disk(
+	struct xfs_merkle_key	*key,
+	uint64_t		pos)
+{
+	key->mk_pos = cpu_to_be64(pos);
+}
+
+/* Retrieve the merkle tree pos from the attr data. */
+uint64_t
+xfs_merkle_key_from_disk(
+	const void		*attr_name,
+	int			namelen)
+{
+	const struct xfs_merkle_key *key = attr_name;
+
+	ASSERT(namelen == sizeof(struct xfs_merkle_key));
+
+	return be64_to_cpu(key->mk_pos);
+}
+
+/* Return true if verity attr name is valid. */
+bool
+xfs_verity_namecheck(
+	unsigned int		attr_flags,
+	const void		*name,
+	int			namelen)
+{
+	if (!(attr_flags & XFS_ATTR_VERITY))
+		return false;
+
+	/*
+	 * Merkle tree pages are stored under u64 indexes; verity descriptor
+	 * blocks are held in a named attribute.
+	 */
+	if (namelen != sizeof(struct xfs_merkle_key) &&
+	    namelen != XFS_VERITY_DESCRIPTOR_NAME_LEN)
+		return false;
+
+	return true;
+}
