adb7a49dc09662b6e7919e0f4c580890b40ec93f
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:22:04 -0700

    xfs: hold an active reference to an rtgroup while processing an EFI
    While we're processing an EFI log item, maintain an active reference to
    the rtgroup object so that it cannot go away underneath us.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/defer_item.c
+++ libxfs/defer_item.c
@@ -86,6 +86,11 @@ xfs_extent_free_defer_add(
 	struct xfs_mount		*mp = tp->t_mountp;
 
 	if (xfs_efi_is_realtime(xefi)) {
+		xfs_rgnumber_t		rgno;
+
+		rgno = xfs_rtb_to_rgno(mp, xefi->xefi_startblock);
+		xefi->xefi_rtg = xfs_rtgroup_get(mp, rgno);
+
 		*dfpp = xfs_defer_add(tp, &xefi->xefi_list,
 				&xfs_rtextent_free_defer_type);
 		return;
@@ -163,6 +168,19 @@ const struct xfs_defer_op_type xfs_extent_free_defer_type = {
 	.cancel_item	= xfs_extent_free_cancel_item,
 };
 
+/* Sort bmap items by rtgroup for efficiency. */
+static int
+xfs_rtextent_free_diff_items(
+	void				*priv,
+	const struct list_head		*a,
+	const struct list_head		*b)
+{
+	struct xfs_extent_free_item	*ra = xefi_entry(a);
+	struct xfs_extent_free_item	*rb = xefi_entry(b);
+
+	return ra->xefi_rtg->rtg_rgno - rb->xefi_rtg->rtg_rgno;
+}
+
 static struct xfs_log_item *
 xfs_rtextent_free_create_intent(
 	struct xfs_trans		*tp,
@@ -170,6 +188,10 @@ xfs_rtextent_free_create_intent(
 	unsigned int			count,
 	bool				sort)
 {
+	struct xfs_mount		*mp = tp->t_mountp;
+
+	if (sort)
+		list_sort(mp, items, xfs_rtextent_free_diff_items);
 	return NULL;
 }
 
@@ -180,6 +202,7 @@ xfs_rtextent_free_cancel_item(
 {
 	struct xfs_extent_free_item	*xefi = xefi_entry(item);
 
+	xfs_rtgroup_put(xefi->xefi_rtg);
 	kmem_cache_free(xfs_extfree_item_cache, xefi);
 }
 
