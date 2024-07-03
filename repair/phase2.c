ca77eee60eef8cd894d5e2c12d1af659a2f7b423
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:21:54 -0700

    xfs_repair: don't let metadata and regular files mix
    Track whether or not inodes thought they were metadata inodes.  We
    cannot allow metadata inodes to appear in the regular directory tree,
    and we cannot allow regular inodes to appear in the metadata directory
    tree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/phase2.c
+++ repair/phase2.c
@@ -692,8 +692,10 @@ phase2(
 		 */
 		ino_rec = set_inode_used_alloc(mp, 0,
 				XFS_INO_TO_AGINO(mp, sb->sb_rootino));
-		for (j = 1; j < inuse; j++)
+		for (j = 1; j < inuse; j++) {
 			set_inode_used(ino_rec, j);
+			set_inode_is_meta(ino_rec, j);
+		}
 
 		for (j = inuse; j < XFS_INODES_PER_CHUNK; j++)
 			set_inode_free(ino_rec, j);
@@ -729,6 +731,7 @@ phase2(
 				else
 					do_warn(_("would correct\n"));
 			}
+			set_inode_is_meta(ino_rec, j);
 			j++;
 		}
 
@@ -740,6 +743,7 @@ phase2(
 			else
 				do_warn(_("would correct\n"));
 		}
+		set_inode_is_meta(ino_rec, j);
 		j++;
 
 		if (is_inode_free(ino_rec, j))  {
@@ -750,6 +754,7 @@ phase2(
 			else
 				do_warn(_("would correct\n"));
 		}
+		set_inode_is_meta(ino_rec, j);
 		j++;
 	}
 
