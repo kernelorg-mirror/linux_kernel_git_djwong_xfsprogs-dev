3a0cf8af13ef1c70e15b38d06fc1238d50d0c65d
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:22:31 -0700

    xfs: report realtime refcount btree corruption errors to the health system
    Whenever we encounter corrupt realtime refcount btree blocks, we should
    report that to the health monitoring system for later reporting.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_fs.h
+++ libxfs/xfs_fs.h
@@ -990,6 +990,7 @@ struct xfs_rtgroup_geometry {
 #define XFS_RTGROUP_GEOM_SICK_SUPER	(1U << 0)  /* superblock */
 #define XFS_RTGROUP_GEOM_SICK_BITMAP	(1U << 1)  /* rtbitmap for this group */
 #define XFS_RTGROUP_GEOM_SICK_RMAPBT	(1U << 2)  /* reverse mappings */
+#define XFS_RTGROUP_GEOM_SICK_REFCNTBT	(1U << 3)  /* reference counts */
 
 /*
  * ioctl commands that are used by Linux filesystems
