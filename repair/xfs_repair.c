30d051c88b94cecba1b6242e33fbf1d016df3b26
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:22:06 -0700

    xfs_repair: support adding rtgroups to a filesystem
    Allow users to add the rtgroups feature to a filesystem if the
    filesystem does not already have a realtime volume attached.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/xfs_repair.c
+++ repair/xfs_repair.c
@@ -76,6 +76,8 @@ enum c_opt_nums {
 	CONVERT_RMAPBT,
 	CONVERT_PARENT,
 	CONVERT_METADIR,
+	CONVERT_RTSUPER,
+	CONVERT_RTGROUPS,
 	C_MAX_OPTS,
 };
 
@@ -90,6 +92,8 @@ static char *c_opts[] = {
 	[CONVERT_RMAPBT]	= "rmapbt",
 	[CONVERT_PARENT]	= "parent",
 	[CONVERT_METADIR]	= "metadir",
+	[CONVERT_RTSUPER]	= "rtsuper",
+	[CONVERT_RTGROUPS]	= "rtgroups",
 	[C_MAX_OPTS]		= NULL,
 };
 
@@ -426,6 +430,24 @@ process_args(int argc, char **argv)
 						do_abort(
 		_("-c metadir only supports upgrades\n"));
 					add_metadir = true;
+					break;
+				case CONVERT_RTSUPER:
+					if (!val)
+						do_abort(
+		_("-c rtsuper requires a parameter\n"));
+					if (strtol(val, NULL, 0) != 1)
+						do_abort(
+		_("-c rtsuper only supports upgrades\n"));
+					add_rtsuper = true;
+					break;
+				case CONVERT_RTGROUPS:
+					if (!val)
+						do_abort(
+		_("-c rtgroups requires a parameter\n"));
+					if (strtol(val, NULL, 0) != 1)
+						do_abort(
+		_("-c rtgroups only supports upgrades\n"));
+					add_rtgroups = true;
 					break;
 				default:
 					unknown('c', val);
