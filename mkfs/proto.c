aeb3567cc866f9e19b1bb1399e1100a67f6f882b
Darrick J. Wong <djwong@kernel.org>
2024-07-03 14:22:26 -0700

    mkfs: create the realtime rmap inode
    Create a realtime rmapbt inode if we format the fs with realtime
    and rmap.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- mkfs/proto.c
+++ mkfs/proto.c
@@ -980,6 +980,30 @@ rtfreesp_init(
 	}
 }
 
+/* Allocate rtgroup inodes and directory tree. */
+static void
+rtgroups_create(
+	struct xfs_mount	*mp)
+{
+	struct xfs_rtgroup	*rtg;
+	xfs_rgnumber_t		rgno;
+	unsigned int		i;
+	int			error;
+
+	error = -libxfs_rtginode_mkdir_parent(mp);
+	if (error)
+		fail(_("rtgroup directory allocation failed"), error);
+
+	for_each_rtgroup(mp, rgno, rtg) {
+		for (i = 0; i < XFS_RTG_MAX; i++) {
+			error = -libxfs_rtginode_create(rtg, i, true);
+			if (error)
+				fail(_("rt group inode creation failed"),
+						error);
+		}
+	}
+}
+
 /*
  * Allocate the realtime bitmap and summary inodes, and fill in data if any.
  */
@@ -989,6 +1013,10 @@ rtinit(
 {
 	rtbitmap_create(mp);
 	rtsummary_create(mp);
+
+	if (xfs_has_rtgroups(mp))
+		rtgroups_create(mp);
+
 	rtfreesp_init(mp);
 }
 
