e951abeb832ecf50d4177679a78028fbb8800943
Darrick J. Wong <djwong@kernel.org>
2024-07-26 13:32:43 -0700

    libfrog: define a autofsck filesystem property
    Now that we have the ability to set properties on filesystems, create an
    "autofsck" property so that sysadmins can control background xfs_scrub
    behaviors.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- libfrog/fsproperties.c
+++ libfrog/fsproperties.c
@@ -29,11 +29,49 @@ __fsprops_lookup(
 #define fsprops_lookup(values, value) \
 	__fsprops_lookup((values), ARRAY_SIZE(values), (value))
 
+/* Automatic background fsck fs property */
+
+static const char *fsprop_autofsck_values[] = {
+	[FSPROP_AUTOFSCK_UNSET]		= NULL,
+	[FSPROP_AUTOFSCK_NONE]		= "none",
+	[FSPROP_AUTOFSCK_CHECK]		= "check",
+	[FSPROP_AUTOFSCK_OPTIMIZE]	= "optimize",
+	[FSPROP_AUTOFSCK_REPAIR]	= "repair",
+};
+
+/* Convert the autofsck property enum to a string. */
+const char *
+fsprop_autofsck_write(
+	enum fsprop_autofsck	x)
+{
+	if (x <= FSPROP_AUTOFSCK_UNSET ||
+	    x >= ARRAY_SIZE(fsprop_autofsck_values))
+		return NULL;
+	return fsprop_autofsck_values[x];
+}
+
+/*
+ * Turn a autofsck value string into an enumerated value, or _UNSET if it's
+ * not recognized.
+ */
+enum fsprop_autofsck
+fsprop_autofsck_read(
+	const char	*value)
+{
+	int ret = fsprops_lookup(fsprop_autofsck_values, value);
+	if (ret < 0)
+		return FSPROP_AUTOFSCK_UNSET;
+	return ret;
+}
+
 /* Return true if a fs property name=value tuple is allowed. */
 bool
 fsprop_validate(
 	const char	*name,
 	const char	*value)
 {
+	if (!strcmp(name, FSPROP_AUTOFSCK_NAME))
+		return fsprops_lookup(fsprop_autofsck_values, value) >= 0;
+
 	return true;
 }
