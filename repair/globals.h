56c022a79f01d3a7f8f3a6952abbd4d25678ceea
Christoph Hellwig <hch@lst.de>
2024-07-30 13:58:38 -0700

    xfs_repair: refactor generate_rtinfo
    Move the allocation of the computed values into generate_rtinfo, and thus
    make the variables holding them private in rt.c, and clean up a few
    formatting nits.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    [djwong: move functions to fix build errors]
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/globals.h
+++ repair/globals.h
@@ -134,11 +134,6 @@ extern int		chunks_pblock;	/* # of 64-ino chunks per allocation */
 extern int		max_symlink_blocks;
 extern int64_t		fs_max_file_offset;
 
-/* realtime info */
-
-extern union xfs_rtword_raw		*btmcompute;
-extern union xfs_suminfo_raw		*sumcompute;
-
 /* inode tree records have full or partial backptr fields ? */
 
 extern int		full_ino_ex_data;/*
