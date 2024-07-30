18889f5a580c1177d37ec88df9a229577126a3a3
Christoph Hellwig <hch@lst.de>
2024-07-30 13:38:26 -0700

    xfs_repair: always call into phase5()
    Make the phase5() function handle the rt file checking for the no-modify
    case instead of special casing it in the main repair flow.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/xfs_repair.c
+++ repair/xfs_repair.c
@@ -1437,14 +1437,7 @@ main(int argc, char **argv)
 	phase4(mp);
 	phase_end(mp, 4);
 
-	if (no_modify) {
-		printf(_("No modify flag set, skipping phase 5\n"));
-
-		if (mp->m_sb.sb_rblocks > 0)
-			check_rtmetadata(mp);
-	} else {
-		phase5(mp);
-	}
+	phase5(mp);
 	phase_end(mp, 5);
 	rcbagbt_destroy_cur_cache();
 
