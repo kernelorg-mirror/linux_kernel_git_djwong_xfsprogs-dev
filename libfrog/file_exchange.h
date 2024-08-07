ba42746e64a3f7ca45f0fba52248a032e9a8e454
Darrick J. Wong <djwong@kernel.org>
2024-08-07 15:53:55 -0700

    libfrog: add support for commit range ioctl family
    Add some library code to support the new file range commit ioctls.  This
    will be used to test the atomic file commit functionality in fstests.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libfrog/file_exchange.h
+++ libfrog/file_exchange.h
@@ -12,4 +12,14 @@ void xfrog_exchangerange_prep(struct xfs_exchange_range *fxr,
 int xfrog_exchangerange(int file2_fd, struct xfs_exchange_range *fxr,
 		uint64_t flags);
 
+int xfrog_commitrange_prep(struct xfs_commit_range *xcr, int file2_fd,
+		off_t file2_offset, int file1_fd, off_t file1_offset,
+		uint64_t length);
+int xfrog_commitrange(int file2_fd, struct xfs_commit_range *xcr,
+		uint64_t flags);
+
+int xfrog_defragrange_prep(struct xfs_commit_range *xdf, int file2_fd,
+		const struct xfs_bulkstat *file2_stat, int file1_fd);
+int xfrog_defragrange(int file2_fd, struct xfs_commit_range *xdf);
+
 #endif	/* __LIBFROG_FILE_EXCHANGE_H__ */
