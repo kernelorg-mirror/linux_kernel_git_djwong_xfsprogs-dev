3707ed982830691dca5e5b01733ce676ac71cfa9
Darrick J. Wong <djwong@kernel.org>
2024-08-07 15:53:56 -0700

    xfs_repair: allow sysadmins to add free inode btree indexes
    Allow the sysadmin to use xfs_repair to upgrade an existing filesystem
    to support the free inode btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/globals.h
+++ repair/globals.h
@@ -94,6 +94,7 @@ extern bool	add_inobtcount;		/* add inode btree counts to AGI */
 extern bool	add_bigtime;		/* add support for timestamps up to 2486 */
 extern bool	add_nrext64;
 extern bool	add_exchrange;		/* add file content exchange support */
+extern bool	add_finobt;		/* add free inode btrees */
 
 /* misc status variables */
 
