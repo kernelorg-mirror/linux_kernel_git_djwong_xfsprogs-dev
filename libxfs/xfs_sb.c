a67b901d8f286d238cfdbf6d83595b46393afb61
Darrick J. Wong <djwong@kernel.org>
2024-08-07 16:04:00 -0700

    xfs: create incore realtime group structures
    Create an incore object that will contain information about a realtime
    allocation group.  This will eventually enable us to shard the realtime
    section in a similar manner to how we shard the data section, but for
    now just a single object for the entire RT subvolume is created.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_sb.c
+++ libxfs/xfs_sb.c
@@ -693,6 +693,9 @@ __xfs_sb_from_disk(
 		to->sb_metadirino = be64_to_cpu(from->sb_metadirino);
 	else
 		to->sb_metadirino = NULLFSINO;
+
+	to->sb_rgcount = 1;
+	to->sb_rgextents = 0;
 }
 
 void
@@ -979,6 +982,10 @@ xfs_mount_sb_set_rextsize(
 {
 	mp->m_rtxblklog = log2_if_power2(sbp->sb_rextsize);
 	mp->m_rtxblkmask = mask64_if_power2(sbp->sb_rextsize);
+
+	mp->m_rgblocks = 0;
+	mp->m_rgblklog = 0;
+	mp->m_rgblkmask = 0;
 }
 
 /*
