a3df6fec3aadb1d4314d2cb7c46e3c13ca6a4012
Christoph Hellwig <hch@lst.de>
2024-08-15 11:56:21 -0700

    fixup

--- repair/rt.c
+++ repair/rt.c
@@ -24,7 +24,10 @@ set_rtword(
 	union xfs_rtword_raw	*word,
 	xfs_rtword_t		value)
 {
-	word->old = value;
+	if (xfs_has_rtgroups(mp))
+		word->rtg = cpu_to_be32(value);
+	else
+		word->old = value;
 }
 
 static inline void
