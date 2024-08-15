b3bbe9aa4aa7e9737392201bf29e7d959a29b20d
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:48:55 -0700

    xfs: create routine to allocate and initialize a realtime rmap btree inode
    Create a library routine to allocate and initialize an empty realtime
    rmapbt inode.  We'll use this for mkfs and repair.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtgroup.c
+++ libxfs/xfs_rtgroup.c
@@ -30,6 +30,7 @@
 #include "xfs_rtbitmap.h"
 #include "xfs_metafile.h"
 #include "xfs_metadir.h"
+#include "xfs_rtrmap_btree.h"
 
 /*
  * Passive reference counting access wrappers to the rtgroup structures.  If
@@ -388,6 +389,7 @@ static const struct xfs_rtginode_ops xfs_rtginode_ops[XFS_RTGI_MAX] = {
 		 * rtrmapbt predicate here.
 		 */
 		.enabled	= xfs_has_rmapbt,
+		.create		= xfs_rtrmapbt_create,
 	},
 };
 
