0f2940f7e1aa4f519295630faa89784b05efa84d
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:48:58 -0700

    xfs: report realtime rmap btree corruption errors to the health system
    Whenever we encounter corrupt realtime rmap btree blocks, we should
    report that to the health monitoring system for later reporting.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtgroup.c
+++ libxfs/xfs_rtgroup.c
@@ -382,6 +382,7 @@ static const struct xfs_rtginode_ops xfs_rtginode_ops[XFS_RTGI_MAX] = {
 	[XFS_RTGI_RMAP] = {
 		.name		= "rmap",
 		.metafile_type	= XFS_METAFILE_RTRMAP,
+		.sick		= XFS_SICK_RG_RMAPBT,
 		.fmt_mask	= 1U << XFS_DINODE_FMT_RMAP,
 		/*
 		 * growfs must create the rtrmap inodes before adding a
