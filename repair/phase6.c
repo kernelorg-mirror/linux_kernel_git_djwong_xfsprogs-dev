01b4273ae74959f93d3cd0c202f842160d3ec228
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:57:40 -0700

    xfs_repair: rebuild the realtime rmap btree
    Rebuild the realtime rmap btree file from the reverse mapping records we
    gathered from walking the inodes.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/phase6.c
+++ repair/phase6.c
@@ -21,6 +21,8 @@
 #include "repair/pptr.h"
 #include "repair/rt.h"
 #include "repair/quotacheck.h"
+#include "repair/slab.h"
+#include "repair/rmap.h"
 
 static xfs_ino_t		orphanage_ino;
 
@@ -682,6 +684,15 @@ ensure_rtgroup_summary(
 	fill_rtsummary(rtg);
 }
 
+static void
+ensure_rtgroup_rmapbt(
+	struct xfs_rtgroup	*rtg,
+	xfs_filblks_t		est_fdblocks)
+{
+	if (ensure_rtgroup_file(rtg, XFS_RTGI_RMAP))
+		populate_rtgroup_rmapbt(rtg, est_fdblocks);
+}
+
 /* Initialize a root directory. */
 static int
 init_fs_root_dir(
@@ -746,10 +757,7 @@ mk_metadir(
 	struct xfs_trans	*tp;
 	int			error;
 
-	if (mp->m_rtdirip) {
-		xfs_irele(mp->m_rtdirip);
-		mp->m_rtdirip = NULL;
-	}
+	libxfs_rtginode_irele(&mp->m_rtdirip);
 
 	error = init_fs_root_dir(mp, mp->m_sb.sb_metadirino, 0,
 			&mp->m_metadirip);
@@ -3368,6 +3376,8 @@ static void
 reset_rt_metadir_inodes(
 	struct xfs_mount	*mp)
 {
+	xfs_filblks_t		metadata_blocks = 0;
+	xfs_filblks_t		est_fdblocks = 0;
 	struct xfs_rtgroup	*rtg;
 	xfs_rgnumber_t		rgno;
 	int			error;
@@ -3391,6 +3401,13 @@ reset_rt_metadir_inodes(
 		mark_ino_metadata(mp, mp->m_rtdirip->i_ino);
 	}
 
+	/* Estimate how much free space will be left after building btrees */
+	for_each_rtgroup(mp, rgno, rtg) {
+		metadata_blocks += estimate_rtrmapbt_blocks(rtg);
+	}
+	if (mp->m_sb.sb_fdblocks > metadata_blocks)
+		est_fdblocks = mp->m_sb.sb_fdblocks - metadata_blocks;
+
 	/*
 	 * This isn't the whole story, but it keeps the message that we've had
 	 * for years and which is expected in xfstests and more.
@@ -3405,6 +3422,7 @@ _("        - resetting contents of realtime bitmap and summary inodes\n"));
 	for_each_rtgroup(mp, rgno, rtg) {
 		ensure_rtgroup_bitmap(rtg);
 		ensure_rtgroup_summary(rtg);
+		ensure_rtgroup_rmapbt(rtg, est_fdblocks);
 	}
 }
 
