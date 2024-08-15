a59a45c5c667b0f56f103f87aeab3c1c222065da
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:57:41 -0700

    xfs_repair: reserve per-AG space while rebuilding rt metadata
    Realtime metadata btrees can consume quite a bit of space on a full
    filesystem.  Since the metadata are just regular files, we need to
    make the per-AG reservations to avoid overfilling any of the AGs while
    rebuilding metadata.  This avoids the situation where a filesystem comes
    straight from repair and immediately trips over not having enough space
    in an AG.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/phase6.c
+++ repair/phase6.c
@@ -3431,10 +3431,43 @@ _("        - resetting contents of realtime bitmap and summary inodes\n"));
 	}
 }
 
+static int
+reserve_ag_blocks(
+	struct xfs_mount	*mp)
+{
+	struct xfs_perag	*pag;
+	xfs_agnumber_t		agno;
+	int			error = 0;
+	int			err2;
+
+	mp->m_finobt_nores = false;
+
+	for_each_perag(mp, agno, pag) {
+		err2 = -libxfs_ag_resv_init(pag, NULL);
+		if (err2 && !error)
+			error = err2;
+	}
+
+	return error;
+}
+
+static void
+unreserve_ag_blocks(
+	struct xfs_mount	*mp)
+{
+	struct xfs_perag	*pag;
+	xfs_agnumber_t		agno;
+
+	for_each_perag(mp, agno, pag)
+		libxfs_ag_resv_free(pag);
+}
+
 void
 phase6(xfs_mount_t *mp)
 {
 	ino_tree_node_t		*irec;
+	bool			reserve_perag;
+	int			error;
 	int			i;
 
 	parent_ptr_init(mp);
@@ -3479,10 +3512,24 @@ phase6(xfs_mount_t *mp)
 		do_warn(_("would reinitialize metadata root directory\n"));
 	}
 
+	reserve_perag = xfs_has_realtime(mp) && !no_modify;
+	if (reserve_perag) {
+		error = reserve_ag_blocks(mp);
+		if (error) {
+			if (error != ENOSPC)
+				do_warn(
+	_("could not reserve per-AG space to rebuild realtime metadata"));
+			reserve_perag = false;
+		}
+	}
+
 	if (xfs_has_rtgroups(mp))
 		reset_rt_metadata_inodes(mp);
 	else
 		reset_rt_sb_inodes(mp);
+
+	if (reserve_perag)
+		unreserve_ag_blocks(mp);
 
 	mark_standalone_inodes(mp);
 
