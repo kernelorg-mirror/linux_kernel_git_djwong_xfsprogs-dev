44c59c32d5d99c608b5fb16112d280e3d55aa62b
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:56:36 -0700

    xfs: allow inode-based btrees to reserve space in the data device
    Source kernel commit: 0f422758c646e3160fb1d1285303c91de56a2d18
    
    Create a new space reservation scheme so that btree metadata for the
    realtime volume can reserve space in the data device to avoid space
    underruns.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- libxfs/xfs_ag_resv.c
+++ libxfs/xfs_ag_resv.c
@@ -112,6 +112,7 @@ xfs_ag_resv_needed(
 	case XFS_AG_RESV_RMAPBT:
 		len -= xfs_perag_resv(pag, type)->ar_reserved;
 		break;
+	case XFS_AG_RESV_METAFILE:
 	case XFS_AG_RESV_NONE:
 		/* empty */
 		break;
@@ -345,6 +346,7 @@ xfs_ag_resv_alloc_extent(
 
 	switch (type) {
 	case XFS_AG_RESV_AGFL:
+	case XFS_AG_RESV_METAFILE:
 		return;
 	case XFS_AG_RESV_METADATA:
 	case XFS_AG_RESV_RMAPBT:
@@ -387,6 +389,7 @@ xfs_ag_resv_free_extent(
 
 	switch (type) {
 	case XFS_AG_RESV_AGFL:
+	case XFS_AG_RESV_METAFILE:
 		return;
 	case XFS_AG_RESV_METADATA:
 	case XFS_AG_RESV_RMAPBT:
