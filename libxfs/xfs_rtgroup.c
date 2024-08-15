22c58cca874c33e30667fea1078deb5eccae29e7
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:57:34 -0700

    xfs: wire up rmap map and unmap to the realtime rmapbt
    Source kernel commit: 3fe80153ddb793f21d2d5fd5063acf594573f943
    
    Connect the map and unmap reverse-mapping operations to the realtime
    rmapbt via the deferred operation callbacks.  This enables us to
    perform rmap operations against the correct btree.
    
    [Contains a minor bugfix from hch]
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- libxfs/xfs_rtgroup.c
+++ libxfs/xfs_rtgroup.c
@@ -243,6 +243,10 @@ xfs_rtgroup_lock(
 	} else if (rtglock_flags & XFS_RTGLOCK_BITMAP_SHARED) {
 		xfs_ilock(rtg->rtg_inodes[XFS_RTGI_BITMAP], XFS_ILOCK_SHARED);
 	}
+
+	if ((rtglock_flags & XFS_RTGLOCK_RMAP) &&
+	    rtg->rtg_inodes[XFS_RTGI_RMAP] != NULL)
+		xfs_ilock(rtg->rtg_inodes[XFS_RTGI_RMAP], XFS_ILOCK_EXCL);
 }
 
 /* Unlock metadata inodes associated with this rt group. */
@@ -254,6 +258,9 @@ xfs_rtgroup_unlock(
 	ASSERT(!(rtglock_flags & ~XFS_RTGLOCK_ALL_FLAGS));
 	ASSERT(!(rtglock_flags & XFS_RTGLOCK_BITMAP_SHARED) ||
 	       !(rtglock_flags & XFS_RTGLOCK_BITMAP));
+
+	if ((rtglock_flags & XFS_RTGLOCK_RMAP) && rtg->rtg_inodes[XFS_RTGI_RMAP])
+		xfs_iunlock(rtg->rtg_inodes[XFS_RTGI_RMAP], XFS_ILOCK_EXCL);
 
 	if (rtglock_flags & XFS_RTGLOCK_BITMAP) {
 		xfs_iunlock(rtg->rtg_inodes[XFS_RTGI_SUMMARY], XFS_ILOCK_EXCL);
@@ -282,6 +289,11 @@ xfs_rtgroup_trans_join(
 		xfs_trans_ijoin(tp, rtg->rtg_inodes[XFS_RTGI_SUMMARY],
 				XFS_ILOCK_EXCL);
 	}
+
+	if ((rtglock_flags & XFS_RTGLOCK_RMAP) &&
+	    rtg->rtg_inodes[XFS_RTGI_RMAP] != NULL)
+		xfs_trans_ijoin(tp, rtg->rtg_inodes[XFS_RTGI_RMAP],
+				XFS_ILOCK_EXCL);
 }
 
 /* Retrieve rt group geometry. */
