3f84606d36bc7f36d200252655872fb57fbe234a
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:56:27 -0700

    xfs_db: support dumping realtime superblocks
    Allow debugging of realtime superblocks, and add the relevant fields in
    the fs superblock that point us at the existence and location of the rt
    supers.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- db/sb.c
+++ db/sb.c
@@ -141,6 +141,10 @@ const field_t	sb_flds[] = {
 	{ "meta_uuid", FLDT_UUID, OI(OFF(meta_uuid)), C1, 0, TYP_NONE },
 	{ "metadirino", FLDT_INO, OI(OFF(metadirino)), metadirfld_count,
 	  FLD_COUNT, TYP_INODE },
+	{ "rgcount", FLDT_RGNUMBER, OI(OFF(rgcount)), metadirfld_count,
+	  FLD_COUNT, TYP_NONE },
+	{ "rgextents", FLDT_RTXLEN, OI(OFF(rgextents)), metadirfld_count,
+	  FLD_COUNT, TYP_NONE },
 	{ NULL }
 };
 
