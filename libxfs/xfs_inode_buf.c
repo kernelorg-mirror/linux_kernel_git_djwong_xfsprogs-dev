a3c9ea3a8924c64bdf27927ccece5ed29e5f8f42
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:49:22 -0700

    xfs: add realtime refcount btree inode to metadata directory
    Add a metadir path to select the realtime refcount btree inode and load
    it at mount time.  The rtrefcountbt inode will have a unique extent format
    code, which means that we also have to update the inode validation and
    flush routines to look for it.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_inode_buf.c
+++ libxfs/xfs_inode_buf.c
@@ -448,6 +448,13 @@ xfs_dinode_verify_fork(
 		if (!(dip->di_flags2 & cpu_to_be64(XFS_DIFLAG2_METADATA)))
 			return __this_address;
 		break;
+	case XFS_DINODE_FMT_REFCOUNT:
+		/* same comment about growfs and rmap inodes applies here */
+		if (!xfs_has_reflink(mp))
+			return __this_address;
+		if (!(dip->di_flags2 & cpu_to_be64(XFS_DIFLAG2_METADATA)))
+			return __this_address;
+		break;
 	default:
 		return __this_address;
 	}
@@ -468,6 +475,7 @@ xfs_dinode_verify_forkoff(
 			return __this_address;
 		break;
 	case XFS_DINODE_FMT_RMAP:
+	case XFS_DINODE_FMT_REFCOUNT:
 		if (!(xfs_has_metadir(mp) && xfs_has_parent(mp)))
 			return __this_address;
 		fallthrough;
@@ -755,6 +763,7 @@ xfs_dinode_verify(
 	if (flags2 & XFS_DIFLAG2_METADATA) {
 		switch (XFS_DFORK_FORMAT(dip, XFS_DATA_FORK)) {
 		case XFS_DINODE_FMT_RMAP:
+		case XFS_DINODE_FMT_REFCOUNT:
 			break;
 		default:
 			if (nextents + naextents == 0 && nblocks != 0)
