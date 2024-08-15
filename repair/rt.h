90491a356c6cc26b6c9c0ae27b0ea8a49199db9f
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:57:38 -0700

    xfs_repair: use realtime rmap btree data to check block types
    Use the realtime rmap btree to pre-populate the block type information
    so that when repair iterates the primary metadata, we can confirm the
    block type.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/rt.h
+++ repair/rt.h
@@ -29,6 +29,10 @@ static inline bool is_rtsummary_inode(xfs_ino_t ino)
 {
 	return is_rtgroup_inode(ino, XFS_RTGI_SUMMARY);
 }
+static inline bool is_rtrmap_inode(xfs_ino_t ino)
+{
+	return is_rtgroup_inode(ino, XFS_RTGI_RMAP);
+}
 
 void mark_rtgroup_inodes_bad(struct xfs_mount *mp, enum xfs_rtg_inodes type);
 bool rtgroup_inodes_were_bad(enum xfs_rtg_inodes type);
