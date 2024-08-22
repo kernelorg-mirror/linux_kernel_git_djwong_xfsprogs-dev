1906834c8da017888ace1e46b23b9e7424f089f7
Darrick J. Wong <djwong@kernel.org>
2024-08-21 18:23:58 -0700

    xfs: pass the icreate args object to xfs_dialloc
    Pass the xfs_icreate_args object to xfs_dialloc since we can extract the
    relevant mode (really just the file type) and parent inumber from there.
    This simplifies the calling convention in preparation for the next
    patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/phase6.c
+++ repair/phase6.c
@@ -873,7 +873,7 @@ mk_orphanage(
 	if (i)
 		res_failed(i);
 
-	error = -libxfs_dialloc(&tp, mp->m_sb.sb_rootino, args.mode, &ino);
+	error = -libxfs_dialloc(&tp, &args, &ino);
 	if (error)
 		do_error(_("%s inode allocation failed %d\n"),
 			ORPHANAGE, error);
