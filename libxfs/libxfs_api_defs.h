13f5bb34dc8a9b0f913e5e406fedc1b21760161f
Christoph Hellwig <hch@lst.de>
2024-09-20 13:07:42 -0700

    xfs: merge the perag freeing helpers
    Source kernel commit: e66ebd3fdd6bac14daaccb70b8e7a2e6e96d5ce8
    
    There is no good reason to have two different routines for freeing perag
    structures for the unmount and error cases.  Add two arguments to specify
    the range of AGs to free to xfs_free_perag, and use that to replace
    xfs_free_unused_perag_range.
    
    The addition RCU grace period for the error case is harmless, and the
    extra check for the AG to actually exist is not required now that the
    callers pass the exact known allocated range.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -167,6 +167,7 @@
 #define xfs_free_extent			libxfs_free_extent
 #define xfs_free_extent_later		libxfs_free_extent_later
 #define xfs_free_perag			libxfs_free_perag
+#define xfs_free_perag_range		libxfs_free_perag_range
 #define xfs_fs_geometry			libxfs_fs_geometry
 #define xfs_get_initial_prid		libxfs_get_initial_prid
 #define xfs_highbit32			libxfs_highbit32
