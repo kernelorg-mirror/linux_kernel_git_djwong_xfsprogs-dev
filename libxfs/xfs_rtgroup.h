fcc435817f377212c13d91868d4b5d9f1269f4ca
Darrick J. Wong <djwong@kernel.org>
2024-09-23 13:42:00 -0700

    xfs: add realtime reverse map inode to metadata directory
    Add a metadir path to select the realtime rmap btree inode and load
    it at mount time.  The rtrmapbt inode will have a unique extent format
    code, which means that we also have to update the inode validation and
    flush routines to look for it.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtgroup.h
+++ libxfs/xfs_rtgroup.h
@@ -13,6 +13,7 @@ struct xfs_trans;
 enum xfs_rtg_inodes {
 	XFS_RTGI_BITMAP,	/* allocation bitmap */
 	XFS_RTGI_SUMMARY,	/* allocation summary */
+	XFS_RTGI_RMAP,		/* rmap btree inode */
 
 	XFS_RTGI_MAX,
 };
@@ -279,6 +280,8 @@ int xfs_rtginode_load(struct xfs_rtgroup *rtg, enum xfs_rtg_inodes type,
 		struct xfs_trans *tp);
 int xfs_rtginode_create(struct xfs_rtgroup *rtg, enum xfs_rtg_inodes type,
 		bool init);
+void xfs_rtginode_irele(struct xfs_inode **ipp);
+
 void xfs_rtginode_irele(struct xfs_inode **ipp);
 
 static inline const char *xfs_rtginode_path(xfs_rgnumber_t rgno,
