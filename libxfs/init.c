58080eb02518a48558e4d2d199bfc149c2485d87
Darrick J. Wong <djwong@kernel.org>
2024-10-08 16:41:12 -0700

    xfs: move the min and max group block numbers to xfs_group
    Move the min and max agblock numbers to the generic xfs_group structure
    so that we can start building validators for extents within an rtgroup.
    While we're at it, use check_add_overflow for the extent length
    computation because that has much better overflow checking.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/init.c
+++ libxfs/init.c
@@ -668,7 +668,6 @@ libxfs_mount(
 {
 	struct xfs_buf		*bp;
 	struct xfs_sb		*sbp;
-	struct xfs_rtgroup	*rtg = NULL;
 	xfs_daddr_t		d;
 	int			i;
 	int			error;
@@ -830,10 +829,6 @@ libxfs_mount(
 			progname);
 		exit(1);
 	}
-
-	while ((rtg = xfs_rtgroup_next(mp, rtg)))
-		rtg->rtg_extents = xfs_rtgroup_extents(mp,
-						rtg->rtg_group.xg_index);
 
 	xfs_set_rtgroup_data_loaded(mp);
 
