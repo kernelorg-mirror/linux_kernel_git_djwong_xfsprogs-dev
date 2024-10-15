0e8a1b0579d2c20ca7c7c263aa24d1bae1e9a98b
Christoph Hellwig <hch@lst.de>
2024-10-15 12:44:19 -0700

    xfs_repair: support realtime groups
    Make repair aware of multiple rtgroups.  This now uses the same code as the
    AG-based data device for block usage tracking instead of the less optimal
    AVL trees and bitmaps used for the traditonal RT device.
    
    Note this is still a bit hacky at the moment by just going beyond the AG
    arrays and not fully supporting the unknown state for RT allocation yet.
    The next patch will clean this up.
    
    All this should be fixable.
    
    Large parts of the code are based on patches from Darrick J. Wong.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -108,6 +108,7 @@
 #define xfs_calc_dquots_per_chunk	libxfs_calc_dquots_per_chunk
 #define xfs_cntbt_init_cursor		libxfs_cntbt_init_cursor
 #define xfs_compute_rextslog		libxfs_compute_rextslog
+#define xfs_compute_rgblklog		libxfs_compute_rgblklog
 #define xfs_create_space_res		libxfs_create_space_res
 #define xfs_da3_node_hdr_from_disk	libxfs_da3_node_hdr_from_disk
 #define xfs_da3_node_read		libxfs_da3_node_read
