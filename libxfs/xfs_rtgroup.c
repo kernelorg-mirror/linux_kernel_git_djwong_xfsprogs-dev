4a21c9286b40ea417b8ee403ff5c82fc69a9b9fa
Darrick J. Wong <djwong@kernel.org>
2024-10-15 12:44:43 -0700

    xfs: wire up realtime refcount btree cursors
    Wire up realtime refcount btree cursors wherever they're needed
    throughout the code base.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtgroup.c
+++ libxfs/xfs_rtgroup.c
@@ -204,6 +204,10 @@ xfs_rtgroup_lock(
 	if ((rtglock_flags & XFS_RTGLOCK_RMAP) &&
 	    rtg->rtg_inodes[XFS_RTGI_RMAP] != NULL)
 		xfs_ilock(rtg->rtg_inodes[XFS_RTGI_RMAP], XFS_ILOCK_EXCL);
+
+	if ((rtglock_flags & XFS_RTGLOCK_REFCOUNT) &&
+	    rtg->rtg_inodes[XFS_RTGI_REFCOUNT] != NULL)
+		xfs_ilock(rtg->rtg_inodes[XFS_RTGI_REFCOUNT], XFS_ILOCK_EXCL);
 }
 
 /* Unlock metadata inodes associated with this rt group. */
@@ -215,6 +219,10 @@ xfs_rtgroup_unlock(
 	ASSERT(!(rtglock_flags & ~XFS_RTGLOCK_ALL_FLAGS));
 	ASSERT(!(rtglock_flags & XFS_RTGLOCK_BITMAP_SHARED) ||
 	       !(rtglock_flags & XFS_RTGLOCK_BITMAP));
+
+	if ((rtglock_flags & XFS_RTGLOCK_REFCOUNT) &&
+			rtg->rtg_inodes[XFS_RTGI_REFCOUNT])
+		xfs_iunlock(rtg->rtg_inodes[XFS_RTGI_REFCOUNT], XFS_ILOCK_EXCL);
 
 	if ((rtglock_flags & XFS_RTGLOCK_RMAP) && rtg->rtg_inodes[XFS_RTGI_RMAP])
 		xfs_iunlock(rtg->rtg_inodes[XFS_RTGI_RMAP], XFS_ILOCK_EXCL);
@@ -250,6 +258,11 @@ xfs_rtgroup_trans_join(
 	if ((rtglock_flags & XFS_RTGLOCK_RMAP) &&
 	    rtg->rtg_inodes[XFS_RTGI_RMAP] != NULL)
 		xfs_trans_ijoin(tp, rtg->rtg_inodes[XFS_RTGI_RMAP],
+				XFS_ILOCK_EXCL);
+
+	if ((rtglock_flags & XFS_RTGLOCK_REFCOUNT) &&
+	    rtg->rtg_inodes[XFS_RTGI_REFCOUNT] != NULL)
+		xfs_trans_ijoin(tp, rtg->rtg_inodes[XFS_RTGI_REFCOUNT],
 				XFS_ILOCK_EXCL);
 }
 
