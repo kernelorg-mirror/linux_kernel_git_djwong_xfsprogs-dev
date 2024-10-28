8177aaec073c6d4e1b2e0befe1eddf8aeb440d6d
Christoph Hellwig <hch@lst.de>
2024-10-27 22:11:48 -0700

    xfs: don't use __GFP_RETRY_MAYFAIL in xfs_initialize_perag
    Source kernel commit: 069cf5e32b700f94c6ac60f6171662bdfb04f325
    
    __GFP_RETRY_MAYFAIL increases the likelyhood of allocations to fail,
    which isn't really helpful during log recovery.  Remove the flag and
    stick to the default GFP_KERNEL policies.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- libxfs/xfs_ag.c
+++ libxfs/xfs_ag.c
@@ -284,7 +284,7 @@ xfs_initialize_perag(
 	int			error;
 
 	for (index = old_agcount; index < new_agcount; index++) {
-		pag = kzalloc(sizeof(*pag), GFP_KERNEL | __GFP_RETRY_MAYFAIL);
+		pag = kzalloc(sizeof(*pag), GFP_KERNEL);
 		if (!pag) {
 			error = -ENOMEM;
 			goto out_unwind_new_pags;
