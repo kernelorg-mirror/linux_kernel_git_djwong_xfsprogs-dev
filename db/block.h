a23b25c510ab07d8f786b4e8f4809b28440bead2
Darrick J. Wong <djwong@kernel.org>
2024-11-25 15:47:48 -0800

    xfs: create incore realtime group structures
    Source kernel commit: 87fe4c34a383d51ec75f254240bcd08828f4ce5a
    
    Create an incore object that will contain information about a realtime
    allocation group.  This will eventually enable us to shard the realtime
    section in a similar manner to how we shard the data section, but for
    now just a single object for the entire RT subvolume is created.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- db/block.h
+++ db/block.h
@@ -11,20 +11,4 @@ struct field;
 extern void	block_init(void);
 extern void	print_block(const struct field *fields, int argc, char **argv);
 
-static inline xfs_daddr_t
-xfs_rtb_to_daddr(
-	struct xfs_mount	*mp,
-	xfs_rtblock_t		rtb)
-{
-	return rtb << mp->m_blkbb_log;
-}
-
-static inline xfs_rtblock_t
-xfs_daddr_to_rtb(
-	struct xfs_mount	*mp,
-	xfs_daddr_t		daddr)
-{
-	return daddr >> mp->m_blkbb_log;
-}
-
 #endif /* __XFS_DB_BLOCK_H */
