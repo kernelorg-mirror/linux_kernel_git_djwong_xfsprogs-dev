c0deab1631f4395530e91bc8505edbbf074f4079
Darrick J. Wong <djwong@kernel.org>
2025-02-03 12:28:39 -0800

    xfs_healer: use the autofsck fsproperty to select mode
    Make the xfs_healer background service query the autofsck filesystem
    property to figure out which operating mode it should use.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/xfs_healer.c
+++ healer/xfs_healer.c
@@ -12,6 +12,8 @@
 #include "libfrog/paths.h"
 #include "libfrog/healthevent.h"
 #include "libfrog/workqueue.h"
+#include "libfrog/fsprops.h"
+#include "libfrog/fsproperties.h"
 #include "xfs_healer.h"
 
 /* Program name; needed for libfrog error reports. */
@@ -142,6 +144,71 @@ handle_event(
 	free(hme);
 }
 
+enum want_repair {
+	WR_REPAIR,
+	WR_LOG,
+	WR_EXIT,
+};
+
+/* Determine want_repair from the autofsck filesystem property. */
+static enum want_repair
+want_repair_from_autofsck(
+	struct healer_ctx	*ctx)
+{
+	struct fsprops_handle	fph;
+	char			valuebuf[FSPROP_MAX_VALUELEN + 1] = { 0 };
+	size_t			valuelen = FSPROP_MAX_VALUELEN;
+	enum fsprop_autofsck	shval;
+	int			ret;
+
+	/*
+	 * Any OS error (including ENODATA) or string parsing error is treated
+	 * the same as an unrecognized value.
+	 */
+	ret = fsprops_open_handle(&ctx->mnt, ctx->fs_path, &fph);
+	if (ret)
+		goto no_advice;
+
+	ret = fsprops_get(&fph, FSPROP_AUTOFSCK_NAME, valuebuf, &valuelen);
+	if (ret)
+		goto no_fph_advice;
+
+	shval = fsprop_autofsck_read(valuebuf);
+	switch (shval) {
+	case FSPROP_AUTOFSCK_NONE:
+		/* don't run at all */
+		ret = WR_EXIT;
+		break;
+	case FSPROP_AUTOFSCK_CHECK:
+	case FSPROP_AUTOFSCK_OPTIMIZE:
+		/* check only */
+		ret = WR_LOG;
+		break;
+	case FSPROP_AUTOFSCK_REPAIR:
+		/* repair stuff */
+		ret = WR_REPAIR;
+		break;
+	case FSPROP_AUTOFSCK_UNSET:
+		goto no_fph_advice;
+	}
+
+	fsprops_free_handle(&fph);
+	return ret;
+
+no_fph_advice:
+	fsprops_free_handle(&fph);
+no_advice:
+	/*
+	 * For an unrecognized value, log but do not fix runtime corruption if
+	 * backref metadata are enabled.  If no backref metadata are available,
+	 * the fs is too old so don't run at all.
+	 */
+	if (healer_has_rmapbt(ctx) || healer_has_parent(ctx))
+		return WR_LOG;
+
+	return WR_EXIT;
+}
+
 enum mon_state {
 	MON_START,
 	MON_EXIT,
@@ -163,15 +230,45 @@ setup_monitor(
 		return MON_ERROR;
 	}
 
-	if (ctx->want_repair) {
-		/* Check that the kernel supports repairs at all. */
-		if (!healer_can_repair(ctx)) {
+	if (ctx->autofsck) {
+		switch (want_repair_from_autofsck(ctx)) {
+		case WR_EXIT:
+			printf("%s: %s\n", ctx->mntpoint,
+ _("Disabling daemon per autofsck directive."));
+			fflush(stdout);
+			close(ctx->mnt.fd);
+			return MON_EXIT;
+		case WR_REPAIR:
+			ctx->want_repair = 1;
+			printf("%s: %s\n", ctx->mntpoint,
+ _("Automatically repairing per autofsck directive."));
+			fflush(stdout);
+			break;
+		case WR_LOG:
+			ctx->want_repair = 0;
+			printf("%s: %s\n", ctx->mntpoint,
+ _("Only logging errors per autofsck directive."));
+			fflush(stdout);
+			break;
+		}
+	}
+
+	/* Check that the kernel supports repairs at all. */
+	if (ctx->want_repair && !healer_can_repair(ctx)) {
+		if (!ctx->autofsck) {
 			fprintf(stderr, "%s: %s\n", ctx->mntpoint,
  _("XFS online repair is not supported, exiting"));
 			close(ctx->mnt.fd);
 			return MON_ERROR;
 		}
 
+		printf("%s: %s\n", ctx->mntpoint,
+ _("XFS online repair is not supported, will report only"));
+		fflush(stdout);
+		ctx->want_repair = 0;
+	}
+
+	if (ctx->want_repair) {
 		/* Check for backref metadata that makes repair effective. */
 		if (!healer_has_rmapbt(ctx))
 			fprintf(stderr, "%s: %s\n", ctx->mntpoint,
@@ -332,6 +429,7 @@ usage(void)
 	fprintf(stderr, _("Usage: %s [OPTIONS] mountpoint\n"), progname);
 	fprintf(stderr, "\n");
 	fprintf(stderr, _("Options:\n"));
+	fprintf(stderr, _("  --autofsck   Use the \"autofsck\" fs property to decide to repair.\n"));
 	fprintf(stderr, _("  --check      Check that health monitoring is supported.\n"));
 	fprintf(stderr, _("  --debug      Enable debugging messages.\n"));
 	fprintf(stderr, _("  --everything Capture all events.\n"));
@@ -366,6 +464,7 @@ main(
 		{"everything",	no_argument,	&ctx.everything, 1 },
 		{"repair",	no_argument,	&ctx.want_repair, 1 },
 		{"check",	no_argument,	&ctx.check, 1 },
+		{"autofsck",	no_argument,	&ctx.autofsck, 1 },
 		{NULL,		0,		NULL, 0 },
 	};
 
@@ -389,6 +488,8 @@ main(
 	}
 
 	if (optind != argc - 1)
+		usage();
+	if (ctx.autofsck && ctx.want_repair)
 		usage();
 
 	ctx.mntpoint = argv[optind];
