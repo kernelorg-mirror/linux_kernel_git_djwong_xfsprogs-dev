7ed75d0323e33ff2d91182ba40d11365066a4889
Darrick J. Wong <djwong@kernel.org>
2025-02-03 12:28:02 -0800

    xfs_scrub: don't report data loss in unlinked inodes twice
    If parent pointers are enabled, report_ioerr_fsmap will report lost file
    data and xattrs for all files, having used the parent pointer ioctls to
    generate the path of the lost file.  For unlinked files, the path lookup
    will fail, but we'll report the inumber of the file that lost data.
    
    Therefore, we don't need to do a separate scan of the unlinked inodes
    in report_all_media_errors after doing the fsmap scan.
    
    Cc: <linux-xfs@vger.kernel.org> # v6.10.0
    Fixes: 9b5d1349ca5fb1 ("xfs_scrub: use parent pointers to report lost file data")
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- scrub/phase6.c
+++ scrub/phase6.c
@@ -569,12 +569,12 @@ report_all_media_errors(
 	 * Scan the directory tree to get file paths if we didn't already use
 	 * directory parent pointers to report the loss.
 	 */
-	if (!can_use_pptrs(ctx)) {
-		ret = scan_fs_tree(ctx, report_dir_loss, report_dirent_loss,
-				vs);
-		if (ret)
-			return ret;
-	}
+	if (can_use_pptrs(ctx))
+		return 0;
+
+	ret = scan_fs_tree(ctx, report_dir_loss, report_dirent_loss, vs);
+	if (ret)
+		return ret;
 
 	/* Scan for unlinked files. */
 	return scrub_scan_all_inodes(ctx, report_inode_loss, 0, vs);
