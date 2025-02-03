daa80f2f761746120ab577886507659be247af35
Darrick J. Wong <djwong@kernel.org>
2025-02-03 12:10:14 -0800

    xfs: report metadata health events through healthmon
    Set up a metadata health event hook so that we can send events to
    userspace as we collect information.  The unmount hook severs the weak
    reference between the health monitor and the filesystem it's monitoring;
    when this happens, we stop reporting events because there's no longer
    any point.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- libxfs/xfs_health.h
+++ libxfs/xfs_health.h
@@ -324,4 +324,9 @@ struct xfs_health_update_params {
 	enum xfs_health_update_domain	domain;
 };
 
+unsigned int xfs_healthmon_inode_mask(unsigned int sick_mask);
+unsigned int xfs_healthmon_rtgroup_mask(unsigned int sick_mask);
+unsigned int xfs_healthmon_perag_mask(unsigned int sick_mask);
+unsigned int xfs_healthmon_fs_mask(unsigned int sick_mask);
+
 #endif	/* __XFS_HEALTH_H__ */
