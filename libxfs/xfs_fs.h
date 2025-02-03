f00f83bbf0b3e51758e0986c96a06cf49292bea0
Darrick J. Wong <djwong@kernel.org>
2025-02-03 12:28:35 -0800

    xfs: create a special file to pass filesystem health to userspace
    Create an ioctl that installs a file descriptor backed by an anon_inode
    file that will convey filesystem health events to userspace.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- libxfs/xfs_fs.h
+++ libxfs/xfs_fs.h
@@ -1097,6 +1097,13 @@ struct xfs_map_freesp {
 	__u64	pad;		/* must be zero */
 };
 
+struct xfs_health_monitor {
+	__u64	flags;		/* flags */
+	__u8	format;		/* output format */
+	__u8	pad1[7];	/* zeroes */
+	__u64	pad2[2];	/* zeroes */
+};
+
 /*
  * ioctl commands that are used by Linux filesystems
  */
@@ -1138,6 +1145,7 @@ struct xfs_map_freesp {
 #define XFS_IOC_RTGROUP_GEOMETRY _IOWR('X', 65, struct xfs_rtgroup_geometry)
 #define XFS_IOC_GETFSREFCOUNTS	_IOWR('X', 66, struct xfs_getfsrefs_head)
 #define XFS_IOC_MAP_FREESP	_IOW ('X', 67, struct xfs_map_freesp)
+#define XFS_IOC_HEALTH_MONITOR	_IOW ('X', 68, struct xfs_health_monitor)
 
 /*
  * ioctl commands that replace IRIX syssgi()'s
