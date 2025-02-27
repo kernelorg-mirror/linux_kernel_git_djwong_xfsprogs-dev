d0a2a34f5c8f546bc535fcf0ebf4d83575f37617
Darrick J. Wong <djwong@kernel.org>
2025-02-26 17:47:44 -0800

    xfs_healer: check for fs features needed for effective repairs
    Online repair relies heavily on back references such as reverse mappings
    and directory parent pointers to add redundancy to the filesystem.  Make
    the rust program check for these two features and whine a bit if they
    are missing.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/src/main.rs
+++ healer/src/main.rs
@@ -10,7 +10,9 @@ use std::path::PathBuf;
 use std::process::ExitCode;
 use xfs_healer::healthmon::event::XfsHealthEvent;
 use xfs_healer::healthmon::json::XfsHealthMonitor as JsonMonitor;
+use xfs_healer::repair;
 use xfs_healer::weakhandle::WeakHandle;
+use xfs_healer::xfs_fs::xfs_fsop_geom;
 use xfs_healer::xfsprogs;
 
 // The struct below is a magic struct that implements argument parsing.
@@ -78,6 +80,32 @@ impl App {
         }
     }
 
+    /// Complain a bit if repairs won't be entirely effective.
+    fn check_repair(&self, fp: &File, fsgeom: &xfs_fsop_geom) -> Option<ExitCode> {
+        if !repair::is_supported(fp) {
+            println!(
+                "{}: XFS online repair is not supported",
+                self.path.display()
+            );
+            return Some(ExitCode::FAILURE);
+        }
+
+        if !fsgeom.has_rmapbt() {
+            println!(
+                "{}: XFS online repair is less effective without rmap btrees",
+                self.path.display()
+            );
+        }
+        if !fsgeom.has_parent() {
+            println!(
+                "{}: XFS online repair is less effective without parent pointers",
+                self.path.display()
+            );
+        }
+
+        None
+    }
+
     /// Main app method
     fn main(&self) -> Result<ExitCode> {
         if self.version {
@@ -86,6 +114,14 @@ impl App {
         }
 
         let fp = File::open(&self.path)?;
+
+        let fsgeom = xfs_fsop_geom::try_from(&fp)?;
+        if self.repair {
+            if let Some(ret) = self.check_repair(&fp, &fsgeom) {
+                return Ok(ret);
+            }
+        }
+
         let fh = WeakHandle::try_new(&fp, &self.path)?;
         let hmon = JsonMonitor::try_new(fp, &self.path, self.everything, self.debug)?;
 
