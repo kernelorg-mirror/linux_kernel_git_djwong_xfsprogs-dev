a328caac5d4e9c16bc55a190cbf280b47ef7c63e
Darrick J. Wong <djwong@kernel.org>
2025-03-23 11:30:54 -0700

    xfs_healer: start building a Rust version
    Update the build infrastructure to support building rust programs and
    add a stub xfs_healer program.  This is a little gross because cargo
    install doesn't handle packaging, according to its maintainers.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

---
+++ healer/src/main.rs
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2025 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+use anyhow::{Context, Result};
+use clap::{value_parser, Arg, ArgAction, ArgMatches, Command};
+use std::fs::File;
+use std::path::PathBuf;
+use std::process::ExitCode;
+use xfs_healer::xfsprogs;
+use xfs_healer::xfsprogs::M_;
+
+/// Contains command line arguments
+#[derive(Debug)]
+struct Cli(ArgMatches);
+
+impl Cli {
+    pub fn new() -> Self {
+        Cli(Command::new("xfs_healer")
+            .disable_version_flag(true)
+            .about(M_("Automatically heal damage to XFS filesystem metadata"))
+            .arg(
+                Arg::new("version")
+                    .short('V')
+                    .help(M_("Print version"))
+                    .action(ArgAction::SetTrue),
+            )
+            .arg(
+                Arg::new("debug")
+                    .long("debug")
+                    .help(M_("Enable debugging messages"))
+                    .action(ArgAction::SetTrue),
+            )
+            .arg(
+                Arg::new("log")
+                    .long("log")
+                    .help(M_("Log health events to stdout"))
+                    .action(ArgAction::SetTrue),
+            )
+            .arg(
+                Arg::new("everything")
+                    .long("everything")
+                    .help(M_("Capture all events"))
+                    .action(ArgAction::SetTrue),
+            )
+            .arg(
+                Arg::new("path")
+                    .help(M_("XFS filesystem mountpoint to monitor"))
+                    .value_parser(value_parser!(PathBuf))
+                    .required_unless_present("version"),
+            )
+            .get_matches())
+    }
+}
+
+/// Contains all the global program state but allows more flexibility.
+#[derive(Debug)]
+struct App {
+    debug: bool,
+    log: bool,
+    everything: bool,
+    path: PathBuf,
+}
+
+impl App {
+    /// Return mountpoint as string, for printing messages
+    fn mountpoint(&self) -> String {
+        self.path.display().to_string()
+    }
+
+    /// Main app method
+    fn main(&self) -> Result<ExitCode> {
+        let _fp = File::open(&self.path).with_context(|| "Opening filesystem failed")?;
+
+        Ok(ExitCode::SUCCESS)
+    }
+}
+
+impl From<Cli> for App {
+    fn from(cli: Cli) -> Self {
+        App {
+            debug: cli.0.get_flag("debug"),
+            log: cli.0.get_flag("log"),
+            everything: cli.0.get_flag("everything"),
+            path: cli.0.get_one::<PathBuf>("path").unwrap().to_path_buf(),
+        }
+    }
+}
+
+fn main() -> ExitCode {
+    let args = Cli::new();
+    if args.0.get_flag("version") {
+        println!("{} {}", M_("xfs_healer version"), xfsprogs::VERSION);
+        return ExitCode::SUCCESS;
+    }
+
+    if args.0.get_flag("debug") {
+        println!("args: {:?}", args);
+    }
+
+    let app: App = args.into();
+    match app.main() {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("{}: {:#}", app.mountpoint(), e);
+            ExitCode::FAILURE
+        }
+    }
+}
