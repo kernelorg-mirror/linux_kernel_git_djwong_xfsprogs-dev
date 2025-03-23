e7c9aebc171238507adc3a13901ef289f7050b28
Darrick J. Wong <djwong@kernel.org>
2025-03-23 11:30:55 -0700

    xfs_healer: make the rust program check if kernel support available
    Teach the rust port program to check if kernel support is available, to
    duplicate existing python functionality.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/src/healthmon/mod.rs
+++ healer/src/healthmon/mod.rs
@@ -3,8 +3,12 @@
  * Copyright (C) 2025 Oracle.  All Rights Reserved.
  * Author: Darrick J. Wong <djwong@kernel.org>
  */
+use crate::xfs_fs;
 use crate::xfs_fs::xfs_health_monitor;
 use nix::ioctl_write_ptr;
+use std::fs::File;
+use std::os::fd::AsRawFd;
+use std::os::fd::FromRawFd;
 
 pub mod cstruct;
 pub mod event;
@@ -13,3 +17,23 @@ pub mod groups;
 pub mod inodes;
 
 ioctl_write_ptr!(xfs_ioc_health_monitor, 'X', 68, xfs_health_monitor);
+
+/// Check if the open file supports a health monitor.
+pub fn is_supported(fp: &File) -> bool {
+    let hminfo = xfs_health_monitor {
+        format: xfs_fs::XFS_HEALTH_MONITOR_FMT_V0 as u8,
+        ..Default::default()
+    };
+
+    // SAFETY: Trusting the kernel not to corrupt our memory, and for it to return a valid file
+    // description number, which we immediately convert to a File and drop to close the fd.
+    unsafe {
+        match xfs_ioc_health_monitor(fp.as_raw_fd(), &hminfo) {
+            Ok(x) => {
+                File::from_raw_fd(x);
+                true
+            }
+            Err(_) => false,
+        }
+    }
+}
