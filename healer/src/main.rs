ad0cd6a33f2e4db45d95e2ce983542228e2cdc70
Darrick J. Wong <djwong@kernel.org>
2025-03-23 11:30:55 -0700

    xfs_healer: create a weak file handle so we don't pin the mount
    Create a custom file handle object that allows us to maintain a "soft"
    reference to a mounted filesystem.  The purpose of this is to avoid
    pinning the mount while xfs_healer runs by not leaving an open fd while
    retaining the ability to reconnect to the filesystem later so that we
    can look up paths for reporting, and run repairs.
    
    This means that the filesystem must still be available at the same path
    at reconnect time, which may result in the program exiting if mount
    --move is used.
    
    Note that we open-code the XFS_IOC_FD_TO_HANDLE call to avoid
    overcomplicating the cargo configuration to link with
    ../libhandle/libhandle.la as a static import for a single function call.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/src/main.rs
+++ healer/src/main.rs
@@ -12,6 +12,7 @@ use xfs_healer::healthmon::cstruct::CStructMonitor;
 use xfs_healer::healthmon::event::XfsHealthEvent;
 use xfs_healer::healthmon::json::JsonMonitor;
 use xfs_healer::printlogln;
+use xfs_healer::weakhandle::WeakHandle;
 use xfs_healer::xfsprogs;
 use xfs_healer::xfsprogs::M_;
 
@@ -97,6 +98,8 @@ impl App {
     /// Main app method
     fn main(&self) -> Result<ExitCode> {
         let fp = File::open(&self.path).with_context(|| M_("Opening filesystem failed"))?;
+        let _fh = WeakHandle::try_new(&fp, &self.path)
+            .with_context(|| M_("Configuring filesystem handle"))?;
 
         if self.json {
             let hmon = JsonMonitor::try_new(fp, &self.path, self.everything, self.debug)
