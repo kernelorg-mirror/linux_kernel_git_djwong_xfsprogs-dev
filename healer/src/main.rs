9b0a2f166b90459d6f7f221a8dcc3a6bb103e001
Darrick J. Wong <djwong@kernel.org>
2025-03-23 11:30:55 -0700

    xfs_healer: make the rust program check if kernel support available
    Teach the rust port program to check if kernel support is available, to
    duplicate existing python functionality.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/src/main.rs
+++ healer/src/main.rs
@@ -62,6 +62,12 @@ impl Cli {
                     .help(M_("Always repair corrupt metadata"))
                     .action(ArgAction::SetTrue),
             )
+            .arg(
+                Arg::new("check")
+                    .long("check")
+                    .help(M_("Check that health monitoring is supported"))
+                    .action(ArgAction::SetTrue),
+            )
             .get_matches())
     }
 }
@@ -72,6 +78,7 @@ struct App {
     log: bool,
     everything: bool,
     repair: bool,
+    check: bool,
     path: PathBuf,
 }
 
@@ -146,6 +153,16 @@ impl App {
             }
         }
 
+        // Now that we know that we can repair if the user wanted to, make sure that the kernel
+        // supports reporting events if that was as far as the user wanted us to go.
+        if self.check {
+            return Ok(if xfs_healer::healthmon::is_supported(&fp) {
+                ExitCode::SUCCESS
+            } else {
+                ExitCode::FAILURE
+            });
+        }
+
         let fh = WeakHandle::try_new(&fp, &self.path, fsgeom)
             .with_context(|| M_("Configuring filesystem handle"))?;
 
@@ -167,6 +184,7 @@ impl From<Cli> for App {
             everything: cli.0.get_flag("everything"),
             path: cli.0.get_one::<PathBuf>("path").unwrap().to_path_buf(),
             repair: cli.0.get_flag("repair"),
+            check: cli.0.get_flag("check"),
         }
     }
 }
