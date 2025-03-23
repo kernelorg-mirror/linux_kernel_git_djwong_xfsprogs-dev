3ab00f921841f2420c94e3873f5ac9057b8a8522
Darrick J. Wong <djwong@kernel.org>
2025-03-23 11:30:55 -0700

    xfs_healer: check for fs features needed for effective repairs
    Online repair relies heavily on back references such as reverse mappings
    and directory parent pointers to add redundancy to the filesystem.  Make
    the rust program check for these two features and whine a bit if they
    are missing.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/src/main.rs
+++ healer/src/main.rs
@@ -11,7 +11,9 @@ use std::process::ExitCode;
 use xfs_healer::healthmon::cstruct::CStructMonitor;
 use xfs_healer::healthmon::event::XfsHealthEvent;
 use xfs_healer::printlogln;
+use xfs_healer::repair::Repair;
 use xfs_healer::weakhandle::WeakHandle;
+use xfs_healer::xfs_fs::xfs_fsop_geom;
 use xfs_healer::xfsprogs;
 use xfs_healer::xfsprogs::M_;
 
@@ -98,9 +100,48 @@ impl App {
         }
     }
 
+    /// Complain if repairs won't be entirely effective.
+    fn check_repair(&self, fp: &File, fsgeom: &xfs_fsop_geom) -> Option<ExitCode> {
+        if !Repair::is_supported(fp) {
+            printlogln!(
+                "{}: {}",
+                self.path.display(),
+                M_("XFS online repair is not supported, exiting")
+            );
+            return Some(ExitCode::FAILURE);
+        }
+
+        if !fsgeom.has_rmapbt() {
+            printlogln!(
+                "{}: {}",
+                self.path.display(),
+                M_("XFS online repair is less effective without rmap btrees")
+            );
+        }
+        if !fsgeom.has_parent() {
+            printlogln!(
+                "{}: {}",
+                self.path.display(),
+                M_("XFS online repair is less effective without parent pointers")
+            );
+        }
+
+        None
+    }
+
     /// Main app method
     fn main(&self) -> Result<ExitCode> {
         let fp = File::open(&self.path).with_context(|| M_("Opening filesystem failed"))?;
+
+        // Make sure that we can initiate repairs
+        let fsgeom =
+            xfs_fsop_geom::try_from(&fp).with_context(|| M_("Querying filesystem geometry"))?;
+        if self.repair {
+            if let Some(ret) = self.check_repair(&fp, &fsgeom) {
+                return Ok(ret);
+            }
+        }
+
         let fh = WeakHandle::try_new(&fp, &self.path)
             .with_context(|| M_("Configuring filesystem handle"))?;
 
