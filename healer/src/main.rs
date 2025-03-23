266efec0078ef31c809bea22ebccc7e17654a75f
Darrick J. Wong <djwong@kernel.org>
2025-03-23 11:30:56 -0700

    xfs_healer: run full scrub after lost corruption events or targeted repair failure
    If we fail to perform a spot repair of metadata or the kernel tells us
    that it lost corruption events due to queue limits, initiate a full run
    of the online fsck service to try to fix the error.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/src/main.rs
+++ healer/src/main.rs
@@ -109,6 +109,7 @@ struct App {
 #[derive(Debug)]
 struct EventThread {
     log: bool,
+    everything: bool,
     repair: bool,
 }
 
@@ -118,6 +119,7 @@ impl EventThread {
     fn new(app: &App) -> Self {
         EventThread {
             log: app.log,
+            everything: app.everything,
             repair: app.repair,
         }
     }
@@ -162,7 +164,7 @@ impl App {
                     };
                 }
                 if et.repair {
-                    for mut repair in event.schedule_repairs() {
+                    for mut repair in event.schedule_repairs(et.everything) {
                         repair.perform(&fh)
                     }
                 }
