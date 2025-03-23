ef1dbd8383e8d3d40289f93c8e16a5293f3458b2
Darrick J. Wong <djwong@kernel.org>
2025-03-23 11:30:56 -0700

    xfs_healer: run full scrub after lost corruption events or targeted repair failure
    If we fail to perform a spot repair of metadata or the kernel tells us
    that it lost corruption events due to queue limits, initiate a full run
    of the online fsck service to try to fix the error.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/src/util.rs
+++ healer/src/util.rs
@@ -55,6 +55,15 @@ macro_rules! display_for_enum {
     };
 }
 
+/// Simple macro for creating errors for random badness.  The only parameter describes why the
+/// badness happened.
+#[macro_export]
+macro_rules! badness {
+    ($message:expr) => {{
+        std::io::Error::new(std::io::ErrorKind::Other, $message)
+    }};
+}
+
 /// Format an enum set into a string
 pub fn format_set<T: EnumSetType + Display>(f: EnumSet<T>) -> String {
     let mut ret = "".to_string();
