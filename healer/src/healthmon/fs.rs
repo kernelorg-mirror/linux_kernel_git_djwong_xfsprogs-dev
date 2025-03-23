b7027122993553c304bee5335300483ad8269822
Darrick J. Wong <djwong@kernel.org>
2025-03-23 11:30:55 -0700

    xfs_healer: use getparents to look up file names
    If the kernel tells about something that happened to a file, use the
    GETPARENTS ioctl to try to look up the path to that file for more
    ergonomic reporting.  In Rust.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/src/healthmon/fs.rs
+++ healer/src/healthmon/fs.rs
@@ -9,10 +9,12 @@ use crate::healthmon::event::XfsHealthEvent;
 use crate::healthmon::event::XfsHealthStatus;
 use crate::repair::Repair;
 use crate::util::format_set;
+use crate::weakhandle::WeakHandle;
 use crate::xfs_types::XfsPhysRange;
 use crate::xfsprogs::M_;
 use enumset::EnumSet;
 use enumset::EnumSetType;
+use std::path::PathBuf;
 use strum_macros::EnumString;
 
 /// Metadata types for an XFS whole-fs metadata
@@ -58,11 +60,14 @@ impl XfsWholeFsEvent {
 }
 
 impl XfsHealthEvent for XfsWholeFsEvent {
-    fn format(&self) -> String {
-        format!(
-            " {}: {}",
-            format_set(self.metadata),
-            self.status
+    fn format(&self, _: &WeakHandle) -> (Option<PathBuf>, String) {
+        (
+            None,
+            format!(
+                " {}: {}",
+                format_set(self.metadata),
+                self.status
+            ),
         )
     }
 
@@ -111,11 +116,14 @@ impl XfsHealthEvent for XfsShutdownEvent {
         true
     }
 
-    fn format(&self) -> String {
-        format!(
-            ": {} {}",
-            M_("filesystem shut down due to"),
-            format_set(self.reasons)
+    fn format(&self, _: &WeakHandle) -> (Option<PathBuf>, String) {
+        (
+            None,
+            format!(
+                ": {} {}",
+                M_("filesystem shut down due to"),
+                format_set(self.reasons)
+            ),
         )
     }
 }
@@ -128,8 +136,8 @@ impl XfsHealthEvent for XfsUnmountEvent {
         true
     }
 
-    fn format(&self) -> String {
-        format!( ": {}", M_("filesystem unmounted"))
+    fn format(&self, _: &WeakHandle) -> (Option<PathBuf>, String) {
+        (None, format!(": {}", M_("filesystem unmounted")))
     }
 }
 
@@ -148,7 +156,7 @@ impl XfsMediaErrorEvent {
 }
 
 impl XfsHealthEvent for XfsMediaErrorEvent {
-    fn format(&self) -> String {
-        format!(" {}: {}", self.range, M_("media error"))
+    fn format(&self, _: &WeakHandle) -> (Option<PathBuf>, String) {
+        (None, format!(" {}: {}", self.range, M_("media error")))
     }
 }
--- healer/src/healthmon/inodes.rs
+++ healer/src/healthmon/inodes.rs
@@ -9,10 +9,12 @@ use crate::healthmon::event::XfsHealthEvent;
 use crate::healthmon::event::XfsHealthStatus;
 use crate::repair::Repair;
 use crate::util::format_set;
+use crate::weakhandle::WeakHandle;
 use crate::xfs_types::{XfsFid, XfsFileRange};
 use crate::xfsprogs::M_;
 use enumset::EnumSet;
 use enumset::EnumSetType;
+use std::path::PathBuf;
 use strum_macros::EnumString;
 
 /// Metadata types for an XFS inode
@@ -71,8 +73,17 @@ impl XfsInodeEvent {
 }
 
 impl XfsHealthEvent for XfsInodeEvent {
-    fn format(&self) -> String {
-        format!(" {} {}: {}", self.fid, format_set(self.metadata), self.status)
+    fn format(&self, fh: &WeakHandle) -> (Option<PathBuf>, String) {
+        match fh.path_for(self.fid) {
+            Some(path) => (
+                Some(path),
+                format!(" {}: {}", format_set(self.metadata), self.status),
+            ),
+            None => (
+                None,
+                format!(" {} {}: {}", self.fid, format_set(self.metadata), self.status),
+            ),
+        }
     }
 
     schedule_repairs!(XfsInodeEvent, |s: &XfsInodeEvent, sm_type| {
@@ -122,7 +133,10 @@ impl XfsFileIoErrorEvent {
 }
 
 impl XfsHealthEvent for XfsFileIoErrorEvent {
-    fn format(&self) -> String {
-        format!(" {} {}: {} {}", self.fid, self.range, self.iotype, M_("failed"))
+    fn format(&self, fh: &WeakHandle) -> (Option<PathBuf>, String) {
+        match fh.path_for(self.fid) {
+            Some(path) => (Some(path), format!(" {}: {} {}", self.range, self.iotype, M_("failed"))),
+            None => (None, format!(" {} {}: {} {}", self.fid, self.range, self.iotype, M_("failed"))),
+        }
     }
 }
