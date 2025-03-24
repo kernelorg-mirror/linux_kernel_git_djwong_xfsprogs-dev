7b10f6c2b6013fe05955d222cb86f452db98dfe0
Darrick J. Wong <djwong@kernel.org>
2025-03-24 10:59:32 -0700

    xfs_healer: define Rust objects for health events and kernel interface
    Create our own Rust types for health events; these objects will know how
    to report themselves and (later) how to initiate repairs.  Create the
    Rust binding to the kernel ioctl interface.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/src/main.rs
+++ healer/src/main.rs
@@ -8,6 +8,7 @@ use std::fs::File;
 use std::io::Result;
 use std::path::PathBuf;
 use std::process::ExitCode;
+use xfs_healer::printlogln;
 use xfs_healer::xfsprogs;
 use xfs_healer::xfsprogs::M_;
 
@@ -93,12 +94,12 @@ fn main() -> ExitCode {
 
     let args = Cli::new();
     if args.0.get_flag("version") {
-        println!("{} {}", M_("xfs_healer version"), xfsprogs::VERSION);
+        printlogln!("{} {}", M_("xfs_healer version"), xfsprogs::VERSION);
         return ExitCode::SUCCESS;
     }
 
     if args.0.get_flag("debug") {
-        println!("args: {:?}", args);
+        printlogln!("args: {:?}", args);
     }
 
     let app: App = args.into();
