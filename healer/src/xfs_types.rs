7c98ad2dc3e4b8fa1720db988d5eafb8abe85b6b
Darrick J. Wong <djwong@kernel.org>
2025-03-24 10:59:32 -0700

    xfs_healer: define Rust objects for health events and kernel interface
    Create our own Rust types for health events; these objects will know how
    to report themselves and (later) how to initiate repairs.  Create the
    Rust binding to the kernel ioctl interface.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

---
+++ healer/src/xfs_types.rs
@@ -0,0 +1,241 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2025 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+use crate::baddata;
+use crate::display_for_enum;
+use crate::xfsprogs::M_;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::io::Error;
+use std::io::Result;
+
+/// Allocation group number on the data device
+#[derive(Debug)]
+pub struct XfsAgNumber(u32);
+
+impl TryFrom<u64> for XfsAgNumber {
+    type Error = Error;
+
+    fn try_from(v: u64) -> Result<Self> {
+        if v > i32::MAX as u64 {
+            Err(baddata!(M_("AG number too large"), Self, v))
+        } else {
+            Ok(XfsAgNumber(v as u32))
+        }
+    }
+}
+
+impl Display for XfsAgNumber {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", M_("agno"), self.0)
+    }
+}
+
+/// Realtime group number on the realtime device
+#[derive(Debug)]
+pub struct XfsRgNumber(u32);
+
+impl Display for XfsRgNumber {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", M_("rgno"), self.0)
+    }
+}
+
+impl TryFrom<u64> for XfsRgNumber {
+    type Error = Error;
+
+    fn try_from(v: u64) -> Result<Self> {
+        if v > i32::MAX as u64 {
+            Err(baddata!(M_("rtgroup number too large"), Self, v))
+        } else {
+            Ok(XfsRgNumber(v as u32))
+        }
+    }
+}
+
+/// Disk devices
+#[derive(Debug)]
+pub enum XfsDevice {
+    Data,
+    Log,
+    Realtime,
+}
+
+display_for_enum!(XfsDevice, {
+    Data     => M_("datadev"),
+    Log      => M_("logdev"),
+    Realtime => M_("rtdev"),
+});
+
+/// Disk address, in 512b units
+#[derive(Debug)]
+pub struct XfsDaddr(u64);
+
+impl Display for XfsDaddr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {:#x}", M_("daddr"), self.0)
+    }
+}
+
+impl From<u64> for XfsDaddr {
+    fn from(v: u64) -> XfsDaddr {
+        XfsDaddr(v)
+    }
+}
+
+/// Disk space length, in 512b units
+#[derive(Debug)]
+pub struct XfsBbcount(u64);
+
+impl Display for XfsBbcount {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {:#x}", M_("bbcount"), self.0)
+    }
+}
+
+impl From<u64> for XfsBbcount {
+    fn from(v: u64) -> XfsBbcount {
+        XfsBbcount(v)
+    }
+}
+
+/// Range of physical storage
+#[derive(Debug)]
+pub struct XfsPhysRange {
+    /// Which device is this?
+    pub device: XfsDevice,
+
+    /// Start of the range, in 512b units
+    pub daddr: XfsDaddr,
+
+    /// Size of the range, in 512b units
+    pub bbcount: XfsBbcount,
+}
+
+impl Display for XfsPhysRange {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.device, self.daddr, self.bbcount)
+    }
+}
+
+/// Inode number
+#[derive(Debug)]
+pub struct XfsIno(u64);
+
+impl Display for XfsIno {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", M_("ino"), self.0)
+    }
+}
+
+impl TryFrom<u64> for XfsIno {
+    type Error = Error;
+
+    fn try_from(v: u64) -> Result<Self> {
+        if v > i64::MAX as u64 {
+            Err(baddata!(M_("inode number too large"), Self, v))
+        } else {
+            Ok(XfsIno(v))
+        }
+    }
+}
+
+/// Inode generation number
+#[derive(Debug)]
+pub struct XfsIgeneration(u32);
+
+impl Display for XfsIgeneration {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {:#x}", M_("gen"), self.0)
+    }
+}
+
+impl TryFrom<u64> for XfsIgeneration {
+    type Error = Error;
+
+    fn try_from(v: u64) -> Result<Self> {
+        if v > u32::MAX as u64 {
+            Err(baddata!(M_("inode generation number too large"), Self, v))
+        } else {
+            Ok(XfsIgeneration(v as u32))
+        }
+    }
+}
+
+/// Miniature FID for a handle
+#[derive(Debug)]
+pub struct XfsFid {
+    /// Inode number
+    pub ino: XfsIno,
+
+    /// Inode generation
+    pub gen: XfsIgeneration,
+}
+
+impl Display for XfsFid {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.ino, self.gen)
+    }
+}
+
+/// File position
+#[derive(Debug)]
+pub struct XfsPos(u64);
+
+impl Display for XfsPos {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", M_("pos"), self.0)
+    }
+}
+
+impl TryFrom<u64> for XfsPos {
+    type Error = Error;
+
+    fn try_from(v: u64) -> Result<Self> {
+        if v > i64::MAX as u64 {
+            Err(baddata!(M_("file position too large"), Self, v))
+        } else {
+            Ok(XfsPos(v))
+        }
+    }
+}
+
+/// File IO length
+#[derive(Debug)]
+pub struct XfsIoLen(i64);
+
+impl Display for XfsIoLen {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", M_("len"), self.0)
+    }
+}
+
+impl TryFrom<u64> for XfsIoLen {
+    type Error = Error;
+
+    fn try_from(v: u64) -> Result<Self> {
+        if v > i64::MAX as u64 {
+            Err(baddata!(M_("file IO length too large"), Self, v))
+        } else {
+            Ok(XfsIoLen(v as i64))
+        }
+    }
+}
+
+/// Range of a file's bytes
+#[derive(Debug)]
+pub struct XfsFileRange {
+    /// Start of range, in bytes
+    pub pos: XfsPos,
+
+    /// Length of range, in bytes
+    pub len: XfsIoLen,
+}
+
+impl Display for XfsFileRange {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.pos, self.len)
+    }
+}
--- m4/package_rust.m4
+++ m4/package_rust.m4
@@ -128,6 +128,10 @@ AC_DEFUN([AC_HAVE_HEALER_CRATES],
     [
 clap = { version = "4.0.32", features = [["derive"]] }
 $gettext_dep
+nix = { version = "0.26.1" }
+enumset = { version = "1.0.12" }
+strum = { version = "0.19.2" }
+strum_macros = { version = "0.19.2" }
 ],
     [yes], [no])
 ])
