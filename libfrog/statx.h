0b9980c70099d0e16e2210ed39b00eaecef379a0
Darrick J. Wong <djwong@kernel.org>
2025-04-16 13:30:00 -0700

    xfs_io: dump new atomic_write_unit_max_opt statx field
    Dump the new atomic writes statx field that's being submitted for 6.16.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- libfrog/statx.h
+++ libfrog/statx.h
@@ -143,7 +143,11 @@ struct statx {
 	__u32	stx_dio_read_offset_align;
 
 	/* 0xb8 */
-	__u64	__spare3[9];	/* Spare space for future expansion */
+	/* Optimised max atomic write unit in bytes */
+	__u32	stx_atomic_write_unit_max_opt;
+	__u32	__spare2[1];
+	/* 0xc0 */
+	__u64	__spare3[8];	/* Spare space for future expansion */
 	/* 0x100 */
 };
 
