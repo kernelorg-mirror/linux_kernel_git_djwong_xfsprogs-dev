b01cdf2b3a886fb7c8e2fb93f90ad8303ea4b39d
Darrick J. Wong <djwong@kernel.org>
2025-10-29 14:15:00 -0700

    xfs_healer: use getparents to look up file names
    If the kernel tells about something that happened to a file, use the
    GETPARENTS ioctl to try to look up the path to that file for more
    ergonomic reporting.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- healer/xfs_healer.h
+++ healer/xfs_healer.h
@@ -55,6 +55,10 @@ static inline bool healer_has_parent(const struct healer_ctx *ctx)
 	return ctx->mnt.fsgeom.flags & XFS_FSOP_GEOM_FLAGS_PARENT;
 }
 
+void lookup_path(struct healer_ctx *ctx,
+		const struct xfs_health_monitor_event *hme,
+		struct hme_prefix *pfx);
+
 /* repair.c */
 int repair_metadata(struct healer_ctx *ctx, const struct hme_prefix *pfx,
 		const struct xfs_health_monitor_event *hme);
@@ -65,5 +69,7 @@ int weakhandle_alloc(int fd, const char *mountpoint, const struct fs_path *fsp,
 		struct weakhandle **whp);
 int weakhandle_reopen(struct weakhandle *wh, int *fd);
 void weakhandle_free(struct weakhandle **whp);
+int weakhandle_getpath_for(struct weakhandle *wh, uint64_t ino, uint32_t gen,
+		char *path, size_t pathlen);
 
 #endif /* XFS_HEALER_XFS_HEALER_H_ */
