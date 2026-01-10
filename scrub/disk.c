ce79248ee445afe13a6cd68ff227341ca1e80fd1
Darrick J. Wong <djwong@kernel.org>
2026-01-09 18:16:30 -0800

    xfs_scrub: use the verify media ioctl during phase 6 if possible
    If the kernel suppots the XFS_IOC_VERIFY_MEDIA ioctl, use that to
    perform the phase 6 media scan instead of pwrite or the SCSI VERIFY
    command.  This enables better integration with xfs_healer and fsnotify;
    and reduces the amount of work that userspace has to do.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- scrub/disk.c
+++ scrub/disk.c
@@ -190,6 +190,7 @@ disk_open(
 	disk = calloc(1, sizeof(struct disk));
 	if (!disk)
 		return NULL;
+	disk->d_verify_fd = -1;
 
 	disk->d_fd = open(pathname, O_RDONLY | O_DIRECT | O_NOATIME);
 	if (disk->d_fd < 0)
@@ -266,6 +267,18 @@ disk_close(
 #define LBASIZE(d)		(1ULL << (d)->d_lbalog)
 #define BTOLBA(d, bytes)	(((uint64_t)(bytes) + LBASIZE(d) - 1) >> (d)->d_lbalog)
 
+#ifndef BTOBB
+# define BTOBB(bytes)		((uint64_t)((bytes) + 511) >> 9)
+#endif
+
+#ifndef BTOBBT
+# define BTOBBT(bytes)		((uint64_t)(bytes) >> 9)
+#endif
+
+#ifndef BBTOB
+# define BBTOB(bytes)		((uint64_t)(bytes) << 9)
+#endif
+
 /* Simulate disk errors. */
 static int
 disk_simulate_read_error(
@@ -329,7 +342,8 @@ disk_read_verify(
 	struct disk		*disk,
 	void			*buf,
 	uint64_t		start,
-	uint64_t		length)
+	uint64_t		length,
+	bool			single_step)
 {
 	if (debug) {
 		int		ret;
@@ -343,6 +357,30 @@ disk_read_verify(
 		/* Don't actually issue the IO */
 		if (getenv("XFS_SCRUB_DISK_VERIFY_SKIP"))
 			return length;
+	}
+
+	if (disk->d_verify_fd >= 0) {
+		const uint64_t	orig_start_daddr = BTOBBT(start);
+		struct xfs_verify_media me = {
+			.me_start_daddr	= orig_start_daddr,
+			.me_end_daddr	= BTOBB(start + length),
+			.me_dev		= disk->d_verify_disk,
+			.me_rest_us	= bg_mode > 2 ? bg_mode - 1 : 0,
+		};
+		int		ret;
+
+		if (single_step)
+			me.me_flags |= XFS_VERIFY_MEDIA_REPORT;
+
+		ret = ioctl(disk->d_verify_fd, XFS_IOC_VERIFY_MEDIA, &me);
+		if (ret < 0)
+			return ret;
+		if (me.me_ioerror) {
+			errno = me.me_ioerror;
+			return -1;
+		}
+
+		return BBTOB(me.me_start_daddr - orig_start_daddr);
 	}
 
 	/* Convert to logical block size. */
