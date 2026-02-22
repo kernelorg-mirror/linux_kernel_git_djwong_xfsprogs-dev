741f950d48fd86e32e8754568e93a89ea4100206
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:41:12 -0800

    libfrog: create healthmon event log library functions
    Add some helper functions to log health monitoring events so that xfs_io
    and xfs_healer can share logging code.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

---
+++ libfrog/flagmap.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2025-2026 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+#ifndef LIBFROG_FLAGMAP_H_
+#define LIBFROG_FLAGMAP_H_
+
+struct flag_map {
+	unsigned long long	flag;
+	const char		*string;
+};
+
+void mask_to_string(const struct flag_map *map, unsigned long long mask,
+		const char *delimiter, char *buf, size_t bufsize);
+
+const char *value_to_string(const struct flag_map *map,
+		unsigned long long value);
+
+#endif /* LIBFROG_FLAGMAP_H_ */
