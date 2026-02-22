17b51a5c1cc2fea1c11c0eddebf5a9f48407e3d1
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:41:09 -0800

    xfs: speed up parent pointer operations when possible
    Source kernel commit: d693534513d8dcdaafcf855986d0fe0476a47462
    
    After a recent fsmark benchmarking run, I observed that the overhead of
    parent pointers on file creation and deletion can be a bit high.  On a
    machine with 20 CPUs, 128G of memory, and an NVME SSD capable of pushing
    750000iops, I see the following results:
    
    $ mkfs.xfs -f -l logdev=/dev/nvme1n1,size=1g /dev/nvme0n1 -n parent=0
    meta-data=/dev/nvme0n1           isize=512    agcount=40, agsize=9767586 blks
    =                       sectsz=4096  attr=2, projid32bit=1
    =                       crc=1        finobt=1, sparse=1, rmapbt=1
    =                       reflink=1    bigtime=1 inobtcount=1 nrext64=1
    =                       exchange=0   metadir=0
    data     =                       bsize=4096   blocks=390703440, imaxpct=5
    =                       sunit=0      swidth=0 blks
    naming   =version 2              bsize=4096   ascii-ci=0, ftype=1, parent=0
    log      =/dev/nvme1n1           bsize=4096   blocks=262144, version=2
    =                       sectsz=4096  sunit=1 blks, lazy-count=1
    realtime =none                   extsz=4096   blocks=0, rtextents=0
    =                       rgcount=0    rgsize=0 extents
    =                       zoned=0      start=0 reserved=0
    
    So we created 40 AGs, one per CPU.  Now we create 40 directories and run
    fsmark:
    
    $ time fs_mark  -D  10000  -S  0  -n  100000  -s  0  -L  8 -d ...
    # Version 3.3, 40 thread(s) starting at Wed Dec 10 14:22:07 2025
    # Sync method: NO SYNC: Test does not issue sync() or fsync() calls.
    # Directories:  Time based hash between directories across 10000 subdirectories with 180 seconds per subdirectory.
    # File names: 40 bytes long, (16 initial bytes of time stamp with 24 random bytes at end of name)
    # Files info: size 0 bytes, written with an IO size of 16384 bytes per write
    # App overhead is time in microseconds spent in the test not doing file writing related system calls.
    
    parent=0               parent=1
    ==================     ==================
    real    0m57.573s      real    1m2.934s
    user    3m53.578s      user    3m53.508s
    sys     19m44.440s     sys     25m14.810s
    
    $ time rm -rf ...
    
    parent=0               parent=1
    ==================     ==================
    real    0m59.649s      real    1m12.505s
    user    0m41.196s      user    0m47.489s
    sys     13m9.566s      sys     20m33.844s
    
    Parent pointers increase the system time by 28% overhead to create 32
    million files that are totally empty.  Removing them incurs a system
    time increase of 56%.  Wall time increases by 9% and 22%.
    
    For most filesystems, each file tends to have a single owner and not
    that many xattrs.  If the xattr structure is shortform, then all xattr
    changes are logged with the inode and do not require the the xattr
    intent mechanism to persist the parent pointer.
    
    Therefore, we can speed up parent pointer operations by calling the
    shortform xattr functions directly if the child's xattr is in short
    format.  Now the overhead looks like:
    
    $ time fs_mark  -D  10000  -S  0  -n  100000  -s  0  -L  8 -d ...
    
    parent=0               parent=1
    ==================     ==================
    real    0m58.030s      real    1m0.983s
    user    3m54.141s      user    3m53.758s
    sys     19m57.003s     sys     21m30.605s
    
    $ time rm -rf ...
    
    parent=0               parent=1
    ==================     ==================
    real    0m58.911s      real    1m4.420s
    user    0m41.329s      user    0m45.169s
    sys     13m27.857s     sys     15m58.564s
    
    Now parent pointers only increase the system time by 8% for creation and
    19% for deletion.  Wall time increases by 5% and 9% now.
    
    Close the performance gap by creating helpers for the attr set, remove,
    and replace operations that will try to make direct shortform updates,
    and fall back to the attr intent machinery if that doesn't work.  This
    works for regular xattrs and for parent pointers.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- libxfs/xfs_parent.c
+++ libxfs/xfs_parent.c
@@ -26,6 +26,7 @@
 #include "xfs_trans_space.h"
 #include "defer_item.h"
 #include "xfs_health.h"
+#include "xfs_attr_leaf.h"
 
 struct kmem_cache		*xfs_parent_args_cache;
 
@@ -199,8 +200,8 @@ xfs_parent_addname(
 	xfs_inode_to_parent_rec(&ppargs->rec, dp);
 	xfs_parent_da_args_init(&ppargs->args, tp, &ppargs->rec, child,
 			child->i_ino, parent_name);
-	xfs_attr_defer_add(&ppargs->args, XFS_ATTR_DEFER_SET);
-	return 0;
+
+	return xfs_attr_setname(&ppargs->args, 0);
 }
 
 /* Remove a parent pointer to reflect a dirent removal. */
@@ -221,8 +222,8 @@ xfs_parent_removename(
 	xfs_inode_to_parent_rec(&ppargs->rec, dp);
 	xfs_parent_da_args_init(&ppargs->args, tp, &ppargs->rec, child,
 			child->i_ino, parent_name);
-	xfs_attr_defer_add(&ppargs->args, XFS_ATTR_DEFER_REMOVE);
-	return 0;
+
+	return xfs_attr_removename(&ppargs->args);
 }
 
 /* Replace one parent pointer with another to reflect a rename. */
@@ -247,12 +248,13 @@ xfs_parent_replacename(
 			child->i_ino, old_name);
 
 	xfs_inode_to_parent_rec(&ppargs->new_rec, new_dp);
+
 	ppargs->args.new_name = new_name->name;
 	ppargs->args.new_namelen = new_name->len;
 	ppargs->args.new_value = &ppargs->new_rec;
 	ppargs->args.new_valuelen = sizeof(struct xfs_parent_rec);
-	xfs_attr_defer_add(&ppargs->args, XFS_ATTR_DEFER_REPLACE);
-	return 0;
+
+	return xfs_attr_replacename(&ppargs->args, 0);
 }
 
 /*
