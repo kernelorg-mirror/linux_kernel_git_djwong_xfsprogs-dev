807e9db7f686a58b20c5e49f61399586d5abb04b
Darrick J. Wong <djwong@kernel.org>
2026-06-08 09:53:49 -0700

    xfs_scrub: fix spacemap external log device scan dev key
    Codex bizarrely complains about the rtstart>0 logic in scan_log_rmaps,
    because it gets confused about the difference between internal logs
    (which are scanned by scan_ag_rmaps) and external logs, which this
    function handles.  I'm ignoring the report, but we shouldn't open-code
    the synthetic device keys so let's fix that by hoisting the fsmap device
    helpers from phase6.c to spacemap.h and using them everywhere.
    
    Cc: <linux-xfs@vger.kernel.org> # v6.15.0
    Fixes: 37591ef3f4f14c ("xfs_scrub: support internal RT device")
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- scrub/phase6.c
+++ scrub/phase6.c
@@ -45,48 +45,6 @@ struct media_verify_state {
 	struct read_verify_pool	*rvp[XFS_DEV_RT + 1];
 };
 
-/* Return XFS device index from fsmap device. */
-static enum xfs_device
-from_fsmap_dev(
-	struct scrub_ctx	*ctx,
-	dev_t			dev)
-{
-	if (ctx->mnt.fsgeom.rtstart) {
-		if (dev < XFS_DEV_DATA || dev > XFS_DEV_RT)
-			abort();
-		return dev;
-	}
-
-	if (dev == ctx->fsinfo.fs_datadev)
-		return XFS_DEV_DATA;
-	if (dev == ctx->fsinfo.fs_logdev)
-		return XFS_DEV_LOG;
-	if (dev == ctx->fsinfo.fs_rtdev)
-		return XFS_DEV_RT;
-	abort();
-}
-
-/* Return fsmap device for XFS device index. */
-static uint32_t
-to_fsmap_dev(
-	struct scrub_ctx	*ctx,
-	enum xfs_device		dev)
-{
-	if (ctx->mnt.fsgeom.rtstart)
-		return dev;
-
-	switch (dev) {
-	case XFS_DEV_DATA:
-		return ctx->fsinfo.fs_datadev;
-	case XFS_DEV_LOG:
-		return ctx->fsinfo.fs_logdev;
-	case XFS_DEV_RT:
-		return ctx->fsinfo.fs_rtdev;
-	default:
-		abort();
-	}
-}
-
 struct disk_ioerr_report {
 	struct scrub_ctx	*ctx;
 	enum xfs_device		dev;
--- scrub/spacemap.h
+++ scrub/spacemap.h
@@ -23,4 +23,46 @@ static inline unsigned int scrub_scan_spacemaps_nproc(struct scrub_ctx *ctx)
 	return scrub_nproc(ctx);
 }
 
+/* Return XFS device index from fsmap device. */
+static inline enum xfs_device
+from_fsmap_dev(
+	struct scrub_ctx	*ctx,
+	dev_t			dev)
+{
+	if (ctx->mnt.fsgeom.rtstart) {
+		if (dev < XFS_DEV_DATA || dev > XFS_DEV_RT)
+			abort();
+		return dev;
+	}
+
+	if (dev == ctx->fsinfo.fs_datadev)
+		return XFS_DEV_DATA;
+	if (dev == ctx->fsinfo.fs_logdev)
+		return XFS_DEV_LOG;
+	if (dev == ctx->fsinfo.fs_rtdev)
+		return XFS_DEV_RT;
+	abort();
+}
+
+/* Return fsmap device for XFS device index. */
+static inline uint32_t
+to_fsmap_dev(
+	struct scrub_ctx	*ctx,
+	enum xfs_device		dev)
+{
+	if (ctx->mnt.fsgeom.rtstart)
+		return dev;
+
+	switch (dev) {
+	case XFS_DEV_DATA:
+		return ctx->fsinfo.fs_datadev;
+	case XFS_DEV_LOG:
+		return ctx->fsinfo.fs_logdev;
+	case XFS_DEV_RT:
+		return ctx->fsinfo.fs_rtdev;
+	default:
+		abort();
+	}
+}
+
 #endif /* XFS_SCRUB_SPACEMAP_H_ */
