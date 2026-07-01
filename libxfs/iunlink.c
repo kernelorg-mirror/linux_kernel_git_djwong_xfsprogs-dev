b8fca8654ed30070bfef30d714c358ea08d35d69
Christoph Hellwig <hch@lst.de>
2026-07-01 12:02:56 -0700

    xfs: massage xfs_imap_to_bp into xfs_read_icluster
    Source kernel commit: caffb4252cf15c5c3bbe7ff1f48f24f4015edb9c
    
    xfs_imap_to_bp only uses the im_blkno field from struct xfs_imap, so pass
    that directly.  Rename the function to xfs_read_icluster, which describes
    the functionality much better and matches other helpers like xfs_read_agf
    and xfs_read_agi.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Carlos Maiolino <cmaiolino@redhat.com>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- libxfs/iunlink.c
+++ libxfs/iunlink.c
@@ -44,7 +44,7 @@ xfs_iunlink_log_dinode(
 	int			offset;
 	int			error;
 
-	error = xfs_imap_to_bp(mp, tp, &ip->i_imap, &ibp);
+	error = xfs_read_icluster(mp, tp, ip->i_imap.im_blkno, &ibp);
 	if (error)
 		return error;
 	/*
