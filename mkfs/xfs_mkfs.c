26cd645efcc69396e74b9c9b73e91de7ddb2a5d1
Darrick J. Wong <djwong@kernel.org>
2026-08-24 07:48:54 -0700

    mkfs: enable metadata directory trees by default
    Metadata directory trees were added to XFS in Linux 6.13 at the end of
    2024.  It's now been two years and we haven't found any significant
    problems, so enable it by default.  Sysadmins will now be able to have
    persistent quota options instead of having to encode that in fstab.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -6009,6 +6009,7 @@ main(
 			.bigtime = true,
 			.nrext64 = true,
 			.exchrange = true,
+			.metadir = true,
 			/*
 			 * When we decide to enable a new feature by default,
 			 * please remember to update the mkfs conf files.
