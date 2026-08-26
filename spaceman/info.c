5bfdfbe2bd3787f3d508175940e7347438df6a31
Darrick J. Wong <djwong@kernel.org>
2026-08-26 10:32:34 -0700

    xfs_spaceman: print configuration file for mounted filesystems
    Teach xfs_spaceman to emit a mkfs.xfs config file for a mounted
    filesystem.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Reviewed-by: Andrey Albershteyn <aalbersh@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- spaceman/info.c
+++ spaceman/info.c
@@ -3,12 +3,16 @@
  * Copyright (C) 2018 Oracle.  All Rights Reserved.
  * Author: Darrick J. Wong <darrick.wong@oracle.com>
  */
+#include <sys/quota.h>
 #include "libxfs.h"
 #include "command.h"
 #include "init.h"
 #include "libfrog/paths.h"
 #include "libfrog/fsgeom.h"
+#include "libfrog/fsproperties.h"
+#include "libfrog/fsprops.h"
 #include "space.h"
+#include "include/xqm.h"
 
 static void
 info_help(void)
@@ -51,8 +55,205 @@ static const struct cmdinfo info_cmd = {
 	.help =		info_help,
 };
 
+static void
+makecfg_help(void)
+{
+	printf(_(
+"\n"
+" Print a mkfs.xfs configuration file for user-visible filesystem features\n"
+" of the current filesystem.  Geometry information are not printed.\n"
+"\n"
+));
+
+}
+
+static int
+get_autofsck(
+	struct fileio		*f,
+	enum fsprop_autofsck	*autofsck)
+{
+	struct fsprops_handle	fph = { };
+	char			valuebuf[FSPROP_MAX_VALUELEN + 1] = { 0 };
+	size_t			valuelen = FSPROP_MAX_VALUELEN;
+	int			ret;
+
+	*autofsck = FSPROP_AUTOFSCK_UNSET;
+
+	ret = fsprops_open_handle(&f->xfd, &f->fs_path, &fph);
+	if (ret == -1 && errno == EOPNOTSUPP)
+		return 0;
+	if (ret)
+		return ret;
+
+	ret = fsprops_get(&fph, FSPROP_AUTOFSCK_NAME, valuebuf, &valuelen);
+	if (ret == -1 && errno == ENODATA) {
+		ret = 0;
+		goto out_fph;
+	}
+	if (ret || !valuelen)
+		goto out_fph;
+
+	*autofsck = fsprop_autofsck_read(valuebuf);
+
+out_fph:
+	fsprops_free_handle(&fph);
+	return ret;
+}
+
+struct qflags_xlate {
+	unsigned int	qs_flag;
+	unsigned int	mkcfg_qflag;
+};
+
+static const struct qflags_xlate qsflags_xlate[] = {
+	{ .qs_flag = XFS_QUOTA_UDQ_ACCT, .mkcfg_qflag = XFS_UQUOTA_ACCT },
+	{ .qs_flag = XFS_QUOTA_UDQ_ENFD, .mkcfg_qflag = XFS_UQUOTA_ENFD },
+	{ .qs_flag = XFS_QUOTA_GDQ_ACCT, .mkcfg_qflag = XFS_GQUOTA_ACCT },
+	{ .qs_flag = XFS_QUOTA_GDQ_ENFD, .mkcfg_qflag = XFS_GQUOTA_ENFD },
+	{ .qs_flag = XFS_QUOTA_PDQ_ACCT, .mkcfg_qflag = XFS_PQUOTA_ACCT },
+	{ .qs_flag = XFS_QUOTA_PDQ_ENFD, .mkcfg_qflag = XFS_PQUOTA_ENFD },
+};
+
+static int
+get_qflags(
+	struct fileio		*f,
+	unsigned int		*qflags)
+{
+	struct fs_quota_stat	qstat;
+	int			i;
+	int			ret;
+
+	*qflags = 0;
+
+	/* GETQSTAT returns qflags for all quota types, not just user */
+	ret = quotactl(QCMD(Q_XGETQSTAT, USRQUOTA), f->fs_path.fs_name, 0,
+			(void *)&qstat);
+	if (ret) {
+		/*
+		 * ENOSYS means quota is not enabled or compiled in; ENODEV
+		 * means that we couldn't find the filesystem in mtab (aka
+		 * the filesystem has been moved).
+		 */
+		if (errno == ENOSYS || errno == ENODEV)
+			return 0;
+		return ret;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(qsflags_xlate); i++)
+		if (qstat.qs_flags & qsflags_xlate[i].qs_flag)
+			*qflags |= qsflags_xlate[i].mkcfg_qflag;
+
+	return 0;
+}
+
+static int makecfg_usage(void);
+
+static int
+makecfg_f(
+	int			argc,
+	char			**argv)
+{
+	struct fsxattr		fsx = { };
+	FILE			*fp;
+	bool			close_fp = false;
+	enum fsprop_autofsck	autofsck;
+	unsigned int		qflags;
+	int			c;
+	int			ret;
+
+	while ((c = getopt(argc, argv, "")) != EOF) {
+		switch (c) {
+		default:
+			fprintf(stderr, _("bad option for makecfg command\n"));
+			return makecfg_usage();
+		}
+	}
+
+	if (optind != argc && optind != argc - 1) {
+		fprintf(stderr, _("bad option for makecfg command\n"));
+		return makecfg_usage();
+	}
+
+	if (fs_table_lookup_mount(file->name) == NULL) {
+		fprintf(stderr, _("%s: Not a XFS mount point.\n"), file->name);
+		return 1;
+	}
+
+	/* ioctl reports bytes, not fsblocks */
+	ret = ioctl(file->xfd.fd, FS_IOC_FSGETXATTR, &fsx);
+	if (ret) {
+		perror(file->name);
+		return 1;
+	}
+	fsx.fsx_extsize /= file->xfd.fsgeom.blocksize;
+	fsx.fsx_cowextsize /= file->xfd.fsgeom.blocksize;
+
+	ret = get_autofsck(file, &autofsck);
+	if (ret) {
+		perror("autofsck");
+		return 1;
+	}
+
+	ret = get_qflags(file, &qflags);
+	if (ret) {
+		perror("quotactl");
+		return 1;
+	}
+
+	if (optind == argc) {
+		fp = stdout;
+	} else {
+		fp = fopen(argv[optind], "w");
+		if (!fp) {
+			perror(argv[optind]);
+			return 1;
+		}
+		close_fp = true;
+	}
+
+	ret = xfrog_write_mkfs_config(&file->xfd.fsgeom, qflags, &fsx,
+			autofsck, fp);
+	if (ret) {
+		if (close_fp)
+			perror(argv[optind]);
+		else
+			perror("makecfg");
+		/* fall through to close fp */
+	}
+
+	if (close_fp) {
+		int	ret2 = fclose(fp);
+
+		if (ret2) {
+			perror(argv[optind]);
+			if (!ret)
+				ret = ret2;
+		}
+	}
+
+	return ret;
+}
+
+static const struct cmdinfo makecfg_cmd = {
+	.name =		"makecfg",
+	.cfunc =	makecfg_f,
+	.argmin =	0,
+	.argmax =	1,
+	.canpush =	0,
+	.args =		NULL,
+	.flags =	CMD_FLAG_ONESHOT,
+	.oneline =	N_("print mkfs.xfs configuration file"),
+	.help =		makecfg_help,
+};
+
+static int makecfg_usage(void)
+{
+	return command_usage(&makecfg_cmd);
+}
+
 void
 info_init(void)
 {
 	add_command(&info_cmd);
+	add_command(&makecfg_cmd);
 }
