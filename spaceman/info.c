453d83ff5bd9f8117d321fbc3c51d629599f8064
Darrick J. Wong <djwong@kernel.org>
2026-08-26 10:32:34 -0700

    xfs_spaceman: print configuration file for mounted filesystems
    Teach spaceman to emit a mkfs.xfs config file for an open filesystem.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- spaceman/info.c
+++ spaceman/info.c
@@ -8,6 +8,8 @@
 #include "init.h"
 #include "libfrog/paths.h"
 #include "libfrog/fsgeom.h"
+#include "libfrog/fsproperties.h"
+#include "libfrog/fsprops.h"
 #include "space.h"
 
 static void
@@ -51,8 +53,94 @@ static const struct cmdinfo info_cmd = {
 	.help =		info_help,
 };
 
+static void
+cfgfile_help(void)
+{
+	printf(_(
+"\n"
+" Print a mkfs.xfs configuration file for user-visible filesystem features\n"
+" of the current filesystem.  Geometry information are not printed.\n"
+"\n"
+));
+
+}
+
+static int
+get_autofsck(
+	struct fileio		*f,
+	enum fsprop_autofsck	*autofsck)
+{
+	struct fsprops_handle	fph = { };
+	char			valuebuf[FSPROP_MAX_VALUELEN + 1] = { 0 };
+	size_t			valuelen = FSPROP_MAX_VALUELEN;
+	int			ret;
+
+	*autofsck = FSPROP_AUTOFSCK_UNSET;
+
+	ret = fsprops_open_handle(&f->xfd, &f->fs_path, &fph);
+	if (ret)
+		return ret;
+
+	ret = fsprops_get(&fph, FSPROP_AUTOFSCK_NAME, valuebuf, &valuelen);
+	if (ret == -1 && errno == ENODATA) {
+		ret = 0;
+		goto out_fph;
+	}
+	if (ret)
+		goto out_fph;
+
+	*autofsck = fsprop_autofsck_read(valuebuf);
+
+out_fph:
+	fsprops_free_handle(&fph);
+	return ret;
+}
+
+static int
+cfgfile_f(
+	int			argc,
+	char			**argv)
+{
+	struct fsxattr		fsx = { };
+	enum fsprop_autofsck	autofsck;
+	int			ret;
+
+	if (fs_table_lookup_mount(file->name) == NULL) {
+		fprintf(stderr, _("%s: Not a XFS mount point.\n"), file->name);
+		return 1;
+	}
+
+	ret = ioctl(file->xfd.fd, FS_IOC_FSGETXATTR, &fsx);
+	if (ret) {
+		perror(file->name);
+		return 1;
+	}
+
+	ret = get_autofsck(file, &autofsck);
+	if (ret) {
+		perror("autofsck");
+		return 1;
+	}
+
+	xfrog_write_mkfs_config(&file->xfd.fsgeom, &fsx, autofsck, stdout);
+	return 0;
+}
+
+static const struct cmdinfo cfgfile_cmd = {
+	.name =		"cfgfile",
+	.cfunc =	cfgfile_f,
+	.argmin =	0,
+	.argmax =	0,
+	.canpush =	0,
+	.args =		NULL,
+	.flags =	CMD_FLAG_ONESHOT,
+	.oneline =	N_("print mkfs.xfs configuration file"),
+	.help =		cfgfile_help,
+};
+
 void
 info_init(void)
 {
 	add_command(&info_cmd);
+	add_command(&cfgfile_cmd);
 }
