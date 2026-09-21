15eb1f96ce307be897d453b9d4f73203f976fab3
Darrick J. Wong <djwong@kernel.org>
2026-09-20 17:37:47 -0700

    xfs_scrub: complain about case-insensitive names
    Make the unicrash detector find case-insensitive names within the same
    namespace that map to different items, and warn about that.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- scrub/xfs_scrub.c
+++ scrub/xfs_scrub.c
@@ -166,6 +166,9 @@ bool				use_force_rebuild;
 /* Should we count informational messages as warnings? */
 bool				info_is_warning;
 
+/* Should we warn about case collision between names in dirs/attrs? */
+bool				report_case_collisions;
+
 #define SCRUB_RET_SUCCESS	(0)	/* no problems left behind */
 #define SCRUB_RET_CORRUPT	(1)	/* corruption remains on fs */
 #define SCRUB_RET_UNOPTIMIZED	(2)	/* fs could be optimized */
@@ -644,6 +647,9 @@ enum o_opt_nums {
 	IWARN = 0,
 	FSTRIM_PCT,
 	AUTOFSCK,
+#ifdef HAVE_LIBICU
+	CASE_COLLISION,
+#endif
 	O_MAX_OPTS,
 };
 
@@ -651,6 +657,9 @@ static char *o_opts[] = {
 	[IWARN]			= "iwarn",
 	[FSTRIM_PCT]		= "fstrim_pct",
 	[AUTOFSCK]		= "autofsck",
+#ifdef HAVE_LIBICU
+	[CASE_COLLISION]	= "case_collision",
+#endif
 	[O_MAX_OPTS]		= NULL,
 };
 
@@ -711,6 +720,16 @@ parse_o_opts(
 			}
 			ctx->mode = SCRUB_MODE_NONE;
 			break;
+#ifdef HAVE_LIBICU
+		case CASE_COLLISION:
+			if (val) {
+				fprintf(stderr,
+ _("-o case_collision does not take an argument\n"));
+				usage();
+			}
+			report_case_collisions = true;
+			break;
+#endif /* HAVE_LIBICU */
 		default:
 			usage();
 			break;
